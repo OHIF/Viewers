56417df3855081d4958040a4381dcfc63d000f22
Erik Ziegler <erik.ziegler@ulg.ac.be>
2015-10-22 17:01:00 +0200

    Styling fixes for tabs, collapsible search box, navbar updates

--- Packages/worklist/components/tabTitle/tabTitle.js
+++ Packages/worklist/components/tabTitle/tabTitle.js
@@ -1,21 +1,17 @@
 Template.tabTitle.events({
     'click .close': function(e) {
-        var tab = $(e.currentTarget).siblings('a[data-toggle="tab"]').eq(0);
+        var tab = $(e.currentTarget).parents('a[data-toggle="tab"]').eq(0);
         var contentId = tab.data("target").replace("#", "");
-        var tabObjectId = tabs.find({contentid: contentId}).fetch()[0]._id;
-        tabs.remove(tabObjectId);
 
         var tabIndex = tab.parent('li').index();
         var newActiveTabIndex = Math.max(tabIndex - 1, 0);
         var newActiveTab = $(".tabTitle").eq(newActiveTabIndex);
         var newActiveTabLink = newActiveTab.find("a[data-toggle=tab]");
         var newContentId = newActiveTabLink.data("target");
-        switchToTab(newContentId);
-    }
-});
 
-Template.tabTitle.helpers({
-    'active': function() {
-        return this.active ? "active" : "";
+        switchToTab(newContentId);
+        
+        var tabObjectId = tabs.find({contentid: contentId}).fetch()[0]._id;
+        tabs.remove(tabObjectId);
     }
 });
