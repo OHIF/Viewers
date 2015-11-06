814e6f774114a446cfe543db038ef96957ca67e5
Erik Ziegler <erik.ziegler@ulg.ac.be>
2015-11-06 12:06:11 +0100

    Fix overriding of viewerbase style in LesionTracker. Add loading icon for worklist

--- Packages/viewerbase/package.js
+++ Packages/viewerbase/package.js
@@ -28,6 +28,9 @@ Package.onUse(function (api) {
     api.addFiles('components/basic/login/login.html', 'client');
     api.addFiles('components/basic/notFound/notFound.html', 'client');
 
+    api.addFiles('components/basic/loadingText/loadingText.html', 'client');
+    api.addFiles('components/basic/loadingText/loadingText.styl', 'client');
+
     api.addFiles('components/basic/hidingPanel/hidingPanel.html', 'client');
     api.addFiles('components/basic/hidingPanel/hidingPanel.js', 'client');
     api.addFiles('components/basic/hidingPanel/hidingPanel.styl', 'client');
