60afb178e8b35833ef3a42168d64682cb121fcf3
radicalrob <rob@radicalimaging.com>
2015-11-10 09:16:14 -0500

    sort worklist by patient name, then study date

--- Packages/worklist/components/worklistResult/worklistResult.js
+++ Packages/worklist/components/worklistResult/worklistResult.js
@@ -2,6 +2,6 @@ Studies = new Mongo.Collection(null);
 
 Template.worklistResult.helpers({
     studies : function() {
-        return Studies.find();
+        return Studies.find({}, {sort: {patientName : 1, studyDate : 1}});
     }
 });
