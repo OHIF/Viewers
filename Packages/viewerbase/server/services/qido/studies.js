6c4eae3699aeb4cb8103d6408adb65ed43881aa1
Erik Ziegler <erik.ziegler@ulg.ac.be>
2016-02-02 22:21:23 +0100

    Fix accession number and study description in study list

--- Packages/viewerbase/server/services/qido/studies.js
+++ Packages/viewerbase/server/services/qido/studies.js
@@ -7,12 +7,17 @@
  * @returns {string} The URL with encoded filter query data
  */
 function filterToQIDOURL(server, filter) {
+    var commaSeparatedFields = [
+        '00081030' // Study Description
+        // Add more fields here if you want them in the Study List
+    ].join(',');
+
     var parameters = {
-        PatientName : filter.patientName,
+        PatientName: filter.patientName,
         PatientID: filter.patientId,
-        AccessionNumber : filter.accessionNumber,
-        limit : filter.limit || 20,
-        includefield : server.qidoSupportsIncludeField ? 'all' : undefined
+        AccessionNumber: filter.accessionNumber,
+        limit: filter.limit || 20,
+        includefield: server.qidoSupportsIncludeField ? 'all' : commaSeparatedFields
     };
     return server.qidoRoot + '/studies?' + encodeQueryData(parameters);
 }
@@ -28,26 +33,26 @@ function resultDataToStudies(resultData) {
 
     resultData.forEach(function(study) {
         studies.push({
-          studyInstanceUid: DICOMWeb.getString(study['0020000D']),
-          // 00080005 = SpecificCharacterSet
-          studyDate: DICOMWeb.getString(study['00080020']),
-          studyTime: DICOMWeb.getString(study['00080030']),
-          accessionNumber: DICOMWeb.getString(study['00080050']),
-          referringPhysicianName: DICOMWeb.getString(study['00080090']),
-          // 00081190 = URL
-          patientName: DICOMWeb.getName(study['00100010']),
-          patientId: DICOMWeb.getString(study['00100020']),
-          patientBirthdate: DICOMWeb.getString(study['00100030']),
-          patientSex: DICOMWeb.getString(study['00100040']),
-          imageCount: DICOMWeb.getString(study['00201208']),
-          studyId: DICOMWeb.getString(study['00200010']),
-          studyDescription: DICOMWeb.getString(study['00081030']),
-          modalities: DICOMWeb.getString(study['00080061'])
+            studyInstanceUid: DICOMWeb.getString(study['0020000D']),
+            // 00080005 = SpecificCharacterSet
+            studyDate: DICOMWeb.getString(study['00080020']),
+            studyTime: DICOMWeb.getString(study['00080030']),
+            accessionNumber: DICOMWeb.getNumber(study['00080050']),
+            referringPhysicianName: DICOMWeb.getString(study['00080090']),
+            // 00081190 = URL
+            patientName: DICOMWeb.getName(study['00100010']),
+            patientId: DICOMWeb.getString(study['00100020']),
+            patientBirthdate: DICOMWeb.getString(study['00100030']),
+            patientSex: DICOMWeb.getString(study['00100040']),
+            studyId: DICOMWeb.getString(study['00200010']),
+            numberOfStudyRelatedSeries: DICOMWeb.getString(study['00201206']),
+            numberOfStudyRelatedInstances: DICOMWeb.getString(study['00201208']),
+            studyDescription: DICOMWeb.getString(study['00081030']),
+            modalities: DICOMWeb.getString(study['00080061'])
         });
     });
     return studies;
 }
-
 
 Services.QIDO.Studies = function(server, filter) {
     var url = filterToQIDOURL(server, filter);
