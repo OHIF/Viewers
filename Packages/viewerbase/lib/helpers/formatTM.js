dbeaba16841e269d28b83dff469d9fb2b9e4030e
Erik Ziegler <erik.ziegler@ulg.ac.be>
2016-03-13 12:34:53 +0100

    Fix DICOM Time helper

--- Packages/viewerbase/lib/helpers/formatTM.js
+++ Packages/viewerbase/lib/helpers/formatTM.js
@@ -1,11 +1,24 @@
 /**
  * A global Blaze UI helper to format a DICOM Time for display using the Moment library
  */
-UI.registerHelper('formatTM', function(context, format) {
+UI.registerHelper('formatTM', function(context, options) {
     if (!context) {
         return;
     }
 
-    var dateTime = moment(context);
-    return dateTime.format(format || "HH:mm:ss");
+    // DICOM Time is stored as HHmmss.SSS, where: 
+    //      HH 24 hour time:
+    //      m mm    0..59   Minutes
+    //      s ss    0..59   Seconds
+    //      S SS SSS    0..999  Fractional seconds
+    //
+    // See MomentJS: http://momentjs.com/docs/#/parsing/string-format/
+    var dateTime = moment(context, 'HHmmss.SSS');
+
+    var format = "HH:mm:ss";
+    if (options && options.format) {
+        format = options.format;
+    }
+
+    return dateTime.format(format);
 });
