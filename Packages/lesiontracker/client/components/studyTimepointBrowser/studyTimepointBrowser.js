a03a6a94c7e11a36126abac0e0b97899c197b7e8
Erik Ziegler <erik.sweed@gmail.com>
2016-05-26 11:17:28 -0400

    WIP

---
+++ Packages/lesiontracker/client/components/studyTimepointBrowser/studyTimepointBrowser.js
@@ -0,0 +1,22 @@
+
+Template.studyTimepointBrowser.helpers({
+    timepoints: function() {
+        return Timepoints.find();
+    },
+    studies : function() {
+        return ViewerStudies.find({selected: true});
+    },
+    thumbnails: function() {
+        var study = this;
+        var stacks = createStacks(study);
+
+        var array = [];
+        stacks.forEach(function(stack, index) {
+            array.push({
+                thumbnailIndex: index,
+                stack: stack
+            });
+        });
+        return array;
+    }
+});
