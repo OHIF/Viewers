4138d7b7bc4761a4da06e3b98385fc4c9f667ddf
Bruno Alves de Faria <brunoalvesdefaria@gmail.com>
2016-06-14 10:36:54 -0300

    LT-251: Improving toggle behavior for studies and series quick switch

--- Packages/lesiontracker/client/components/studySeriesQuickSwitch/studySeriesQuickSwitch.js
+++ Packages/lesiontracker/client/components/studySeriesQuickSwitch/studySeriesQuickSwitch.js
@@ -3,38 +3,13 @@ Template.studySeriesQuickSwitch.onCreated(function studySeriesQuickSwitchOnCreat
 
     var viewportIndex = instance.data.viewportIndex;
 
-    instance.seriesOpen = new ReactiveVar(false);
-    instance.studiesOpen = new ReactiveVar(false);
-
     instance.data.timepointViewType = new ReactiveVar();
     instance.data.timepointViewType.set('key');
-});
-
-Template.studySeriesQuickSwitch.events({
-    'mouseenter .js-show-series'(event, instance) {
-        instance.seriesOpen.set(true);
-    },
-
-    'mouseenter .js-show-studies'(event, instance) {
-        instance.studiesOpen.set(true);
-    },
-
-    'mouseleave .js-hide-series'(event, instance) {
-        instance.seriesOpen.set(false);
-    },
-
-    'mouseleave .js-hide-studies'(event, instance) {
-        instance.studiesOpen.set(false);
-    }
 });
 
 Template.studySeriesQuickSwitch.helpers({
     seriesOpen() {
         return Template.instance().seriesOpen.get();
-    },
-
-    studiesOpen() {
-        return Template.instance().studiesOpen.get();
     },
 
     currentStudy() {
