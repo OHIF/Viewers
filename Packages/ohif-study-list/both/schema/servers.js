a30cfc95f97ad250a571af7244be5eb53b5b3b45
Emanuel F. Oliveira <emanuel@radicalimaging.com>
2016-09-26 17:24:09 +0000

    PWV-2 Added custom field to settings schema.

--- Packages/ohif-study-list/both/schema/servers.js
+++ Packages/ohif-study-list/both/schema/servers.js
@@ -165,6 +165,11 @@ export const PublicServerConfig = new SimpleSchema({
     ui: {
         type: UISettings,
         label: 'UI Settings'
+    },
+    custom: {
+        type: Object,
+        label: 'Custom Settings',
+        optional: true
     }
 });
 
@@ -194,11 +199,6 @@ export const ServerConfiguration = new SimpleSchema({
     public: {
         type: PublicServerConfig,
         label: 'Public Server Configuration',
-    },
-    origin: {
-        type: String,
-        label: 'Origin',
-        optional: true
     },
     origin: {
         type: String,
