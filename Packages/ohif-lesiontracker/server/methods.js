a0dca5761add0180be207a15e1b242d534593562
Bruno Alves de Faria <brunoalvesdefaria@gmail.com>
2016-10-03 15:07:35 -0300

    PWV-1: Fixing modal incrementing body padding issues

--- Packages/ohif-lesiontracker/server/methods.js
+++ Packages/ohif-lesiontracker/server/methods.js
@@ -1,65 +1,67 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { OHIF } from 'meteor/ohif:core';
 import { measurementTools } from 'meteor/ohif:lesiontracker/both/configuration/measurementTools';
 
 let MeasurementCollections = {};
 measurementTools.forEach(tool => {
-	MeasurementCollections[tool.id] = new Mongo.Collection(tool.id);
+    MeasurementCollections[tool.id] = new Mongo.Collection(tool.id);
 });
-
-
 
 Timepoints = new Mongo.Collection('timepoints');
 
 // Drop our collections for testing purposes
 Meteor.startup(() => {
-	Timepoints.remove({});
-	measurementTools.forEach(tool => {
-		MeasurementCollections[tool.id].remove({});
-	});
-})
+    Timepoints.remove({});
+    measurementTools.forEach(tool => {
+        MeasurementCollections[tool.id].remove({});
+    });
+});
 
-
-// TODO: Make storage use update instead of clearing the entire collection and 
+// TODO: Make storage use update instead of clearing the entire collection and
 // re-inserting everything.
 Meteor.methods({
-	storeTimepoints(timepoints) {
-		console.log('Storing Timepoints on the Server')
-		console.log(JSON.stringify(timepoints, null, 2));
-		Timepoints.remove({});
-		timepoints.forEach(timepoint => {
-			delete timepoint._id;
-			Timepoints.insert(timepoint);
-		});
-	},
+    storeTimepoints(timepoints) {
+        OHIF.log.info('Storing Timepoints on the Server');
+        OHIF.log.info(JSON.stringify(timepoints, null, 2));
+        Timepoints.remove({});
+        timepoints.forEach(timepoint => {
+            delete timepoint._id;
+            Timepoints.insert(timepoint);
+        });
+    },
 
-	retrieveTimepoints() {
-		console.log('Retrieving Timepoints from the Server');
-		return Timepoints.find().fetch();
-	},
+    retrieveTimepoints() {
+        OHIF.log.info('Retrieving Timepoints from the Server');
+        return Timepoints.find().fetch();
+    },
 
-	storeMeasurements(measurementData) {
-		console.log('Storing Measurements on the Server')
-		console.log(JSON.stringify(measurementData, null, 2));
+    storeMeasurements(measurementData) {
+        OHIF.log.info('Storing Measurements on the Server');
+        OHIF.log.info(JSON.stringify(measurementData, null, 2));
 
-		Object.keys(measurementData).forEach(toolId => {
-			MeasurementCollections[toolId].remove({});
+        Object.keys(measurementData).forEach(toolId => {
+            if (!MeasurementCollections[toolId]) {
+                return;
+            }
 
-			const measurements = measurementData[toolId];
-			measurements.forEach(measurement => {
-				MeasurementCollections[toolId].insert(measurement);
-			})
-		});
-	},
+            MeasurementCollections[toolId].remove({});
 
-	retrieveMeasurements() {
-		console.log('Retrieving Measurements from the Server');
-		let measurementData = {};
+            const measurements = measurementData[toolId];
+            measurements.forEach(measurement => {
+                MeasurementCollections[toolId].insert(measurement);
+            });
+        });
+    },
 
-		measurementTools.forEach(tool => {
-			measurementData[tool.id] = MeasurementCollections[tool.id].find().fetch();
-		});
+    retrieveMeasurements() {
+        OHIF.log.info('Retrieving Measurements from the Server');
+        let measurementData = {};
 
-		return measurementData;
-	}
+        measurementTools.forEach(tool => {
+            measurementData[tool.id] = MeasurementCollections[tool.id].find().fetch();
+        });
+
+        return measurementData;
+    }
 });
