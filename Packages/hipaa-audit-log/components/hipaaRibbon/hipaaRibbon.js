a8f1b3b9d7157e45a3c0b0784cf0bc39f42f1ba1
Leonardo Campos <leonardo.campos@bloomingdales.com>
2016-11-21 15:40:05 -0200

    LT-342: Audit Log :: style

--- Packages/hipaa-audit-log/components/hipaaRibbon/hipaaRibbon.js
+++ Packages/hipaa-audit-log/components/hipaaRibbon/hipaaRibbon.js
@@ -39,36 +39,6 @@ Meteor.startup(function(){
 });
 
 Template.hipaaRibbon.helpers({
-  getSearchFilterStyling: function () {
-    // spacing between inputs should be 20px each
-    // if the overall width of the audit log is less than N, show the fullwidth
-    if(Session.get('ribbonWidth') > ribbonBreakPoint){
-      var searchFilterWidth = Session.get('ribbonWidth') - 575;
-      return "margin-left: 10px; margin-right: 5px; width:" + searchFilterWidth + "px;";
-    }else{
-      var ribbonWidth = Session.get('ribbonWidth') - 40;
-      return "width: " + ribbonWidth + "px; margin-left: 10px; margin-right: 10px;";
-    }
-  },
-  getDateRangeStyling: function () {
-    // spacing between inputs should be 20px each
-    // date range inputs shouldn't be less than 170 px
-    // if the overall width of the audit log is less than N, dont even show them
-    if(Session.get('ribbonWidth') > ribbonBreakPoint){
-      return "visbility: visible; margin-left: 5px; margin-right: 5px; width: 170px;";
-    }else{
-      return "display: none; visibility: hidden";
-    }
-  },
-  getSelectStyling: function () {
-    // spacing between inputs should be 20px each
-    // select input should be the same as daterange
-    if(Session.get('ribbonWidth') > ribbonBreakPoint){
-      return "visbility: visible; margin-left: 5px; margin-right: 5px; width: 170px;";
-    }else{
-      return "display: none; visibility: hidden";
-    }
-  },
   getRibbonClass: function () {
     var hipaaAuditLog = Session.get('HipaaAuditLogConfig');
     if (hipaaAuditLog && hipaaAuditLog.classes) {
