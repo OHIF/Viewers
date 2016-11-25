87d61298b438604140a671cfb03449ae36498f58
Eloízio Salgado <eloisalgado@gmail.com>
2016-11-25 17:38:07 -0200

    LT-397: Check that everything works when internet is disconnected; Adding Google Fonts Local Copies

--- Packages/ohif-viewerbase/package.js
+++ Packages/ohif-viewerbase/package.js
@@ -20,7 +20,64 @@ Package.onUse(function(api) {
     api.use('ohif:core');
     api.use('ohif:cornerstone');
 
-    api.addAssets('assets/icons.svg', 'client');
+    const assets = [
+        'assets/icons.svg',
+        'assets/fonts/Roboto-Black-latin-ext.woff',
+        'assets/fonts/Roboto-Black-latin-ext.woff2',
+        'assets/fonts/Roboto-Black-latin.woff',
+        'assets/fonts/Roboto-Black-latin.woff2',
+        'assets/fonts/Roboto-BlackItalic-latin-ext.woff',
+        'assets/fonts/Roboto-BlackItalic-latin-ext.woff2',
+        'assets/fonts/Roboto-BlackItalic-latin.woff',
+        'assets/fonts/Roboto-BlackItalic-latin.woff2',
+        'assets/fonts/Roboto-Bold-latin-ext.woff',
+        'assets/fonts/Roboto-Bold-latin-ext.woff2',
+        'assets/fonts/Roboto-Bold-latin.woff',
+        'assets/fonts/Roboto-Bold-latin.woff2',
+        'assets/fonts/Roboto-BoldItalic-latin-ext.woff',
+        'assets/fonts/Roboto-BoldItalic-latin-ext.woff2',
+        'assets/fonts/Roboto-BoldItalic-latin.woff',
+        'assets/fonts/Roboto-BoldItalic-latin.woff2',
+        'assets/fonts/Roboto-Italic-latin-ext.woff',
+        'assets/fonts/Roboto-Italic-latin-ext.woff2',
+        'assets/fonts/Roboto-Italic-latin.woff',
+        'assets/fonts/Roboto-Italic-latin.woff2',
+        'assets/fonts/Roboto-Light-latin-ext.woff',
+        'assets/fonts/Roboto-Light-latin-ext.woff2',
+        'assets/fonts/Roboto-Light-latin.woff',
+        'assets/fonts/Roboto-Light-latin.woff2',
+        'assets/fonts/Roboto-LightItalic-latin-ext.woff',
+        'assets/fonts/Roboto-LightItalic-latin-ext.woff2',
+        'assets/fonts/Roboto-LightItalic-latin.woff',
+        'assets/fonts/Roboto-LightItalic-latin.woff2',
+        'assets/fonts/Roboto-Medium-latin-ext.woff',
+        'assets/fonts/Roboto-Medium-latin-ext.woff2',
+        'assets/fonts/Roboto-Medium-latin.woff',
+        'assets/fonts/Roboto-Medium-latin.woff2',
+        'assets/fonts/Roboto-MediumItalic-latin-ext.woff',
+        'assets/fonts/Roboto-MediumItalic-latin-ext.woff2',
+        'assets/fonts/Roboto-MediumItalic-latin.woff',
+        'assets/fonts/Roboto-MediumItalic-latin.woff2',
+        'assets/fonts/Roboto-Regular-latin-ext.woff',
+        'assets/fonts/Roboto-Regular-latin-ext.woff2',
+        'assets/fonts/Roboto-Regular-latin.woff',
+        'assets/fonts/Roboto-Regular-latin.woff2',
+        'assets/fonts/Roboto-Thin-latin-ext.woff',
+        'assets/fonts/Roboto-Thin-latin-ext.woff2',
+        'assets/fonts/Roboto-Thin-latin.woff',
+        'assets/fonts/Roboto-Thin-latin.woff2',
+        'assets/fonts/Roboto-ThinItalic-latin-ext.woff',
+        'assets/fonts/Roboto-ThinItalic-latin-ext.woff2',
+        'assets/fonts/Roboto-ThinItalic-latin.woff',
+        'assets/fonts/Roboto-ThinItalic-latin.woff2',
+        'assets/fonts/Sanchez-Regular-latin-ext.woff',
+        'assets/fonts/Sanchez-Regular-latin-ext.woff2',
+        'assets/fonts/Sanchez-Regular-latin.woff',
+        'assets/fonts/Sanchez-Regular-latin.woff2'
+    ];
+
+    api.addAssets(assets, 'client');
+
 
     // TODO: Use NPM depends for these
     api.addFiles('client/compatibility/jquery.hotkeys.js', 'client', {
@@ -38,6 +95,7 @@ Package.onUse(function(api) {
 
     // Basic components
     api.addFiles('client/components/basic/layout/layout.html', 'client');
+    api.addFiles('client/components/basic/layout/layout.styl', 'client');
     api.addFiles('client/components/basic/loadingText/loadingText.html', 'client');
     api.addFiles('client/components/basic/loadingText/loadingText.styl', 'client');
 
