a91894c42db0e67df9afbab555c0f7c2c832fe3e
Bruno Alves de Faria <brunoalvesdefaria@gmail.com>
2017-01-06 18:07:47 -0200

    LT-115: Fixing toolbar box window overflowing

--- Packages/ohif-viewerbase/client/components/viewer/toolbarSectionTools/toolbarSectionTools.js
+++ Packages/ohif-viewerbase/client/components/viewer/toolbarSectionTools/toolbarSectionTools.js
@@ -6,6 +6,41 @@ Template.toolbarSectionTools.events({
         const $target = $(event.currentTarget);
         const isExpanded = $target.hasClass('expanded');
         $target.toggleClass('expanded', !isExpanded);
+
+        // Remove the previously set repositioning css attribute
+        const $box = $target.find('.toolbarSectionDrawerContainer:first');
+        $box.css('left', '');
+
+        // Stop here if the tool group is not expanded
+        if (isExpanded) {
+            return;
+        }
+
+        // Move the box left or right if it is overflowing the window
+        const transitionendHandler = event => {
+            const originalEvent = event.originalEvent;
+            const propertyName = originalEvent && originalEvent.propertyName;
+            if (propertyName && propertyName === 'transform') {
+                $target.off('transitionend', transitionendHandler);
+            } else {
+                return;
+            }
+
+            const boxWidth = $box.outerWidth();
+            const start = $box.offset().left;
+            const bodyWidth = $(document.body).outerWidth();
+            const end = start + boxWidth;
+
+            if (start < 0) {
+                $box.css('left', `calc(50% - ${start}px)`);
+            } else if (end > bodyWidth) {
+                const diff = end - bodyWidth;
+                $box.css('left', `calc(50% - ${diff}px)`);
+            }
+        };
+
+        // Attach the handler to deal with position fixing
+        $target.on('transitionend', transitionendHandler);
     },
 
     'focusout .expandable'(event, instance) {
