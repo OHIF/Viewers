e40649533c2a913e9c126d420e365e1bfa194ad9
Eloízio Salgado <eloisalgado@gmail.com>
2017-01-19 15:13:33 -0200

    Removing duplicated files/functions

--- Packages/ohif-viewerbase/client/lib/toggleDialog.js
+++
@@ -1,6 +0,0 @@
-toggleDialog = element => {
-    const $element = $(element);
-    const isClosed = $element.hasClass('dialog-open');
-    $element.toggleClass('dialog-closed', isClosed);
-    $element.toggleClass('dialog-open', !isClosed);
-};
