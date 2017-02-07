6adc31ce40ac833d00f80688f651cf11c3830b9f
Leonardo Campos <leonardoscampos@gmail.com>
2017-02-07 11:45:00 -0200

    fix toolbar buttons

--- StandaloneViewer/StandaloneViewer/client/components/toolbarSection/toolbarSection.js
+++ StandaloneViewer/StandaloneViewer/client/components/toolbarSection/toolbarSection.js
@@ -1,6 +1,14 @@
 import { OHIF } from 'meteor/ohif:core';
 import 'meteor/ohif:viewerbase';
 
+Template.toolbarSection.onCreated(() => {
+    const instance = Template.instance();
+
+    if (OHIF.uiSettings.leftSidebarOpen) {
+        instance.data.state.set('leftSidebar', 'studies');
+    }
+});
+
 Template.toolbarSection.helpers({
     leftSidebarToggleButtonData() {
         const instance = Template.instance();
@@ -10,7 +18,7 @@ Template.toolbarSection.helpers({
             value: instance.data.state,
             options: [{
                 value: 'studies',
-                svgLink: '/packages/ohif_viewerbase/assets/icons.svg#icon-studies',
+                svgLink: 'packages/ohif_viewerbase/assets/icons.svg#icon-studies',
                 svgWidth: 15,
                 svgHeight: 13,
                 bottomLabel: 'Series'
@@ -19,40 +27,99 @@ Template.toolbarSection.helpers({
     },
 
     toolbarButtons() {
+        const extraTools = [];
+
+        extraTools.push({
+            id: 'stackScroll',
+            title: 'Stack Scroll',
+            classes: 'imageViewerTool',
+            iconClasses: 'fa fa-bars'
+        });
+
+        extraTools.push({
+            id: 'magnify',
+            title: 'Magnify',
+            classes: 'imageViewerTool toolbarSectionButton',
+            iconClasses: 'fa fa-circle'
+        });
+
+        extraTools.push({
+            id: 'wwwcRegion',
+            title: 'ROI Window',
+            classes: 'imageViewerTool',
+            iconClasses: 'fa fa-square'
+        });
+
+        extraTools.push({
+            id: 'dragProbe',
+            title: 'Probe',
+            classes: 'imageViewerTool',
+            iconClasses: 'fa fa-dot-circle-o'
+        });
+
+        extraTools.push({
+            id: 'ellipticalRoi',
+            title: 'Ellipse',
+            classes: 'imageViewerTool',
+            iconClasses: 'fa fa-circle-o'
+        });
+
+        extraTools.push({
+            id: 'rectangleRoi',
+            title: 'Rectangle',
+            classes: 'imageViewerTool',
+            iconClasses: 'fa fa-square-o'
+        });
+
+        extraTools.push({
+            id: 'invert',
+            title: 'Invert',
+            classes: 'imageViewerCommand',
+            iconClasses: 'fa fa-adjust'
+        });
+
+        extraTools.push({
+            id: 'clearTools',
+            title: 'Clear',
+            classes: 'imageViewerCommand',
+            iconClasses: 'fa fa-trash'
+        });
+
         var buttonData = [];
+
         buttonData.push({
             id: 'zoom',
             title: 'Zoom',
             classes: 'imageViewerTool',
-            svgLink: '/packages/ohif_viewerbase/assets/icons.svg#icon-tools-zoom'
+            svgLink: 'packages/ohif_viewerbase/assets/icons.svg#icon-tools-zoom'
         });
 
         buttonData.push({
             id: 'wwwc',
             title: 'Levels',
             classes: 'imageViewerTool',
-            svgLink: '/packages/ohif_viewerbase/assets/icons.svg#icon-tools-levels'
+            svgLink: 'packages/ohif_viewerbase/assets/icons.svg#icon-tools-levels'
         });
 
         buttonData.push({
             id: 'pan',
             title: 'Pan',
             classes: 'imageViewerTool',
-            svgLink: '/packages/ohif_viewerbase/assets/icons.svg#icon-tools-pan'
+            svgLink: 'packages/ohif_viewerbase/assets/icons.svg#icon-tools-pan'
         });
 
         buttonData.push({
             id: 'length',
             title: 'Length',
             classes: 'imageViewerTool toolbarSectionButton',
-            svgLink: '/packages/ohif_viewerbase/assets/icons.svg#icon-tools-measure-temp'
+            svgLink: 'packages/ohif_viewerbase/assets/icons.svg#icon-tools-measure-temp'
         });
 
         buttonData.push({
             id: 'annotate',
             title: 'Annotate',
             classes: 'imageViewerTool',
-            svgLink: '/packages/ohif_viewerbase/assets/icons.svg#icon-tools-measure-non-target'
+            svgLink: 'packages/ohif_viewerbase/assets/icons.svg#icon-tools-measure-non-target'
         });
 
         buttonData.push({
@@ -69,19 +136,39 @@ Template.toolbarSection.helpers({
             iconClasses: 'fa fa-undo'
         });
 
-        buttonData.push({
-            id: 'toggleCinePlay',
-            title: 'Toggle CINE Play',
-            classes: 'imageViewerCommand',
-            buttonTemplateName: 'playClipButton'
-        });
+        if (!OHIF.uiSettings.displayEchoUltrasoundWorkflow) {
 
-        buttonData.push({
-            id: 'toggleCineDialog',
-            title: 'CINE',
-            classes: 'imageViewerCommand',
-            iconClasses: 'fa fa-youtube-play'
-        });
+            buttonData.push({
+                id: 'previousDisplaySet',
+                title: 'Previous',
+                classes: 'imageViewerCommand',
+                buttonTemplateName: 'displaySetNavigation',
+                isNext: false
+            });
+
+            buttonData.push({
+                id: 'nextDisplaySet',
+                title: 'Next',
+                classes: 'imageViewerCommand',
+                buttonTemplateName: 'displaySetNavigation',
+                isNext: true
+            });
+
+            buttonData.push({
+                id: 'toggleCinePlay',
+                title: 'Toggle CINE Play',
+                classes: 'imageViewerCommand',
+                buttonTemplateName: 'playClipButton'
+            });
+
+            buttonData.push({
+                id: 'toggleCineDialog',
+                title: 'CINE',
+                classes: 'imageViewerCommand',
+                iconClasses: 'fa fa-youtube-play',
+                disableFunction: OHIF.viewerbase.viewportUtils.hasMultipleFrames
+            });
+        }
 
         buttonData.push({
             id: 'layout',
@@ -90,69 +177,15 @@ Template.toolbarSection.helpers({
             buttonTemplateName: 'layoutButton'
         });
 
+        buttonData.push({
+            id: 'toggleMore',
+            title: 'More',
+            classes: 'rp-x-1 rm-l-3',
+            svgLink: 'packages/ohif_viewerbase/assets/icons.svg#icon-tools-more',
+            subTools: extraTools
+        });
+
         return buttonData;
-    },
-
-    extraToolbarButtons() {
-        let buttonData = [];
-
-        buttonData.push({
-            id: 'stackScroll',
-            title: 'Stack Scroll',
-            classes: 'imageViewerTool',
-            iconClasses: 'fa fa-bars'
-        });
-
-        buttonData.push({
-            id: 'magnify',
-            title: 'Magnify',
-            classes: 'imageViewerTool toolbarSectionButton',
-            iconClasses: 'fa fa-circle'
-        });
-        
-        buttonData.push({
-            id: 'wwwcRegion',
-            title: 'ROI Window',
-            classes: 'imageViewerTool',
-            iconClasses: 'fa fa-square'
-        });
-
-        buttonData.push({
-            id: 'dragProbe',
-            title: 'Probe',
-            classes: 'imageViewerTool',
-            iconClasses: 'fa fa-dot-circle-o'
-        });
-
-        buttonData.push({
-            id: 'ellipticalRoi',
-            title: 'Ellipse',
-            classes: 'imageViewerTool',
-            iconClasses: 'fa fa-circle-o'
-        });
-
-        buttonData.push({
-            id: 'rectangleRoi',
-            title: 'Rectangle',
-            classes: 'imageViewerTool',
-            iconClasses: 'fa fa-square-o'
-        });
-
-        buttonData.push({
-            id: 'invert',
-            title: 'Invert',
-            classes: 'imageViewerCommand',
-            iconClasses: 'fa fa-adjust'
-        });
-
-        buttonData.push({
-            id: 'clearTools',
-            title: 'Clear',
-            classes: 'imageViewerCommand',
-            iconClasses: 'fa fa-trash'
-        });
-
-        return buttonData;   
     },
 
     hangingProtocolButtons() {
