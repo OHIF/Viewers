21054f17efc7a6ff30a57a7f2d46a0e6319227e4
Erik Ziegler <erik.sweed@gmail.com>
2017-04-12 12:01:38 +0200

    Remove unnecessary dropCollection function. Make it opt-in for Lesion Tracker testing

--- Packages/ohif-lesiontracker/server/publications.js
+++ Packages/ohif-lesiontracker/server/publications.js
@@ -35,21 +35,3 @@ Meteor.publish('singlePatientImageMeasurements', function(patientId) {
 Meteor.publish('reviewers', function() {
     return Reviewers.find();
 });
-
-// Temporary fix to drop all Collections on server restart
-// http://stackoverflow.com/questions/23891631/meteor-how-can-i-drop-all-mongo-collections-and-clear-all-data-on-startup
-Meteor.startup(function() {
-    if (!Meteor.settings.dropCollections) {
-        return;
-    }
-
-    for (var property in global) {
-        var object = global[property];
-        if (object instanceof Meteor.Collection) {
-            if (!(/^server|currentServer$/).test(object._name)) {
-                console.warn(`Dropping: ${object._debugName || object._name}`);
-                object.remove({});
-            }
-        }
-    }
-});
