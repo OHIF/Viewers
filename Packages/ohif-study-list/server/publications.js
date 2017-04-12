bf97c3ebe2d4a57b86c2b57a0cb43b1c8235b67a
Erik Ziegler <erik.sweed@gmail.com>
2017-04-12 12:00:44 +0200

    Prevent requestOptions.headers from being published to the client (in case it contains Authentication info)

--- Packages/ohif-study-list/server/publications.js
+++ Packages/ohif-study-list/server/publications.js
@@ -3,6 +3,12 @@ import { OHIF } from 'meteor/ohif:core';
 
 Meteor.publish('studyImportStatus', () => OHIF.studylist.collections.StudyImportStatus.find());
 
-Meteor.publish('servers', () => Servers.find());
+// When publishing Servers Collection, do not publish the requestOptions.headers
+// field in case any authentication information is being passed
+Meteor.publish('servers', () => Servers.find({}, {
+	fields: {
+		'requestOptions.headers': 0
+	}
+}));
 
 Meteor.publish('currentServer', () => CurrentServer.find());
