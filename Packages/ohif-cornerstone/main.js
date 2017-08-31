f41b154d0068af28b898e75065fb9b996ec5dcba
Bruno Alves de Faria <brunoalvesdefaria@gmail.com>
2017-08-31 19:26:51 -0300

    PWV-92: Overriding cornerstoneTools mouseInput

--- Packages/ohif-cornerstone/main.js
+++ Packages/ohif-cornerstone/main.js
@@ -8,13 +8,13 @@ import { OHIF } from './namespace.js';
  * Import scripts that will populate the Cornerstone namespace as a side effect only import. This is effectively the public API...
  */
 
-import './client/'; // which is actually: import './client/index.js';
-
 import * as cornerstone from 'cornerstone-core';
 import * as cornerstoneTools from 'cornerstone-tools';
 import * as cornerstoneMath from 'cornerstone-math';
 import * as cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
 import * as dicomParser from 'dicom-parser';
+
+import './client/'; // which is actually: import './client/index.js';
 
 export {
     cornerstone,
