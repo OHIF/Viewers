d9daa4295462ac71d469886068608c18aab47d42
Bruno Alves de Faria <brunoalvesdefaria@gmail.com>
2017-08-31 19:26:51 -0300

    PWV-92: Overriding cornerstoneTools mouseInput

--- Packages/ohif-cornerstone/client/index.js
+++ Packages/ohif-cornerstone/client/index.js
@@ -1,5 +1,6 @@
 import { OHIF } from '../namespace';
 import './renderer.js';
 
+import './lib/cornerstoneToolsMouseInputOverrides.js';
 import { MetadataProvider } from './lib/classes/MetadataProvider';
 OHIF.cornerstone.metadataProvider = new MetadataProvider();
