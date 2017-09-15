b2a52b171fe6a54ca5ecbe158f218d5c782c16d3
Andre Botelho <andrebot_almeida@hotmail.com>
2017-09-15 16:59:22 -0300

    Using meteor error in StudyListSearch to better display same error in the client

--- Packages/ohif-study-list/server/methods/studylistSearch.js
+++ Packages/ohif-study-list/server/methods/studylistSearch.js
@@ -13,7 +13,7 @@ Meteor.methods({
         const server = OHIF.servers.getCurrentServer();
 
         if (!server) {
-            throw 'No properly configured server was available over DICOMWeb or DIMSE.';
+            throw new Meteor.Error('improper-server-config', 'No properly configured server was available over DICOMWeb or DIMSE.');
         }
 
         if (server.type === 'dicomWeb') {
