f5af70a218102a1a526ea3ebc53f4f89cd66b837
Bruno Alves de Faria <brunoalvesdefaria@gmail.com>
2017-10-27 23:47:30 -0200

    Improving polyfill inclusion and fixing SVG issues

--- Packages/ohif-polyfill/client/lib/writeScript.js
+++ Packages/ohif-polyfill/client/lib/writeScript.js
@@ -1,7 +1,14 @@
 /* jshint -W060 */
 import absoluteUrl from './absoluteUrl';
 
-export default function(fileName) {
-    const src = absoluteUrl(`/packages/ohif_polyfill/public/js/${fileName}`);
-    document.write(`<script src="${src}"><\/script>`);
+export default function(fileName, callback) {
+    const script = document.createElement('script');
+    script.src = absoluteUrl(`/packages/ohif_polyfill/public/js/${fileName}`);
+    script.onload = () => {
+        if (typeof callback === 'function') {
+            callback(script);
+        }
+    };
+
+    document.body.appendChild(script);
 }
