8af73dc438caa82f02c68869c2f6202c6d69a06d
Bruno Alves de Faria <brunoalvesdefaria@gmail.com>
2017-10-28 07:45:53 -0200

    Preventing svg polyfill from interefering on frames rendering

--- Packages/ohif-polyfill/client/ie.js
+++ Packages/ohif-polyfill/client/ie.js
@@ -1,12 +1,16 @@
 import { Meteor } from 'meteor/meteor';
+import { $ } from 'meteor/jquery';
 import writeScript from './lib/writeScript';
 
-Meteor.startup(() => {
-    // Check if browser is IE and add the polyfill scripts
-    if (navigator && /MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
+// Check if browser is IE and add the polyfill scripts
+if (navigator && /MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
+    Meteor.startup(() => {
+        // This is needed before window loading because it's used to parse DICOM images data
         writeScript('typedarray.min.js');
 
-        // Fix SVG+USE issues by calling the SVG polyfill
-        writeScript('svg4everybody.min.js', () => window.svg4everybody());
-    }
-});
+        $(window).on('load', () => {
+            // Fix SVG+USE issues by calling the SVG polyfill
+            writeScript('svg4everybody.min.js', () => window.svg4everybody());
+        });
+    });
+}
