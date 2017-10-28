d8471d84a04b345000243e56dc42ebdaa17aaa62
Bruno Alves de Faria <brunoalvesdefaria@gmail.com>
2017-10-27 23:47:30 -0200

    Improving polyfill inclusion and fixing SVG issues

--- Packages/ohif-polyfill/client/ie.js
+++ Packages/ohif-polyfill/client/ie.js
@@ -1,6 +1,12 @@
+import { Meteor } from 'meteor/meteor';
 import writeScript from './lib/writeScript';
 
-// Check if browser is IE and add the polyfill scripts
-if (navigator && /MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
-    writeScript('typedarray.min.js');
-}
+Meteor.startup(() => {
+    // Check if browser is IE and add the polyfill scripts
+    if (navigator && /MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
+        writeScript('typedarray.min.js');
+
+        // Fix SVG+USE issues by calling the SVG polyfill
+        writeScript('svg4everybody.min.js', () => window.svg4everybody());
+    }
+});
--- Packages/ohif-polyfill/client/index.js
+++ Packages/ohif-polyfill/client/index.js
@@ -1,2 +1,1 @@
-import './all.js';
 import './ie.js';
