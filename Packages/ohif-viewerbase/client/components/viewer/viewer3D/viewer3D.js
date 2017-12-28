dd3d9f08bff8d0335ee9a26482a3ddf8bc985347
gustavo82mdq <gustavo82mdq@gmail.com>
2017-12-27 22:43:05 -0300

    27122017 raycasting working YEEEEEEHAAAAAAA!!!

--- Packages/ohif-viewerbase/client/components/viewer/viewer3D/viewer3D.js
+++ Packages/ohif-viewerbase/client/components/viewer/viewer3D/viewer3D.js
@@ -5,6 +5,9 @@ import 'meteor/fds:threejs';
 var scene, camera, renderer, controls;
 var geometry, material, sphere, geometry2, material2, sphere2, edges;
 var aspectRelation; // Width / Height
+var objects = [];
+var mouse , INTERSECTED, SELECTED_OBJECT;
+var canvas;
 
 var Detector = {
 
@@ -80,41 +83,65 @@ if ( typeof module === 'object' ) {
 
 }
 
+function onMouseClick(event) {
+    event.preventDefault();
+    SELECTED_OBJECT = INTERSECTED;
+}
+
+function onMouseMove(event) {
+    event.preventDefault();
+    var x = event.offsetX == undefined ? event.layerX : event.offsetX;
+    var y = event.offsetY == undefined ? event.layerY : event.offsetY;
+    mouse.x = ( x / renderer.domElement.width ) * 2 - 1;
+    mouse.y = - ( y / renderer.domElement.height ) * 2 + 1;
+    var raycaster =  new THREE.Raycaster();
+    raycaster.setFromCamera( mouse, camera );
+    var intersects = raycaster.intersectObjects( scene.children );
+    console.log(intersects);
+
+    if (intersects.length > 0) {
+        if (INTERSECTED != intersects[0].object) {
+            if (INTERSECTED) INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
+
+            INTERSECTED = intersects[ 0 ].object;
+            INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
+            INTERSECTED.material.emissive.setHex( 0xff0000 );
+        }
+    } else {
+        if (INTERSECTED) INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
+        INTERSECTED = null;
+    }
+}
+
 function init() {
+    //Initialize vars
+    mouse = new THREE.Vector2();
+
+    //Scene
     scene = new THREE.Scene();
 
-    renderer = new THREE.WebGLRenderer({canvas: document.getElementById('wglcanvas')});
+    //Renderer
+    canvas = document.getElementById('wglcanvas');
+    renderer = new THREE.WebGLRenderer({canvas: canvas});
+    renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( renderer.domElement.offsetWidth, renderer.domElement.offsetHeight );
+
+    //Camera
     aspectRelation = renderer.getSize().width / renderer.getSize().height;
-    geometry = new THREE.SphereGeometry(1, 64, 64, 3 * Math.PI / 2, Math.PI / 2);
-    material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    material.transparent = false;
-    material.opacity = 0.5;
-    sphere = new THREE.Mesh( geometry, material );
-    sphere.scale.set(1, 1, 1);
-    edges = new THREE.EdgesHelper(sphere);
-    geometry2 = new THREE.SphereGeometry(2, 64, 64, 0, 3 * Math.PI / 2);
-    material2 = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-    material2.transparent = true;
-    material2.opacity = 0.1;
-    sphere2 = new THREE.Mesh( geometry2, material2 );
-    sphere2.scale.set(1, 1, 1);
+    camera = new THREE.PerspectiveCamera( 70, aspectRelation, 0.1, 10000 );
+    camera.updateProjectionMatrix();
+    camera.position.z = 5;
+    scene.add(camera);
 
-    var loader = new THREE.VTKLoader();
-    loader.load("/artery.vtk", function (geometry) {
-        geometry.center();
-        geometry.computeVertexNormals();
-        var material = new THREE.MeshLambertMaterial( { color: 0x2A54DD, side: THREE.DoubleSide } );
-        var mesh = new THREE.Mesh(geometry, material);
-        mesh.scale.set(0.1, 0.1, 0.1);
-       mesh.scale.multiplyScalar(0.1);
-        scene.add(mesh);
-        render();
-    });
+    //Light
+    var dirLight = new THREE.DirectionalLight( 0xffffff );
+    dirLight.position.set( 200, 200, 1000 ).normalize();
+    camera.add( dirLight );
+    camera.add( dirLight.target );
 
-    camera = new THREE.PerspectiveCamera( 75, aspectRelation, 0.1, 1000 );
+
+    //Controls
     controls = new THREE.TrackballControls(camera, renderer.domElement);
-
     controls.rotateSpeed = 1.0;
     controls.zoomSpeed = 1.2;
     controls.panSpeed = 0.8;
@@ -125,22 +152,75 @@ function init() {
     controls.keys = [ 65, 83, 68 ];
     controls.addEventListener( 'change', render );
 
-    var dirLight = new THREE.DirectionalLight( 0xffffff );
-    dirLight.position.set( 200, 200, 1000 ).normalize();
-    camera.add( dirLight );
-    camera.add( dirLight.target );
+    // geometry = new THREE.SphereGeometry(1, 64, 64, 3 * Math.PI / 2, Math.PI / 2);
+    // geometry.center();
+    // material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
+    // material.transparent = false;
+    // material.opacity = 0.5;
+    // sphere = new THREE.Mesh( geometry, material );
+    // sphere.scale.set(1, 1, 1);
+    // edges = new THREE.EdgesHelper(sphere);
+    // geometry2 = new THREE.SphereGeometry(2, 64, 64, 0, 3 * Math.PI / 2);
+    // geometry2.center();
+    // geometry2.computeBoundingBox();
+    // material2 = new THREE.MeshLambertMaterial( { color: 0xff0000 } );
+    // material2.transparent = true;
+    // material2.opacity = 0.1;
+    // sphere2 = new THREE.Mesh( geometry2, material2 );
+    // sphere2.scale.set(1, 1, 1);
 
-    camera.position.z = 5;
+    // scene.add(sphere);
+    // objects.push(sphere);
+    // scene.add(sphere2);
+    // objects.push(sphere2);
 
-    scene.add(camera);
+    var loader = new THREE.VTKLoader();
+    loader.load("/artery.vtk", function (geometry) {
+        geometry.center();
+        geometry.computeVertexNormals();
+        var material = new THREE.MeshLambertMaterial( { color: 0x2A54DD, side: THREE.DoubleSide } );
+        var mesh = new THREE.Mesh(geometry, material);
+        mesh.updateMatrixWorld();
+        // document.getElementById("sp_x_sr").innerHTML = geometry.boundingBox.min.x + ' / ' + geometry.boundingBox.max.x;
+        // document.getElementById("sp_y_sr").innerHTML = geometry.boundingBox.min.y + ' / ' + geometry.boundingBox.max.y;
+        // document.getElementById("sp_z_sr").innerHTML = geometry.boundingBox.min.z + ' / ' + geometry.boundingBox.max.z;
+        scene.add(mesh);
+        objects.push(mesh);
+    });
 
-    render();
+    loader.load("/bone.vtk", function (geometry) {
+        geometry.center();
+        geometry.computeVertexNormals();
+        var material = new THREE.MeshLambertMaterial( { color: 0x66ff99, side: THREE.DoubleSide } );
+        var mesh = new THREE.Mesh(geometry, material);
+        mesh.updateMatrixWorld();
+        // document.getElementById("sp_x_sr").innerHTML = geometry.boundingBox.min.x + ' / ' + geometry.boundingBox.max.x;
+        // document.getElementById("sp_y_sr").innerHTML = geometry.boundingBox.min.y + ' / ' + geometry.boundingBox.max.y;
+        // document.getElementById("sp_z_sr").innerHTML = geometry.boundingBox.min.z + ' / ' + geometry.boundingBox.max.z;
+        scene.add(mesh);
+        objects.push(mesh);
+    });
+
+
+
+    var dir_x = new THREE.Vector3( 1, 0, 0 ); dir_x.normalize();
+    var dir_y = new THREE.Vector3( 0, 1, 0 ); dir_y.normalize();
+    var dir_z = new THREE.Vector3( 0, 0, 1 ); dir_z.normalize();
+
+    var origin = new THREE.Vector3( 0, 0, 0 );
+    var length = 3;
+
+    var arrowHelper_x = new THREE.ArrowHelper( dir_x, origin, length, 0xff0000 );
+    var arrowHelper_y = new THREE.ArrowHelper( dir_y, origin, length, 0x00ff00 );
+    var arrowHelper_z = new THREE.ArrowHelper( dir_z, origin, length, 0xffff00 );
+    scene.add( arrowHelper_x );
+    scene.add( arrowHelper_y );
+    scene.add( arrowHelper_z );
 }
 
 function animate() {
     requestAnimationFrame( animate );
-    // sphere.rotation.x += 0.1;
-    // sphere.rotation.y += 0.1;
+    render();
     controls.update();
 }
 
@@ -157,4 +237,7 @@ Template.viewer3D.onRendered(() => {
         init();
         animate();
     }
+
+    canvas.addEventListener('mousemove', onMouseMove, false);
+    canvas.addEventListener('click', onMouseClick, false);
 });
