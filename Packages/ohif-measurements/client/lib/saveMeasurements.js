19e7363c77531cc1041b602e288c8dd5c79e55a4
Bruno Alves de Faria <brunoalvesdefaria@gmail.com>
2018-01-30 07:30:52 -0200

    Preventing saving with criteria nonconformities

--- Packages/ohif-measurements/client/lib/saveMeasurements.js
+++ Packages/ohif-measurements/client/lib/saveMeasurements.js
@@ -4,6 +4,10 @@ OHIF.measurements.saveMeasurements = (measurementApi, timepointId) => {
     const { unsavedChanges, notifications, showDialog } = OHIF.ui;
     const basePath = `viewer.studyViewer.measurements.${timepointId}`;
 
+    // Stop here if there are nonconformities in the timepoints
+    const nonconformities = OHIF.viewer.conformanceCriteria.nonconformities.get();
+    if (nonconformities.length) return;
+
     // Stop here if there were no changes to the timepoint
     if (unsavedChanges.probe(basePath) === 0) return;
 
