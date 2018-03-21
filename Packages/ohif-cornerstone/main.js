04da9310d1ad876cc638ca22ab06a5532c9094bb
Evren Ozkan <evren217@gmail.com>
2018-03-21 13:20:37 -0400

    Fix the issue with the reference of dicomParser.DataSet class by importing dicomParser from 'dicom-parser' (not from dist)

--- Packages/ohif-cornerstone/main.js
+++ Packages/ohif-cornerstone/main.js
@@ -3,7 +3,7 @@ import * as cornerstone from 'cornerstone-core/dist/cornerstone.js';
 import * as cornerstoneMath from 'cornerstone-math/dist/cornerstoneMath.js';
 import * as cornerstoneTools from 'cornerstone-tools/dist/cornerstoneTools.js';
 import * as cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader/dist/cornerstoneWADOImageLoader.js';
-import * as dicomParser from 'dicom-parser/dist/dicomParser.js';
+import * as dicomParser from 'dicom-parser'; // Importing from dist breaks instance reference of dicomParser.DataSet class
 
 cornerstoneTools.external.Hammer = Hammer;
 cornerstoneTools.external.cornerstone = cornerstone;
