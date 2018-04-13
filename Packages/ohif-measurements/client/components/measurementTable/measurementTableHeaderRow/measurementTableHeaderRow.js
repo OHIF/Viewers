664b78f42c176cf48c812ee2790a6724870c7fb2
Aysel Afsar <aysel.afsar@gmail.com>
2018-04-13 12:08:27 -0400

    fix(lesionTable) Remove plus icon from measurements header

--- Packages/ohif-measurements/client/components/measurementTable/measurementTableHeaderRow/measurementTableHeaderRow.js
+++ Packages/ohif-measurements/client/components/measurementTable/measurementTableHeaderRow/measurementTableHeaderRow.js
@@ -29,37 +29,5 @@ Template.measurementTableHeaderRow.helpers({
         } else if (toolGroupId === 'newTargets') {
             return conformanceCriteria.maxNewTargets.get();
         }
-    },
-
-    anyUnmarkedLesionsLeft() {
-        // Skip New Lesions section
-        const instance = Template.instance();
-        const { toolGroup, measurementRows, timepointApi, measurementApi } = instance.data;
-        if (!measurementRows) return;
-
-        const config = OHIF.measurements.MeasurementApi.getConfiguration();
-        if (config.newLesions && config.newLesions.find(o => o.id === toolGroup.id)) return;
-
-        const current = timepointApi.current();
-        const prior = timepointApi.prior();
-        if (!prior) return true;
-
-        const currentFilter = { timepointId: current.timepointId };
-        const priorFilter = { timepointId: prior.timepointId };
-        const toolGroupId = toolGroup.id;
-
-        const numCurrent = measurementApi.fetch(toolGroupId, currentFilter).length;
-        const numPrior = measurementApi.fetch(toolGroupId, priorFilter).length;
-        const remaining = Math.max(numPrior - numCurrent, 0);
-        return remaining > 0;
-    }
-});
-
-Template.measurementTableHeaderRow.events({
-    'click .js-setTool'(event, instance) {
-        const { toolGroup } = instance.data;
-        const toolType = toolGroup.childTools[0].cornerstoneToolType;
-        const activeToolId = Array.isArray(toolType) ? toolType[0] : toolType;
-        Viewerbase.toolManager.setActiveTool(activeToolId);
     }
 });
