2d44f8fd1f725fe54c1d54eb344a3e9d301d2f2b
Aysel Afsar <aysel.afsar@gmail.com>
2018-05-03 14:47:59 -0400

    Do not send servers auth credentials to client

--- Packages/ohif-servers/server/publications.js
+++ Packages/ohif-servers/server/publications.js
@@ -4,7 +4,10 @@ import { Servers, CurrentServer } from 'meteor/ohif:servers/both/collections';
 // When publishing Servers Collection, do not publish the requestOptions.headers
 // field in case any authentication information is being passed
 Meteor.publish('servers', () => Servers.find({}, {
-    fields: { 'requestOptions.headers': 0 }
+    fields: {
+        'requestOptions.headers': 0,
+        'requestOptions.auth': 0,
+    }
 }));
 
 Meteor.publish('currentServer', () => CurrentServer.find());
