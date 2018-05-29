781f6039fa6e6717e02abd06c756d21026ba0693
Erik Ziegler <erik.sweed@gmail.com>
2018-05-29 08:55:34 +0200

    fix(routing) Stop defining Route in Meteor.startup

--- Packages/ohif-user/client/routes.js
+++ Packages/ohif-user/client/routes.js
@@ -1,9 +1,6 @@
-import { Meteor } from 'meteor/meteor';
 import { Router } from 'meteor/iron:router';
 
-Meteor.startup(() => {
-    Router.route('/login', function() {
-        this.layout('mainLayout', { data: {} });
-        this.render('userLogin');
-    }, { name: 'userLogin' });
-});
+Router.route('/login', function() {
+    this.layout('mainLayout', { data: {} });
+    this.render('userLogin');
+}, { name: 'userLogin' });
