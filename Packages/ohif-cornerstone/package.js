5bf1459561eaf039ecfb553099b9ff4a9669af5e
Erik Ziegler <erik.sweed@gmail.com>
2018-07-11 17:14:23 +0200

    Update to WADO Image Loader 2.1.3

--- Packages/ohif-cornerstone/package.js
+++ Packages/ohif-cornerstone/package.js
@@ -10,7 +10,7 @@ Npm.depends({
     'cornerstone-tools': '2.3.3',
     'cornerstone-math': '0.1.6',
     'dicom-parser': '1.8.0',
-    'cornerstone-wado-image-loader': '2.1.2'
+    'cornerstone-wado-image-loader': '2.1.3'
 });
 
 Package.onUse(function(api) {
