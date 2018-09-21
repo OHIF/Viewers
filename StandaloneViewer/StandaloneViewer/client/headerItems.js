29808863246efa293cf14f4a98209bf4a4e6cc06
Juan Francisco Del Solar <juandelsolar@gmail.com>
2018-09-21 15:50:49 -0300

    First changes to get i18n working

--- StandaloneViewer/StandaloneViewer/client/headerItems.js
+++ StandaloneViewer/StandaloneViewer/client/headerItems.js
@@ -1,14 +1,16 @@
 import { OHIF } from 'meteor/ohif:core';
 
+console.log('18n', TAPi18n.getLanguages());
+
 OHIF.header.dropdown.setItems([{
     action: () => OHIF.ui.showDialog('themeSelectorModal'),
-    text: 'Themes',
+    text: TAPi18n.__('Themes'),
     iconClasses: 'theme',
     iconSvgUse: 'packages/ohif_viewerbase/assets/icons.svg#theme',
     separatorAfter: false
 }, {
     action: () => OHIF.ui.showDialog('userPreferencesDialog'),
-    text: 'Preferences',
+    text: TAPi18n.__('Preferences'),
     icon: 'fa fa-user',
     separatorAfter: false
 }]);
