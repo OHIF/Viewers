55cb2402fb3b66ba06906e28924e169b824f49ff
Erik Ziegler <erik.sweed@gmail.com>
2018-10-16 10:17:45 +0200

    Fix typo

--- Packages/ohif-measurement-table/client/utils/retrieveDataFromMeasurements.js
+++ Packages/ohif-measurement-table/client/utils/retrieveDataFromMeasurements.js
@@ -2,7 +2,7 @@ import { dcmjs } from 'meteor/ohif:cornerstone';
 
 export default retrieveDataFromMeasurements = (measurements) => {
     const { MeasurementReport } = dcmjs.adapters.Cornerstone;
-    const report = MeasurementReport.generateReport(measurements, cornerstone.metadata);
+    const report = MeasurementReport.generateReport(measurements, cornerstone.metaData);
 
     return report.dataset;
 }
