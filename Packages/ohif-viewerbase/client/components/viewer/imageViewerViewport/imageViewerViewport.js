0a5feb9a0de765295da06f10f4088bcbd19abe9d
Evren Ozkan <evren217@gmail.com>
2018-10-29 10:58:53 -0400

    Integrate Cornerstone Tools v3 (#235) - WIP - fix(stack-scroll): Activate mouse wheel and scrollbar stack scroll - fix(touch): Activate pinch zoom, two-finger pan and three (or more) finger stack scroll

--- Packages/ohif-viewerbase/client/components/viewer/imageViewerViewport/imageViewerViewport.js
+++ Packages/ohif-viewerbase/client/components/viewer/imageViewerViewport/imageViewerViewport.js
@@ -267,8 +267,8 @@ const loadDisplaySetIntoViewport = (data, templateData) => {
             cornerstoneTools.playClip(element);
         }
 
-        // InstantiateTools on the new element.
-        toolManager.instantiateTools(element);
+        // Instantiate the tools
+        toolManager.instantiateTools();
 
         // Use the tool manager to enable the currently active tool for this
         // newly rendered element
@@ -646,17 +646,13 @@ Template.imageViewerViewport.onDestroyed(function() {
         return;
     }
 
-    // Disable mouse functions
-    cornerstoneTools.mouseInput.disable(element);
-    cornerstoneTools.touchInput.disable(element);
-    cornerstoneTools.mouseWheelInput.disable(element);
+    // Remove all tools for the destroyed element
+    toolManager.removeToolsForElement(element);
 
     OHIF.viewer.updateImageSynchronizer.remove(element);
 
     // Clear the stack prefetch data
-    let stackPrefetchData = cornerstoneTools.getToolState(element, 'stackPrefetch');
-    stackPrefetchData = [];
-    cornerstoneTools.stackPrefetch.disable(element);
+    cornerstoneTools.clearToolState(element, 'stackPrefetch');
 
     // Try to stop any currently playing clips
     // Otherwise the interval will continuously throw errors
