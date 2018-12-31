f91fd93462f991cf94eb03a747e40a51826f5eaf
Rodrigo Antinarelli <rodrigoantinarelli@gmail.com>
2018-12-31 14:29:53 -0200

    add Back to Viewer btn

--- OHIFViewer-react/src/Header/Header.js
+++ OHIFViewer-react/src/Header/Header.js
@@ -1,21 +1,38 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 import { Dropdown } from "../components"
 import Icons from "../images/icons.svg"
 import './Header.css'
 import list from './HeaderMenuList.json'
 
-function Header({ home }) {
+function Header({ home, lastStudy, location }) {
+  const { state } = location
+
   return (
     <div className={`entry-header ${home ? 'header-big' : ''}`}>
-      <a target="_blank" rel="noopener noreferrer" className="header-brand" href="http://ohif.org">
-        <svg className="header-logo-image">
-          <use xlinkHref={`${Icons}#icon-ohif-logo`} />
-        </svg>
-        <div className="header-logo-text">Open Health Imaging Foundation</div>
-      </a>
+      <div className='header-left-box'>
+        {
+          state && state.studyLink &&
+          <Link to={state.studyLink} className="header-btn header-viewerLink">
+            Back to Viewer
+          </Link>
+        }
 
-      {!home && <Link className='header-btn header-studyListLinkSection' to="/">Study list</Link>}
+        <a target="_blank" rel="noopener noreferrer" className="header-brand" href="http://ohif.org">
+          <svg className="header-logo-image">
+            <use xlinkHref={`${Icons}#icon-ohif-logo`} />
+          </svg>
+          <div className="header-logo-text">Open Health Imaging Foundation</div>
+        </a>
+
+        {!home &&
+          <Link className='header-btn header-studyListLinkSection' to={{
+            pathname: "/",
+            state: { studyLink: location.pathname }
+          }}>Study list</Link>
+        }
+      </div>
+
 
       <div className="header-menu">
         {/* TODO: research-use */}
@@ -31,4 +48,4 @@ function Header({ home }) {
   )
 }
 
-export default Header
+export default withRouter(Header)
