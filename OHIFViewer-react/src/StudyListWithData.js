3248dc9441a937f5fd1237b8bae50fb704e3f250
maltempi <tmmaltempi@gmail.com>
2019-01-11 15:41:54 -0200

    Reverting studylistwithdata

--- OHIFViewer-react/src/StudyListWithData.js
+++ OHIFViewer-react/src/StudyListWithData.js
@@ -66,11 +66,11 @@ class StudyListWithData extends Component {
                 studies: fixedStudies,
             });
         }).catch(error => {
-            // this.setState({
-            //     error: true,
-            // });
+            this.setState({
+                error: true,
+            });
 
-            // throw new Error(error);
+            throw new Error(error);
         });
     }
 
