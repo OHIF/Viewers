991539caa22ec728132567e8c894db5f8237c677
Siegel <ssiegel@fme.lan>
2019-01-29 14:56:34 +0000

    Different entry points for development and publish

--- Packages-react/ohif-viewer/src/index.js
+++ Packages-react/ohif-viewer/src/index.js
@@ -1,10 +1,13 @@
+/**
+ * Entry point index.js for development
+ */
+
+
 import App from './App.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export {
-  App
-};
+export { App };
 
 var routerBasename = 'http://localhost:5000';
 var props = {
@@ -52,23 +55,22 @@ props.oidc = [{
 // Try going to:
 // http://localhost:5000/viewer/1.3.6.1.4.1.14519.5.2.1.1706.4996.216859690032335293073513900362
 props.servers = {
-    "dicomWeb": [
-        {
-            "name": "DCM4CHEE",
-            "wadoUriRoot": "https://dcm4che.ohif.club/dcm4chee-arc/aets/DCM4CHEE/wado",
-            "qidoRoot": "https://dcm4che.ohif.club/dcm4chee-arc/aets/DCM4CHEE/rs",
-            "wadoRoot": "https://dcm4che.ohif.club/dcm4chee-arc/aets/DCM4CHEE/rs",
-            "qidoSupportsIncludeField": true,
-            "imageRendering": "wadors",
-            "thumbnailRendering": "wadors",
-            "requestOptions": {
-                "requestFromBrowser": true,
-                "auth": "cloud:healthcare"
-            }
-        }
-    ]
+  dicomWeb: [
+    {
+      name: 'DCM4CHEE',
+      wadoUriRoot: 'https://dcm4che.ohif.club/dcm4chee-arc/aets/DCM4CHEE/wado',
+      qidoRoot: 'https://dcm4che.ohif.club/dcm4chee-arc/aets/DCM4CHEE/rs',
+      wadoRoot: 'https://dcm4che.ohif.club/dcm4chee-arc/aets/DCM4CHEE/rs',
+      qidoSupportsIncludeField: true,
+      imageRendering: 'wadors',
+      thumbnailRendering: 'wadors',
+      requestOptions: {
+        requestFromBrowser: true,
+        auth: 'cloud:healthcare'
+      }
+    }
+  ]
 };
-
 
 /*
 UI settings
@@ -79,7 +81,7 @@ Plugins
     - Custom Sop Class Interpreters
 */
 
-  /*"PUBLIC_SETTINGS": {
+/*"PUBLIC_SETTINGS": {
     "ui": {
       "studyListFunctionsEnabled": true,
       "leftSidebarOpen": false,
@@ -101,16 +103,21 @@ White labelling example
 // https://babeljs.io/repl/#?babili=false&browsers=&build=&builtIns=false&spec=false&loose=false&code_lz=GYVwdgxgLglg9mABACwKYBt1wBQEpEDeAUIogE6pQhlIA8AholPWQOaUC8ARAPoBG6emADWXchm5g4cAA6owqMoikVgiimTERBAZx0A5egFtU3NPQAmigLR8yQi2OSqzUKDIBcAei_2LMCHp0GCN6VhgwVgA6CDgjLgA-ElIU1LS02hDWRB0yCA4AcmQ3Tx8_AKCQsIjo2KMvAHcZa1iwKHkoLxAZLEsdLwAmAAYARgB2LxGRrwgyWTkLFrmZBetwMGNURYGomUiCxC8k9JOU2i96BIBuIgBfIiA&debug=false&forceAllTransforms=false&shippedProposals=false&circleciRepo=&evaluate=false&fileSize=false&timeTravel=false&sourceType=module&lineWrap=true&presets=react&prettier=false&targets=&version=6.26.0&envVersion=
 */
 function RadicalImagingLogo() {
-    return React.createElement(
-            "a",
-            { target: "_blank", rel: "noopener noreferrer", className: "header-brand", href: "http://radicalimaging.com" },
-            React.createElement("h5", {}, 'RADICAL IMAGING')
-    );
+  return React.createElement(
+    'a',
+    {
+      target: '_blank',
+      rel: 'noopener noreferrer',
+      className: 'header-brand',
+      href: 'http://radicalimaging.com'
+    },
+    React.createElement('h5', {}, 'RADICAL IMAGING')
+  );
 }
 
 props.whiteLabelling = {
   logoComponent: RadicalImagingLogo()
-}
+};
 
 // TODO: Why do we need to do this? Something is wrong with our rollup settings
 //var Viewer = window.OHIFStandaloneViewer.App;
