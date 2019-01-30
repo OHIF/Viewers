fea71df48efbc59bb08e440d59c9de297e5b04f8
maltempi <tmmaltempi@gmail.com>
2019-01-30 16:32:41 -0200

    Mmoving from OHIFViewer-react to ohif-viewer

---
+++ Packages-react/ohif-viewer/src/redux/localStorageState.js
@@ -0,0 +1,19 @@
+export const loadState = () => {
+  try {
+    const serializedState = window.localStorage.getItem('state');
+    if (!serializedState) {
+      return undefined;
+    }
+
+    return JSON.parse(serializedState);
+  } catch (e) {
+    return undefined;
+  }
+};
+
+export const saveState = state => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (e) {}
+};
