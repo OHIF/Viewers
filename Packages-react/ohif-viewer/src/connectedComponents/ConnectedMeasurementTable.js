de8dd084bdf5d948c06c1467ff3d9a11a367d780
Gustavo Lelis <galelis@gmail.com>
2019-02-14 04:12:18 -0200

    feat(measurementTable): Create a convert function to transform measurements from ohif-core into tableData

--- Packages-react/ohif-viewer/src/connectedComponents/ConnectedMeasurementTable.js
+++ Packages-react/ohif-viewer/src/connectedComponents/ConnectedMeasurementTable.js
@@ -3,6 +3,98 @@ import { MeasurementTable } from 'react-viewerbase';
 import OHIF from 'ohif-core';
 import moment from 'moment';
 
+function groupBy(list, props) {
+  return list.reduce((a, b) => {
+     (a[b[props]] = a[b[props]] || []).push(b);
+     return a;
+  }, {});
+}
+
+function getAllTools() {
+  const config = OHIF.measurements.MeasurementApi.getConfiguration();
+  let tools = [];
+  config.measurementTools.forEach( toolGroup => 
+    tools = tools.concat(toolGroup.childTools)
+  );
+
+  return tools;
+}
+
+function getMeasurementText(measurementData) {
+  const { location, description} = measurementData;
+  let text = '...';
+  if (location) {
+    text = location;
+    if (description) {
+      text += `(${description})`;
+    }
+  }
+  return text;
+}
+
+function getDataForEachMeasurementNumber(measurementNumberList, timepoints, displayFunction) {
+  const data = [];
+  // on each measurement number we should get each measurement data by available timepoint
+  measurementNumberList.forEach( measurement => {
+    timepoints.forEach( timepoint => {
+      const eachData = {
+        displayText: '...'
+      }
+      if (measurement.timepointId === timepoint.timepointId) {
+        eachData.displayText = displayFunction(measurement);
+      }
+      data.push(eachData);
+    });
+  });
+
+  return data;
+}
+
+function convertMeasurementsToTableData(toolCollections, timepoints) {
+  const config = OHIF.measurements.MeasurementApi.getConfiguration();
+  const toolGroups = config.measurementTools;
+  const tools = getAllTools();
+
+  const tableMeasurements = toolGroups.map( toolGroup => {
+    return {
+      groupName: toolGroup.name,
+      groupId: toolGroup.id,
+      measurements: []
+    }
+  });
+
+  Object.keys(toolCollections).forEach(toolId => {
+    const toolMeasurements = toolCollections[toolId];
+    const tool = tools.find(tool => tool.id === toolId);
+    const { displayFunction } = tool.options.measurementTable;
+
+    // Group by measurementNumber so we can display then all in the same line
+    const groupedMeasurements = groupBy(toolMeasurements, 'measurementNumber');
+
+    Object.keys(groupedMeasurements).forEach( groupedMeasurementsIndex => {
+      const measurementNumberList = groupedMeasurements[groupedMeasurementsIndex];
+      //check if all measurements with same measurementNumber will have same LABEL
+      const tableMeasurement = {
+        label: getMeasurementText(measurementNumberList[0]), 
+        hasWarnings: false, //TODO
+        warningTitle: '', //TODO
+        isSplitLesion: false, //TODO
+        warningList: [], //TODO
+        data: getDataForEachMeasurementNumber(measurementNumberList, timepoints, displayFunction)
+      };
+
+      // find the group object for the tool
+      const toolGroupMeasurements = tableMeasurements.find( group => {
+        return group.groupId === tool.toolGroup;
+      });
+      // inject the new measurement for this measurementNumer
+      toolGroupMeasurements.measurements.push(tableMeasurement);
+    });
+  });
+
+  return tableMeasurements;
+}
+
 function convertTimepointsToTableData(timepoints) {
   if (!timepoints || !timepoints.length) {
     return [];
@@ -16,49 +108,11 @@ function convertTimepointsToTableData(timepoints) {
   ];
 }
 
-function convertMeasurementsToTableData(measurements) {
-  const tableData = [
-    {
-      groupName: 'Measurements',
-      measurements: []
-    }
-  ];
-
-  if (measurements) {
-    const config = OHIF.measurements.MeasurementApi.getConfiguration();
-    const tools = config.measurementTools.find(
-      toolGroup => toolGroup.id === 'allTools'
-    ).childTools;
-    Object.keys(measurements).forEach(toolId => {
-      const tool = tools.find(tool => tool.id === toolId);
-      const measurementsData = measurements[toolId];
-
-      measurementsData.forEach(measurementData => {
-        const tableMeasurement = {
-          label: '(No Location)',
-          hasWarnings: false,
-          isSplitLesion: false,
-          data: [
-            {
-              displayText: tool.options.measurementTable.displayFunction(
-                measurementData
-              )
-            }
-          ]
-        };
-        tableData[0].measurements.push(tableMeasurement);
-      });
-    });
-  }
-
-  return tableData;
-}
-
 const mapStateToProps = state => {
   const { timepoints, measurements } = state.timepointManager;
   return {
     timepoints: convertTimepointsToTableData(timepoints),
-    measurementCollection: convertMeasurementsToTableData(measurements)
+    measurementCollection: convertMeasurementsToTableData(measurements, timepoints)
   };
 };
 
