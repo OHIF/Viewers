6177a02f18b59d5e10b2c17198baed71fc186d0a
dannyrb <danny.ri.brown@gmail.com>
2019-04-29 13:29:37 -0400

    Shift default config to own file

---
+++ public/config/default.js
@@ -0,0 +1,20 @@
+window.config = {
+  routerBasename: '/',
+  rootUrl: 'http://localhost:5000',
+  servers: {
+    dicomWeb: [
+      {
+        name: 'DCM4CHEE',
+        wadoUriRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/wado',
+        qidoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
+        wadoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
+        qidoSupportsIncludeField: true,
+        imageRendering: 'wadors',
+        thumbnailRendering: 'wadors',
+        requestOptions: {
+          requestFromBrowser: true,
+        },
+      },
+    ],
+  },
+}
--- src/index.js
+++ src/index.js
@@ -8,27 +8,7 @@ import ReactDOM from 'react-dom'
 
 export { App }
 
-const applicationProps = window.config || {
-  routerBasename: '/',
-  rootUrl: 'http://localhost:5000',
-  servers: {
-    dicomWeb: [
-      {
-        name: 'DCM4CHEE',
-        wadoUriRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/wado',
-        qidoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
-        wadoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
-        qidoSupportsIncludeField: true,
-        imageRendering: 'wadors',
-        thumbnailRendering: 'wadors',
-        requestOptions: {
-          requestFromBrowser: true,
-        },
-      },
-    ],
-  },
-}
-
+const applicationProps = window.config
 const app = React.createElement(App, applicationProps, null)
 
 ReactDOM.render(app, document.getElementById('root'))
