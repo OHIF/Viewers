775a91152cace3118150d6c7a63c3b68f7ad7439
dannyrb <danny.ri.brown@gmail.com>
2019-04-29 13:19:17 -0400

    Specify our netlify config

---
+++ public/config/netlify.js
@@ -0,0 +1,20 @@
+window.config = {
+  routerBasename: '/demo',
+  rootUrl: '/demo',
+  servers: {
+    dicomWeb: [
+      {
+        name: 'DCM4CHEE',
+        wadoUriRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/wado',
+        qidoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
+        wadoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
+        qidoSupportsIncludeField: true,
+        imageRendering: 'wadors',
+        thumbnailRendering: 'wadors',
+        requestOptions: {
+          requestFromBrowser: true,
+        },
+      },
+    ],
+  },
+}
