f495ccfa3ff3fa6ee3afd45fe0d8503a7ac5d43d
dannyrb <danny.ri.brown@gmail.com>
2019-05-07 15:42:11 -0400

    Clean up oidc config

--- public/config/example_openidc.js
+++ public/config/example_openidc.js
@@ -10,9 +10,6 @@ window.config = {
         wadoUriRoot: 'http://127.0.0.1/pacs/wado',
         qidoRoot: 'http://127.0.0.1/pacs/dicom-web',
         wadoRoot: 'http://127.0.0.1/pacs/dicom-web',
-        // wadoUriRoot: 'http://localhost:8899/wado',
-        // qidoRoot: 'http://localhost:8899/dicom-web',
-        // wadoRoot: 'http://localhost:8899/dicom-web',
         qidoSupportsIncludeField: false,
         imageRendering: 'wadors',
         thumbnailRendering: 'wadors',
@@ -27,26 +24,15 @@ window.config = {
   },
   oidc: [
     {
-      authServerUrl: 'http://127.0.0.1/auth/realms/master',
-      // It looks like we have this route hardcoded in `OHIFStandaloneViewer.js`
-      authRedirectUri: '/callback', // backup: /studylist\
-      // Also hardcoded?
-      postLogoutRedirectUri: '/logout-redirect.html',
-      responseType: 'id_token',
-      scope: 'openid',
-      clientId: 'pacs',
+      // ~ REQUIRED
+      // Authorization Server URL
+      authority: 'http://127.0.0.1/auth/realms/master',
+      client_id: 'ohif-viewer',
+      redirect_uri: '/callback', // `OHIFStandaloneViewer.js`
+      response_type: 'id_token',
+      scope: 'openid', // email profile openid
+      // ~ OPTIONAL
+      post_logout_redirect_uri: '/logout-redirect.html',
     },
-    // ccc.js
-    // {
-    //   authRedirectUri: "http://localhost:5000/callback",
-    //   postLogoutRedirectUri: "http://localhost:5000/logout-redirect.html",
-    //   responseType: "id_token token",
-    //   scope: "email profile openid",
-    //   revokeAccessTokenOnSignout: true,
-    //   extraQueryParams: {
-    //     kc_idp_hint: "crowds-cure-cancer-auth0-oidc",
-    //     client_id: "crowds-cure-cancer"
-    //   }
-    // }
   ],
 }
