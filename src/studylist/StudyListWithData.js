b4b955f48c8484dd1f4cb6e2ff81b5146af4f176
biharck <biharck@gmail.com>
2019-05-28 10:36:40 -0300

    fixing date format when sorting

--- src/studylist/StudyListWithData.js
+++ src/studylist/StudyListWithData.js
@@ -1,39 +1,39 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import OHIF from 'ohif-core';
-import { withRouter } from 'react-router-dom';
-import { StudyList } from 'react-viewerbase';
-import ConnectedHeader from '../connectedComponents/ConnectedHeader.js';
-import moment from 'moment';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import OHIF from 'ohif-core'
+import { withRouter } from 'react-router-dom'
+import { StudyList } from 'react-viewerbase'
+import ConnectedHeader from '../connectedComponents/ConnectedHeader.js'
+import moment from 'moment'
 
 class StudyListWithData extends Component {
   state = {
     searchData: {},
     studies: null,
-    error: null
-  };
+    error: null,
+  }
 
   static propTypes = {
     patientId: PropTypes.string,
     server: PropTypes.object,
     user: PropTypes.object,
-    history: PropTypes.object
-  };
+    history: PropTypes.object,
+  }
 
-  static rowsPerPage = 25;
-  static defaultSort = { field: 'patientName', order: 'desc' };
+  static rowsPerPage = 25
+  static defaultSort = { field: 'patientName', order: 'desc' }
 
-  static studyListDateFilterNumDays = 25000; // TODO: put this in the settings
+  static studyListDateFilterNumDays = 25000 // TODO: put this in the settings
   static defaultStudyDateFrom = moment()
     .subtract(StudyListWithData.studyListDateFilterNumDays, 'days')
-    .toDate();
-  static defaultStudyDateTo = new Date();
+    .toDate()
+  static defaultStudyDateTo = new Date()
 
   componentDidMount() {
     // TODO: Avoid using timepoints here
     //const params = { studyInstanceUids, seriesInstanceUids, timepointId, timepointsFilter={} };
 
-    this.searchForStudies();
+    this.searchForStudies()
   }
 
   searchForStudies = (
@@ -42,10 +42,10 @@ class StudyListWithData extends Component {
       rowsPerPage: StudyListWithData.rowsPerPage,
       studyDateFrom: StudyListWithData.defaultStudyDateFrom,
       studyDateTo: StudyListWithData.defaultStudyDateTo,
-      sortData: StudyListWithData.defaultSort
+      sortData: StudyListWithData.defaultSort,
     }
   ) => {
-    const { server } = this.props;
+    const { server } = this.props
     const filter = {
       patientId: searchData.patientId,
       patientName: searchData.patientName,
@@ -55,77 +55,79 @@ class StudyListWithData extends Component {
       studyDateFrom: searchData.studyDateFrom,
       studyDateTo: searchData.studyDateTo,
       limit: searchData.rowsPerPage,
-      offset: searchData.currentPage * searchData.rowsPerPage
-    };
+      offset: searchData.currentPage * searchData.rowsPerPage,
+    }
 
     // TODO: add sorting
-    const promise = OHIF.studies.searchStudies(server, filter);
+    const promise = OHIF.studies.searchStudies(server, filter)
 
     // Render the viewer when the data is ready
     promise
       .then(studies => {
         if (!studies) {
-          studies = [];
+          studies = []
         }
 
-        const { field, order } = searchData.sortData;
+        const { field, order } = searchData.sortData
         const sortedStudies = studies
           .sort(function(a, b) {
             if (order === 'desc') {
               if (a[field] < b[field]) {
-                return -1;
+                return -1
               }
               if (a[field] > b[field]) {
-                return 1;
+                return 1
               }
-              return 0;
+              return 0
             } else {
               if (a[field] > b[field]) {
-                return -1;
+                return -1
               }
               if (a[field] < b[field]) {
-                return 1;
+                return 1
               }
-              return 0;
+              return 0
             }
           })
           .map(study => {
-            study.studyDate = moment(study.studyDate, 'YYYYMMDD').format(
-              'MMM DD, YYYY'
-            );
-            return study;
-          });
+            if (!moment(study.studyDate, 'MMM DD, YYYY', true).isValid()) {
+              study.studyDate = moment(study.studyDate, 'YYYYMMDD').format(
+                'MMM DD, YYYY'
+              )
+            }
+            return study
+          })
 
         this.setState({
-          studies: sortedStudies
-        });
+          studies: sortedStudies,
+        })
       })
       .catch(error => {
         this.setState({
-          error: true
-        });
+          error: true,
+        })
 
-        throw new Error(error);
-      });
-  };
+        throw new Error(error)
+      })
+  }
 
   onImport = () => {
     //console.log('onImport');
-  };
+  }
 
   onSelectItem = studyInstanceUID => {
-    this.props.history.push(`/viewer/${studyInstanceUID}`);
-  };
+    this.props.history.push(`/viewer/${studyInstanceUID}`)
+  }
 
   onSearch = searchData => {
-    this.searchForStudies(searchData);
-  };
+    this.searchForStudies(searchData)
+  }
 
   render() {
     if (this.state.error) {
-      return <div>Error: {JSON.stringify(this.state.error)}</div>;
+      return <div>Error: {JSON.stringify(this.state.error)}</div>
     } else if (this.state.studies === null) {
-      return <div>Loading...</div>;
+      return <div>Loading...</div>
     }
 
     return (
@@ -144,8 +146,8 @@ class StudyListWithData extends Component {
           onSearch={this.onSearch}
         />
       </>
-    );
+    )
   }
 }
 
-export default withRouter(StudyListWithData);
+export default withRouter(StudyListWithData)
