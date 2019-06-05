70a716ca160b9283373085774d260e9c2d535126
Erik Ziegler <erik.sweed@gmail.com>
2019-06-05 22:32:57 +0200

    fix(initWebWorkers): Fix duplicate slashes for web worker paths

--- src/utils/initWebWorkers.js
+++ src/utils/initWebWorkers.js
@@ -5,19 +5,28 @@ import cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
  * @param {String} baseDirectory
  * @param {String} webWorkScriptsPath
  */
-export default function(baseDirectory, webWorkScriptsPath) {
-  const scriptsPath = `${window.location.protocol}//${
+export default function initWebWorkers(
+  baseDirectory = '/',
+  webWorkScriptsPath = ''
+) {
+  let scriptsPath = `${window.location.protocol}//${
     window.location.host
-  }${baseDirectory}/${webWorkScriptsPath}`;
+  }${baseDirectory}${webWorkScriptsPath}`;
+
+  // Ensure the last character is a slash
+  if (scriptsPath[scriptsPath.length - 1] !== '/') {
+    scriptsPath += '/';
+  }
+
   const config = {
     maxWebWorkers: Math.max(navigator.hardwareConcurrency - 1, 1),
     startWebWorkersOnDemand: true,
-    webWorkerPath: `${scriptsPath}/cornerstoneWADOImageLoaderWebWorker.min.js`,
+    webWorkerPath: `${scriptsPath}cornerstoneWADOImageLoaderWebWorker.min.js`,
     taskConfiguration: {
       decodeTask: {
         loadCodecsOnStartup: true,
         initializeCodecsOnStartup: false,
-        codecsPath: `${scriptsPath}/cornerstoneWADOImageLoaderCodecs.min.js`,
+        codecsPath: `${scriptsPath}cornerstoneWADOImageLoaderCodecs.min.js`,
         usePDFJS: false,
         strict: false,
       },
