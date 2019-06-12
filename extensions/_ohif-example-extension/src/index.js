29f86d41e680781c1e32adc54e8ec4b098bcc6db
dannyrb <danny.ri.brown@gmail.com>
2019-06-12 16:09:06 -0400

    Begin stubbing an example plugin with all options as basic docs/recipe

---
+++ extensions/_ohif-example-extension/src/index.js
@@ -0,0 +1,15 @@
+/**
+ *
+ */
+export default {
+  /**
+   * Only required property. Should be a unique value across all extensions.
+   */
+  id: 'example-extension',
+
+  getViewportModule() {},
+  getSopClassHandler() {},
+  getPanelModule() {},
+  getToolbarModule() {},
+  getCommandsModule() {},
+};
