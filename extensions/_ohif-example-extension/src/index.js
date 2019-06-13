05e8c803c907214770e480d486073ea8dd0ac0a9
dannyrb <danny.ri.brown@gmail.com>
2019-06-13 09:49:09 -0400

    Update example for commandsModule

--- extensions/_ohif-example-extension/src/index.js
+++ extensions/_ohif-example-extension/src/index.js
@@ -11,5 +11,27 @@ export default {
   getSopClassHandler() {},
   getPanelModule() {},
   getToolbarModule() {},
-  getCommandsModule() {},
+  getCommandsModule(/* store */) {
+    return commandsModule;
+  },
+};
+
+/**
+ *
+ */
+const commandsModule = {
+  actions: {
+    // Store Contexts + Options
+    exampleAction: ({ viewports, param1 }) => {
+      console.log(`There are ${viewports.length} viewports`);
+      console.log(`param1's value is: ${param1}`);
+    },
+  },
+  definitions: {
+    exampleActionDef: {
+      commandFn: this.actions.exampleAction,
+      storeContexts: ['viewports'],
+      options: { param1: 'hello world' },
+    },
+  },
 };
