9573f67132a92520909969acecafa6126889e272
Erik Ziegler <erik.sweed@gmail.com>
2019-06-15 11:51:56 +0200

    refactor(@ohif/extension-vtk): Update VTK extension to add basic MPR functionality

--- extensions/ohif-vtk-extension/src/ConnectedToolbarSection.js
+++ extensions/ohif-vtk-extension/src/ConnectedToolbarSection.js
@@ -5,33 +5,50 @@ import OHIF from 'ohif-core';
 const { setToolActive } = OHIF.redux.actions;
 
 const mapStateToProps = state => {
-  const activeButton = state.tools.buttons.find(tool => tool.active === true);
-
   return {
     buttons: [
+      {
+        command: 'Crosshairs',
+        type: 'tool',
+        text: 'Crosshairs',
+        icon: 'crosshairs',
+        active: true,
+        onClick: () => {
+          // TODO: Make these use setToolActive instead
+          window.commandsManager.runCommand('enableCrosshairsTool', {}, 'vtk');
+        }
+      },
+      {
+        command: 'WWWC',
+        type: 'tool',
+        text: 'WWWC',
+        icon: 'level',
+        active: true,
+        onClick: () => {
+          // TODO: Make these use setToolActive instead
+          window.commandsManager.runCommand('enableLevelTool', {}, 'vtk');
+        }
+      }
+      /* Disabled temporarily due to issues with the interactor style
       {
         command: 'Rotate',
         type: 'tool',
         text: 'Rotate',
         icon: '3d-rotate',
-        active: true
-      }
+        active: false,
+        onClick: () => {
+          // TODO: Make these use setToolActive instead
+          window.commandsManager.runCommand('enableRotateTool', {}, 'vtk');
+        }
+      },*/
     ],
-    activeCommand: 'Rotate'
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    setToolActive: tool => {
-      //dispatch(setToolActive(tool.command))
-    }
+    activeCommand: 'Crosshairs'
   };
 };
 
 const ConnectedToolbarSection = connect(
   mapStateToProps,
-  mapDispatchToProps
+  null
 )(ToolbarSection);
 
 export default ConnectedToolbarSection;
