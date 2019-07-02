2a5b6a13d4189c62924f5c7d6994a2e752593864
dannyrb <danny.ri.brown@gmail.com>
2019-07-01 23:54:52 -0400

    Support for panel width

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -9,14 +9,25 @@ class SidePanel extends Component {
     from: PropTypes.string.isRequired,
     isOpen: PropTypes.bool.isRequired,
     children: PropTypes.node,
+    width: PropTypes.string,
   };
 
   render() {
     const fromSideClass =
       this.props.from === 'right' ? 'from-right' : 'from-left';
 
+    const styles = this.props.width
+      ? {
+          maxWidth: this.props.width,
+          marginRight: this.props.isOpen
+            ? '0'
+            : Number.parseInt(this.props.width) * -1,
+        }
+      : {};
+
     return (
       <section
+        style={styles}
         className={classNames('sidepanel', fromSideClass, {
           'is-open': this.props.isOpen,
         })}
