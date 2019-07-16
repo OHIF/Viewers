9449ae43148c570f1378bfd73411131315aebad9
dannyrb <danny.ri.brown@gmail.com>
2019-07-16 14:23:41 -0400

    Try alternative jest transform

--- babel.config.js
+++ babel.config.js
@@ -1,27 +1,28 @@
-const aliases = require('./aliases.config');
-const path = require('path');
+const aliases = require("./aliases.config");
+const path = require("path");
 
 module.exports = {
   // https://babeljs.io/docs/en/options#babelrcroots
   presets: [
     [
-      '@babel/preset-env',
+      "@babel/preset-env",
       {
         targets: {
-          ie: '11',
-        },
-      },
+          ie: "11"
+        }
+      }
     ],
-    '@babel/preset-react',
+    "@babel/preset-react"
   ],
+  babelrcRoots: ["./platform/*", "./extensions/*"],
   plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-transform-regenerator',
-    '@babel/plugin-transform-runtime',
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-syntax-dynamic-import",
+    "@babel/plugin-transform-regenerator",
+    "@babel/plugin-transform-runtime",
     [
-      'module-resolver',
+      "module-resolver",
       {
         // https://github.com/tleunen/babel-plugin-module-resolver/issues/338
         // There seem to be a bug with module-resolver with a mono-repo setup:
@@ -31,18 +32,18 @@ module.exports = {
           // This will return undefined if aliases has no key for the sourcePath,
           // in which case module-resolver will fallback on its default behaviour.
           return aliases[sourcePath];
-        },
-      },
-    ],
+        }
+      }
+    ]
   ],
   env: {
     debug: {
-      sourceMaps: 'inline',
-      retainLines: true,
+      sourceMaps: "inline",
+      retainLines: true
     },
     build: {
-      ignore: ['**/*.test.jsx', '**/*.test.js', '__snapshots__', '__tests__'],
-    },
+      ignore: ["**/*.test.jsx", "**/*.test.js", "__snapshots__", "__tests__"]
+    }
   },
-  ignore: ['node_modules'],
+  ignore: ["node_modules"]
 };
--- platform/viewer/jest.config.js
+++ platform/viewer/jest.config.js
@@ -11,6 +11,9 @@ module.exports = {
   ],
   reporters: ["default", "jest-junit"],
   //
+  transform: {
+    "^.+\\.js$": path.resolve(__dirname, "./jestBabelTransform.js")
+  },
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/src/__mocks__/fileMock.js",
