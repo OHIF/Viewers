9e717bac0645e4c8630562185c13475c4ff67f15
Erik Ziegler <erik.sweed@gmail.com>
2019-07-21 09:03:53 +0200

    fix(config/google.js): Add metadata endpoints and signingKeys to avoid CORS errors on Google's certificate endpoint

--- public/config/google.js
+++ public/config/google.js
@@ -19,8 +19,33 @@ window.config = {
       // ~ OPTIONAL
       post_logout_redirect_uri: '/logout-redirect.html',
       revoke_uri: 'https://accounts.google.com/o/oauth2/revoke?token=',
-      "automaticSilentRenew": true,
-      "revokeAccessTokenOnSignout": true,
+      automaticSilentRenew: true,
+      revokeAccessTokenOnSignout: true,
+      metadata: {
+        issuer: "https://accounts.google.com",
+        authorization_endpoint: "https://accounts.google.com/o/oauth2/v2/auth",
+        token_endpoint: "https://www.googleapis.com/oauth2/v4/token",
+        userinfo_endpoint: "https://www.googleapis.com/oauth2/v3/userinfo",
+        jwks_uri: "https://www.googleapis.com/oauth2/v3/certs",
+      },
+      signingKeys: [
+        {
+          "kid": "6e5508d27965ad7907c232212defa48ed763727e",
+          "e": "AQAB",
+          "kty": "RSA",
+          "alg": "RS256",
+          "n": "vOsmJlsBscTxCcOLa6IfCuUnvXI5cBC-o-NIDC1R5O782U_BC67p9dtMLL2UHpQ1wj_b6I4R8cHzddPrPDZ7eTKJw5q18pZU4B5hCmFe9A0JvzyQe3VFPYhKSI5LV_6UIwWdGWjJic8tCJ05AJaVOogSUCn17ss_8KQHSbs66zVbwls9p_ObdHGuzLQE-Y-fkxO5aD9S09DB5dNKuZNL76wgZAhc-HEyo1HkTbDaGAPCk-EpoqfjjoPY2FZGHg5QRCUMdxnYoebjzyO6oaJ8yVniVpOkf-MNF6HltRBbOzE3u6Y2VGjWoj_W0AUidrLC_KQ57URkcAfk1BhMUVJekQ",
+          "use": "sig"
+        },
+        {
+          "use": "sig",
+          "kid": "84f294c45160088d079fee68138f52133d3e228c",
+          "e": "AQAB",
+          "kty": "RSA",
+          "alg": "RS256",
+          "n": "iyzj9wpDDZLCbgbr2zKv3bs8zqjflcVEd7PYMjKGYpoaY2LdqfjFxrwTqd9Ea4m3NIR2giOx9JLQhtqqSSpBJpBBpHmaEd2FCPwd4GQTKJurEP6Ho9HWAuRTMhs8W04pd__HQ0Bc22AEamieGLtzcYfIaAc9g5RCxZdRVbGK0Z0vSOAwN1PC_S76nWGphouHukU40EiwjqC-D9G2xYFbKNb0_NJMxJ5UCenN85FjEii5-oW0wCBmt_1Sr76Q_e0INxfGu6dRf0vGXPvqxkINz2knjl9ec2SvOK2hnmRN4O9zToKH70_DBrsZE0ePDScTOWPHJU2wOyE6gzkL6FdaFQ"
+        }
+      ]
     },
   ],
 }
