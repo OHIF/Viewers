54b70e5fa4418db790238b4b46f7a98c6c8105a1
Erik Ziegler <erik.sweed@gmail.com>
2019-07-29 09:08:36 +0200

    @ohif/i18n: Update i18n locales from Locize source of truth (0.2.3)

--- extensions/ohif-i18n/src/locales/index.js
+++ extensions/ohif-i18n/src/locales/index.js
@@ -1,9 +1,13 @@
 import en_US from './en-US/';
 import es from './es/';
 import nl from './nl/';
+import pt_BR from './pt-BR/';
+import zh from './zh/';
 
 export default {
   ...en_US,
   ...es,
   ...nl,
+  ...pt_BR,
+  ...zh,
 };
