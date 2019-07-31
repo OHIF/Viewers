94b2c2aa6014279ccb133ef2773278ea46a5ab83
Erik Ziegler <erik.sweed@gmail.com>
2019-07-31 12:05:18 +0200

    @ohif/i18n - Update Chinese translations (0.2.5)

--- extensions/ohif-i18n/src/locales/zh/index.js
+++ extensions/ohif-i18n/src/locales/zh/index.js
@@ -2,6 +2,8 @@ import Buttons from "./Buttons.json";
 import CineDialog from "./CineDialog.json";
 import Common from "./Common.json";
 import Header from "./Header.json";
+import MeasurementTable from "./MeasurementTable.json";
+import StudyList from "./StudyList.json";
 import UserPreferencesModal from "./UserPreferencesModal.json";
 
 export default { 
@@ -10,6 +12,8 @@ export default {
     CineDialog,
     Common,
     Header,
+    MeasurementTable,
+    StudyList,
     UserPreferencesModal,
   }
 };
