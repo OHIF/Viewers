6f5d7d72129250b75935206e5fcb558878d3d70d
Erik Ziegler <erik.sweed@gmail.com>
2019-07-31 10:02:17 +0200

    @ohif/i18n - Add Vietnamese translation (Thanks @lam0620!) - Improve Chinese translations (Thanks @MarlonLiu1991!) (0.2.4)

--- extensions/ohif-i18n/src/locales/vi/index.js
+++ extensions/ohif-i18n/src/locales/vi/index.js
@@ -2,14 +2,16 @@ import Buttons from "./Buttons.json";
 import CineDialog from "./CineDialog.json";
 import Common from "./Common.json";
 import Header from "./Header.json";
+import StudyList from "./StudyList.json";
 import UserPreferencesModal from "./UserPreferencesModal.json";
 
 export default { 
-  'zh': {
+  'vi': {
     Buttons,
     CineDialog,
     Common,
     Header,
+    StudyList,
     UserPreferencesModal,
   }
 };
