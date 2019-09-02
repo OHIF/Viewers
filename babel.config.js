8eda74f7978e7db1946418ec3be48c3aaf00b5be
dannyrb <danny.ri.brown@gmail.com>
2019-09-02 15:40:27 -0400

    ditch babel plugins that should be covered by preset-env

--- babel.config.js
+++ babel.config.js
@@ -21,24 +21,21 @@ module.exports = {
     'react-hot-loader/babel',
     'inline-react-svg',
     '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-transform-regenerator',
-    '@babel/plugin-transform-runtime',
-    [
-      'module-resolver',
-      {
-        // https://github.com/tleunen/babel-plugin-module-resolver/issues/338
-        // There seem to be a bug with module-resolver with a mono-repo setup:
-        // It doesn't resolve paths correctly when using root/alias combo, so we
-        // use this function instead.
-        resolvePath(sourcePath, currentFile, opts) {
-          // This will return undefined if aliases has no key for the sourcePath,
-          // in which case module-resolver will fallback on its default behaviour.
-          return aliases[sourcePath];
-        },
-      },
-    ],
+    // We don't currently use aliases, but this is a nice snippet that would help
+    // [
+    //   'module-resolver',
+    //   {
+    //     // https://github.com/tleunen/babel-plugin-module-resolver/issues/338
+    //     // There seem to be a bug with module-resolver with a mono-repo setup:
+    //     // It doesn't resolve paths correctly when using root/alias combo, so we
+    //     // use this function instead.
+    //     resolvePath(sourcePath, currentFile, opts) {
+    //       // This will return undefined if aliases has no key for the sourcePath,
+    //       // in which case module-resolver will fallback on its default behaviour.
+    //       return aliases[sourcePath];
+    //     },
+    //   },
+    // ],
   ],
   env: {
     debug: {
