019ca90743351295d11146bc439484fe26b3ce28
dannyrb <danny.ri.brown@gmail.com>
2019-09-04 10:34:59 -0400

    Let's not do so much heavy lifting for a dev-server build

--- .webpack/rules/transpileJavaScript.js
+++ .webpack/rules/transpileJavaScript.js
@@ -1,21 +1,26 @@
 const excludeNodeModulesExcept = require('./../helpers/excludeNodeModulesExcept.js');
 
 function transpileJavaScript(mode) {
+  const exclude =
+    mode === 'production'
+      ? excludeNodeModulesExcept([
+          'vtk.js',
+          'dicomweb-client',
+          'react-dnd',
+          'dcmjs', // contains: loglevelnext
+          'loglevelnext',
+          // '@ohif/extension-dicom-microscopy' contains:
+          'dicom-microscopy-viewer',
+          'ol',
+        ])
+      : excludeNodeModulesExcept([]);
+
   return {
     test: /\.jsx?$/,
     // These are packages that are not transpiled to our lowest supported
     // JS version (currently ES5). Most of these leverage ES6+ features,
     // that we need to transpile to a different syntax.
-    exclude: excludeNodeModulesExcept([
-      'vtk.js',
-      'dicomweb-client',
-      'react-dnd',
-      'dcmjs', // contains: loglevelnext
-      'loglevelnext',
-      // '@ohif/extension-dicom-microscopy' contains:
-      'dicom-microscopy-viewer',
-      'ol',
-    ]),
+    exclude,
     loader: 'babel-loader',
     options: {
       // Find babel.config.js in monorepo root
