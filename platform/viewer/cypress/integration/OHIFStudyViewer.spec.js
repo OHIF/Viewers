fe806c00cc8435c2337a15184efa5c5d72793c9c
Mirna Silva <mirna.silva@radicalimaging.com>
2019-09-12 19:47:19 -0300

    Changing Patient to MisterMR

--- platform/viewer/cypress/integration/OHIFStudyViewer.spec.js
+++ platform/viewer/cypress/integration/OHIFStudyViewer.spec.js
@@ -1,12 +1,13 @@
 describe('OHIF Study Viewer Page', () => {
   
   before(() => {
-      cy.visit('/');
-      cy.get('#patientName')
-        .type("Dummy");
-      cy.get('.studylistStudy > .patientName')
-        .contains("Dummy")
-        .click();
+      // cy.visit('/');
+      // cy.get('#patientName')
+      //   .type("Dummy");
+      // cy.get('.studylistStudy > .patientName')
+      //   .contains("Dummy")
+      //   .click();
+      cy.openStudy("MISTER^MR");
     });
   
     it('checks if series thumbnails are being displayed', ()=> {
@@ -17,11 +18,12 @@ describe('OHIF Study Viewer Page', () => {
 
     it('drags and drop a series thumbnail into viewport', () => {
       cy.wait(3000); 
-      cy.get('.ThumbnailEntryContainer:nth-child(3)')
+      cy.get('.ThumbnailEntryContainer:nth-child(2)')
         .drag('.cornerstone-canvas');
       cy.wait(1000); 
-      cy.get(':nth-child(2) > h1')
-        .should('have.text', 'Imaging Measurement Report (126000 - DCM)');
+      
+      cy.get('div.ViewportOverlay > div.bottom-left.overlay-element > div')
+      .should('have.text','Ser: 2Img: 1 1/13512 x 512Loc: -17.60 mm Thick: 3.00 mm');
     });
   
     
