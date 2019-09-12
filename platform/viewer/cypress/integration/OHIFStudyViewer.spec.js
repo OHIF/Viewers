d8e915976cb320662e0c4a39905351eebcad118f
dannyrb <danny.ri.brown@gmail.com>
2019-09-12 12:57:16 -0400

    Add wait's to command

--- platform/viewer/cypress/integration/OHIFStudyViewer.spec.js
+++ platform/viewer/cypress/integration/OHIFStudyViewer.spec.js
@@ -1,6 +1,12 @@
 describe('OHIF Study Viewer Page', () => {
-    before(() => {
-      cy.openStudy('Dummy');
+  
+  before(() => {
+      cy.visit('/');
+      cy.get('#patientName')
+        .type("Dummy");
+      cy.get('.studylistStudy > .patientName')
+        .contains("Dummy")
+        .click();
     });
   
     it('checks if series thumbnails are being displayed', ()=> {
@@ -10,8 +16,10 @@ describe('OHIF Study Viewer Page', () => {
     });
 
     it('drags and drop a series thumbnail into viewport', () => {
+      cy.wait(3000); 
       cy.get('.ThumbnailEntryContainer:nth-child(3)')
-        .drag('.cornerstone-canvas'); 
+        .drag('.cornerstone-canvas');
+      cy.wait(1000); 
       cy.get(':nth-child(2) > h1')
         .should('have.text', 'Imaging Measurement Report (126000 - DCM)');
     });
