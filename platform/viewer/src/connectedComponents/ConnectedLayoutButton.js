f0ea2db65ce072d679ed510f32328eb7d59ea5e0
dannyrb <danny.ri.brown@gmail.com>
2019-09-29 22:02:52 -0400

    Simplify layout button; only set numRows and columns

--- platform/viewer/src/connectedComponents/ConnectedLayoutButton.js
+++ platform/viewer/src/connectedComponents/ConnectedLayoutButton.js
@@ -4,39 +4,24 @@ import { connect } from 'react-redux';
 
 const { setLayout } = OHIF.redux.actions;
 
-const mapStateToProps = state => {
-  return {
-    currentLayout: state.viewports.layout,
-  };
-};
-
 const mapDispatchToProps = dispatch => {
   return {
-    // TODO: Change if layout switched becomes more complex
     onChange: selectedCell => {
-      let viewports = [];
-      const rows = selectedCell.row + 1;
-      const columns = selectedCell.col + 1;
-      const numViewports = rows * columns;
-      for (let i = 0; i < numViewports; i++) {
-        viewports.push({
-          height: `${100 / rows}%`,
-          width: `${100 / columns}%`,
-        });
-      }
-      const layout = {
-        viewports,
-      };
+      const numRows = selectedCell.row + 1;
+      const numColumns = selectedCell.col + 1;
 
-      console.log('CONNECTED-LAYOUT-BUTTON', layout, selectedCell);
-
-      dispatch(setLayout(layout));
+      dispatch(
+        setLayout({
+          numRows,
+          numColumns,
+        })
+      );
     },
   };
 };
 
 const ConnectedLayoutButton = connect(
-  mapStateToProps,
+  undefined,
   mapDispatchToProps
 )(LayoutButton);
 
