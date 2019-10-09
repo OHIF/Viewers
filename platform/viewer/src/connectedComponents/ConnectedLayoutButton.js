1f712f0680f810a6515c345b3dff77cecee6e228
Igor <igoroctaviano@gmail.com>
2019-10-09 15:18:59 -0300

    fix: 🐛 set current viewport as active when switching layouts
    check if current viewport index is less than the current layout lenght
    and set 0 if so or keep current selected cell index as active viewport
    
    Closes: 999

--- platform/viewer/src/connectedComponents/ConnectedLayoutButton.js
+++ platform/viewer/src/connectedComponents/ConnectedLayoutButton.js
@@ -2,18 +2,19 @@ import { LayoutButton } from '@ohif/ui';
 import OHIF from '@ohif/core';
 import { connect } from 'react-redux';
 
-const { setLayout } = OHIF.redux.actions;
+const { setLayout, setViewportActive } = OHIF.redux.actions;
 
 const mapStateToProps = state => {
   return {
     currentLayout: state.viewports.layout,
+    activeViewportIndex: state.viewports.activeViewportIndex
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
     // TODO: Change if layout switched becomes more complex
-    onChange: (selectedCell, currentLayout) => {
+    onChange: (selectedCell, currentLayout, activeViewportIndex) => {
       let viewports = [];
       const rows = selectedCell.row + 1;
       const columns = selectedCell.col + 1;
@@ -36,6 +37,12 @@ const mapDispatchToProps = dispatch => {
         viewports,
       };
 
+      const { row, col } = selectedCell;
+      const selectedCellIndex = (row * currentLayout.viewports.length + col) || 0;
+      if (selectedCellIndex < activeViewportIndex) {
+        dispatch(setViewportActive(0));
+      }
+
       dispatch(setLayout(layout));
     },
   };
@@ -43,10 +50,10 @@ const mapDispatchToProps = dispatch => {
 
 const mergeProps = (propsFromState, propsFromDispatch) => {
   const onChangeFromDispatch = propsFromDispatch.onChange;
-  const { currentLayout } = propsFromState;
+  const { currentLayout, activeViewportIndex } = propsFromState;
 
   return {
-    onChange: (selectedCell) => onChangeFromDispatch(selectedCell, currentLayout)
+    onChange: selectedCell => onChangeFromDispatch(selectedCell, currentLayout, activeViewportIndex)
   };
 }
 
