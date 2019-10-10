a36cb63926d2cf439e7e68f29be160cc4799eaa3
Mirna Silva <mirna.silva@radicalimaging.com>
2019-10-10 16:46:41 -0300

    tests: Test cases implemented: Cornerstone tools Clear and Eraser; Measurement panel Relabel and Description (#1014)
    * Fix for test scripts that were failing in CI
    
    * Test cases for tools: Length and Angle
    
    * Test cases implemented: Scroll series, Levels, Pan, Reset, Zoom
    
    * Added test cases for tools: Cine, More, Layout, Invert, Rotate, FlipH, FlipV
    
    * Fixes made according to comments on PR review
    
    * Fixes for failing scenarios: Layout and Cine
    
    * Test cases implemented: Cornerstone tools Clear and Eraser; Measurement panel Relabel and Description
    
    * Fix for CI failure

---
+++ platform/viewer/cypress/support/aliases.js
@@ -0,0 +1,34 @@
+//Creating aliases for Cornerstone tools buttons
+export function initCornerstoneToolsAliases() {
+  cy.get('.ToolbarRow > :nth-child(2)').as('stackScrollBtn');
+  cy.get('.ToolbarRow > :nth-child(3)').as('zoomBtn');
+  cy.get('.ToolbarRow > :nth-child(4)').as('levelsBtn');
+  cy.get('.ToolbarRow > :nth-child(5)').as('panBtn');
+  cy.get('.ToolbarRow > :nth-child(6)').as('lengthBtn');
+  cy.get('.ToolbarRow > :nth-child(7)').as('annotateBtn');
+  cy.get('.ToolbarRow > :nth-child(8)').as('angleBtn');
+  cy.get('.ToolbarRow > :nth-child(9)').as('resetBtn');
+  cy.get('.ToolbarRow > :nth-child(10)').as('cineBtn');
+  cy.get('.expandableToolMenu').as('moreBtn');
+  cy.get('.PluginSwitch > .toolbar-button').as('twodmprBtn');
+  cy.get('.btn-group > .toolbar-button').as('layoutBtn');
+}
+
+//Creating aliases for Common page elements
+export function initCommonElementsAliases() {
+  cy.get(
+    '.pull-right > .RoundedButtonGroup > .roundedButtonWrapper > .roundedButton'
+  ).as('measurementsBtn');
+  cy.get('.viewport-element').as('viewport');
+  cy.get('section.sidepanel.from-right').as('measurementsPanel');
+  cy.get(
+    '.pull-left > .RoundedButtonGroup > .roundedButtonWrapper > .roundedButton'
+  ).as('seriesBtn');
+  cy.get('section.sidepanel.from-left').as('seriesPanel');
+  cy.get('div.ViewportOverlay > div.bottom-left.overlay-element > div').as(
+    'viewportInfoBottomLeft'
+  );
+  cy.get('div.ViewportOverlay > div.bottom-right.overlay-element > div').as(
+    'viewportInfoBottomRight'
+  );
+}
