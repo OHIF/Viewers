fcfa1e2f8cd9a1904451219759efd9cf16ad66cb
dannyrb <danny.ri.brown@gmail.com>
2019-10-14 13:53:29 -0400

    Test w/ no prompt

--- platform/viewer/public/init-service-worker.js
+++ platform/viewer/public/init-service-worker.js
@@ -26,26 +26,26 @@ if (supportsServiceWorker) {
 
     // Assumes your app has some sort of prompt UI element
     // that a user can either accept or reject.
-    const prompt = createUIPrompt({
-      onAccept: async () => {
-        // Assuming the user accepted the update, set up a listener
-        // that will reload the page as soon as the previously waiting
-        // service worker has taken control.
-        wb.addEventListener('controlling', event => {
-          window.location.reload();
-        });
-
-        // Send a message telling the service worker to skip waiting.
-        // This will trigger the `controlling` event handler above.
-        // Note: for this to work, you have to add a message
-        // listener in your service worker. See below.
-        wb.messageSW({ type: 'SKIP_WAITING' });
-      },
-
-      onReject: () => {
-        prompt.dismiss();
-      },
+    // const prompt = createUIPrompt({
+    //  onAccept: async () => {
+    // Assuming the user accepted the update, set up a listener
+    // that will reload the page as soon as the previously waiting
+    // service worker has taken control.
+    wb.addEventListener('controlling', event => {
+      window.location.reload();
     });
+
+    // Send a message telling the service worker to skip waiting.
+    // This will trigger the `controlling` event handler above.
+    // Note: for this to work, you have to add a message
+    // listener in your service worker. See below.
+    wb.messageSW({ type: 'SKIP_WAITING' });
+    // },
+
+    // onReject: () => {
+    //   prompt.dismiss();
+    // },
+    // });
   });
 
   wb.register();
