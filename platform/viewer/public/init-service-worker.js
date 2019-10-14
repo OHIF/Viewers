e66ed2a92c6ced584267ac108ee31b8c88de7556
dannyrb <danny.ri.brown@gmail.com>
2019-10-14 11:34:26 -0400

    use workbox-window

--- platform/viewer/public/init-service-worker.js
+++ platform/viewer/public/init-service-worker.js
@@ -1,46 +1,51 @@
+// https://developers.google.com/web/tools/workbox/modules/workbox-window
+// All major browsers that support service worker also support native JavaScript
+// modules, so it's perfectly fine to serve this code to any browsers
+// (older browsers will just ignore it)
+//
+import { Workbox } from 'https://storage.googleapis.com/workbox-cdn/releases/5.0.0-beta.1/workbox-window.prod.mjs';
+
+var supportsServiceWorker = 'serviceWorker' in navigator;
 var isNotLocalDevelopment =
   ['localhost', '127'].indexOf(location.hostname) === -1;
 
-window.isUpdateAvailable = new Promise(function(resolve, reject) {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker
-        .register('/sw.js')
-        .then(registration => {
-          // ~~ Update Found
-          registration.onupdatefound = () => {
-            var installingWorker = reg.installing;
-            installingWorker.onstatechange = () => {
-              var updateAvailable =
-                installingWorker.state === 'installed' &&
-                navigator.serviceWorker.controller;
-              if (updateAvailable) {
-                resolve(true);
-              } else {
-                resolve(false);
-              }
-            };
-          };
-          //
-          console.log('SW registered: ', registration);
-          // https://developers.google.com/web/fundamentals/push-notifications/subscribing-a-user
-          // registration.pushManager
-          //   .subscribe({
-          //     userVisibleOnly: true,
-          //     applicationServerKey: urlBase64ToUint8Array(
-          //       'BEl62iUYgUivxIkv69yViEuiBIa-Ib9-SkvMeAtA3LFgDzkrxZJjSgSnfckjBJuBkr3qBUYIHBQFLXYp5Nksh8U'
-          //     ),
-          //   })
-          //   .then(hi => console.log(hi))
-          //   .catch(err => console.log(err));
-        })
-        .catch(registrationError => {
-          console.warn('SW Error: ', registrationError);
-        });
-    });
-  }
-});
+if (supportsServiceWorker) {
+  const wb = new Workbox('/sw.js');
 
-window.isUpdateAvailable.then(isAvailable => {
-  console.warn('~~~~~~~~~~~~~~~ UPDATE AVAILABLE');
-});
+  // Add an event listener to detect when the registered
+  // service worker has installed but is waiting to activate.
+  wb.addEventListener('waiting', event => {
+    // customize the UI prompt accordingly.
+    const isFirstTimeUpdatedServiceWorkerIsWaiting =
+      event.wasWaitingBeforeRegister === false;
+    console.log(
+      'isFirstTimeUpdatedServiceWorkerIsWaiting',
+      isFirstTimeUpdatedServiceWorkerIsWaiting
+    );
+
+    // Assumes your app has some sort of prompt UI element
+    // that a user can either accept or reject.
+    const prompt = createUIPrompt({
+      onAccept: async () => {
+        // Assuming the user accepted the update, set up a listener
+        // that will reload the page as soon as the previously waiting
+        // service worker has taken control.
+        wb.addEventListener('controlling', event => {
+          window.location.reload();
+        });
+
+        // Send a message telling the service worker to skip waiting.
+        // This will trigger the `controlling` event handler above.
+        // Note: for this to work, you have to add a message
+        // listener in your service worker. See below.
+        wb.messageSW({ type: 'SKIP_WAITING' });
+      },
+
+      onReject: () => {
+        prompt.dismiss();
+      },
+    });
+  });
+
+  wb.register();
+}
--- platform/viewer/src/service-worker.js
+++ platform/viewer/src/service-worker.js
@@ -1,15 +1,45 @@
+// https://developers.google.com/web/tools/workbox/guides/troubleshoot-and-debug
 importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/5.0.0-beta.1/workbox-sw.js'
 );
 
-workbox.core.skipWaiting();
+// workbox.core.skipWaiting();
 workbox.core.clientsClaim();
 
+// Cache static assets that aren't precached
 workbox.routing.registerRoute(
-  new RegExp('https://hacker-news.firebaseio.com'),
-  new workbox.strategies.StaleWhileRevalidate()
+  /\.(?:js|css)$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'static-resources',
+  })
 );
 
+// Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.googleapis\.com/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+  })
+);
+
+// Cache the underlying font files with a cache-first strategy for 1 year.
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.gstatic\.com/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'google-fonts-webfonts',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365, // 1 Year
+        maxEntries: 30,
+      }),
+    ],
+  })
+);
+
+// PUSH EVENTS HANDLER
 self.addEventListener('push', event => {
   const title = 'Get Started With Workbox';
   const options = {
@@ -18,4 +48,54 @@ self.addEventListener('push', event => {
   event.waitUntil(self.registration.showNotification(title, options));
 });
 
+// MESSAGE HANDLER
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    switch (event.data.type) {
+      case 'SKIP_WAITING':
+        workbox.core.skipWaiting();
+        break;
+
+      default:
+        console.warn(`SW: Invalid message type: ${event.data.type}`);
+    }
+  }
+});
+
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+// This "catch" handler is triggered when any of the other routes fail to
+// generate a response.
+workbox.routing.setCatchHandler(({ event }) => {
+  // The FALLBACK_URL entries must be added to the cache ahead of time, either via runtime
+  // or precaching.
+  // If they are precached, then call workbox.precaching.getCacheKeyForURL(FALLBACK_URL)
+  // to get the correct cache key to pass in to caches.match().
+  //
+  // Use event, request, and url to figure out how to respond.
+  // One approach would be to use request.destination, see
+  // https://medium.com/dev-channel/service-worker-caching-strategies-based-on-request-types-57411dd7652c
+  switch (event.request.destination) {
+    case 'document':
+      return caches.match(FALLBACK_HTML_URL);
+      break;
+
+    case 'image':
+      return caches.match(FALLBACK_IMAGE_URL);
+      break;
+
+    case 'font':
+      return caches.match(FALLBACK_FONT_URL);
+      break;
+
+    default:
+      // If we don't have a fallback, just return an error response.
+      return Response.error();
+  }
+});
+
+// TODO: Cache API
+// https://developers.google.com/web/fundamentals/instant-and-offline/web-storage/cache-api
+// Store DICOMs?
+// Clear Service Worker cache?
+// navigator.storage.estimate().then(est => console.log(est)); (2GB?)
