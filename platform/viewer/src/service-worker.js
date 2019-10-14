b3c9d551fe858cbaa325c6317b5424d5da77faca
dannyrb <danny.ri.brown@gmail.com>
2019-10-14 16:23:59 -0400

    comment out cache handler for now

--- platform/viewer/src/service-worker.js
+++ platform/viewer/src/service-worker.js
@@ -64,33 +64,33 @@ workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 // This "catch" handler is triggered when any of the other routes fail to
 // generate a response.
-workbox.routing.setCatchHandler(({ event }) => {
-  // The FALLBACK_URL entries must be added to the cache ahead of time, either via runtime
-  // or precaching.
-  // If they are precached, then call workbox.precaching.getCacheKeyForURL(FALLBACK_URL)
-  // to get the correct cache key to pass in to caches.match().
-  //
-  // Use event, request, and url to figure out how to respond.
-  // One approach would be to use request.destination, see
-  // https://medium.com/dev-channel/service-worker-caching-strategies-based-on-request-types-57411dd7652c
-  switch (event.request.destination) {
-    case 'document':
-      return caches.match(FALLBACK_HTML_URL);
-      break;
+// workbox.routing.setCatchHandler(({ event }) => {
+//   // The FALLBACK_URL entries must be added to the cache ahead of time, either via runtime
+//   // or precaching.
+//   // If they are precached, then call workbox.precaching.getCacheKeyForURL(FALLBACK_URL)
+//   // to get the correct cache key to pass in to caches.match().
+//   //
+//   // Use event, request, and url to figure out how to respond.
+//   // One approach would be to use request.destination, see
+//   // https://medium.com/dev-channel/service-worker-caching-strategies-based-on-request-types-57411dd7652c
+//   switch (event.request.destination) {
+//     case 'document':
+//       return caches.match(FALLBACK_HTML_URL);
+//       break;
 
-    case 'image':
-      return caches.match(FALLBACK_IMAGE_URL);
-      break;
+//     case 'image':
+//       return caches.match(FALLBACK_IMAGE_URL);
+//       break;
 
-    case 'font':
-      return caches.match(FALLBACK_FONT_URL);
-      break;
+//     case 'font':
+//       return caches.match(FALLBACK_FONT_URL);
+//       break;
 
-    default:
-      // If we don't have a fallback, just return an error response.
-      return Response.error();
-  }
-});
+//     default:
+//       // If we don't have a fallback, just return an error response.
+//       return Response.error();
+//   }
+// });
 
 // TODO: Cache API
 // https://developers.google.com/web/fundamentals/instant-and-offline/web-storage/cache-api
