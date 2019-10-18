45e53d36c0fa64bab00dd62a744e366f1f352e3d
James A. Petts <jamesapetts@gmail.com>
2019-10-18 13:27:11 +0100

    remove debugger.

--- platform/core/src/utils/isDisplaySetReconstructable.js
+++ platform/core/src/utils/isDisplaySetReconstructable.js
@@ -7,8 +7,6 @@
 export default function isDisplaySetReconstructable(series, instances) {
   // Can't reconstruct if we only have one image.
 
-  debugger;
-
   const modality = series._data.modality; // TODO -> Is there a better way to get this?
   const isMultiframe = instances[0].getRawValue('x00280008') > 1;
 
