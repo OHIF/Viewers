5a0fb23ca5b0a46c44a61d34f8d1f1a31da76d3a
dannyrb <danny.ri.brown@gmail.com>
2019-10-22 12:10:01 -0400

    Remove irrelevant test

--- platform/viewer/cypress/integration/common/OHIFStandaloneViewer.spec.js
+++ platform/viewer/cypress/integration/common/OHIFStandaloneViewer.spec.js
@@ -4,20 +4,11 @@ describe('OHIFStandaloneViewer', () => {
   });
 
   it('loads route with at least 2 rows', () => {
-    cy.screenshot();
-    cy.percySnapshot();
-
     cy.get('[data-cy="study-list-results"] tr')
       .its('length')
       .should('be.gt', 2);
-  });
 
-  it('first 2 rows has values', () => {
-    cy.get(
-      '[data-cy="study-list-results"] > :nth-child(1) > .patientId'
-    ).should('be.visible');
-    cy.get(
-      '[data-cy="study-list-results"] > :nth-child(2) > .patientId'
-    ).should('be.visible');
+    cy.screenshot();
+    cy.percySnapshot('Study List', { width: [340, 768, 1000, 1780] });
   });
 });
