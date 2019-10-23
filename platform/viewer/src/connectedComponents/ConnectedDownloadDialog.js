f92e3b3b5324ec14c5e6f8fe0df07aa4c08ef361
Igor <igoroctaviano@gmail.com>
2019-10-23 00:43:56 -0300

    WIP: Refactoring DownloadDialog

--- platform/viewer/src/connectedComponents/ConnectedDownloadDialog.js
+++ platform/viewer/src/connectedComponents/ConnectedDownloadDialog.js
@@ -1,41 +1,18 @@
 import { connect } from 'react-redux';
 import { DownloadDialog } from '@ohif/ui';
-import OHIF from '@ohif/core';
-import DownloadViewportEngine from '../lib/DownloadViewportEngine';
-
-const downloadEngine = new DownloadViewportEngine();
-
-const { setViewportSpecificData } = OHIF.redux.actions;
 
 const mapStateToProps = state => {
   const { viewportSpecificData, activeViewportIndex } = state.viewports;
   const { dom: activeEnabledElement } = viewportSpecificData[activeViewportIndex] || {};
 
   return {
-    activeEnabledElement,
-    activeViewportIndex: state.viewports.activeViewportIndex,
-    save: downloadEngine.save,
-    mountPreview:  downloadEngine.mountPreview,
-    cleanViewPortClone: downloadEngine.clean,
-    resize: downloadEngine.resize,
-    toggleAnnotations: downloadEngine.toggleAnnotations,
-    setCacheReferences: downloadEngine.updateCache,
-    getInfo: downloadEngine.getInfo,
-    resetSize: downloadEngine.resetSize,
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    dispatchSetViewportSpecificData: (viewportIndex, data) => {
-      dispatch(setViewportSpecificData(viewportIndex, data));
-    },
+    activeViewport: activeEnabledElement
   };
 };
 
 const ConnectedDownloadDialog = connect(
   mapStateToProps,
-  mapDispatchToProps
+  null
 )(DownloadDialog);
 
 export default ConnectedDownloadDialog;
