bb4fbd58b7b33fe854778ae40575b3ce158d0dc1
dannyrb <danny.ri.brown@gmail.com>
2019-10-25 14:44:02 -0400

    test: some dark magic to make it possible to take screenshots of WebGL canvases

--- platform/viewer/cypress/plugins/index.js
+++ platform/viewer/cypress/plugins/index.js
@@ -31,5 +31,20 @@ module.exports = (on, config) => {
     }
   });
 
+  on('window:before:load', window => {
+    // Override our `getContext` function so all contexts that are webGl are
+    // Created with `preserveDrawingBuffer = true`; this is required for percy
+    // Snapshot
+    window.HTMLCanvasElement.prototype.getContext = (function(oldGetContextFn) {
+      return function(type, attrs) {
+        attrs = attrs || {};
+        if (type === 'webgl') {
+          attrs.preserveDrawingBuffer = true;
+        }
+        return oldGetContextFn.apply(this, type, attrs);
+      };
+    })(HTMLCanvasElement.prototype.getContext);
+  });
+
   on('task', percyHealthCheck);
 };
