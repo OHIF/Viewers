2677170d67659ee178cf77307414d54cfe9cb563
Emanuel Fiuza de Oliveira <emfolg@gmail.com>
2019-10-26 01:00:34 -0300

    fix: 🐛 JSON launch not working properly (#1089) (#1093)
    * fix: 🐛 JSON launch not working properly (#1089)
    
    JSON launch not working properly after QIDO/WADO switch #1089
    
    Closes: #1089
    
    * fix: 🐛 addressing review feedback (#1089)
    
    Closes: #1089

--- platform/viewer/src/connectedComponents/ViewerMain.js
+++ platform/viewer/src/connectedComponents/ViewerMain.js
@@ -59,12 +59,7 @@ class ViewerMain extends Component {
     // Get all the display sets for the viewer studies
     if (this.props.studies) {
       const displaySets = this.getDisplaySets(this.props.studies);
-
-      this.setState({
-        displaySets,
-      });
-
-      this.fillEmptyViewportPanes();
+      this.setState({ displaySets }, this.fillEmptyViewportPanes);
     }
   }
 
@@ -78,12 +73,7 @@ class ViewerMain extends Component {
       (viewportAmount !== prevViewportAmount && !isVtk)
     ) {
       const displaySets = this.getDisplaySets(this.props.studies);
-
-      this.setState({
-        displaySets,
-      });
-
-      this.fillEmptyViewportPanes();
+      this.setState({ displaySets }, this.fillEmptyViewportPanes);
     }
   }
 
