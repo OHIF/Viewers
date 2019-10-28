7d34deb2c648c966bc461dc7d226c96292449737
dannyrb <danny.ri.brown@gmail.com>
2019-10-28 12:28:45 -0400

    Remaining canvas fixes

--- platform/viewer/cypress/support/index.js
+++ platform/viewer/cypress/support/index.js
@@ -16,17 +16,39 @@
 import './commands';
 
 Cypress.on('window:before:load', window => {
+  //
+  // window.addEventListener('load', evt => {
+  //   _injectStyles(
+  //     '.hide-canvas-offscreen { position: absolute; right: 50000px; }'
+  //   );
+  // });
+
   // Override our `getContext` function so all contexts that are webGl are
   // Created with `preserveDrawingBuffer = true`; this is required for percy
   // Snapshot
   window.HTMLCanvasElement.prototype.getContext = (function(oldGetContextFn) {
     return function(type, attrs) {
       attrs = attrs || {};
-      // webgl, 2d, experimental-webgl,
-      if (type === 'webgl' || type === 'experimental-webgl') {
+      if (
+        type === 'webgl' ||
+        type === 'webgl2' ||
+        type === 'experimental-webgl'
+      ) {
         attrs.preserveDrawingBuffer = true;
       }
       return oldGetContextFn.apply(this, [type, attrs]);
     };
   })(HTMLCanvasElement.prototype.getContext);
 });
+
+// Originally used to fix VTK canvas screenshot issue; but canvas's will stay
+// hidden until interacted with :+1:
+// Keeping here as a reference for injecting styles at runtime
+// function _injectStyles(rule) {
+//   var body = document.querySelector('body');
+//   console.log(body);
+//   var div = document.createElement('div');
+//   div.innerHTML = '&shy;&lt;style&gt;' + rule + '&lt;/style&gt;';
+
+//   body.appendChild(div);
+// }
