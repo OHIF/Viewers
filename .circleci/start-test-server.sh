050f31224402ee3628e2a4464a146e0e3e5899c4
dannyrb <danny.ri.brown@gmail.com>
2019-10-29 00:26:57 -0400

    monitor network

--- .circleci/start-test-server.sh
+++ .circleci/start-test-server.sh
@@ -9,5 +9,7 @@ git clone git://github.com/ohif/viewer-testdata ./test-server/
 
 cd test-server
 
+set -x
 docker-compose build
-docker-compose up
+docker-compose up -d
+docker run --network container:contacts appropriate/curl --retry 10 --retry-delay 1 --retry-connrefused http://localhost:5985
