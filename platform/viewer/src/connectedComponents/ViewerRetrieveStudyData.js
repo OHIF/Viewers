d39f5b006300c868c4aefcd30ecb7a285a0fa14e
Rodolfo Ladeira <rodolfo_radical@hotmail.com>
2019-10-31 17:09:30 -0300

    feat: 🎸 Changing component to functional component

--- platform/viewer/src/connectedComponents/ViewerRetrieveStudyData.js
+++ platform/viewer/src/connectedComponents/ViewerRetrieveStudyData.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { metadata, studies, utils, log } from '@ohif/core';
 
 import ConnectedViewer from './ConnectedViewer.js';
 import PropTypes from 'prop-types';
 import { extensionManager } from './../App.js';
-import { withSnackbar } from '@ohif/ui';
+import { useSnackbarContext } from '@ohif/ui';
 
 const { OHIFStudyMetadata, OHIFSeriesMetadata } = metadata;
 const { retrieveStudiesMetadata } = studies;
 const { studyMetadataManager, updateMetaDataManager } = utils;
 
-const NOT_FILTER = true;
+// Contexts
+import AppContext from '../context/AppContext';
 
 const _promoteToFront = (list, value, searchMethod) => {
   let response = [...list];
@@ -28,20 +29,27 @@ const _promoteToFront = (list, value, searchMethod) => {
 
   return {
     promoted,
-    data: response
+    data: response,
   };
 };
 
-const _promoteList = (study, studyMetadata, filters) => {
+/**
+ * Promote series to front if find found equivalent on filters object
+ * @param {Object} study - study reference to promote series against
+ * @param {Object} [filters] - Object containing filters to be applied
+ * @param {string} [filter.seriesInstanceUID] - series instance uid to filter results against
+ * @param {boolean} isFilterStrategy - if filtering by query param strategy ON
+ */
+const _promoteList = (study, studyMetadata, filters, isFilterStrategy) => {
   let promoted = false;
   // Promote only if no filter should be applied
-  if (NOT_FILTER) {
+  if (!isFilterStrategy) {
     _sortStudyDisplaySet(study, studyMetadata);
     promoted = _promoteStudyDisplaySet(study, studyMetadata, filters);
   }
 
   return promoted;
-}
+};
 
 const _promoteStudyDisplaySet = (study, studyMetadata, filters) => {
   let promoted = false;
@@ -49,28 +57,32 @@ const _promoteStudyDisplaySet = (study, studyMetadata, filters) => {
   const shouldPromoteToFront = queryParamsLength > 0;
 
   if (shouldPromoteToFront) {
-    const {
-      seriesInstanceUID
-    } = filters;
+    const { seriesInstanceUID } = filters;
 
     const _seriesLookup = (valueToCompare, displaySet) => {
-      return displaySet.seriesInstanceUid === valueToCompare
+      return displaySet.seriesInstanceUid === valueToCompare;
     };
-    const promotedResponse = _promoteToFront(studyMetadata.getDisplaySets(), seriesInstanceUID, _seriesLookup);
+    const promotedResponse = _promoteToFront(
+      studyMetadata.getDisplaySets(),
+      seriesInstanceUID,
+      _seriesLookup
+    );
 
     study.displaySets = promotedResponse.data;
     promoted = promotedResponse.promoted;
   }
 
   return promoted;
-}
+};
 
 /**
  * Method to identify if query param (from url) was applied to given list
- * @param {Array} studies list of studies to be inspected
- * @param {Object} filters filters to be checked against
+ * @param {Object} study - study reference to promote series against
+ * @param {Object} [filters] - Object containing filters to be applied
+ * @param {string} [filter.seriesInstanceUID] - series instance uid to filter results against
+ * @param {boolean} isFilterStrategy - if filtering by query param strategy ON
  */
-const _isQueryParamApplied = (studies = [], filters = {}) => {
+const _isQueryParamApplied = (study, filters = {}, isFilterStrategy) => {
   const { seriesInstanceUID } = filters;
   let applied = true;
   // skip in case no filter or no toast manager
@@ -78,30 +90,25 @@ const _isQueryParamApplied = (studies = [], filters = {}) => {
     return applied;
   }
 
-  const firstStudy = studies[0] || {};
-  const { seriesList = [], displaySets = [] } = firstStudy;
-  const firstSeries = NOT_FILTER ? displaySets[0] : seriesList[0];
+  const { seriesList = [], displaySets = [] } = study;
+  const firstSeries = isFilterStrategy ? seriesList[0] : displaySets[0];
 
   if (!firstSeries || firstSeries.seriesInstanceUid !== seriesInstanceUID) {
     applied = false;
   }
 
   return applied;
-}
-
-const _showUserMessage = (filterApplied, message, dialog) => {
-
-  if (filterApplied || !dialog) {
+};
+const _showUserMessage = (queryParamApplied, message, dialog = {}) => {
+  if (queryParamApplied) {
     return;
   }
 
-  const {
-    show: showUserMessage = () => { }
-  } = dialog;
+  const { show: showUserMessage = () => {} } = dialog;
   showUserMessage({
     message,
-  })
-}
+  });
+};
 
 const _addSeriesToStudy = (studyMetadata, series) => {
   const sopClassHandlerModules =
@@ -116,7 +123,7 @@ const _addSeriesToStudy = (studyMetadata, series) => {
   );
   study.displaySets = studyMetadata.getDisplaySets();
   _updateMetaDataManager(study, series.seriesInstanceUid);
-}
+};
 
 const _updateMetaDataManager = (study, studyMetadata, series) => {
   updateMetaDataManager(study, series);
@@ -126,148 +133,165 @@ const _updateMetaDataManager = (study, studyMetadata, series) => {
   if (!studyMetadataManager.get(studyInstanceUID)) {
     studyMetadataManager.add(studyMetadata);
   }
-}
+};
 
-const _updateStudyDisplaySets = (study, studyMetadata, shouldSort) => {
+const _updateStudyDisplaySets = (study, studyMetadata) => {
   const sopClassHandlerModules =
     extensionManager.modules['sopClassHandlerModule'];
 
   if (!study.displaySets) {
-    study.displaySets = studyMetadata.createDisplaySets(
-      sopClassHandlerModules,
-      shouldSort
-    );
+    study.displaySets = studyMetadata.createDisplaySets(sopClassHandlerModules);
   }
 
-  studyMetadata.setDisplaySets(study.displaySets, shouldSort);
-}
+  studyMetadata.setDisplaySets(study.displaySets);
+};
 
 const _sortStudyDisplaySet = (study, studyMetadata) => {
   studyMetadata.sortDisplaySets(study.displaySets);
-}
+};
+const _loadRemainingSeries = studyMetadata => {
+  const { seriesLoader } = studyMetadata.getData();
+  if (!seriesLoader) {
+    return Promise.resolve();
+  }
+  const promisesLoaders = [];
+  while (seriesLoader.hasNext()) {
+    promisesLoaders.push(
+      seriesLoader
+        .next()
+        .then(
+          series => void _addSeriesToStudy(studyMetadata, series),
+          error => void log.error(error)
+        )
+    );
+  }
 
-class ViewerRetrieveStudyData extends Component {
-  static propTypes = {
-    studyInstanceUids: PropTypes.array.isRequired,
-    seriesInstanceUIDs: PropTypes.array,
-    server: PropTypes.object,
-    clearViewportSpecificData: PropTypes.func.isRequired,
+  return Promise.all(promisesLoaders);
+};
+
+function ViewerRetrieveStudyData({
+  server,
+  studyInstanceUids,
+  seriesInstanceUIDs,
+  clearViewportSpecificData,
+}) {
+  // hooks
+  const [error, setError] = useState(false);
+  const [studies, setStudies] = useState([]);
+  const snackbarContext = useSnackbarContext();
+  const { appConfig = {} } = useContext(AppContext);
+  const { filterQueryParam: isFilterStrategy = false } = appConfig;
+
+  /**
+   * Callback method when study is totally loaded
+   * @param {object} study study loaded
+   * @param {object} studyMetadata studyMetadata for given study
+   * @param {Object} [filters] - Object containing filters to be applied
+   * @param {string} [filter.seriesInstanceUID] - series instance uid to filter results against
+   */
+  const studyDidLoad = (study, studyMetadata, filters) => {
+    // User message
+    const promoted = _promoteList(
+      study,
+      studyMetadata,
+      filters,
+      isFilterStrategy
+    );
+
+    // Clear viewport to allow new promoted one to be displayed
+    if (promoted) {
+      clearViewportSpecificData(0);
+    }
+
+    const isQueryParamApplied = _isQueryParamApplied(
+      study,
+      filters,
+      isFilterStrategy
+    );
+    // Show message in case not promoted neither filtered but should to
+    _showUserMessage(
+      isQueryParamApplied,
+      'Query parameters were not applied. Using original series list for given study.',
+      snackbarContext
+    );
+
+    setStudies([...studies, study]);
   };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      studies: null,
-      error: null,
-    };
-  }
-  async loadStudies() {
-    try {
-      const { server, studyInstanceUids, seriesInstanceUIDs } = this.props;
-      const filters = {};
-
-      // Use the first, discard others
-      const seriesInstanceUID = seriesInstanceUIDs && seriesInstanceUIDs[0];
-
-      const retrieveParams = [
-        server,
-        studyInstanceUids
-      ];
-
-      if (seriesInstanceUID) {
-        filters.seriesInstanceUID = seriesInstanceUID;
-        // Query param filtering controlled by appConfig property
-        if (!NOT_FILTER) {
-          retrieveParams.push(filters);
-        }
-      }
-
-      const studies = await retrieveStudiesMetadata(...retrieveParams);
-      this.setStudies(studies, filters);
-    } catch (e) {
-      this.setState({ error: true });
-      log.error(e);
-    }
-  }
-
-  setStudies(givenStudies, filters) {
-    if (Array.isArray(givenStudies) && givenStudies.length > 0) {
+  /**
+   * Method to process studies. It will update displaySet, studyMetadata, load remaining series, ...
+   * @param {Array} studiesData Array of studies retrieved from server
+   * @param {Object} [filters] - Object containing filters to be applied
+   * @param {string} [filter.seriesInstanceUID] - series instance uid to filter results against
+   */
+  const processStudies = (studiesData, filters) => {
+    if (Array.isArray(studiesData) && studiesData.length > 0) {
       // Map studies to new format, update metadata manager?
-      const studies = givenStudies.map(study => {
+      const studies = studiesData.map(study => {
         const studyMetadata = new OHIFStudyMetadata(
           study,
           study.studyInstanceUid
         );
 
-        _updateStudyDisplaySets(study, studyMetadata, true);
+        _updateStudyDisplaySets(study, studyMetadata);
         _updateMetaDataManager(study, studyMetadata);
 
         // Attempt to load remaning series if any
-        this._loadRemainingSeries(studyMetadata).then(this._studyDidLoad.bind(this, study, studyMetadata, filters));
+        _loadRemainingSeries(studyMetadata).then(
+          studyDidLoad.bind(undefined, study, studyMetadata, filters)
+        );
 
         return study;
       });
-      this.setState({ studies });
+
+      setStudies(studies);
     }
-  }
-  _studyDidLoad(study, studyMetadata, filters) {
-    // User message
-    const studies = this.state.studies;
-    const { snackbarContext } = this.props;
+  };
 
-    const promoted = _promoteList(study, studyMetadata, filters);
+  const loadStudies = async () => {
+    try {
+      const filters = {};
+      // Use the first, discard others
+      const seriesInstanceUID = seriesInstanceUIDs && seriesInstanceUIDs[0];
 
-    // Clear viewport to allow new promoted one to be displayed
-    if (promoted) {
-      this.props.clearViewportSpecificData(0);
+      const retrieveParams = [server, studyInstanceUids];
+
+      if (seriesInstanceUID) {
+        filters.seriesInstanceUID = seriesInstanceUID;
+        // Query param filtering controlled by appConfig property
+        if (isFilterStrategy) {
+          retrieveParams.push(filters);
+        }
+      }
+
+      const studies = await retrieveStudiesMetadata(...retrieveParams);
+      processStudies(studies, filters);
+    } catch (e) {
+      setError(true);
+      log.error(e);
     }
+  };
 
-    const isFiltersApplied = _isQueryParamApplied(studies, filters);
-    // Show message in case not promoted neither filtered but should to
-    _showUserMessage(isFiltersApplied, 'Query parameters were not applied. Using default series list.', snackbarContext);
-
-    // Update studies to reflect latest changes
-    this.setState(function (state) {
-      return { studies: state.studies.slice() };
-    });
-  }
-
-  _loadRemainingSeries(studyMetadata) {
-    const { seriesLoader } = studyMetadata.getData();
-    if (!seriesLoader) {
-      return Promise.resolve();
-    }
-    const promisesLoaders = [];
-    while (seriesLoader.hasNext()) {
-      promisesLoaders.push(seriesLoader
-        .next()
-        .then(
-          series => void _addSeriesToStudy(studyMetadata, series),
-          error => void log.error(error)
-        ));
-    }
-
-    return Promise.all(promisesLoaders);
-  }
-
-  componentDidMount() {
-    // TODO: CLEAR THIS SOMEWHERE ELSE
+  // Did mount
+  useEffect(() => {
     studyMetadataManager.purge();
-    this.loadStudies();
+    loadStudies();
+  }, []);
+
+  if (error) {
+    return <div>Error: {JSON.stringify(error)}</div>;
   }
 
-  render() {
-    if (this.state.error) {
-      return <div>Error: {JSON.stringify(this.state.error)}</div>;
-    }
-
-    return (
-      <ConnectedViewer
-        studies={this.state.studies}
-        studyInstanceUids={this.props.studyInstanceUids}
-      />
-    );
-  }
+  return (
+    <ConnectedViewer studies={studies} studyInstanceUids={studyInstanceUids} />
+  );
 }
 
-export default withSnackbar(ViewerRetrieveStudyData);
+ViewerRetrieveStudyData.propTypes = {
+  studyInstanceUids: PropTypes.array.isRequired,
+  seriesInstanceUIDs: PropTypes.array,
+  server: PropTypes.object,
+  clearViewportSpecificData: PropTypes.func.isRequired,
+};
+
+export default ViewerRetrieveStudyData;
