71116799a929e74a16fd12afb04d1324c7eb11c2
Gustavo Lelis <galelis@gmail.com>
2019-10-31 13:31:25 -0300

    feat: 🎸 Remove console logs and start using OHIF.log

--- platform/viewer/src/lib/DICOMSR/dataExchange.js
+++ platform/viewer/src/lib/DICOMSR/dataExchange.js
@@ -3,7 +3,7 @@ import { retrieveMeasurementFromSR, stowSRFromMeasurements } from './handleSR';
 import { getLatestSRSeries } from './srUtils';
 
 export const retrieveMeasurements = () => {
-  console.info('[DICOMSR] retrieveMeasurements');
+  OHIF.log.info('[DICOMSR] retrieveMeasurements');
 
   const latestSeries = getLatestSRSeries();
 
@@ -13,7 +13,7 @@ export const retrieveMeasurements = () => {
 };
 
 export const storeMeasurements = (measurementData, filter) => {
-  console.info('[DICOMSR] storeMeasurements');
+  OHIF.log.info('[DICOMSR] storeMeasurements');
 
   // TODO SR: Come back with this when we have access to the server here and throw an error if its not dicomWeb
   // if (!server || server.type !== 'dicomWeb') {
