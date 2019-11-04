fdc9593ca00a04313503505c50b058115a87fd08
Mirna Silva <mirna.silva@radicalimaging.com>
2019-11-04 19:13:47 -0300

    Fix based on reviews and JSON file for PT/CT Study added

--- platform/viewer/cypress/integration/common/OHIFRoutes.spec.js
+++ platform/viewer/cypress/integration/common/OHIFRoutes.spec.js
@@ -1,5 +1,4 @@
-// type definitions for Cypress object "cy"
-/// <reference types="cypress" />
+//We excluded the tests for '**/studies/**' because the bulk/all of our other study/viewer tests use this route
 
 describe('OHIF Routes', function() {
   beforeEach(function() {
@@ -7,9 +6,11 @@ describe('OHIF Routes', function() {
   });
 
   it('checks PT/CT json url study route', function() {
-    cy.visit(
-      '/viewer?url=https://s3.eu-central-1.amazonaws.com/ohif-viewer/JSON/PTCTStudy.json'
-    );
+    cy.fixture('./../fixtures/PTCTStudy.json').as('PTCTStudyJSON');
+
+    cy.visit('/');
+    cy.request('viewer?url=PTCTStudyJSON');
+
     cy.server();
     cy.route('GET', '**/PTCTStudy/**').as('getPTCTStudy');
 
@@ -17,26 +18,8 @@ describe('OHIF Routes', function() {
     cy.get('@getPTCTStudy').should($route => {
       expect($route.status).to.be.eq(200);
     });
-  });
 
-  it('checks studies route', function() {
-    cy.openStudy('MISTER^MR');
-
-    cy.server();
-    cy.route('GET', '**/studies/**').as('getStudies');
-    cy.route('GET', '**/studies/**/series').as('getSeries');
-    cy.route('GET', '**/studies/**/series/**/metadata').as('getMetadata');
-    cy.route('GET', '**/studies/**/series/**/instances/**/frames/1').as(
-      'getFrames'
-    );
-
-    cy.wait(['@getStudies', '@getSeries', '@getMetadata', '@getFrames']).spread(
-      (getStudies, getSeries, getMetadata, getFrames) => {
-        expect(getStudies.status).to.be.eq(200);
-        expect(getSeries.status).to.be.eq(200);
-        expect(getMetadata.status).to.be.eq(200);
-        expect(getFrames.status).to.be.eq(200);
-      }
-    );
+    cy.screenshot();
+    cy.percyCanvasSnapshot('PT/CT json study route');
   });
 });
