b633b30818c5f7a12b9f63ce3fe11379c5c2c12e
Rodrigo Antinarelli <rodrigoantinarelli@gmail.com>
2019-11-25 17:59:36 -0300

    i18n updates

--- platform/i18n/src/locales/pt-BR/index.js
+++ platform/i18n/src/locales/pt-BR/index.js
@@ -1,12 +1,12 @@
-import AboutModal from './AboutModal.json';
-import Buttons from './Buttons.json';
-import CineDialog from './CineDialog.json';
-import Common from './Common.json';
-import DatePicker from './DatePicker.json';
-import Header from './Header.json';
-import UserPreferencesModal from './UserPreferencesModal.json';
+import AboutModal from "./AboutModal.json";
+import Buttons from "./Buttons.json";
+import CineDialog from "./CineDialog.json";
+import Common from "./Common.json";
+import DatePicker from "./DatePicker.json";
+import Header from "./Header.json";
+import UserPreferencesModal from "./UserPreferencesModal.json";
 
-export default {
+export default { 
   'pt-BR': {
     AboutModal,
     Buttons,
@@ -15,5 +15,5 @@ export default {
     DatePicker,
     Header,
     UserPreferencesModal,
-  },
+  }
 };
