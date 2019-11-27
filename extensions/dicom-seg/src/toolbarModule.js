f184ae8fe145020d5559e4b16c8c4f584d95cb85
dannyrb <danny.ri.brown@gmail.com>
2019-11-27 14:22:07 -0500

    Add buttons for seg magic

--- extensions/dicom-seg/src/toolbarModule.js
+++ extensions/dicom-seg/src/toolbarModule.js
@@ -6,13 +6,65 @@ const TOOLBAR_BUTTON_TYPES = {
 
 const definitions = [
   {
-    id: 'GoofyButton',
-    label: '🎉 Goofy 🎉',
-    icon: 'bars',
-    //
-    type: TOOLBAR_BUTTON_TYPES.COMMAND,
-    commandName: 'doGoofyStuff',
-    // commandOptions: { toolName: 'StackScroll' },
+    id: 'SegDropdown',
+    label: 'Segmentation',
+    icon: 'ellipse-circle',
+    buttons: [
+      {
+        id: 'Brush',
+        label: 'Brush',
+        icon: 'circle',
+        //
+        type: TOOLBAR_BUTTON_TYPES.SET_TOOL_ACTIVE,
+        commandName: 'setToolActive',
+        commandOptions: { toolName: 'Brush' },
+      },
+      {
+        id: 'SphericalBrush',
+        label: 'Spherical',
+        icon: 'circle',
+        //
+        type: TOOLBAR_BUTTON_TYPES.SET_TOOL_ACTIVE,
+        commandName: 'setToolActive',
+        commandOptions: { toolName: 'SphericalBrush' },
+      },
+      {
+        id: 'FreehandScissors',
+        label: 'Freehand Scissors',
+        icon: 'circle',
+        //
+        type: TOOLBAR_BUTTON_TYPES.SET_TOOL_ACTIVE,
+        commandName: 'setToolActive',
+        commandOptions: { toolName: 'FreehandScissors' },
+      },
+      {
+        id: 'RectangleScissors',
+        label: 'Rectangle Scissors',
+        icon: 'circle',
+        //
+        type: TOOLBAR_BUTTON_TYPES.SET_TOOL_ACTIVE,
+        commandName: 'setToolActive',
+        commandOptions: { toolName: 'RectangleScissors' },
+      },
+      {
+        id: 'CircleScissors',
+        label: 'Circle Scissors',
+        icon: 'circle',
+        //
+        type: TOOLBAR_BUTTON_TYPES.SET_TOOL_ACTIVE,
+        commandName: 'setToolActive',
+        commandOptions: { toolName: 'CircleScissors' },
+      },
+      {
+        id: 'CorrectionScissors',
+        label: 'Correction Scissors',
+        icon: 'circle',
+        //
+        type: TOOLBAR_BUTTON_TYPES.SET_TOOL_ACTIVE,
+        commandName: 'setToolActive',
+        commandOptions: { toolName: 'CorrectionScissors' },
+      },
+    ],
   },
 ];
 
