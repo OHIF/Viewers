afdfb1af5ce8bcede7a9b79b42a2ec80320c6e83
Igor <igoroctaviano@gmail.com>
2019-12-03 16:55:12 -0300

    Update test

--- platform/core/src/index.test.js
+++ platform/core/src/index.test.js
@@ -14,6 +14,7 @@ describe('Top level exports', () => {
       'createUIModalService',
       'createUIDialogService',
       'createUIContextMenuService',
+      'createUILabellingFlowService',
       //
       'utils',
       'studies',
