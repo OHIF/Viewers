b74aa029b375df3f510318a83f8c61a920d93a39
Igor <igoroctaviano@gmail.com>
2019-12-03 14:52:35 -0300

    Remove cornerstone from toolcontextmenu

--- platform/viewer/src/connectedComponents/ToolContextMenu.js
+++ platform/viewer/src/connectedComponents/ToolContextMenu.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
-import cornerstone from 'cornerstone-core';
-import cornerstoneTools from 'cornerstone-tools';
+import { commandsManager } from './../App.js';
 
 import './ToolContextMenu.css';
 
@@ -15,102 +14,48 @@ const toolTypes = [
   'RectangleRoi',
 ];
 
-class ToolContextMenu extends Component {
-  static propTypes = {
-    isTouchEvent: PropTypes.bool.isRequired,
-    eventData: PropTypes.object,
-    onClose: PropTypes.func,
-  };
-
-  static defaultProps = {
-    isTouchEvent: false,
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.mainElement = React.createRef();
-    this.defaultDropdownItems = [
-      {
-        actionType: 'Delete',
-        action: ({ nearbyToolData, eventData }) => {
-          const element = eventData.element;
-
-          cornerstoneTools.removeToolState(
-            element,
-            nearbyToolData.toolType,
-            nearbyToolData.tool
-          );
-
-          cornerstone.updateImage(element);
-        },
-      },
-      {
-        actionType: 'setLabel',
-        action: ({ nearbyToolData, eventData }) => {
-          const { tool: measurementData } = nearbyToolData;
-          this.props.onSetLabel(eventData, measurementData);
-        },
-      },
-      {
-        actionType: 'setDescription',
-        action: ({ nearbyToolData, eventData }) => {
-          const { tool: measurementData } = nearbyToolData;
-          this.props.onSetDescription(eventData, measurementData);
-        },
-      },
-    ];
-  }
-
-  getNearbyToolData(element, coords, toolTypes) {
-    const nearbyTool = {};
-    let pointNearTool = false;
-
-    toolTypes.forEach(toolType => {
-      const toolData = cornerstoneTools.getToolState(element, toolType);
-      if (!toolData) {
-        return;
-      }
-
-      toolData.data.forEach(function(data, index) {
-        // TODO: Fix this, it's ugly
-        let toolInterface = cornerstoneTools.getToolForElement(
+const ToolContextMenu = ({
+  onSetLabel,
+  onSetDescription,
+  isTouchEvent,
+  eventData,
+  onClose,
+}) => {
+  const mainElement = useRef();
+  const defaultDropdownItems = [
+    {
+      actionType: 'Delete',
+      action: ({ nearbyToolData, eventData }) => {
+        const element = eventData.element;
+        commandsManager.runCommand('removeToolState', {
           element,
-          toolType
-        );
-        if (!toolInterface) {
-          toolInterface = cornerstoneTools.getToolForElement(
-            element,
-            `${toolType}Tool`
-          );
-        }
+          toolType: nearbyToolData.toolType,
+          tool: nearbyToolData.tool,
+        });
+      },
+    },
+    {
+      actionType: 'setLabel',
+      action: ({ nearbyToolData, eventData }) => {
+        const { tool: measurementData } = nearbyToolData;
+        onSetLabel(eventData, measurementData);
+      },
+    },
+    {
+      actionType: 'setDescription',
+      action: ({ nearbyToolData, eventData }) => {
+        const { tool: measurementData } = nearbyToolData;
+        onSetDescription(eventData, measurementData);
+      },
+    },
+  ];
 
-        if (!toolInterface) {
-          throw new Error('Tool not found.');
-        }
-
-        if (toolInterface.pointNearTool(element, data, coords)) {
-          pointNearTool = true;
-          nearbyTool.tool = data;
-          nearbyTool.index = index;
-          nearbyTool.toolType = toolType;
-        }
-      });
-
-      if (pointNearTool) {
-        return false;
-      }
+  const getDropdownItems = (eventData, isTouchEvent = false) => {
+    const nearbyToolData = commandsManager.runCommand('getNearbyToolData', {
+      element: eventData.element,
+      canvasCoordinates: eventData.currentPoints.canvas,
+      availableToolTypes: toolTypes,
     });
-
-    return pointNearTool ? nearbyTool : undefined;
-  }
-
-  getDropdownItems(eventData, isTouchEvent = false) {
-    const nearbyToolData = this.getNearbyToolData(
-      eventData.element,
-      eventData.currentPoints.canvas,
-      toolTypes
-    );
 
     // Annotate tools for touch events already have a press handle to edit it, has a better UX for deleting it
     if (
@@ -123,7 +68,7 @@ class ToolContextMenu extends Component {
 
     let dropdownItems = [];
     if (nearbyToolData) {
-      this.defaultDropdownItems.forEach(function(item) {
+      defaultDropdownItems.forEach(item => {
         item.params = {
           eventData,
           nearbyToolData,
@@ -148,44 +93,46 @@ class ToolContextMenu extends Component {
     }
 
     return dropdownItems;
-  }
+  };
 
-  render() {
-    if (!this.props.eventData) {
-      return null;
+  const itemOnClickHandler = (action, params, onClose) => {
+    action(params);
+    if (onClose) {
+      onClose();
     }
+  };
 
-    const { isTouchEvent, eventData } = this.props;
-    const dropdownItems = this.getDropdownItems(eventData, isTouchEvent);
+  const dropdownItems = getDropdownItems(eventData, isTouchEvent);
 
-    // Skip if there is no dropdown item
-    if (!dropdownItems.length) {
-      return '';
-    }
-
-    const dropdownComponents = dropdownItems.map(item => {
-      const itemOnClick = event => {
-        item.action(item.params);
-        if (this.props.onClose) {
-          this.props.onClose();
-        }
-      };
-
-      return (
-        <li key={item.actionType}>
-          <button className="form-action" onClick={itemOnClick}>
-            <span key={item.actionType}>{item.text}</span>
-          </button>
-        </li>
-      );
-    });
-
-    return (
-      <div className="ToolContextMenu" ref={this.mainElement}>
-        <ul className="bounded">{dropdownComponents}</ul>
+  return (
+    dropdownItems.length &&
+    eventData && (
+      <div className="ToolContextMenu" ref={mainElement}>
+        <ul className="bounded">
+          {dropdownItems.map(({ params, action, text, actionType }) => (
+            <li key={actionType}>
+              <button
+                className="form-action"
+                onClick={() => itemOnClickHandler(action, params, onClose)}
+              >
+                <span key={actionType}>{text}</span>
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
-    );
-  }
-}
+    )
+  );
+};
+
+ToolContextMenu.propTypes = {
+  isTouchEvent: PropTypes.bool.isRequired,
+  eventData: PropTypes.object,
+  onClose: PropTypes.func,
+};
+
+ToolContextMenu.defaultProps = {
+  isTouchEvent: false,
+};
 
 export default ToolContextMenu;
