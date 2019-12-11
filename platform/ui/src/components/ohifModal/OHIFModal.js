f4570c96c23728d52ad8d0efcd3b1cded47e8e36
Mirna Silva <mirna.silva@radicalimaging.com>
2019-12-11 13:46:10 -0300

    tests: Download Image File Modal (#1219)
    * E2E Tests: Download Image File Modal
    
    * CI fix
    
    * Fixes for PR comments
    
    * Small change just to check CI behavior
    
    * Fix for CI failure: comparing classes instead of image src
    
    * Fixed Download button test and blocked Download Dialog event
    
    * Deleting file ConnectedViewportDownloadForm.js
    
    This file was not supposed to be created by this PR. It was already deleted by another user on Master branch.
    
    * Removing wrong import line
    
    * Fixes based on PR comments
    
    * Fixes based on PR comments
    
    * test: remove assignment to window

--- platform/ui/src/components/ohifModal/OHIFModal.js
+++ platform/ui/src/components/ohifModal/OHIFModal.js
@@ -29,7 +29,7 @@ const OHIFModal = ({
   const renderHeader = () => {
     return (
       title && (
-        <div className="OHIFModal__header">
+        <div className="OHIFModal__header" data-cy="modal-header">
           <h4>{title}</h4>
           {closeButton && (
             <button data-cy="close-button" onClick={onClose}>
@@ -44,6 +44,7 @@ const OHIFModal = ({
   return (
     <Modal
       className={classNames('OHIFModal', className)}
+      data-cy="modal"
       shouldCloseOnEsc={shouldCloseOnEsc}
       isOpen={isOpen}
       title={title}
@@ -51,7 +52,9 @@ const OHIFModal = ({
     >
       <>
         {renderHeader()}
-        <div className="OHIFModal__content">{children}</div>
+        <div className="OHIFModal__content" data-cy="modal-content">
+          {children}
+        </div>
       </>
     </Modal>
   );
