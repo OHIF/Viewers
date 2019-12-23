63f340062abeed8ce08343a9711dea3fab6d5c92
dannyrb <danny.ri.brown@gmail.com>
2019-12-23 14:09:38 -0500

    Fix the record plugin's registration

--- platform/core/src/classes/hotkeys/index.js
+++ platform/core/src/classes/hotkeys/index.js
@@ -1,19 +1,16 @@
 // Only imported in environment w/ `window`
 // So we need to mock these for tests
 import Mousetrap from 'mousetrap';
-import pausePlugin from 'mousetrap/plugins/pause/mousetrap-pause.js';
+import pausePlugin from './pausePlugin';
 import recordPlugin from './recordPlugin';
 
-// import pausePlugin from './pausePlugin.js';
-// import recordPlugin from './recordPlugin.js';
+Mousetrap.pause = pausePlugin;
 
-// // // TODO: May need to bind these so Mousetrap = this in plugins;
-// pausePlugin(Mousetrap);
-// recordPlugin(Mousetrap);
-
-// console.log(Mousetrap);
-// console.log(Object.keys(Mousetrap));
-
-Mousetrap.record = recordPlugin;
+// Initialize record plugin
+// https://github.com/ccampbell/mousetrap/blob/master/plugins/record/mousetrap-record.js#L189-L201
+//
+// Adds: .record(callback)
+// Adds: .handleKey()
+recordPlugin(Mousetrap);
 
 export default Mousetrap;
