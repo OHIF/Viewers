312cf398cb08a1376aeb0072ee8bf8a98a9b6b3b
Igor <igoroctaviano@gmail.com>
2020-02-04 10:07:49 -0300

    Remove non used extension

--- extensions/cornerstone-spherical-growcut/src/utils/calculateIterationCountFromExtent.js
+++
@@ -1,9 +0,0 @@
-export default function calculateNumberOfIterations(extent) {
-  return Math.floor(
-    Math.sqrt(
-      Math.pow(extent.width, 2) +
-        Math.pow(extent.height, 2) +
-        Math.pow(extent.numFrames, 2)
-    ) / 2
-  );
-}
--- extensions/cornerstone-spherical-growcut/src/utils/subVolume/index.js
+++
@@ -1,9 +0,0 @@
-import setPixelDataOfSubVolume from "./setPixelDataOfSubVolume";
-import getPointsInSubVolume from "./getPointsInSubVolume";
-import getExtentOfSphere from "./getExtentOfSphere";
-
-export default {
-  setPixelDataOfSubVolume,
-  getPointsInSubVolume,
-  getExtentOfSphere
-};
