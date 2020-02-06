bf9609dab0c322778eb05693884f5a484d612f2d
Danny Brown <danny.ri.brown@gmail.com>
2020-02-06 13:48:05 -0500

    fix: a more appropriate library name

--- extensions/lesion-tracker/.webpack/webpack.prod.js
+++ extensions/lesion-tracker/.webpack/webpack.prod.js
@@ -29,7 +29,7 @@ module.exports = (env, argv) => {
     },
     output: {
       path: ROOT_DIR,
-      library: 'OHIFExtDicomPdf',
+      library: 'OHIFExtLesionTracker',
       libraryTarget: 'umd',
       libraryExport: 'default',
       filename: pkg.main,
