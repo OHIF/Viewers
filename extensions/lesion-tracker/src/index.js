d57d4656415704f49601720986962a7aff47e9f7
dannyrb <danny.ri.brown@gmail.com>
2020-02-06 13:35:48 -0500

    chore: dev build for lesion-tracker extension

--- extensions/lesion-tracker/src/index.js
+++ extensions/lesion-tracker/src/index.js
@@ -20,32 +20,56 @@ export default {
    * @param {CommandsManager} params.commandsManager
    */
   getPanelModule({ servicesManager, commandsManager }) {
+    return undefined;
+    // {
+    // TODO:
+    // - Right, alternative StudyBrowser
+    // - Left, alternative Measurements Table
+    // menuOptions: [
+    //   {
+    //     // A suggested icon
+    //     // Available icons determined by consuming app
+    //     icon: 'list',
+    //     // A suggested label
+    //     label: 'Magic',
+    //     // 'right' or 'left'
+    //     from: 'right',
+    //     // The target component to toggle open/close
+    //     target: 'target-component-id',
+    //     // Overrides `defaultContext`, if specified
+    //     context: ['ACTIVE_VIEWPORT:MAGIC'],
+    //   },
+    // ],
+    // components: [
+    //   {
+    //     id: 'target-component-id',
+    //     component: MyComponent,
+    //   },
+    // ],
+    // defaultContext: ['ROUTE:VIEWER'],
+    // };
+  },
+
+  /**
+   * @param {object} params
+   * @param {ServicesManager} params.servicesManager
+   * @param {CommandsManager} params.commandsManager
+   * @returns Object
+   */
+  getToolbarModule({ servicesManager, commandsManager }) {
+    // TODO: Zoom, Levels, Pan, More (see lesiontracker.ohif.org)
+    // These may be driven through configuration of `cornerstone` extension instead of defined here
     return {
-      // TODO:
-      // - Right, alternative StudyBrowser
-      // - Left, alternative Measurements Table
-      // menuOptions: [
-      //   {
-      //     // A suggested icon
-      //     // Available icons determined by consuming app
-      //     icon: 'list',
-      //     // A suggested label
-      //     label: 'Magic',
-      //     // 'right' or 'left'
-      //     from: 'right',
-      //     // The target component to toggle open/close
-      //     target: 'target-component-id',
-      //     // Overrides `defaultContext`, if specified
-      //     context: ['ACTIVE_VIEWPORT:MAGIC'],
-      //   },
-      // ],
-      // components: [
-      //   {
-      //     id: 'target-component-id',
-      //     component: MyComponent,
-      //   },
-      // ],
-      // defaultContext: ['ROUTE:VIEWER'],
+      definitions: [
+        {
+          id: 'say-hell-world',
+          label: '🎉 HELLO WORLD 🎉',
+          icon: 'exclamation-triangle',
+          type: 'command',
+          commandName: 'sayHelloWorld',
+        },
+      ],
+      defaultContext: 'VIEWER',
     };
   },
 
@@ -53,15 +77,25 @@ export default {
    * @param {object} params
    * @param {ServicesManager} params.servicesManager
    * @param {CommandsManager} params.commandsManager
+   * @returns Object
    */
-  getToolbarModule({ servicesManager, commandsManager }) {
-    // TODO: Zoom, Levels, Pan, More (see lesiontracker.ohif.org)
-    // These may be driven through configuration of `cornerstone` extension instead of defined here
+  getCommandsModule({ servicesManager }) {
+    const { UINotificationService } = servicesManager.services;
     return {
-      // definitions: [
-      //   /* Array of definitions */
-      // ],
-      // defaultContext: ['ROUTE:VIEWER'],
+      definitions: {
+        sayHelloWorld: {
+          commandFn: function() {
+            console.log(UINotificationService);
+            UINotificationService.show({
+              title: 'What does a nosey pepper do?',
+              message: 'Gets jalapeno business!',
+            });
+          },
+          storeContexts: [],
+          options: {},
+        },
+      },
+      defaultContext: ['VIEWER'],
     };
   },
 };
