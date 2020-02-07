7a0683c3baf78e370b41b78c74dc93c96c0d01e2
Igor <igoroctaviano@gmail.com>
2020-02-07 18:34:01 -0300

    Remove lesion tracker from defaults

--- platform/viewer/src/index.js
+++ platform/viewer/src/index.js
@@ -26,7 +26,6 @@ import OHIFVTKExtension from '@ohif/extension-vtk';
 import OHIFDicomHtmlExtension from '@ohif/extension-dicom-html';
 import OHIFDicomMicroscopyExtension from '@ohif/extension-dicom-microscopy';
 import OHIFDicomPDFExtension from '@ohif/extension-dicom-pdf';
-import OHIFLesionTrackerExtension from '@ohif/extension-lesion-tracker';
 
 /*
  * Default Settings
@@ -44,7 +43,6 @@ const appProps = {
     OHIFDicomHtmlExtension,
     OHIFDicomMicroscopyExtension,
     OHIFDicomPDFExtension,
-    OHIFLesionTrackerExtension,
   ],
 };
 
