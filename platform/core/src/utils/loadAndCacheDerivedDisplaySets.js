7b6dc5a50cfec8f16989d5fa2c37c3e570da69b0
igoroctaviano <igoroctaviano@gmail.com>
2020-02-20 16:10:20 -0500

    Add jsdocs for util

--- platform/core/src/utils/loadAndCacheDerivedDisplaySets.js
+++ platform/core/src/utils/loadAndCacheDerivedDisplaySets.js
@@ -1,6 +1,54 @@
 import studyMetadataManager from './studyMetadataManager';
 
-export default function(referencedDisplaySet, studies) {
+/**
+ * Study schema
+ *
+ * @typedef {Object} Study
+ * @property {Array} seriesList -
+ * @property {Object} seriesMap -
+ * @property {Object} seriesLoader -
+ * @property {string} wadoUriRoot -
+ * @property {string} wadoRoot -
+ * @property {string} qidoRoot -
+ * @property {string} patientName -
+ * @property {string} patientId -
+ * @property {number} patientAge -
+ * @property {number} patientSize -
+ * @property {number} patientWeight -
+ * @property {string} accessionNumber -
+ * @property {string} studyDate -
+ * @property {string} studyTime -
+ * @property {string} modalities -
+ * @property {string} studyDescription -
+ * @property {string} imageCount -
+ * @property {string} studyInstanceUid -
+ * @property {string} institutionName -
+ * @property {Array} displaySets -
+ */
+
+/**
+ * Factory function to load and cache derived display sets.
+ *
+ * @param {object} referencedDisplaySet Display set
+ * @param {string} referencedDisplaySet.displaySetInstanceUid Display set instance uid
+ * @param {string} referencedDisplaySet.seriesDate
+ * @param {string} referencedDisplaySet.seriesTime
+ * @param {string} referencedDisplaySet.seriesInstanceUid
+ * @param {string} referencedDisplaySet.seriesNumber
+ * @param {string} referencedDisplaySet.seriesDescription
+ * @param {number} referencedDisplaySet.numImageFrames
+ * @param {string} referencedDisplaySet.frameRate
+ * @param {string} referencedDisplaySet.modality
+ * @param {boolean} referencedDisplaySet.isMultiFrame
+ * @param {number} referencedDisplaySet.instanceNumber
+ * @param {boolean} referencedDisplaySet.isReconstructable
+ * @param {string} referencedDisplaySet.studyInstanceUid
+ * @param {Array} referencedDisplaySet.sopClassUids
+ * @param {string} referencedDisplaySet.plugin
+ * @param {Study[]} studies Collection of studies
+ * @returns void
+ */
+const loadAndCacheDerivedDisplaySets = (referencedDisplaySet, studies) => {
   const { studyInstanceUid, seriesInstanceUid } = referencedDisplaySet;
 
   const studyMetadata = studyMetadataManager.get(studyInstanceUid);
@@ -55,4 +103,6 @@ export default function(referencedDisplaySet, studies) {
 
     recentDisplaySet.load(referencedDisplaySet, studies);
   });
-}
+};
+
+export default loadAndCacheDerivedDisplaySets;
