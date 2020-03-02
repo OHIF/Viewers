130b17399e167ebfb02931c2e573c3d8b2048b1f
Rodrigo Antinarelli <rodrigoantinarelli@gmail.com>
2020-03-02 16:48:37 -0300

    fix docz wrapper propTypes

--- platform/ui/src/gatsby-theme-docz/wrapper.js
+++ platform/ui/src/gatsby-theme-docz/wrapper.js
@@ -1,6 +1,12 @@
 import * as React from 'react';
 import './tailwind.css';
 import './theme.css';
+import PropTypes from 'prop-types';
 
 const Wrapper = ({ children }) => <React.Fragment>{children}</React.Fragment>;
+
+Wrapper.propTypes = {
+  children: PropTypes.node,
+};
+
 export default Wrapper;
