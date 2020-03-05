93b7d50fea39e135e09403e2c9d6c4c5bb459b34
Rodrigo Antinarelli <rodrigoantinarelli@gmail.com>
2020-03-05 18:12:57 -0300

    export ui components

---
+++ platform/ui/index.js
@@ -0,0 +1,6 @@
+import ThemeWrapper from './src/components/ThemeWrapper/';
+import Button from './src/components/Button';
+import IconButton from './src/components/IconButton';
+import Typography from './src/components/Typography';
+
+export { ThemeWrapper, Button, IconButton, Typography };
