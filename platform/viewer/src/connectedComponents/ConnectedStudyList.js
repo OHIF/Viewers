53ab9f0c5c0f643b9beabbfd7ddf02fce4f03639
Rodrigo Antinarelli <rodrigoantinarelli@gmail.com>
2020-03-05 18:11:25 -0300

    create initial studylist placeholder files

---
+++ platform/viewer/src/connectedComponents/ConnectedStudyList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import StudyList from '../components/StudyList/StudyList';
+
+const ConnectedStudyList = () => {
+  return <StudyList />;
+};
+
+export default ConnectedStudyList;
