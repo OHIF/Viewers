8c91997b35d108a33ae3fee15fcd8e82218da8aa
Gustavo Lelis <galelis@gmail.com>
2020-03-09 15:57:52 -0300

    Basic implementation for the header

--- platform/viewer/src/components/StudyList/components/OHIFLogo.js
+++ platform/viewer/src/components/StudyList/components/OHIFLogo.js
@@ -1,18 +1,17 @@
-import { Icon } from '@ohif/ui';
 import React from 'react';
-import classnames from 'classnames';
 
 function OHIFLogo() {
   return (
-    <a
-      target="_blank"
-      rel="noopener noreferrer"
-      className={classnames()}
-      href="http://ohif.org"
-    >
-      <Icon name="ohif-logo" className={classnames()} />
-      <Icon name="ohif-text-logo" className={classnames()} />
-    </a>
+    <React.Fragment>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        className=""
+        href="http://ohif.org"
+      >
+        ICON - Open Health Imaging Foundation
+      </a>
+    </React.Fragment>
   );
 }
 
