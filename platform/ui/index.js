9cbb70fdf1c52e4522f337f47d4619994583dd19
Gustavo Lelis <galelis@gmail.com>
2020-03-09 19:10:22 -0300

    Export icon

--- platform/ui/index.js
+++ platform/ui/index.js
@@ -1,6 +1,8 @@
-import ThemeWrapper from './src/components/ThemeWrapper/';
 import Button from './src/components/Button';
+import ButtonGroup from './src/components/ButtonGroup';
+import Icon from './src/components/Icon';
 import IconButton from './src/components/IconButton';
+import ThemeWrapper from './src/components/ThemeWrapper/';
 import Typography from './src/components/Typography';
 
-export { ThemeWrapper, Button, IconButton, Typography };
+export { Button, ButtonGroup, Icon, IconButton, ThemeWrapper, Typography };
--- platform/ui/src/components/Icon/index.js
+++ platform/ui/src/components/Icon/index.js
@@ -1,4 +1,2 @@
-import { ICONS } from './getIcon.js';
 import Icon from './Icon.js';
-
-export { Icon, ICONS };
+export default Icon;
