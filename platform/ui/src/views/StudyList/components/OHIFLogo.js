b2ce7f024c10241b523a4a456d322d6bb2658d47
Gustavo Lelis <galelis@gmail.com>
2020-03-10 18:00:12 -0300

    Remove link from OHIF logo

--- platform/ui/src/views/StudyList/components/OHIFLogo.js
+++ platform/ui/src/views/StudyList/components/OHIFLogo.js
@@ -1,18 +1,7 @@
 import React from 'react';
 
 function OHIFLogo() {
-  return (
-    <React.Fragment>
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        className=""
-        href="http://ohif.org"
-      >
-        ICON - Open Health Imaging Foundation
-      </a>
-    </React.Fragment>
-  );
+  return <React.Fragment>ICON - Open Health Imaging Foundation</React.Fragment>;
 }
 
 export default OHIFLogo;
