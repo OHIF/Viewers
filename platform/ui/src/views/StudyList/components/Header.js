4fb0ce042a689c7bd6717b07f39bff12c0abdadb
Gustavo Lelis <galelis@gmail.com>
2020-03-10 17:00:25 -0300

    Smnall refactor on header

--- platform/ui/src/views/StudyList/components/Header.js
+++ platform/ui/src/views/StudyList/components/Header.js
@@ -5,23 +5,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
 
-import { IconButton } from '@ohif/ui';
+import { IconButton, Icon } from '@ohif/ui';
 
 import OHIFLogo from './OHIFLogo.js';
 
-function Header({ children = OHIFLogo(), t }) {
+function Header({ appLogo = OHIFLogo(), children, t }) {
+  const showSettingsDropdown = () => {
+    // TODO: Update once dropdown component is created
+  };
+
   return (
     <div className="bg-custom-navy flex flex-row justify-between p-2 text-white">
-      <div className="flex items-center">{children}</div>
-
       <div className="flex items-center">
-        <span className="mr-3 color-custom-grayLight">
+        <div>{appLogo}</div>
+        <div>{children}</div>
+      </div>
+      <div className="flex items-center">
+        <span className="mr-3 text-custom-grayLight">
           {t('FOR INVESTIGATIONAL USE ONLY')}
         </span>
         <IconButton
           variant="text"
           color="inherit"
-          className="color-custom-blueRight"
+          className="text-custom-blueBright"
+          onClick={showSettingsDropdown}
         >
           <React.Fragment>
             <Icon name="settings" />
@@ -34,6 +41,7 @@ function Header({ children = OHIFLogo(), t }) {
 }
 
 Header.propTypes = {
+  appLogo: PropTypes.node,
   children: PropTypes.node,
   t: PropTypes.func.isRequired,
 };
