1795cc6440df977e21f5371118a5dd0d91fbf749
Gustavo Lelis <galelis@gmail.com>
2020-03-10 19:08:08 -0300

    Style updates on filter

--- platform/ui/src/views/StudyList/components/StudyListFilter.js
+++ platform/ui/src/views/StudyList/components/StudyListFilter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Button, Icon, Input } from '@ohif/ui';
+import { Button, Icon, Input, Typography } from '@ohif/ui';
 
 const StudyListFilter = ({
   patientName,
@@ -11,27 +11,65 @@ const StudyListFilter = ({
   modality,
   accession,
   instances,
+  numOfStudies = 0,
+  sortByFilter,
+  onSortByChange,
   onFilterChange,
 }) => {
   return (
-    <div>
-      <div>
-        <div>
-          <div>Study List</div>
-          <div>
-            <Button startIcon={<Icon name="info-link" />}>Learn more</Button>
+    <div className="flex flex-col mt-5 mb-3">
+      <div className="flex flex-row justify-between">
+        <div className="flex flex-row">
+          <Typography variant="h4" className="text-custom-aquaBright mr-6">
+            Study List
+          </Typography>
+          <div className="flex flex-row items-start">
+            <Button
+              variant="text"
+              className="text-custom-blueBright"
+              startIcon={<Icon name="info-link" />}
+            >
+              Learn more
+            </Button>
           </div>
         </div>
-        <div>> 100 Studies</div>
+        <div className="flex flex-row">
+          <Typography variant="h5" className="text-white">
+            {numOfStudies > 100 ? '>100' : numOfStudies}
+          </Typography>
+          <Typography variant="body" className="text-custom-grayLight">
+            Studies
+          </Typography>
+        </div>
       </div>
-      <div>
-        <Input label="Patient name" value={patientName} />
-        <Input label="MRN" value={mrn} />
-        <Input label="Study date" value={studyDate} />
-        <Input label="Description" value={description} />
-        <Input label="Modality" value={modality} />
-        <Input label="Accession" value={accession} />
-        <Input label="Instances" value={instances} />
+      <div className="flex flex-row">
+        <Input
+          label="Patient name"
+          labelClassName="text-white"
+          value={patientName}
+        />
+        <Input label="MRN" labelClassName="text-white" value={mrn} />
+        <Input
+          label="Study date"
+          labelClassName="text-white"
+          value={studyDate}
+        />
+        <Input
+          label="Description"
+          labelClassName="text-white"
+          value={description}
+        />
+        <Input label="Modality" labelClassName="text-white" value={modality} />
+        <Input
+          label="Accession"
+          labelClassName="text-white"
+          value={accession}
+        />
+        <Input
+          label="Instances"
+          labelClassName="text-white"
+          value={instances}
+        />
       </div>
     </div>
   );
@@ -45,6 +83,9 @@ StudyListFilter.propTypes = {
   modality: PropTypes.string,
   accession: PropTypes.string,
   instances: PropTypes.any,
+  numOfStudies: PropTypes.number,
+  sortByFilter: PropTypes.string,
+  onSortByChange: PropTypes.func,
   onFilterChange: PropTypes.func.isRequired,
 };
 
