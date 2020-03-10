1cc48348425a8f96016616fd86696b39ff1e7d51
Gustavo Lelis <galelis@gmail.com>
2020-03-10 20:27:40 -0300

    Adding ohif logo into header

--- platform/ui/src/views/StudyList/components/OHIFLogo.js
+++ platform/ui/src/views/StudyList/components/OHIFLogo.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Svg } from '@ohif/ui';
 
 function OHIFLogo() {
-  return <React.Fragment>ICON - Open Health Imaging Foundation</React.Fragment>;
+  return (
+    <React.Fragment>
+      <Svg name="logo-ohif" />
+    </React.Fragment>
+  );
 }
 
 export default OHIFLogo;
