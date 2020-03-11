7ab5f4975ff141a91d415eaedde21309f7d9f0dd
igoroctaviano <igoroctaviano@gmail.com>
2020-03-11 14:32:48 -0300

    hello world

--- platform/core/src/utils/isDisplaySetReconstructable.js
+++ platform/core/src/utils/isDisplaySetReconstructable.js
@@ -172,6 +172,7 @@ function _getPerpendicularDistance(a, b) {
   );
 }
 
+const constructableModalities = ['MR', 'CT', 'PT', 'NM'];
 const reconstructionIssues = {
   MISSING_FRAMES: 'missingframes',
   IRREGULAR_SPACING: 'irregularspacing',
