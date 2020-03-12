afce3e9a02111c7f5502b8da099b7524bb975544
Rodrigo Antinarelli <rodrigoantinarelli@gmail.com>
2020-03-12 13:23:05 -0300

    chore: uiv2 docz custom page (#1511)
    * feat: Customized Docz Layout
    
    * manually apply padding into playground
    
    * fix playground bug -- sticky items
    
    * fix window
    
    * remove specifc tailwind style for docz

---
+++ platform/ui/src/gatsby-theme-docz/components/Layout/index.js
@@ -0,0 +1,50 @@
+/** @jsx jsx */
+import { useRef, useState } from 'react';
+import { jsx, Layout as BaseLayout, Main } from 'theme-ui';
+import classnames from 'classnames';
+
+import { Header } from '../Header';
+import { Sidebar } from '../Sidebar';
+import * as styles from 'gatsby-theme-docz/src/components/Layout/styles';
+
+const getSidebarStatus = () =>
+  typeof window !== 'undefined' && window.location.pathname === '/';
+
+export const Layout = ({ children }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(getSidebarStatus());
+  const nav = useRef();
+
+  const handleSidebarToggle = () => {
+    setSidebarOpen(s => !s);
+  };
+
+  return (
+    <BaseLayout sx={{ '& > div': { flex: '1 1 auto' } }} data-testid="layout">
+      <Main
+        sx={styles.main}
+        className={classnames({
+          'overflow-hidden h-screen lg:overflow-auto': sidebarOpen,
+        })}
+      >
+        <Header onOpen={handleSidebarToggle} />
+        <div className={classnames('flex', {})}>
+          <Sidebar
+            ref={nav}
+            sidebarOpen={sidebarOpen}
+            onFocus={() => setSidebarOpen(true)}
+            onBlur={() => setSidebarOpen(false)}
+            onClick={() => setSidebarOpen(false)}
+          />
+          <div
+            className={classnames('py-10 w-full', {
+              'px-4 lg:px-20': sidebarOpen,
+              'px-4 lg:px-8': !sidebarOpen,
+            })}
+          >
+            {children}
+          </div>
+        </div>
+      </Main>
+    </BaseLayout>
+  );
+};
---
+++ platform/ui/src/gatsby-theme-docz/components/Sidebar/index.js
@@ -0,0 +1,79 @@
+/** @jsx jsx */
+/** @jsxFrag React.Fragment */
+import React, { useState, useRef, useEffect } from 'react';
+import classnames from 'classnames';
+
+import { jsx } from 'theme-ui';
+import { useMenus, useCurrentDoc } from 'docz';
+
+import { NavSearch } from 'gatsby-theme-docz/src/components/NavSearch';
+import { NavLink } from 'gatsby-theme-docz/src/components/NavLink';
+import { NavGroup } from 'gatsby-theme-docz/src/components/NavGroup';
+
+export const Sidebar = React.forwardRef((props, ref) => {
+  const [query, setQuery] = useState('');
+  const menus = useMenus({ query });
+  const currentDoc = useCurrentDoc();
+  const currentDocRef = useRef();
+  const handleChange = ev => {
+    setQuery(ev.target.value);
+  };
+  useEffect(() => {
+    if (ref.current && currentDocRef.current) {
+      ref.current.scrollTo(0, currentDocRef.current.offsetTop);
+    }
+  }, [ref]);
+
+  return (
+    <>
+      {props.sidebarOpen && (
+        <>
+          <div className="block lg:hidden">
+            <div
+              onClick={() => props.onClick()}
+              className="fixed z-10 left-0 w-full h-full bg-black opacity-80"
+              style={{ top: '81px' }}
+            />
+          </div>
+          <div
+            ref={ref}
+            data-testid="sidebar"
+            className={classnames(
+              'p-8 flex-col bg-white top-0 overflow-auto z-10 border-r border-gray-400 hidden fixed left-0 bottom-0 lg:block lg:sticky lg:h-screen lg:top-0'
+            )}
+            style={{
+              display: props.sidebarOpen ? 'block' : 'none',
+              top: '81px',
+              minWidth: 250,
+            }}
+          >
+            <NavSearch
+              placeholder="Type to search..."
+              value={query}
+              onChange={handleChange}
+            />
+            {menus &&
+              menus.map(menu => {
+                if (!menu.route)
+                  return (
+                    <NavGroup key={menu.id} item={menu} sidebarRef={ref} />
+                  );
+                if (menu.route === currentDoc.route) {
+                  return (
+                    <NavLink key={menu.id} item={menu} ref={currentDocRef}>
+                      {menu.name}
+                    </NavLink>
+                  );
+                }
+                return (
+                  <NavLink key={menu.id} item={menu}>
+                    {menu.name}
+                  </NavLink>
+                );
+              })}
+          </div>
+        </>
+      )}
+    </>
+  );
+});
