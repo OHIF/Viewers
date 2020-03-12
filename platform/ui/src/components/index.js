075492a783a41f43a08687b5bacb82261a8d758c
Gustavo André Lelis <galelis@gmail.com>
2020-03-12 13:26:31 -0300

    feat: Study List Filter (#1504)
    * Inital changes for study list filters
    
    * Style updates on filter
    
    * Small style changes on filter
    
    * Style changes updates on Input and Label and finish fitler
    
    * Refactor on study list filter
    
    * Remove not used callbackFunction
    
    * Small style change
    
    * Adding underline on Learn more link
    
    * Make sure does not show sortable if have no results
    
    * Some style polish
    
    * Fixing margin on filter
    
    * Adding inherit as color on buttons
    
    * Adding inherit on learn more button color
    
    * Addin border with opacity

--- platform/ui/src/components/index.js
+++ platform/ui/src/components/index.js
@@ -2,8 +2,18 @@ import Button from './Button';
 import ButtonGroup from './ButtonGroup';
 import Icon from './Icon';
 import IconButton from './IconButton';
+import Input from './Input';
 import Svg from './Svg';
 import ThemeWrapper from './ThemeWrapper/';
 import Typography from './Typography';
 
-export { Button, ButtonGroup, Icon, IconButton, Svg, ThemeWrapper, Typography };
+export {
+  Button,
+  ButtonGroup,
+  Icon,
+  IconButton,
+  Input,
+  Svg,
+  ThemeWrapper,
+  Typography,
+};
