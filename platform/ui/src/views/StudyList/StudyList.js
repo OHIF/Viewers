b2d650739fa115ee4a484856c356caa85a015876
Rodrigo Antinarelli <rodrigoantinarelli@gmail.com>
2020-03-12 13:13:26 -0300

    feat: Data mocking for studies

--- platform/ui/src/views/StudyList/StudyList.js
+++ platform/ui/src/views/StudyList/StudyList.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import Header from './components/Header';
 import StudyListFilter from './components/StudyListFilter';
 import StudyListTable from './components/StudyListTable';
 import StudyListPagination from './components/StudyListPagination';
 
-const StudyList = () => {
+import studyListMock from '../../mocks/studyList.json';
+
+const DEFAULT_MOCKED_STUDIES_AMOUNT = 50;
+
+const StudyList = ({ studies }) => {
+  console.log('studies', studies);
   return (
     <div>
       <Header />
@@ -13,6 +20,26 @@ const StudyList = () => {
       <StudyListPagination />
     </div>
   );
+};
+
+const getMockedStudies = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+
+  if (!urlParams) {
+    return new Array(DEFAULT_MOCKED_STUDIES_AMOUNT).fill(
+      studyListMock.studies[0]
+    );
+  }
+
+  const myParam = urlParams.get('studiesAmount');
+};
+
+StudyList.defaultProps = {
+  studies: studyListMock.studies,
+};
+
+StudyList.propTypes = {
+  studies: PropTypes.object,
 };
 
 export default StudyList;
