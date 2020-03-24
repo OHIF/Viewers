bf2c7318078a75096d3c73cf7b59f592e6fb072b
Rodrigo Antinarelli <rodrigoantinarelli@gmail.com>
2020-03-24 15:43:48 -0300

    fix export for exposed files

--- platform/ui/index.js
+++ platform/ui/index.js
@@ -1,7 +1,34 @@
+/** UTILS */
 import * as utils from './src/utils/';
-export * from './src/components';
-
 export { utils };
 
+/** CONTEXT/HOOKS */
+export {
+  ModalProvider,
+  ModalConsumer,
+  useModal,
+  withModal,
+} from './src/contextProviders';
+
+/** COMPONENTS */
+export {
+  Button,
+  ButtonGroup,
+  DateRange,
+  Icon,
+  IconButton,
+  Select,
+  Svg,
+  Input,
+  ThemeWrapper,
+  Table,
+  TableBody,
+  TableHead,
+  TableRow,
+  TableCell,
+  Typography,
+} from './src/components';
+
+/** VIEWS */
 export { StudyList } from './src/views';
 export { ModalProvider, ModalConsumer, useModal, withModal, utils };
