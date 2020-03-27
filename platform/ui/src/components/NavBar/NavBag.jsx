69ff46fa5c383046dbfb6c6e32c4e23b4c0788ff
Gustavo Lelis <galelis@gmail.com>
2020-03-27 11:58:28 -0300

    Adding NavBag into Header

--- platform/ui/src/components/NavBar/NavBag.jsx
+++ platform/ui/src/components/NavBar/NavBag.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
-const NavBag = ({}) => {
-  return <></>;
+const NavBag = ({ className, children }) => {
+  return (
+    <div
+      className={classnames(
+        'flex flex-row items-center bg-custom-navy px-3 py-1 sticky top-0 z-10',
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+NavBag.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
 };
 
 export default NavBag;
