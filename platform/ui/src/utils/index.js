e7dee67abaf0778af8fc820d1040ccf49c930d48
Rodrigo Antinarelli <rodrigoantinarelli@gmail.com>
2020-03-31 23:14:09 -0300

    remove unused files

--- platform/ui/src/utils/index.js
+++ platform/ui/src/utils/index.js
@@ -1,6 +1,3 @@
-import capitalize from './capitalize';
 import getMockedStudies from './getMockedStudies';
-import getModalities from './getModalities';
-import getInstances from './getInstances';
 
-export { capitalize, getMockedStudies, getModalities, getInstances };
+export { getMockedStudies };
