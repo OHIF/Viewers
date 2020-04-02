22746d4ce4f41652f67464e7a43d31812391adcd
kedar.netelixir <kedar.choudhary@netelixir.com>
2020-04-02 11:57:35 +0530

    removed dead code in comments

--- platform/viewer/src/config.js
+++ platform/viewer/src/config.js
@@ -1,8 +1,5 @@
-// import OHIF from '@ohif/core';
 import cornerstone from 'cornerstone-core';
-// import dicomParser from 'dicom-parser';
 import version from './version.js';
-// import { getCornerstoneWADOImageLoader } from './utils/cornerstoneWADOImageLoader';
 
 let homepage;
 const { process } = window;
@@ -16,32 +13,3 @@ window.info = {
 };
 
 window.cornerstone = cornerstone;
-// getCornerstoneWADOImageLoader().then(cornerstoneWADOImageLoader => {
-//   // For debugging
-//   //if (process.env.node_env === 'development') {
-//   window.cornerstoneWADOImageLoader = cornerstoneWADOImageLoader;
-//   //}
-
-//   cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
-//   cornerstoneWADOImageLoader.external.dicomParser = dicomParser;
-
-//   OHIF.user.getAccessToken = () => {
-//     // TODO: Get the Redux store from somewhere else
-//     const state = window.store.getState();
-//     if (!state.oidc || !state.oidc.user) {
-//       return;
-//     }
-
-//     return state.oidc.user.accesstoken;
-//   };
-
-//   cornerstoneWADOImageLoader.configure({
-//     beforeSend: function (xhr) {
-//       const headers = OHIF.DICOMWeb.getAuthorizationHeader();
-
-//       if (headers.Authorization) {
-//         xhr.setRequestHeader('Authorization', headers.Authorization);
-//       }
-//     },
-//   });
-// });
