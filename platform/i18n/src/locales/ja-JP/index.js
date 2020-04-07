b5b851ac7c95f9054eef9232fe26c805f22f89f4
Erik Ziegler <erik.sweed@gmail.com>
2020-04-07 10:32:20 +0200

    chore(i18n): Pull latest translations from Locize to obtain French translation (thanks @salimkanoun!) (#1613)
    * Pull latest translations from Locize to obtain French translation
    
    * Fix newlines

--- platform/i18n/src/locales/ja-JP/index.js
+++ platform/i18n/src/locales/ja-JP/index.js
@@ -1,8 +1,8 @@
-import Buttons from "./Buttons.json";
-import CineDialog from "./CineDialog.json";
-import Common from "./Common.json";
-import Header from "./Header.json";
-import UserPreferencesModal from "./UserPreferencesModal.json";
+import Buttons from './Buttons.json';
+import CineDialog from './CineDialog.json';
+import Common from './Common.json';
+import Header from './Header.json';
+import UserPreferencesModal from './UserPreferencesModal.json';
 
 export default { 
   'ja-JP': {
