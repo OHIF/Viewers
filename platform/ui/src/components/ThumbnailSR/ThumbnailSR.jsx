6c8407a5cb6f85f0a0f52341106e9deaa0e06cd5
Gustavo Lelis <galelis@gmail.com>
2020-04-10 12:27:27 -0300

    Update ThumbnailSR style

--- platform/ui/src/components/ThumbnailSR/ThumbnailSR.jsx
+++ platform/ui/src/components/ThumbnailSR/ThumbnailSR.jsx
@@ -5,12 +5,12 @@ import { Icon } from '@ohif/ui';
 
 const ThumbnailSR = ({ seriesDescription, seriesDate }) => {
   return (
-    <div className="flex flex-row flex-1">
-      <div>
-        <Icon name="list-bullets" />
+    <div className="flex flex-row flex-1 px-4 py-5">
+      <div className="flex flex-2 px-4 ">
+        <Icon name="list-bullets" className="text-secondary-light" />
       </div>
       <div className="flex flex-col flex-1">
-        <div className="flex flex-row flex-1">
+        <div className="flex flex-row flex-1 items-center mb-2">
           <div className="px-6 py-1 bg-primary-main rounded-sm mr-4">SR</div>
           <span className="text-blue-300">{seriesDate}</span>
         </div>
