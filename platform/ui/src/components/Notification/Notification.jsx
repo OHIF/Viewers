3223d6a8a11c4e2c4901c7ffe44c636ce242e611
Rodrigo Antinarelli <rodrigoantinarelli@gmail.com>
2020-04-14 11:38:49 -0300

    fix notification margin

--- platform/ui/src/components/Notification/Notification.jsx
+++ platform/ui/src/components/Notification/Notification.jsx
@@ -35,7 +35,7 @@ const Notification = ({ type, text, actionButtons }) => {
   const { icon, color } = getIconData();
 
   return (
-    <div className="mx-2 p-2 flex flex-col bg-common-bright rounded">
+    <div className="mx-2 mt-2 p-2 flex flex-col bg-common-bright rounded">
       <div className="flex flex-grow">
         <Icon name={icon} className={classnames('w-5', color)} />
         <span className="text-base text-black ml-2">{text}</span>
