5204e32816485d04e49a5efae4b5b7b2aab4feb8
Rodrigo Antinarelli <rodrigoantinarelli@gmail.com>
2020-04-27 18:58:01 -0300

    quick navigation between routes

--- platform/viewer/src/containers/StudyListContainer/StudyListContainer.jsx
+++ platform/viewer/src/containers/StudyListContainer/StudyListContainer.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const StudyListContainer = () => {
-  return <div>StudyListContainer</div>;
+  return (
+    <div>
+      <div>StudyListContainer</div>
+      <Link to="/viewer/123">Go to Viewer Route</Link>
+    </div>
+  );
 };
 
 export default StudyListContainer;
--- platform/viewer/src/containers/ViewerContainer/ViewerContainer.jsx
+++ platform/viewer/src/containers/ViewerContainer/ViewerContainer.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ViewerContainer = () => {
-  return <div>ViewerContainer</div>;
+  return (
+    <div>
+      <div>ViewerContainer</div>
+      <Link to="/">Go to StudyList Route</Link>
+    </div>
+  );
 };
 
 export default ViewerContainer;
