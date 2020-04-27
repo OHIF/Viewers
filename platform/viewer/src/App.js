314b69ab52e8597f224ff475eba96a2d3dab347a
Rodrigo Antinarelli <rodrigoantinarelli@gmail.com>
2020-04-27 19:20:02 -0300

    configure webpack to read .env variables

--- platform/viewer/src/App.js
+++ platform/viewer/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { ThemeWrapper } from '@ohif/ui';
 
 import routes from './routes';
 
 const App = () => {
+  const RouterComponent = JSON.parse(process.env.USE_HASH_ROUTER)
+    ? HashRouter
+    : BrowserRouter;
   return (
-    <BrowserRouter>
+    <RouterComponent>
       <ThemeWrapper>{routes()}</ThemeWrapper>
-    </BrowserRouter>
+    </RouterComponent>
   );
 };
 
