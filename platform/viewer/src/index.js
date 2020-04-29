dff405f7957eb568d8492251cd66cf17a712cbd7
dannyrb <danny.ri.brown@gmail.com>
2020-04-29 14:03:37 -0400

    update PWA entrypoint to pass configuration on window to App as props

--- platform/viewer/src/index.js
+++ platform/viewer/src/index.js
@@ -2,14 +2,16 @@
  * Entry point for development and production PWA builds.
  * Packaged (NPM) builds go through `index-umd.js`
  */
-
 import 'regenerator-runtime/runtime';
-
 import App from './App.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const appProps = {};
+/** Combine our appConfiguration and "baked-in" extensions */
+const appProps = {
+  config: window ? window.config : {},
+  defaultExtensions: [],
+};
 
 /** Create App */
 const app = React.createElement(App, appProps, null);
