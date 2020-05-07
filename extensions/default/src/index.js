b8ca7059c671ba63a00556c454d528b3f539e005
Rodrigo Antinarelli <rodrigoantinarelli@gmail.com>
2020-05-07 10:28:42 -0300

    Co-authored-by: Danny Brown <danny.ri.brown@gmail.com>

--- extensions/default/src/index.js
+++ extensions/default/src/index.js
@@ -1,17 +1,9 @@
-import webApi from './modules/webApi';
-
-/**
- * Defines the default modules.
- */
-const defaultModules = [webApi];
+import getDataSourcesModule from './getDataSourcesModule.js';
 
 export default {
   /**
    * Only required property. Should be a unique value across all extensions.
    */
-  id: 'default',
-
-  getDataSourcesModule() {
-    return defaultModules;
-  },
+  id: 'org.ohif.default',
+  getDataSourcesModule,
 };
