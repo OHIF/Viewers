f5ea1fe924b4bf0e9738e5def3c028f5114da903
dannyrb <danny.ri.brown@gmail.com>
2020-05-07 12:16:44 -0400

    init modes folder

---
+++ modes/example/src/index.js
@@ -0,0 +1,38 @@
+export default function mode({ modeConfiguration }) {
+  return {
+    id: 'example-mode',
+    validationTags: {
+      study: [],
+      series: [],
+    },
+    isValidMode: (studyTags, seriesTags) => {
+      // All series are welcome in this mode!
+      return true;
+    },
+    routes: [
+      {
+        path: 'viewer',
+        preInit: ({ toolbarManager }) => {
+          toolbarManager &&
+            toolbarManager.setDefaultLoadOut([
+              [], // Primary
+              [], // Secondary
+            ]);
+        },
+        layoutTemplate: ({ routeProps }) => {
+          return {
+            id: 'org.ohif.defaults.viewerlayout',
+            props: {
+              // named slots
+              leftPanels: ['org.ohif.defaults.seriesList'],
+              rightPanels: ['org.ohif.defaults.measure'],
+              viewports: ['org.ohif.defaults.cornerstoneViewport'],
+            },
+          };
+        },
+      },
+    ],
+    extensions: ['org.ohif.defaults', 'org.ohif.cornerstone'],
+    sopClassHandlers: ['org.ohif.defaults.stack'],
+  };
+}
