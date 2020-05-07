b848ad8f0e274a11c59bb2cac1abb28a3ea2ed37
Rodrigo Antinarelli <rodrigoantinarelli@gmail.com>
2020-05-07 11:19:58 -0300

    app configuration and App entrypoint update

--- platform/viewer/src/App.js
+++ platform/viewer/src/App.js
@@ -2,17 +2,57 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { ThemeWrapper } from '@ohif/ui';
-
+import {
+  CommandsManager,
+  ExtensionManager,
+  ServicesManager,
+  HotkeysManager,
+  UINotificationService,
+  UIModalService,
+  UIDialogService,
+  MeasurementService,
+  utils,
+  redux as reduxOHIF,
+} from '@ohif/core';
 import routes from './routes';
 
-function App({ config }) {
-  const { routerBasename } = config;
-  const Router = JSON.parse(process.env.USE_HASH_ROUTER)
-    ? HashRouter
-    : BrowserRouter;
+/**
+ * ENV Variable to determine routing behavior
+ */
+const Router = JSON.parse(process.env.USE_HASH_ROUTER)
+  ? HashRouter
+  : BrowserRouter;
+
+const commandsManagerConfig = {
+  /** Used by commands to inject `viewports` from "redux" */
+  getAppState: () => store.getState(),
+  /** Used by commands to determine active context */
+  getActiveContexts: () => getActiveContexts(store.getState()),
+};
+const commandsManager = new CommandsManager(commandsManagerConfig);
+const servicesManager = new ServicesManager();
+// const hotkeysManager = new HotkeysManager(commandsManager, servicesManager);
+const extensionManager = new ExtensionManager({
+  commandsManager,
+  servicesManager,
+  appConfig,
+  api: {
+    contexts: CONTEXTS,
+    hooks: {
+      useAppContext,
+    },
+  },
+});
+
+extensionManager.registerExtensions(/* Array of Extensions */);
+
+function App({ config, defaultExtensions }) {
+  const appConfig = {
+    ...(typeof config === 'function' ? config({ servicesManager }) : config),
+  };
 
   return (
-    <Router basename={routerBasename}>
+    <Router basename={appConfig.routerBasename}>
       <ThemeWrapper>{routes()}</ThemeWrapper>
     </Router>
   );
@@ -36,6 +76,17 @@ App.propTypes = {
 
 App.defaultProps = {
   config: {
+    /**
+     * Relative route from domain root that OHIF instance is installed at.
+     * For example:
+     *
+     * Hosted at: https://ohif.org/where-i-host-the/viewer/
+     * Value: `/where-i-host-the/viewer/`
+     * */
+    routerBaseName: '/',
+    /**
+     *
+     */
     showStudyList: true,
     oidc: [],
     extensions: [],
