ee69a5a40b26ce9176f0b4429bfd2fe93a2cf394
Rodrigo Antinarelli <rodrigoantinarelli@gmail.com>
2020-05-07 10:56:26 -0300

    create dicom web api

--- extensions/default/src/getDataSourcesModule.js
+++ extensions/default/src/getDataSourcesModule.js
@@ -2,36 +2,17 @@
 // TODO: Use constructor to create an instance of IWebClientApi
 // TODO: Use existing DICOMWeb configuration (previously, appConfig, to configure instance)
 
+import { createDicomWebApi } from './DicomWebDataSource/index.js';
+
 /**
  *
  */
 function getDataSourcesModule() {
   return [
     {
-      name: 'dicomweb-client',
+      name: 'dicomweb',
       type: 'webApi',
-      /**
-       * TODO: Create JSDoc for this Data Source implementation.
-       */
-      createDataSource: dataSourceConfiguration => {
-        // instantiate DicomWebClient
-        // const dicomWebClient = new DicomWebClient(dataSourceConfiguration);
-
-        return {
-          query: {
-            studies: {
-              mapParams: params => params,
-              searchStudies: params => {
-                /** dicomWebClient.searchStudies(params) **/
-              },
-              searchSeries: params => {
-                /** dicomWebClient.searchSeries(params) **/
-              },
-              processResults: results => results,
-            },
-          },
-        };
-      },
+      createDataSource: createDicomWebApi,
     },
   ];
 }
