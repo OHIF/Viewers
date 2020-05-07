655b9900efe72b639c0b073fc525f166f2078b2f
dannyrb <danny.ri.brown@gmail.com>
2020-05-07 14:05:57 -0400

    Playing with how routes access ViewModel context and SopClassHandlers that map meta to ViewModel (displaySets)

---
+++ platform/viewer/src/routes/ViewModelContext.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+const ViewModelContext = React.createContext({
+  displaySetInstanceUids: [],
+  setDisplaySetInstanceUids: () => {},
+});
+
+class ViewModelProvider extends Component {
+  state = {
+    displaySetInstanceUids: [],
+  };
+
+  render() {
+    const setDisplaySetInstanceUids = displaySetInstanceUids => {
+      this.setState({ displaySetInstanceUids });
+    };
+
+    return (
+      <ViewModelContext.Provider
+        value={{
+          displaySetInstanceUids: this.state.displaySetInstanceUids,
+          setDisplaySetInstanceUids,
+        }}
+      >
+        {this.props.children}
+      </ViewModelContext.Provider>
+    );
+  }
+}
+
+export default ViewModelContext;
+
+export { ViewModelProvider };
