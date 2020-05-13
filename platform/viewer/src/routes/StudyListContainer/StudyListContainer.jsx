12535dd2b32bd828ac939201d6b088101f44f5f9
dannyrb <danny.ri.brown@gmail.com>
2020-05-13 00:31:50 -0400

    Remove unused filtersMeta

--- platform/viewer/src/routes/StudyListContainer/StudyListContainer.jsx
+++ platform/viewer/src/routes/StudyListContainer/StudyListContainer.jsx
@@ -139,65 +139,6 @@ function StudyListContainer({ history, data: studies, dataSource }) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [expandedRows, studies]);
 
-  const filtersMeta = [
-    {
-      name: 'patientName',
-      displayName: 'Patient Name',
-      inputType: 'Text',
-      isSortable: true,
-      gridCol: 4,
-    },
-    {
-      name: 'mrn',
-      displayName: 'MRN',
-      inputType: 'Text',
-      isSortable: true,
-      gridCol: 2,
-    },
-    {
-      name: 'studyDate',
-      displayName: 'Study date',
-      inputType: 'DateRange',
-      isSortable: true,
-      gridCol: 5,
-    },
-    {
-      name: 'description',
-      displayName: 'Description',
-      inputType: 'Text',
-      isSortable: true,
-      gridCol: 4,
-    },
-    {
-      name: 'modalities',
-      displayName: 'Modality',
-      inputType: 'MultiSelect',
-      inputProps: {
-        options: [
-          { value: 'SEG', label: 'SEG' },
-          { value: 'CT', label: 'CT' },
-          { value: 'MR', label: 'MR' },
-          { value: 'SR', label: 'SR' },
-        ],
-      },
-      isSortable: true,
-      gridCol: 3,
-    },
-    {
-      name: 'accession',
-      displayName: 'Accession',
-      inputType: 'Text',
-      isSortable: true,
-      gridCol: 4,
-    },
-    {
-      name: 'instances',
-      displayName: 'Instances',
-      inputType: 'None',
-      isSortable: true,
-      gridCol: 2,
-    },
-  ];
   const isFiltering = (filterValues, defaultFilterValues) => {
     return Object.keys(defaultFilterValues).some(name => {
       return filterValues[name] !== defaultFilterValues[name];
