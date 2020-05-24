3a78d313acc593efe78208287ab0235875a7d0ed
dannyrb <danny.ri.brown@gmail.com>
2020-05-23 20:48:09 -0400

    remove dead code

--- extensions/default/src/getPanelModule.js
+++ extensions/default/src/getPanelModule.js
@@ -45,35 +45,3 @@ function getPanelModule({
 }
 
 export default getPanelModule;
-
-// // TODO -> Need some way of selecting which displaySets hit the viewports.
-// const { DisplaySetService } = servicesManager.services;
-
-// // TODO -> Make a HangingProtocolService
-// const HangingProtocolService = displaySets => {
-//   const displaySetInstanceUid = displaySets[Object.keys(displaySets)[0]][0].displaySetInstanceUid;
-
-//   return {
-//     numRows: 1,
-//     numCols: 1,
-//     activeViewportIndex: 0,
-//     viewports: [
-//       {
-//         displaySetInstanceUid,
-//       },
-//     ],
-//   };
-// };
-
-// const handleDisplaySetSubscription = useCallback(displaySets => {
-//   setViewportGrid(HangingProtocolService(displaySets));
-// });
-
-// useEffect(() => {
-//   const { unsubscribe } = DisplaySetService.subscribe(
-//     DisplaySetService.EVENTS.DISPLAY_SET_ADDED,
-//     handleDisplaySetSubscription
-//   );
-
-//   return unsubscribe;
-// }, []);
