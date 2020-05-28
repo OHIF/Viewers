84a31392fd0ccdf06309d991e09f2063be2835d8
James A. Petts <jamesapetts@gmail.com>
2020-05-28 09:44:46 +0100

    Fix PWA e2e.

--- extensions/dicom-html/src/DicomHtmlViewport.js
+++ extensions/dicom-html/src/DicomHtmlViewport.js
@@ -250,7 +250,7 @@ class DicomHtmlViewport extends Component {
         data-cy="dicom-html-viewport"
         className="DicomHtmlViewport"
         onClick={this.setViewportActiveHandler}
-        onScroll={this.seÎtViewportActiveHandler}
+        onScroll={this.setViewportActiveHandler}
       >
         {content}
         {error && <h2>{JSON.stringify(error)}</h2>}
