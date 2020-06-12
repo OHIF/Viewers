c2c7c6408164ed3dd19b6d63300152886eaccc96
Erik Ziegler <erik.sweed@gmail.com>
2020-06-12 11:28:54 +0200

    Add electron-builder, fix issues with data storage paths (only tested on OSX)

--- platform/desktop/src/main.js
+++ platform/desktop/src/main.js
@@ -55,6 +55,3 @@ app.on('activate', function() {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
--- platform/desktop/src/pouchdb.js
+++ platform/desktop/src/pouchdb.js
@@ -1,18 +1,58 @@
-console.log('Starting PouchDB...');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const PouchDB = require('pouchdb');
 
-var express = require('express');
-var morgan = require('morgan');
-var PouchDB = require('pouchdb');
-var app = express();
+// https://stackoverflow.com/questions/60881343/electron-problem-creating-file-error-erofs-read-only-file-system
+function getAppDataPath(appName) {
+  switch (process.platform) {
+    case "darwin": {
+      return path.join(process.env.HOME, "Library", "Application Support", appName);
+    }
+    case "win32": {
+      return path.join(process.env.APPDATA, appName);
+    }
+    case "linux": {
+      return path.join(process.env.HOME, `.${appName}`);
+    }
+    default: {
+      console.log("Unsupported platform!");
+      process.exit(1);
+    }
+  }
+}
 
-app.use(morgan('dev'));
+// todo if dev use appDataDirPath=${__dirname}/../
+const appDataDirPath = getAppDataPath('OHIFViewer');
+const pouchDBFolder = `${appDataDirPath}/__ohif-pouchdb-storage/`;
 
-var CustomPouchDB = PouchDB.defaults({
-  prefix: `${__dirname}/../__ohif-pouchdb-storage/`,
+// TODO: mkdir -p instead
+if (!fs.existsSync(appDataDirPath)) {
+    fs.mkdirSync(appDataDirPath);
+}
+
+if (!fs.existsSync(pouchDBFolder)) {
+  fs.mkdirSync(pouchDBFolder);
+}
+
+const CustomPouchDB = PouchDB.defaults({
+  prefix: pouchDBFolder,
 });
 
-app.use('/', require('express-pouchdb')(CustomPouchDB));
+const app = express();
 
-var chronicle = new PouchDB('chronicle');
+// Create a write stream (in append mode)
+const logStream = fs.createWriteStream(path.join(appDataDirPath, 'data.log'), { flags: 'a' })
+const pouchdbLog = path.join(appDataDirPath, 'pouchdb.log')
+const options = { logPath: pouchdbLog };
 
-app.listen(5984);
+// Setup the logger
+app.use(morgan('dev', { stream: logStream }));
+app.use('/', require('express-pouchdb')(CustomPouchDB, options));
+
+const chronicle = new PouchDB('chronicle');
+
+app.listen(5984, () => console.log(`PouchDB server listening on port 5984`));
+
+module.exports = app;
