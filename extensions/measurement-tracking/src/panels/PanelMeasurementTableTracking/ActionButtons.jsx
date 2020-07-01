4f2ff438b7d964a282d55cb68e77a3a7d201e4af
Rodrigo Antinarelli <rodrigoantinarelli@gmail.com>
2020-07-01 17:45:26 -0300

    measurements action buttons hover state

--- extensions/measurement-tracking/src/panels/PanelMeasurementTableTracking/ActionButtons.jsx
+++ extensions/measurement-tracking/src/panels/PanelMeasurementTableTracking/ActionButtons.jsx
@@ -5,16 +5,16 @@ import { Button, ButtonGroup, Icon, IconButton } from '@ohif/ui';
 function ActionButtons({ onExportClick, onCreateReportClick }) {
   return (
     <React.Fragment>
-      <ButtonGroup onClick={onExportClick}>
+      <ButtonGroup className="group outline-none" onClick={onExportClick}>
         <Button
-          className="px-2 py-2 text-base text-white bg-black border-primary-main"
+          className="px-2 py-2 text-base text-white bg-black border-primary-main group-hover:border-black group-hover:bg-primary-main group-focus:bg-primary-main group-focus:border-black"
           size="initial"
           color="inherit"
         >
           Export
         </Button>
         <IconButton
-          className="px-2 text-white bg-black border-primary-main"
+          className="px-2 text-white bg-black border-primary-main group-hover:bg-primary-main group-hover:border-black group-focus:bg-primary-main group-focus:border-black"
           color="inherit"
           size="initial"
         >
@@ -22,7 +22,7 @@ function ActionButtons({ onExportClick, onCreateReportClick }) {
         </IconButton>
       </ButtonGroup>
       <Button
-        className="px-2 py-2 ml-2 text-base text-white bg-black border border-primary-main"
+        className="px-2 py-2 ml-2 text-base text-white bg-black border border-primary-main hover:bg-primary-main hover:border-black focus:bg-primary-main focus:border-black"
         variant="outlined"
         size="initial"
         color="inherit"
