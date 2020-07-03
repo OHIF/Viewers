31fb0a6606db050002ecc1bae0869e4f7d619edc
dannyrb <danny.ri.brown@gmail.com>
2020-07-02 23:55:51 -0400

    Don't render the `0` of data.length used to check a render condition

--- platform/ui/src/components/MeasurementTable/MeasurementTable.jsx
+++ platform/ui/src/components/MeasurementTable/MeasurementTable.jsx
@@ -8,37 +8,30 @@ const MeasurementTable = ({ data, title, amount, onClick, onEdit }) => {
   return (
     <div>
       <div className="flex justify-between px-2 py-1 bg-secondary-main">
-        <span className="text-base font-bold text-white tracking-widest uppercase">
+        <span className="text-base font-bold tracking-widest text-white uppercase">
           {title}
         </span>
         <span className="text-base font-bold text-white">{amount}</span>
       </div>
-      <div className="overflow-y-auto overflow-x-hidden ohif-scrollbar max-h-112">
-        {data.length && data.map((measurementItem, index) => (
-          <MeasurementItem
-            key={measurementItem.id}
-            id={measurementItem.id}
-            index={index + 1}
-            label={measurementItem.label}
-            isActive={measurementItem.isActive}
-            displayText={measurementItem.displayText}
-            onClick={onClick}
-            onEdit={onEdit}
-          />
-        ))}
-        {!data.length && (
-          <div
-            className={classnames(
-              'group flex cursor-default bg-black border border-transparent transition duration-300 '
-            )}
-          >
-            <div
-              className={classnames(
-                'text-center w-6 py-1 text-base transition duration-300 bg-primary-dark text-primary-light group-hover:bg-secondary-main'
-              )}
-            ></div>
-            <div className="px-2 py-4 flex flex-1 items-center justify-between">
-              <span className="text-base text-primary-light mb-1 flex items-center flex-1">
+      <div className="overflow-x-hidden overflow-y-auto ohif-scrollbar max-h-112">
+        {data.length !== 0 &&
+          data.map((measurementItem, index) => (
+            <MeasurementItem
+              key={measurementItem.id}
+              id={measurementItem.id}
+              index={index + 1}
+              label={measurementItem.label}
+              isActive={measurementItem.isActive}
+              displayText={measurementItem.displayText}
+              onClick={onClick}
+              onEdit={onEdit}
+            />
+          ))}
+        {data.length === 0 && (
+          <div className="flex transition duration-300 bg-black border border-transparent cursor-default group">
+            <div className="w-6 py-1 text-base text-center transition duration-300 bg-primary-dark text-primary-light group-hover:bg-secondary-main"></div>
+            <div className="flex items-center justify-between flex-1 px-2 py-4">
+              <span className="flex items-center flex-1 mb-1 text-base text-primary-light">
                 No tracked measurements
               </span>
             </div>
@@ -52,8 +45,8 @@ const MeasurementTable = ({ data, title, amount, onClick, onEdit }) => {
 MeasurementTable.defaultProps = {
   amount: null,
   data: [],
-  onClick: () => { },
-  onEdit: () => { },
+  onClick: () => {},
+  onEdit: () => {},
 };
 
 MeasurementTable.propTypes = {
