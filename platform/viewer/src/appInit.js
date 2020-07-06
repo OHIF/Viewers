066ba97bc962376deb3681dcd57fea91cb1c323f
dannyrb <danny.ri.brown@gmail.com>
2020-07-06 08:42:06 -0400

    Temporarily disable SegmentationMode

--- platform/viewer/src/appInit.js
+++ platform/viewer/src/appInit.js
@@ -72,7 +72,7 @@ function appInit(appConfigOrFunc, defaultExtensions) {
   // TODO: Remove this
   if (!appConfig.modes.length) {
     appConfig.modes.push(window.longitudinalMode);
-    appConfig.modes.push(window.segmentationMode);
+    // appConfig.modes.push(window.segmentationMode);
   }
 
   return {
