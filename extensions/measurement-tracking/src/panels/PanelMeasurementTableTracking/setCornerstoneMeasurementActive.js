a214d9c202ac62e5308ff8b6a24c5e5223f9f537
James A. Petts <jamesapetts@gmail.com>
2020-07-09 11:00:39 +0100

    Set cornerstone tool active when jumping to measurement.

---
+++ extensions/measurement-tracking/src/panels/PanelMeasurementTableTracking/setCornerstoneMeasurementActive.js
@@ -0,0 +1,33 @@
+import cornerstone from 'cornerstone-core';
+import cornerstoneTools from 'cornerstone-tools';
+
+const { globalImageIdSpecificToolStateManager } = cornerstoneTools;
+
+export default function setMeasurementActive(measurement) {
+  const { id } = measurement;
+
+  const toolState = globalImageIdSpecificToolStateManager.saveToolState();
+
+  Object.keys(toolState).forEach(imageId => {
+    const imageIdSpecificToolState = toolState[imageId];
+    debugger;
+
+    Object.keys(imageIdSpecificToolState).forEach(toolType => {
+      const toolSpecificToolState = imageIdSpecificToolState[toolType];
+
+      const toolSpecificToolData = toolSpecificToolState.data;
+
+      if (toolSpecificToolData && toolSpecificToolData.length) {
+        toolSpecificToolData.forEach(data => {
+          data.active = data.id === id ? true : false;
+        });
+      }
+    });
+  });
+
+  const enabledElements = cornerstoneTools.store.state.enabledElements;
+
+  enabledElements.forEach(element => {
+    cornerstone.updateImage(element);
+  });
+}
