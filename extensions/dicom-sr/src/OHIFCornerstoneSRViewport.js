1507f9eb611b0870a4bad0059e94fc9146fb7336
Igor Octaviano <igoroctaviano@gmail.com>
2020-07-10 11:29:57 -0300

    Add missing props (#1879)

--- extensions/dicom-sr/src/OHIFCornerstoneSRViewport.js
+++ extensions/dicom-sr/src/OHIFCornerstoneSRViewport.js
@@ -433,8 +433,8 @@ function OHIFCornerstoneSRViewport({
             spacing:
               PixelSpacing && PixelSpacing.length
                 ? `${PixelSpacing[0].toFixed(2)}mm x ${PixelSpacing[1].toFixed(
-                    2
-                  )}mm`
+                  2
+                )}mm`
                 : '',
             scanner: ManufacturerModelName || '',
           },
@@ -560,6 +560,8 @@ async function _getViewportAndActiveDisplaySetData(
     SeriesDescription: image0.SeriesDescription,
     SeriesInstanceUID: image0.SeriesInstanceUID,
     SeriesNumber: image0.SeriesNumber,
+    ManufacturerModelName: image0.ManufacturerModelName,
+    PixelSpacing: image0.PixelSpacing,
     displaySetInstanceUID,
   };
 
