0b634501863969da3f9d8d8d0715221969d6155d
James A. Petts <jamesapetts@gmail.com>
2020-07-13 14:38:49 +0100

    fix: 🐛 If no contours for structure, cleanly exit and notify
    ✅ Closes: #1892

--- platform/viewer/cypress/integration/common/OHIFRoutes.spec.js
+++ platform/viewer/cypress/integration/common/OHIFRoutes.spec.js
@@ -5,20 +5,18 @@ describe('OHIF Routes', function() {
     cy.openStudyList();
   });
 
-  /*
   // TODO -> Bring back when testJSON is hosted again.
-  it('checks TEST json url study route', function() {
-    cy.visit(
-      '/viewer?url=https://ohif-viewer.s3.eu-central-1.amazonaws.com/JSON/testJSON.json'
-    );
+  // it('checks TEST json url study route', function() {
+  //   cy.visit(
+  //     '/viewer?url=https://ohif-viewer.s3.eu-central-1.amazonaws.com/JSON/testJSON.json'
+  //   );
 
-    cy.server();
-    cy.route('GET', '**/ MRStudy; /**').as('getTESTStudy');
+  //   cy.server();
+  //   cy.route('GET', '**/ MRStudy; /**').as('getTESTStudy');
 
-    cy.wait('@getTESTStudy.all');
-    cy.get('@getTESTStudy').should($route => {
-      expect($route.status).to.be.eq(200);
-    });
-  });
-  */
+  //   cy.wait('@getTESTStudy.all');
+  //   cy.get('@getTESTStudy').should($route => {
+  //     expect($route.status).to.be.eq(200);
+  //   });
+  // });
 });
