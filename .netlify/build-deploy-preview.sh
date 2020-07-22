f95a8c153092293e72a69093066c74049ad4b980
Erik Ziegler <erik.sweed@gmail.com>
2020-07-22 19:41:10 +0200

    fix: Use same config for branch deploys as for deploy previews (#1893)
    * fix: Use same config for branch deploys as for deploy previews
    
    * chore: remove unnecessary duplicate build-deploy-preview.sh file
    
    * fix: Attempt to workaround Netlify cache issues for Lerna repos
    
    * chore: Remove useless debug file
    
    * fix: try to make Gatsby less slow

--- .netlify/build-deploy-preview.sh
+++ .netlify/build-deploy-preview.sh
@@ -20,8 +20,16 @@ mv platform/viewer/dist/* .netlify/www/pwa -v
 # yarn run build:package
 
 # Build && Move Docz Output
-yarn run build:ui:deploy-preview
+# Using local yarn install to prevent Gatsby from needing to access
+# node_modules above the platform/ui folder
+cd platform/ui
+yarn install
+yarn run build
+cd ../..
 mkdir -p ./.netlify/www/ui
 mv platform/ui/.docz/dist/* .netlify/www/ui -v
 
+# Cache all of the node_module dependencies in
+# extensions, modules, and platform packages
+yarn run lerna:cache
 echo 'Nothing left to see here. Go home, folks.'
