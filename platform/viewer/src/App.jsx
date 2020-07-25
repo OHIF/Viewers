6483789ebf76d5e247264c5db1426b25d0ba2362
Erik Ziegler <erik.sweed@gmail.com>
2020-07-25 15:26:08 +0200

    OIDC works

--- platform/viewer/src/App.jsx
+++ platform/viewer/src/App.jsx
@@ -4,9 +4,10 @@ import PropTypes from 'prop-types';
 import i18n from '@ohif/i18n';
 import { I18nextProvider } from 'react-i18next';
 import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import { Provider, connect } from 'react-redux';
 import { OidcProvider, reducer as oidcReducer } from 'redux-oidc';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { withRouter } from 'react-router';
+import { BrowserRouter, HashRouter, Route, Switch } from 'react-router-dom';
 import {
   DialogProvider,
   Modal,
@@ -21,6 +22,7 @@ import {
 // TODO: Should this influence study list?
 import { AppConfigProvider } from '@state';
 import createRoutes from './routes';
+import CallbackPage from './routes/CallbackPage';
 import appInit from './appInit.js';
 import UserManagerContext from './context/UserManagerContext';
 import getUserManagerForOpenIdConnectClient from './utils/getUserManagerForOpenIdConnectClient.js';
@@ -30,6 +32,7 @@ import '@ohif/mode-longitudinal';
 import '@ohif/mode-segmentation';
 
 const store = createStore(oidcReducer);
+window.store = store;
 
 /**
  * ENV Variable to determine routing behavior
@@ -119,7 +122,11 @@ function App({ config, defaultExtensions }) {
                           <SnackbarProvider service={UINotificationService}>
                             <DialogProvider service={UIDialogService}>
                               <ModalProvider modal={Modal} service={UIModalService}>
-                                {appRoutes}
+                                <ConnectedAuthenticatorWithRouter
+                                  appRoutes={appRoutes}
+                                  userManager={userManager}
+                                  oidcAuthority={oidc[0].authority}
+                                />
                               </ModalProvider>
                             </DialogProvider>
                           </SnackbarProvider>
@@ -216,6 +223,127 @@ function _makeAbsoluteIfNecessary(url, base_url) {
 
   return base_url + url;
 }
+
+function Authenticator({
+  appRoutes,
+  userManager,
+  user,
+  oidcAuthority,
+  location
+            }) {
+  const userLoggedIn = userManager && user && !user.expired;
+
+  if (userLoggedIn) {
+    return appRoutes;
+  }
+
+  const { pathname, search } = location;
+
+  if (pathname !== '/callback') {
+    sessionStorage.setItem(
+      'ohif-redirect-to',
+      JSON.stringify({ pathname, search })
+    );
+  }
+
+  return (
+    <Switch>
+      <Route
+        exact
+        path="/silent-refresh.html"
+        onEnter={window.location.reload}
+      />
+      <Route
+        exact
+        path="/logout-redirect"
+        render={() => (
+          <SignoutCallbackComponent
+            userManager={userManager}
+            successCallback={() => console.log('Signout successful')}
+            errorCallback={error => {
+              console.warn(error);
+              console.warn('Signout failed');
+            }}
+          />
+        )}
+      />
+      <Route
+        path="/callback"
+        render={() => <CallbackPage userManager={userManager} />}
+      />
+      <Route
+        path="/login"
+        component={() => {
+          const queryParams = new URLSearchParams(location.search);
+          const iss = queryParams.get('iss');
+          const loginHint = queryParams.get('login_hint');
+          const targetLinkUri = queryParams.get('target_link_uri');
+          if (iss !== oidcAuthority) {
+            console.error(
+              'iss of /login does not match the oidc authority'
+            );
+            return null;
+          }
+
+          userManager.removeUser().then(() => {
+            if (targetLinkUri !== null) {
+              const ohifRedirectTo = {
+                pathname: new URL(targetLinkUri).pathname,
+              };
+              sessionStorage.setItem(
+                'ohif-redirect-to',
+                JSON.stringify(ohifRedirectTo)
+              );
+            } else {
+              const ohifRedirectTo = {
+                pathname: '/',
+              };
+              sessionStorage.setItem(
+                'ohif-redirect-to',
+                JSON.stringify(ohifRedirectTo)
+              );
+            }
+
+            if (loginHint !== null) {
+              userManager.signinRedirect({ login_hint: loginHint });
+            } else {
+              userManager.signinRedirect();
+            }
+          });
+
+          return null;
+        }}
+      />
+      <Route
+        component={() => {
+          userManager.getUser().then(user => {
+            if (user) {
+              userManager.signinSilent();
+            } else {
+              userManager.signinRedirect();
+            }
+          });
+
+          return null;
+        }}
+      />
+    </Switch>
+  );
+}
+
+
+const mapStateToProps = state => {
+  return {
+    user: state.user,
+  };
+};
+
+const ConnectedAuthenticator = connect(
+  mapStateToProps,
+  null
+)(Authenticator);
+
+const ConnectedAuthenticatorWithRouter = withRouter(ConnectedAuthenticator);
 
 export default App;
 
