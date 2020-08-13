07baca0e0511430cddea33257ce2212baa937293
igoroctaviano <igoroctaviano@gmail.com>
2020-08-13 10:10:19 -0300

    Copy config to avoid breaking build

--- platform/ui/src/pages/Colors/BackgroundColor.jsx
+++ platform/ui/src/pages/Colors/BackgroundColor.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import tailwindConfig from '../../../tailwind.config';
+import tailwindConfig from './tailwind.config';
 
 const BackgroundColor = ({ color }) => {
   const getColorValue = () => {
