6ae2c99feea94f6b3d6b69151058661c4fc317cd
igoroctaviano <igoroctaviano@gmail.com>
2020-08-13 11:09:30 -0300

    Update and lock versions

--- platform/ui/gatsby-config.js
+++ platform/ui/gatsby-config.js
@@ -1,7 +1,16 @@
 module.exports = {
   plugins: [
+    'gatsby-theme-docz',
     'gatsby-plugin-react-svg',
     'gatsby-plugin-sass',
     `gatsby-plugin-postcss`,
   ],
 };
+
+exports.createSchemaCustomization = ({ actions }) => {
+  actions.createTypes(`
+    type SitePage implements Node @dontInfer {
+      path: String!
+    }
+  `)
+}
