f41f5abdecfb61d9ce7b827d5ca331d4972055f3
igoroctaviano <igoroctaviano@gmail.com>
2020-08-13 11:29:10 -0300

    Remove scaling config

--- platform/ui/gatsby-config.js
+++ platform/ui/gatsby-config.js
@@ -6,11 +6,3 @@ module.exports = {
     `gatsby-plugin-postcss`,
   ],
 };
-
-exports.createSchemaCustomization = ({ actions }) => {
-  actions.createTypes(`
-    type SitePage implements Node @dontInfer {
-      path: String!
-    }
-  `)
-}
