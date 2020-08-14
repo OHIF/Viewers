ef163cd7025ebf09a2aceb5192ab87ae38d60a0e
Igor Octaviano <igoroctaviano@gmail.com>
2020-08-14 15:25:08 -0300

    Fix broken UI package build (#1990)
    * Use path instead of alias
    
    * Add missing pacakges
    
    * Use path in viewers
    
    * Use path in contextProviders
    
    * Add base to config
    
    * Add conditionals for window
    
    * Copy config to avoid breaking build
    
    * Update and lock versions
    
    * Update lock file
    
    * Remove scaling config
    
    * Fix broken versions of react date
    
    * Fix broken dev

--- platform/ui/src/components/StudyListPagination/StudyListPagination.js
+++ platform/ui/src/components/StudyListPagination/StudyListPagination.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, ButtonGroup, Icon, Typography } from '@ohif/ui';
+import { Button, ButtonGroup, Icon, Typography } from '../';
 
 const StudyListPagination = ({
   onChangePage,
