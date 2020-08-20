a04af40e7e562039b759969d0b7cf5580886abbd
dannyrb <danny.ri.brown@gmail.com>
2020-08-19 22:49:11 -0400

    fix: about menu positioning for viewer template

--- platform/ui/src/components/NavBar/NavBar.jsx
+++ platform/ui/src/components/NavBar/NavBar.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-const stickyClasses = 'sticky top-0 z-20';
+const stickyClasses = 'sticky top-0';
+const notStickyClasses = 'relative';
 
 const NavBar = ({ className, children, isSticky }) => {
   return (
     <div
       className={classnames(
-        'flex flex-row items-center bg-secondary-dark px-3 py-1 min-h-16 border-b-4 border-black',
+        'flex flex-row items-center bg-secondary-dark px-3 py-1 min-h-16 border-b-4 border-black z-20',
         isSticky && stickyClasses,
+        !isSticky && notStickyClasses,
         className
       )}
     >
