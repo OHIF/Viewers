f38979037dca77335609eddcb30797eae50c9d00
James Petts <jamesapetts@gmail.com>
2020-09-03 14:48:32 +0100

    IDC-1532 multiple series search google (#2026)
    * Multiple series search for google cloud adapter.
    
    * Revert IDC config.
    
    * fix: 🐛 Series filtering on multiple series for google
    
    * Revert changes to default config.
    
    * Address reviewers comments.
    
    * Fixed spelling mistakes

--- platform/viewer/public/config/idc.js
+++ platform/viewer/public/config/idc.js
@@ -7,9 +7,11 @@ window.config = function(props) {
     enableGoogleCloudAdapter: true,
     enableGoogleCloudAdapterUI: false,
     showStudyList: true,
+    filterQueryParam: true,
     httpErrorHandler: error => {
       // This is 429 when rejected from the public idc sandbox too often.
       console.warn(error.status);
+
       // Could use services manager here to bring up a dialog/modal if needed.
       console.warn('test, navigate to https://ohif.org/');
       window.location = 'https://ohif.org/';
