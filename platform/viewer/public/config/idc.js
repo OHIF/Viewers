4fdbf5957b0ab9dab0ff70928c5ad9770368a013
James A. Petts <jamesapetts@gmail.com>
2020-09-03 10:54:14 +0100

    fix: 🐛 Series filtering on multiple series for google

--- platform/viewer/public/config/idc.js
+++ platform/viewer/public/config/idc.js
@@ -12,6 +12,7 @@ window.config = function(props) {
     httpErrorHandler: error => {
       // This is 429 when rejected from the public idc sandbox too often.
       console.warn(error.status);
+
       // Could use services manager here to bring up a dialog/modal if needed.
       console.warn('test, navigate to https://ohif.org/');
       window.location = 'https://ohif.org/';
