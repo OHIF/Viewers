bfecf3ecd6ea4abe808e8dffc6aae1747f3c2482
wlongabaugh <wlongabaugh@systemsbiology.org>
2020-11-27 17:28:03 -0800

    debug

--- idc-deploy-shell/gcloud_authenticate.sh
+++ idc-deploy-shell/gcloud_authenticate.sh
@@ -2,11 +2,12 @@ if [ ! -f "deployment.key.json" ]; then
     echo ${DEPLOYMENT_KEY} | base64 --decode --ignore-garbage > deployment.key.json
 fi
 
-gcloud auth activate-service-account --key-file deployment.key.json
-echo "Setting deployment client email to ${DEPLOYMENT_CLIENT_EMAIL}" | sed -e 's/google//'
-gcloud auth list | sed -e 's/google//'
+gcloud auth activate-service-account --key-file deployment.key.json | sed -e 's/gserviceaccount/gsa/'
+echo "Setting deployment client email to ${DEPLOYMENT_CLIENT_EMAIL}" | sed -e 's/gserviceaccount/gsa/'
+gcloud auth list | sed -e 's/gserviceaccount/gsa/'
 echo "Return code for gcloud auth list is $?"
 
 gcloud config set account $DEPLOYMENT_CLIENT_EMAIL
-echo "Setting deployment project to ${DEPLOYMENT_PROJECT_ID}" | sed -e 's/idc//'
+gcloud config list | sed -e 's/gserviceaccount/gsa/'
+echo "Setting deployment project to ${DEPLOYMENT_PROJECT_ID}" | sed -e 's/idc/cid/'
 gcloud config set project "$DEPLOYMENT_PROJECT_ID"
