ad35510785b7f8320e064e89da137a5eb8faa1f1
wlongabaugh <wlongabaugh@systemsbiology.org>
2020-11-27 17:42:45 -0800

    debug

--- idc-deploy-shell/gcloud_authenticate.sh
+++ idc-deploy-shell/gcloud_authenticate.sh
@@ -2,6 +2,8 @@ if [ ! -f "deployment.key.json" ]; then
     echo ${DEPLOYMENT_KEY} | base64 --decode --ignore-garbage > deployment.key.json
 fi
 
+
+sum deployment.key.json
 gcloud auth activate-service-account --key-file deployment.key.json | sed -e 's/gserviceaccount/gsa/'
 echo "Setting deployment client email to ${DEPLOYMENT_CLIENT_EMAIL}" | sed -e 's/gserviceaccount/gsa/'
 gcloud auth list | sed -e 's/gserviceaccount/gsa/'
