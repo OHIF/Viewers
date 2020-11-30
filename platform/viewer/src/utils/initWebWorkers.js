b309918826263c9c7923896318ccc0f1d9a80437
matthiasg <m.goetzke@curasystems.de>
2020-11-30 13:17:27 +0100

    fix: added MAX_CONCURRENCY limit as each webworker costs RAM, and machines with e.g 24 cores crash the process (#2161)

--- platform/viewer/src/utils/initWebWorkers.js
+++ platform/viewer/src/utils/initWebWorkers.js
@@ -2,9 +2,11 @@ import cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
 
 let initialized = false;
 
+const MAX_CONCURRENCY = 6;
+
 export default function initWebWorkers() {
   const config = {
-    maxWebWorkers: Math.max(navigator.hardwareConcurrency - 1, 1),
+    maxWebWorkers: Math.max(Math.min(navigator.hardwareConcurrency - 1, MAX_CONCURRENCY), 1),
     startWebWorkersOnDemand: true,
     taskConfiguration: {
       decodeTask: {
