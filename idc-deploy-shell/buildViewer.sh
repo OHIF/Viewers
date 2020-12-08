49b220bc7b11fa8f6890e922308d60533e9ce7b5
wlongabaugh <wlongabaugh@systemsbiology.org>
2020-12-08 12:03:52 -0800

    Force update

--- idc-deploy-shell/buildViewer.sh
+++ idc-deploy-shell/buildViewer.sh
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# FORCE UPDATE
 
 if [ "${CONFIG_ONLY}" != "True" ]; then
   yarn install
