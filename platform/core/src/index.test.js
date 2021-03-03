df8c4db2ed4ed66c0a03163ea2ad59fb349507dd
igoroctaviano <igoroctaviano@gmail.com>
2021-03-03 20:06:58 -0300

    Update broken test

--- platform/core/src/index.test.js
+++ platform/core/src/index.test.js
@@ -14,6 +14,7 @@ describe('Top level exports', () => {
       'UIModalService',
       'UIDialogService',
       'MeasurementService',
+      'LoggerService',
       //
       'utils',
       'hotkeys',
