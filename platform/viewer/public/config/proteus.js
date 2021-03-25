ca87383741554e9c69e34dcd2d3d348f4e33e3b5
Tomas Kenda <kenda.tomas@gmail.com>
2021-03-25 15:07:43 -0300

    add development scripts

--- platform/viewer/public/config/proteus.js
+++ platform/viewer/public/config/proteus.js
@@ -18,8 +18,25 @@ window.config = {
       },
     ],
   },
-  // Extensions should be able to suggest default values for these?
-  // Or we can require that these be explicitly set
+  whiteLabeling: {
+    /* Optional: Should return a React component to be rendered in the "Logo" section of the application's Top Navigation bar */
+    createLogoComponentFn: function(React) {
+      return React.createElement('a', {
+        target: '_self',
+        rel: 'noopener noreferrer',
+        className: 'header-brand',
+        href: '/',
+        style: {
+          display: 'block',
+          textIndent: '-9999px',
+          background: 'url(/svg-file-hosted-at-domain-root.svg)',
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat',
+          width: '200px',
+        },
+      });
+    },
+  },
   hotkeys: [
     // ~ Global
     {
