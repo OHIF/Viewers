85147607681b00a43cefa96162279f49c14959e4
Christian El Salloum <christian.elsalloum@emtensor.com>
2021-07-16 12:51:16 +0200

    exisiting displaysets in a study are used to initialize _displaySets in the constructor

--- platform/core/src/classes/metadata/StudyMetadata.js
+++ platform/core/src/classes/metadata/StudyMetadata.js
@@ -35,7 +35,7 @@ class StudyMetadata extends Metadata {
         configurable: false,
         enumerable: false,
         writable: false,
-        value: [],
+        value: this.getData().displaySets ? this.getData().displaySets : [], 
       },
       _derivedDisplaySets: {
         configurable: false,
