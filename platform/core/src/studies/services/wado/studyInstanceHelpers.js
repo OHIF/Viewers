27ec17c5b5ac5677ab65d389724dbdd65c1adb8a
Christian El Salloum <christian.elsalloum@emtensor.com>
2021-07-17 20:07:49 +0200

    set SeriesDate and SeriesTime only when they are undefined

--- platform/core/src/studies/services/wado/studyInstanceHelpers.js
+++ platform/core/src/studies/services/wado/studyInstanceHelpers.js
@@ -117,8 +117,10 @@ async function makeSOPInstance(server, study, instance) {
     study.seriesMap[SeriesInstanceUID] = series;
     study.series.push(series);
   } else {
-    series.SeriesDate = naturalizedInstance.SeriesDate;
-    series.SeriesTime = naturalizedInstance.SeriesTime;
+    if (series.SeriesDate == undefined)
+      series.SeriesDate = naturalizedInstance.SeriesDate;
+    if (series.SeriesTime == undefined)
+      series.SeriesTime = naturalizedInstance.SeriesTime;
   }
 
   const wadouri = buildInstanceWadoUrl(
