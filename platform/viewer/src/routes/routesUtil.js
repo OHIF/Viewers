0e62e4bec4dcb957521b9ce8778045c7ba75cf5d
robtoth <34462353+robtoth@users.noreply.github.com>
2021-07-20 00:08:14 -0400

    #1 typo

--- platform/viewer/src/routes/routesUtil.js
+++ platform/viewer/src/routes/routesUtil.js
@@ -42,7 +42,7 @@ const ROUTES_DEF = {
       component: StandaloneRouting,
     },
     list: {
-      path: ['/studylist', '/', '/callback],
+      path: ['/studylist', '/', '/callback'],
       component: StudyListRouting,
       condition: appConfig => {
         return appConfig.showStudyList;
