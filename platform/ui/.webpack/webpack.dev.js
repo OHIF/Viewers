de77afa00e291edbf142a465816ff0893b98696e
Dani Hodovic <dani.hodovic@gmail.com>
2021-09-01 15:28:21 +0200

    Build v2 main

--- platform/ui/.webpack/webpack.dev.js
+++ platform/ui/.webpack/webpack.dev.js
@@ -1,8 +1,23 @@
+const { merge } = require('webpack-merge');
 const path = require('path');
-const webpackCommon = require('./../../../.webpack/webpack.commonjs.js');
+const webpackCommon = require('./../../../.webpack/webpack.base.js');
+const pkg = require('./../package.json');
 const SRC_DIR = path.join(__dirname, '../src');
 const DIST_DIR = path.join(__dirname, '../dist');
+const ROOT_DIR = path.join(__dirname, './..');
 
 module.exports = (env, argv) => {
-  return webpackCommon(env, argv, { SRC_DIR, DIST_DIR });
+  const commonConfig = webpackCommon(env, argv, { SRC_DIR, DIST_DIR });
+  return merge(commonConfig, {
+    output: {
+      path: ROOT_DIR,
+      library: 'ohifUi',
+      libraryTarget: 'umd',
+      filename: pkg.main,
+    },
+    externals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+    },
+  })
 };
