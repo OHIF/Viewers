06b1ad82c917e77235b3f25fb6849c14739e67e4
Alireza <ar.sedghi@gmail.com>
2022-01-03 22:12:04 -0500

    feat: add Icon and input docs

--- platform/ui/src/components/Icon/Icon.jsx
+++ platform/ui/src/components/Icon/Icon.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import getIcon from './getIcon';
 
-const Icon = ({ name, ...otherProps }) => {
+const Icon = ({ name, className, ...otherProps }) => {
+  if (className) {
+    return (
+      <div className={className}>
+        {getIcon(name, className, { ...otherProps })}
+      </div>
+    );
+  }
+
   return <React.Fragment>{getIcon(name, { ...otherProps })}</React.Fragment>;
 };
 
 Icon.propTypes = {
   name: PropTypes.string.isRequired,
+  className: PropTypes.string,
 };
 
 export default Icon;
