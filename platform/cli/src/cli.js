413be131d1d32d791ae06961d4340975818365cd
Alireza <ar.sedghi@gmail.com>
2022-01-13 14:44:06 -0500

    feat: add copying template files

--- platform/cli/src/cli.js
+++ platform/cli/src/cli.js
@@ -8,21 +8,21 @@ export function cli(argv) {
   program.version('0.0.1').description('OHIF CLI');
 
   // For debugging
-  program
-    .command('create-extension <name>')
-    .description('Create a new extension')
-    .action((name) => {
-      createExtension({ name });
-    });
-
   // program
-  //   .command('create-extension')
+  //   .command('create-extension <name>')
   //   .description('Create a new extension')
-  //   .action((name) => {
-  //     prompt(QUESTIONS.createExtension).then((answers) => {
-  //       createExtension(answers);
-  //     });
+  //   .action(name => {
+  //     createExtension({ name });
   //   });
+
+  program
+    .command('create-extension')
+    .description('Create a new extension')
+    .action(name => {
+      prompt(QUESTIONS.createExtension).then(answers => {
+        createExtension(answers);
+      });
+    });
 
   program.parse(process.argv);
 }
