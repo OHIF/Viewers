a954aad683dcba16f57c379b3acad2ce0b02a5bc
Alireza <ar.sedghi@gmail.com>
2022-01-14 15:51:04 -0500

    feat: Add list of tasks to add-mode

--- platform/cli/src/commands/addMode.js
+++ platform/cli/src/commands/addMode.js
@@ -1,3 +1,6 @@
+import Listr from 'listr';
+import chalk from 'chalk';
+
 import installNPMPackage from './utils/installNPMPackage.js';
 import getPackageVersion from './utils/getPackageVersion.js';
 import readPluginConfigFile from './utils/readPluginConfigFile.js';
@@ -7,26 +10,44 @@ import writePluginConfig from './utils/writePluginConfig.js';
 export default async function addMode(packageName, version) {
   console.log('Adding ohif mode...');
   console.log(
-    'Note: There is currently no validation that this npm package is an ohif-extension.'
+    '%s There is currently no validation that this npm package is an ohif-extension.',
+    chalk.yellow.bold('Warning')
   );
-  await await installNPMPackage(packageName, version);
 
-  // Find the version actually installed using yarn info, as version is optional
-  version = await getPackageVersion(packageName);
+  const tasks = new Listr(
+    [
+      {
+        title: `Installing npm package: ${packageName}`,
+        task: async () => await installNPMPackage(packageName, version),
+      },
+      {
+        title: 'Adding Mode to the Config file',
+        task: async () => {
+          const installedVersion = await getPackageVersion(packageName);
+          const pluginConfig = readPluginConfigFile();
 
-  const pluginConfig = readPluginConfigFile();
+          if (!pluginConfig) {
+            pluginConfig = {
+              extensions: [],
+              modes: [],
+            };
+          }
 
-  if (!pluginConfig) {
-    pluginConfig = {
-      extensions: [],
-      modes: [],
-    };
-  }
+          addModeToConfig(pluginConfig, {
+            packageName,
+            version: installedVersion,
+          });
+          writePluginConfig(pluginConfig);
+        },
+      },
+    ],
+    {
+      exitOnError: true,
+    }
+  );
 
-  addModeToConfig(pluginConfig, { packageName, version });
-  writePluginConfig(pluginConfig);
+  await tasks.run();
 
   // TODO parse mode and add extensions
-
-  console.log('Mode Added');
+  console.log('%s Mode Added', chalk.green.bold('DONE'));
 }
