e518d8d30b4e2ee593bbf9bc6a0974ec398fb89c
Davide Punzo <punzodavide@hotmail.it>
2022-02-03 09:33:02 +0100

    IDC (Re #1215): Feat/v2 annotations (#2658)
    * Add scoord3d parse / display tool to v2
    
    * Update measurement panel with sr measurements
    
    * Update measurement panel to display read only items
    
    * Cleanup debugger statement
    
    * Check for empty measurement
    
    * fix SCOORD3D coordinates
    
    * Update getRenderableData.js
    
    * fix scoord3d coordinate transformation
    
    * implement logic to detect the ReferencedSOPInstanceUID if missing in the measurement. Fix also world to IJ coordinates.
    
    * Fix broken unit test / import
    
    * fix comment style
    
    * Init tools only once
    
    * restore default config dcmjs server
    
    Co-authored-by: igoroctaviano <igoroctaviano@gmail.com>

--- extensions/dicom-html/src/OHIFDicomHtmlSopClassHandler.js
+++ extensions/dicom-html/src/OHIFDicomHtmlSopClassHandler.js
@@ -5,11 +5,12 @@ const SOP_CLASS_UIDS = {
   BASIC_TEXT_SR: '1.2.840.10008.5.1.4.1.1.88.11',
   ENHANCED_SR: '1.2.840.10008.5.1.4.1.1.88.22',
   COMPREHENSIVE_SR: '1.2.840.10008.5.1.4.1.1.88.33',
+  COMPREHENSIVE_3D_SR: '1.2.840.10008.5.1.4.1.1.88.34',
   PROCEDURE_LOG_STORAGE: '1.2.840.10008.5.1.4.1.1.88.40',
   MAMMOGRAPHY_CAD_SR: '1.2.840.10008.5.1.4.1.1.88.50',
   CHEST_CAD_SR: '1.2.840.10008.5.1.4.1.1.88.65',
   X_RAY_RADIATION_DOSE_SR: '1.2.840.10008.5.1.4.1.1.88.67',
-  ACQUISITION_CONTEXT_SR_STORAGE: '1.2.840.10008.5.1.4.1.1.88.71',
+  ACQUISITION_CONTEXT_SR_STORAGE: '1.2.840.10008.5.1.4.1.1.88.71'
 };
 
 const sopClassUIDs = Object.values(SOP_CLASS_UIDS);
@@ -46,6 +47,8 @@ const OHIFDicomHtmlSopClassHandler = {
       SeriesTime,
       SeriesNumber,
       authorizationHeaders,
+      sopClassUids: sopClassUIDs,
+      images: series._instances,
     };
   },
 };
