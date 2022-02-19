b768c0b998bcec1bdb9deed7217d9d0501d4b2ce
Bill Wallace <wayfarer3130@gmail.com>
2022-02-18 19:54:30 -0500

    Add a basic e2e test for video display

---
+++ platform/viewer/cypress/integration/OHIFVideoDisplay.spec.js
@@ -0,0 +1,24 @@
+describe('OHIF Video Display', function () {
+  before(() => {
+    cy.openStudyInViewer(
+      '2.25.96975534054447904995905761963464388233'
+    );
+  });
+
+  beforeEach(function () {
+    cy.resetViewport().wait(50);
+  });
+
+  it('checks if series thumbnails are being displayed', function () {
+    cy.get('[data-cy="study-browser-thumbnail"]')
+      .its('length')
+      .should('be.gt', 1);
+  });
+
+  it('performs double-click to load thumbnail in active viewport', () => {
+    cy.get('[data-cy="study-browser-thumbnail"]:nth-child(2)').dblclick();
+
+    //const expectedText = 'Ser: 3';
+    //cy.get('@viewportInfoBottomLeft').should('contains.text', expectedText);
+  });
+});
