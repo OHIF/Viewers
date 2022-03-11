db16857dbc77a761b35e587ea7136ad1f0bf5196
Bill Wallace <wayfarer3130@gmail.com>
2022-03-11 11:08:24 -0500

    PR comments - added params description

--- platform/core/src/utils/metadataProvider/fetchPaletteColorLookupTableData.js
+++ platform/core/src/utils/metadataProvider/fetchPaletteColorLookupTableData.js
@@ -1,15 +1,15 @@
-import { api } from 'dicomweb-client';
-import DICOMWeb from '../../DICOMWeb';
-import str2ab from '../str2ab';
-
-import errorHandler from '../../errorHandler';
-
 /**
  * Gets the palette color data for the specified tag - red/green/blue,
  * either from the given UID or from the tag itself.
  * Returns an array if the data is immediately available, or a promise
  * which resolves to the data if the data needs to be loaded.
  * Returns undefined if the palette isn't specified.
+ *
+ * @param {*} item containing the palette colour data and description
+ * @param {*} tag is the tag for the palette data
+ * @param {*} descriptorTag is the tag for the descriptor
+ * @returns Array view containing the palette data, or a promise to return one.
+ * Returns undefined if the palette data is absent.
  */
 export default function fetchPaletteColorLookupTableData(
   item, tag, descriptorTag
@@ -17,7 +17,7 @@ export default function fetchPaletteColorLookupTableData(
   const { PaletteColorLookupTableUID } = item;
   const paletteData = item[tag];
   if (paletteData === undefined && PaletteColorLookupTableUID === undefined) return;
-  // TODO - read UID and cache by UID
+  // performance optimization - read UID and cache by UID
   return _getPaletteColor(
     item[tag],
     item[descriptorTag]
