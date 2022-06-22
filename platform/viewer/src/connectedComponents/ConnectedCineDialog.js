fcae61000bba775f7aa8866cb4479111a69f0e02
horner <horner@mieweb.com>
2022-06-22 06:20:51 -0400

    Cine FrameRate now initializes from dicom metadata

--- platform/viewer/src/connectedComponents/ConnectedCineDialog.js
+++ platform/viewer/src/connectedComponents/ConnectedCineDialog.js
@@ -20,9 +20,19 @@ const mapStateToProps = state => {
   const { cine } = viewportSpecificData[activeViewportIndex] || {};
   const dom = commandsManager.runCommand('getActiveViewportEnabledElement');
 
+  let fr = viewportSpecificData[activeViewportIndex].frameRate;
+  let playnow = false;
+
+  if (fr) {
+    fr = 1000/fr;
+    playnow = true;
+  } else {
+    fr = 24;
+  }
+
   const cineData = cine || {
     isPlaying: false,
-    cineFrameRate: 24,
+    cineFrameRate: fr,
   };
 
   // New props we're creating?
