45cf4083ed60e8f994eadb829f795fe1f7ce82e1
Bill Wallace <wayfarer3130@gmail.com>
2022-09-19 15:33:08 -0400

    Add consumer type

--- platform/core/src/types/Consumer.ts
+++ platform/core/src/types/Consumer.ts
@@ -1,3 +1,5 @@
-import Params from './ParamsType';
-type Consumer = (props: Params) => void;
+/**
+ * Just a function that consumes a single argument, with no return.
+ */
+type Consumer = (props: Record<string, unknown>) => void;
 export default Consumer;
