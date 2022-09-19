bad1a3a8b53def18a6f628a7b03d60f66e5c896a
Alireza <ar.sedghi@gmail.com>
2022-09-19 12:39:25 -0400

    apply review

--- platform/ui/src/components/StudyListPagination/StudyListPagination.tsx
+++ platform/ui/src/components/StudyListPagination/StudyListPagination.tsx
@@ -35,7 +35,7 @@ const StudyListPagination = ({
         <div className="flex justify-between">
           <div className="flex items-center">
             <Select
-              id={'rows-per-page'}
+              id="rows-per-page"
               className="relative mr-3 w-16 border-primary-main"
               options={ranges}
               value={selectedRange}
