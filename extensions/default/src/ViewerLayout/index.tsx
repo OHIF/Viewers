1bcc9ccd89b66873cba1fa7e2481dba43e7e89cb
Ouwen Huang <ouwen.huang@duke.edu>
2022-09-28 15:46:58 -0400

    feat: added default close option to viewer layout for sidebar ui (#2961)

--- extensions/default/src/ViewerLayout/index.tsx
+++ extensions/default/src/ViewerLayout/index.tsx
@@ -86,6 +86,8 @@ function ViewerLayout({
   // From Modes
   leftPanels,
   rightPanels,
+  leftPanelDefaultClosed,
+  rightPanelDefaultClosed,
   viewports,
   ViewportGridComp,
 }) {
@@ -209,7 +211,9 @@ function ViewerLayout({
           <ErrorBoundary context="Left Panel">
             <SidePanel
               side="left"
-              defaultComponentOpen={leftPanelComponents[0].name}
+              defaultComponentOpen={
+                leftPanelDefaultClosed ? null : leftPanelComponents[0].name
+              }
               childComponents={leftPanelComponents}
             />
           </ErrorBoundary>
@@ -230,7 +234,9 @@ function ViewerLayout({
           <ErrorBoundary context="Right Panel">
             <SidePanel
               side="right"
-              defaultComponentOpen={rightPanelComponents[0].name}
+              defaultComponentOpen={
+                rightPanelDefaultClosed ? null : rightPanelComponents[0].name
+              }
               childComponents={rightPanelComponents}
             />
           </ErrorBoundary>
@@ -249,6 +255,8 @@ ViewerLayout.propTypes = {
   // From modes
   leftPanels: PropTypes.array,
   rightPanels: PropTypes.array,
+  leftPanelDefaultClosed: PropTypes.bool.isRequired,
+  rightPanelDefaultClosed: PropTypes.bool.isRequired,
   /** Responsible for rendering our grid of viewports; provided by consuming application */
   children: PropTypes.oneOfType([PropTypes.node, PropTypes.func]).isRequired,
 };
@@ -256,6 +264,8 @@ ViewerLayout.propTypes = {
 ViewerLayout.defaultProps = {
   leftPanels: [],
   rightPanels: [],
+  leftPanelDefaultClosed: false,
+  rightPanelDefaultClosed: false,
 };
 
 export default ViewerLayout;
