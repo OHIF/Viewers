08c58b0634690c179401d47c39c0d4252b9b8284
wangxuan <wx_0322@163.com>
2022-11-11 02:29:55 +0800

    fix: deprecated syntax of lower substr (#3013)
    * fixed import error syntax
    
    * replace lower.substr with lower.substring
    
    * remove redundant parameter
    
    * fixed import syntax error
    
    * fixed import syntax error

--- platform/viewer/src/index.js
+++ platform/viewer/src/index.js
@@ -2,7 +2,7 @@
  * Entry point for development and production PWA builds.
  */
 import 'regenerator-runtime/runtime';
-import App from './App.tsx';
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
