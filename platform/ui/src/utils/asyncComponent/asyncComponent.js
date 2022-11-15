ca86c2106939f7edf2ebc03724f76ea5d77cb67f
igoroctaviano <igoroctaviano@gmail.com>
2022-11-15 06:55:20 -0300

    Fix retry function typo

--- platform/ui/src/utils/asyncComponent/asyncComponent.js
+++ platform/ui/src/utils/asyncComponent/asyncComponent.js
@@ -3,18 +3,22 @@ import { ErrorPage } from '@ohif/ui';
 
 export const retryImport = (fn, retriesLeft = 5, interval = 1000) =>
   new Promise((resolve, reject) => {
-    fn().then(resolve).catch((error) => {
-      setTimeout(() => {
-        if (retriesLeft === 1) {
-          /* reject('maximum retries exceeded'); */
-          reject(error);
-          return;
-        }
+    fn()
+      .then(resolve)
+      .catch(error => {
+        console.error('Failed to import:', fn, error);
 
-        /* Passing on "reject" is the important part */
-        retry(fn, retriesLeft - 1, interval).then(resolve, reject);
-      }, interval);
-    });
+        setTimeout(() => {
+          if (retriesLeft === 1) {
+            /* reject('maximum retries exceeded'); */
+            reject(error);
+            return;
+          }
+
+          /* Passing on "reject" is the important part */
+          retryImport(fn, retriesLeft - 1, interval).then(resolve, reject);
+        }, interval);
+      });
   });
 
 const onError = (error, setState) => setState({ component: ErrorPage });
