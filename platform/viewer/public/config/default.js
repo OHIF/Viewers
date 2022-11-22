ccd371f113c6c0e28283ac1187eca79452b35212
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2022-11-21 23:15:51 -0500

    fix(env): updates default config

--- platform/viewer/public/config/default.js
+++ platform/viewer/public/config/default.js
@@ -7,11 +7,11 @@ window.config = {
     dicomWeb: [
       {
         wadoUriRoot:
-          'https://healthcare.googleapis.com/v1/projects/nih-nci-ccr-dicom-sbox-34ad/locations/us/datasets/mib-air-dicom-dev-dataset/dicomStores/mib-air-dicom-dev-data-store/dicomWeb',
+          'https://healthcare.googleapis.com/v1/projects/nih-nci-ccr-mib-air-dev-2a0b/locations/us/datasets/mib-air-dicom-dataset-dev/dicomStores/mib-air-dicom-datastore-dev/dicomWeb',
         qidoRoot:
-          'https://healthcare.googleapis.com/v1/projects/nih-nci-ccr-dicom-sbox-34ad/locations/us/datasets/mib-air-dicom-dev-dataset/dicomStores/mib-air-dicom-dev-data-store/dicomWeb',
+          'https://healthcare.googleapis.com/v1/projects/nih-nci-ccr-mib-air-dev-2a0b/locations/us/datasets/mib-air-dicom-dataset-dev/dicomStores/mib-air-dicom-datastore-dev/dicomWeb',
         wadoRoot:
-          'https://healthcare.googleapis.com/v1/projects/nih-nci-ccr-dicom-sbox-34ad/locations/us/datasets/mib-air-dicom-dev-dataset/dicomStores/mib-air-dicom-dev-data-store/dicomWeb',
+          'https://healthcare.googleapis.com/v1/projects/nih-nci-ccr-mib-air-dev-2a0b/locations/us/datasets/mib-air-dicom-dataset-dev/dicomStores/mib-air-dicom-datastore-dev/dicomWeb',
         qidoSupportsIncludeField: true,
         imageRendering: 'wadors',
         thumbnailRendering: 'wadors',
