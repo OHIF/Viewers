6c2f7d003cc7277f452ffd868170388cff9dcdf1
wang xuan <wx_0322@163.com>
2023-02-06 13:59:09 +0800

    汉化

--- platform/i18n/src/locales/zh/index.js
+++ platform/i18n/src/locales/zh/index.js
@@ -5,6 +5,16 @@ import Header from './Header.json';
 import MeasurementTable from './MeasurementTable.json';
 import StudyList from './StudyList.json';
 import UserPreferencesModal from './UserPreferencesModal.json';
+import StudyBrowser from './StudyBrowser.json';
+import SidePanel from './SidePanel.json';
+import Modes from './Modes.json';
+import PatientInfo from './PatientInfo.json';
+import Notification from './Notification.json';
+import ContextMenu from './ContextMenu.json';
+import Dialog from './Dialog.json';
+import Modals from './Modals.json';
+import Local from './Local.json';
+import ErrorBoundary from './ErrorBoundary.json';
 
 export default {
   zh: {
@@ -15,5 +25,15 @@ export default {
     MeasurementTable,
     StudyList,
     UserPreferencesModal,
+    StudyBrowser,
+    SidePanel,
+    Modes,
+    PatientInfo,
+    Notification,
+    ContextMenu,
+    Dialog,
+    Modals,
+    Local,
+    ErrorBoundary,
   },
 };
