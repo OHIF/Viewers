ac20c045f4f8ce221c2ed95a237249cb4e6e4bf2
wx0322 <wx_0322@163.com>
2023-02-06 22:40:06 +0800

    添加汉化文件

--- platform/i18n/src/locales/zh/index.js
+++ platform/i18n/src/locales/zh/index.js
@@ -1,19 +1,64 @@
+<<<<<<< HEAD
+import AboutModal from './AboutModal.json';
 import Buttons from './Buttons.json';
 import CineDialog from './CineDialog.json';
 import Common from './Common.json';
+import DatePicker from './DatePicker.json';
+=======
+import Buttons from './Buttons.json';
+import CineDialog from './CineDialog.json';
+import Common from './Common.json';
+>>>>>>> a706f0f71 (first commit)
 import Header from './Header.json';
 import MeasurementTable from './MeasurementTable.json';
 import StudyList from './StudyList.json';
 import UserPreferencesModal from './UserPreferencesModal.json';
+<<<<<<< HEAD
+import ViewportDownloadForm from './ViewportDownloadForm.json';
+
+export default { 
+  'zh': {
+    AboutModal,
+    Buttons,
+    CineDialog,
+    Common,
+    DatePicker,
+=======
+import StudyBrowser from './StudyBrowser.json';
+import SidePanel from './SidePanel.json';
+import Modes from './Modes.json';
+import PatientInfo from './PatientInfo.json';
+import Notification from './Notification.json';
+import ContextMenu from './ContextMenu.json';
+import Dialog from './Dialog.json';
+import Modals from './Modals.json';
+import Local from './Local.json';
+import ErrorBoundary from './ErrorBoundary.json';
 
 export default {
   zh: {
     Buttons,
     CineDialog,
     Common,
+>>>>>>> a706f0f71 (first commit)
     Header,
     MeasurementTable,
     StudyList,
     UserPreferencesModal,
+<<<<<<< HEAD
+    ViewportDownloadForm,
+  }
+=======
+    StudyBrowser,
+    SidePanel,
+    Modes,
+    PatientInfo,
+    Notification,
+    ContextMenu,
+    Dialog,
+    Modals,
+    Local,
+    ErrorBoundary,
   },
+>>>>>>> a706f0f71 (first commit)
 };
