f00d175c8aef1fb2dd02121c20d189305596f10c
Daniel <schwartzray8@gmail.com>
2023-02-24 13:25:55 -0500

    hot key validation errors resolved

--- platform/ui/src/components/HotkeysPreferences/HotkeysPreferences.jsx
+++ platform/ui/src/components/HotkeysPreferences/HotkeysPreferences.jsx
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { HotkeyField, Typography } from '../';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-
+import { HotkeyField, Typography } from '../';
 
 /* TODO: Move these configs and utils to core? */
 import { MODIFIER_KEYS } from './hotkeysConfig';
-import { validate, splitHotkeyDefinitionsAndCreateTuples } from './utils';
+import { extractInfoFromError, splitHotkeyDefinitionsAndCreateTuples, validate } from './utils';
 
-const HotkeysPreferences = ({ disabled, hotkeyDefinitions, errors: controlledErrors, onChange, hotkeysModule }) => {
+const HotkeysPreferences = ({
+  disabled,
+  hotkeyDefinitions,
+  errors: controlledErrors,
+  onChange,
+  hotkeysModule,
+}) => {
   const { t } = useTranslation('UserPreferencesModal');
 
   const visibleHotkeys = Object.keys(hotkeyDefinitions)
@@ -27,11 +32,30 @@ const HotkeysPreferences = ({ disabled, hotkeyDefinitions, errors: controlledErr
   }
 
   const onHotkeyChangeHandler = (id, definition) => {
-    const { error } = validate({
+    if (definition.keys.join("+") === hotkeyDefinitions[id].keys.join("+")) {
+      return
+    }
+
+    let {error, currentErrors} = validate({
       commandName: id,
       pressedKeys: definition.keys,
       hotkeys: hotkeyDefinitions,
+      currentErrors: {currentErrors: errors}
     });
+    
+    // Make sure new errors are consistent with old errors
+    Object.keys(currentErrors.currentErrors).forEach((key) => {
+      if (error && currentErrors.currentErrors[key]) {
+        const [errorToolName, errorKey] = extractInfoFromError(currentErrors.currentErrors[key]);
+        const [newErrorToolName, newErrorKey] = extractInfoFromError(error);
+        if (newErrorKey === errorKey && newErrorToolName !== errorToolName) {
+          error = error.replace(
+            `"${newErrorToolName}"`,
+            `"${errorToolName}"`
+          );
+        }
+      }
+    })
 
     setErrors(prevState => {
       const errors = { ...prevState, [id]: error };
@@ -42,51 +66,74 @@ const HotkeysPreferences = ({ disabled, hotkeyDefinitions, errors: controlledErr
   };
 
   return (
-    <div className='flex flex-row justify-center'>
-      <div className='flex flex-row justify-evenly w-full'>
+    <div className="flex flex-row justify-center">
+      <div className="flex flex-row justify-evenly w-full">
         {splitedHotkeys.map((hotkeys, index) => {
           return (
-            <div key={`HotkeyGroup@${index}`} className='flex flex-row'>
-              <div className='p-2 text-right flex flex-col'>
+            <div key={`HotkeyGroup@${index}`} className="flex flex-row">
+              <div className="p-2 text-right flex flex-col">
                 {hotkeys.map((hotkey, hotkeyIndex) => {
                   const [id, definition] = hotkey;
                   const isFirst = hotkeyIndex === 0;
                   const error = errors[id];
 
-                  const onChangeHandler = keys => onHotkeyChangeHandler(id, { ...definition, keys });
+                  const onChangeHandler = keys =>
+                    onHotkeyChangeHandler(id, { ...definition, keys });
 
                   return (
-                    <div key={`HotkeyItem@${hotkeyIndex}`} className='flex flex-row justify-end mb-2'>
-                      <div className='flex flex-col items-center'>
+                    <div
+                      key={`HotkeyItem@${hotkeyIndex}`}
+                      className="flex flex-row justify-end mb-2"
+                    >
+                      <div className="flex flex-col items-center">
                         <Typography
-                          variant='subtitle'
-                          className={classNames('pr-6 w-full text-right text-primary-light', !isFirst && 'hidden')}
+                          variant="subtitle"
+                          className={classNames(
+                            'pr-6 w-full text-right text-primary-light',
+                            !isFirst && 'hidden'
+                          )}
                         >
                           {t('Function')}
-                      </Typography>
+                        </Typography>
                         <Typography
-                          variant='subtitle'
-                          className={classNames('pr-6 h-full flex flex-row items-center whitespace-nowrap', isFirst && 'mt-5')}>
+                          variant="subtitle"
+                          className={classNames(
+                            'pr-6 h-full flex flex-row items-center whitespace-nowrap',
+                            isFirst && 'mt-5'
+                          )}
+                        >
                           {definition.label}
                         </Typography>
                       </div>
-                      <div className='flex flex-col'>
+                      <div className="flex flex-col">
                         <Typography
-                          variant='subtitle'
-                          className={classNames('pr-6 pl-0 text-left text-primary-light', !isFirst && 'hidden')}
+                          variant="subtitle"
+                          className={classNames(
+                            'pr-6 pl-0 text-left text-primary-light',
+                            !isFirst && 'hidden'
+                          )}
                         >
                           {t('Shortcut')}
-                      </Typography>
-                        <div className={classNames('flex flex-col w-32', isFirst && 'mt-5')}>
+                        </Typography>
+                        <div
+                          className={classNames(
+                            'flex flex-col w-32',
+                            isFirst && 'mt-5'
+                          )}
+                        >
                           <HotkeyField
                             disabled={disabled}
                             keys={definition.keys}
                             modifierKeys={MODIFIER_KEYS}
                             onChange={onChangeHandler}
                             hotkeys={hotkeysModule}
-                            className='text-lg h-8'
+                            className="text-lg h-8"
                           />
-                          {error && <span className='p-2 text-left text-red-600 text-sm'>{error}</span>}
+                          {error && (
+                            <span className="p-2 text-left text-red-600 text-sm">
+                              {error}
+                            </span>
+                          )}
                         </div>
                       </div>
                     </div>
@@ -101,7 +148,7 @@ const HotkeysPreferences = ({ disabled, hotkeyDefinitions, errors: controlledErr
   );
 };
 
-const noop = () => { };
+const noop = () => {};
 
 HotkeysPreferences.propTypes = {
   onChange: PropTypes.func,
@@ -113,12 +160,12 @@ HotkeysPreferences.propTypes = {
     unpause: PropTypes.func.isRequired,
     startRecording: PropTypes.func.isRequired,
     record: PropTypes.func.isRequired,
-  }).isRequired
+  }).isRequired,
 };
 
 HotkeysPreferences.defaultProps = {
   onChange: noop,
-  disabled: false
+  disabled: false,
 };
 
 export default HotkeysPreferences;
--- platform/ui/src/components/HotkeysPreferences/utils.js
+++ platform/ui/src/components/HotkeysPreferences/utils.js
@@ -22,28 +22,92 @@ const splitHotkeyDefinitionsAndCreateTuples = hotkeyDefinitions => {
 };
 
 /**
+ * Remove validation errors as necessary
+ *
+ * @param {Object} currentErrors
+ * @param {array} pressedKeys new keys
+ * @param {string} id id
+ * @param {array} arguments.hotkeys current hotkeys
+ * @returns {Object} {error} validation error
+ */
+const removeErrors = (currentErrors, pressedKeys, id, hotkeys) => {
+  const pressedLabel = hotkeys[id].label;
+  let newLabel;
+  if (Object.keys(currentErrors.currentErrors).length) {
+    // First delete the error once we delete the error
+    Object.keys(currentErrors.currentErrors).every((key) => {
+      if (currentErrors.currentErrors[key]) {
+        const [errorLabel, errorKeys] = extractInfoFromError(currentErrors.currentErrors[key]);
+        if (errorLabel === pressedLabel && pressedKeys.join("+") !== errorKeys) {
+          hotkeys[key]
+          newLabel = hotkeys[key].label
+          currentErrors.currentErrors[key] = undefined
+          return false
+        }
+      }
+      return true
+    })
+    // Then we relabel old errors so that all duplicate keys have the same error
+    Object.keys(currentErrors.currentErrors).forEach((key) => {
+      const error = currentErrors.currentErrors[key]
+      if (error) {
+        const [errorLabel, errorKeys] = extractInfoFromError(error);
+        if (errorLabel === pressedLabel && pressedKeys.join("+") !== errorKeys) { 
+          currentErrors.currentErrors[key] = currentErrors.currentErrors[key].replace(
+            `"${errorLabel}"`,
+            `"${newLabel}"`
+          )
+        }
+      }
+    })
+  }
+  
+  return {currentErrors: currentErrors}
+}
+
+/**
  * Validate a hotkey change
  *
  * @param {Object} arguments
  * @param {string} arguments.commandName command name or id
  * @param {array} arguments.pressedKeys new keys
  * @param {array} arguments.hotkeys current hotkeys
+ * @param {Object} currentErrors
  * @returns {Object} {error} validation error
  */
-const validate = ({ commandName, pressedKeys, hotkeys }) => {
+const validate = ({ commandName, pressedKeys, hotkeys, currentErrors }) => {
+  
+  const updatedErrors = removeErrors(currentErrors, pressedKeys, commandName, hotkeys)
   for (const validator of hotkeysValidators) {
     const validation = validator({
       commandName,
       pressedKeys,
-      hotkeys,
+      hotkeys
     });
-
+    
     if (validation && validation.error) {
-      return validation;
+      return {...validation, ...updatedErrors};
     }
   }
-
-  return { error: undefined };
+  return { error: undefined, ...updatedErrors };
 };
 
-export { validate, splitHotkeyDefinitionsAndCreateTuples };
+/**
+ * Extract relevant toolName and key data from a validation error
+ *
+ * @param {Object} error {error}
+ * @returns {array} [toolName, key] toolName and key from error
+ */
+const extractInfoFromError = error => {
+  const regex = /"([^"]+)"[^"]+"([^"]+)"/;
+  const match = error.match(regex);
+  if (match !== null) {
+    const toolName = match[1];
+    const key = match[2];
+    return [toolName, key];
+  } else {
+    return null;
+  }
+};
+
+export { validate, splitHotkeyDefinitionsAndCreateTuples, extractInfoFromError };
