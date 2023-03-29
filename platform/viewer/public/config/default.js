6845a49f642b0611004695c840d3dff48833871c
Alireza <ar.sedghi@gmail.com>
2023-03-29 09:26:18 -0400

    add review comments

--- platform/viewer/public/config/default.js
+++ platform/viewer/public/config/default.js
@@ -18,6 +18,8 @@ window.config = {
   maxNumRequests: {
     interaction: 100,
     thumbnail: 75,
+    // Prefetch number is dependent on the http protocol. For http 2 or
+    // above, the number of requests can be go a lot higher.
     prefetch: 25,
   },
   // filterQueryParam: false,
