f1f05cf0b7b29a528345b59993fa040a79486132
Alireza <ar.sedghi@gmail.com>
2023-03-29 22:04:45 -0400

    feat(tmtv): add more stages to pt/ct

--- extensions/tmtv/src/getHangingProtocolModule.js
+++ extensions/tmtv/src/getHangingProtocolModule.js
@@ -1,3 +1,16 @@
+import {
+  ctAXIAL,
+  ctCORONAL,
+  ctSAGITTAL,
+  fusionAXIAL,
+  fusionCORONAL,
+  fusionSAGITTAL,
+  mipSAGITTAL,
+  ptAXIAL,
+  ptCORONAL,
+  ptSAGITTAL,
+} from './utils/hpViewports';
+
 const ptCT = {
   id: '@ohif/extension-tmtv.hangingProtocolModule.ptCT',
   locked: true,
@@ -173,425 +186,110 @@ const ptCT = {
         },
       },
       viewports: [
-        {
-          viewportOptions: {
-            viewportId: 'ctAXIAL',
-            viewportType: 'volume',
-            orientation: 'axial',
-            toolGroupId: 'ctToolGroup',
-            initialImageOptions: {
-              // index: 5,
-              preset: 'first', // 'first', 'last', 'middle'
-            },
-            syncGroups: [
-              {
-                type: 'cameraPosition',
-                id: 'axialSync',
-                source: true,
-                target: true,
-              },
-              {
-                type: 'voi',
-                id: 'ctWLSync',
-                source: true,
-                target: true,
-              },
-            ],
-          },
-          displaySets: [
-            {
-              id: 'ctDisplaySet',
-            },
-          ],
-        },
-        {
-          viewportOptions: {
-            viewportId: 'ctSAGITTAL',
-            viewportType: 'volume',
-            orientation: 'sagittal',
-            toolGroupId: 'ctToolGroup',
-            syncGroups: [
-              {
-                type: 'cameraPosition',
-                id: 'sagittalSync',
-                source: true,
-                target: true,
-              },
-              {
-                type: 'voi',
-                id: 'ctWLSync',
-                source: true,
-                target: true,
-              },
-            ],
-          },
-          displaySets: [
-            {
-              id: 'ctDisplaySet',
-            },
-          ],
-        },
-        {
-          viewportOptions: {
-            viewportId: 'ctCORONAL',
-            viewportType: 'volume',
-            orientation: 'coronal',
-            toolGroupId: 'ctToolGroup',
-            syncGroups: [
-              {
-                type: 'cameraPosition',
-                id: 'coronalSync',
-                source: true,
-                target: true,
-              },
-              {
-                type: 'voi',
-                id: 'ctWLSync',
-                source: true,
-                target: true,
-              },
-            ],
-          },
-          displaySets: [
-            {
-              id: 'ctDisplaySet',
-            },
-          ],
-        },
-        {
-          viewportOptions: {
-            viewportId: 'ptAXIAL',
-            viewportType: 'volume',
-            background: [1, 1, 1],
-            orientation: 'axial',
-            toolGroupId: 'ptToolGroup',
-            initialImageOptions: {
-              // index: 5,
-              preset: 'first', // 'first', 'last', 'middle'
-            },
-            syncGroups: [
-              {
-                type: 'cameraPosition',
-                id: 'axialSync',
-                source: true,
-                target: true,
-              },
-              {
-                type: 'voi',
-                id: 'ptWLSync',
-                source: true,
-                target: true,
-              },
-              {
-                type: 'voi',
-                id: 'ptFusionWLSync',
-                source: true,
-                target: false,
-              },
-            ],
-          },
-          displaySets: [
-            {
-              options: {
-                voi: {
-                  windowWidth: 5,
-                  windowCenter: 2.5,
-                },
-                voiInverted: true,
-              },
-              id: 'ptDisplaySet',
-            },
-          ],
-        },
-        {
-          viewportOptions: {
-            viewportId: 'ptSAGITTAL',
-            viewportType: 'volume',
-            orientation: 'sagittal',
-            background: [1, 1, 1],
-            toolGroupId: 'ptToolGroup',
-            syncGroups: [
-              {
-                type: 'cameraPosition',
-                id: 'sagittalSync',
-                source: true,
-                target: true,
-              },
-              {
-                type: 'voi',
-                id: 'ptWLSync',
-                source: true,
-                target: true,
-              },
-              {
-                type: 'voi',
-                id: 'ptFusionWLSync',
-                source: true,
-                target: false,
-              },
-            ],
-          },
-          displaySets: [
-            {
-              options: {
-                voi: {
-                  windowWidth: 5,
-                  windowCenter: 2.5,
-                },
-                voiInverted: true,
-              },
-              id: 'ptDisplaySet',
-            },
-          ],
-        },
-        {
-          viewportOptions: {
-            viewportId: 'ptCORONAL',
-            viewportType: 'volume',
-            orientation: 'coronal',
-            background: [1, 1, 1],
-            toolGroupId: 'ptToolGroup',
-            syncGroups: [
-              {
-                type: 'cameraPosition',
-                id: 'coronalSync',
-                source: true,
-                target: true,
-              },
-              {
-                type: 'voi',
-                id: 'ptWLSync',
-                source: true,
-                target: true,
-              },
-              {
-                type: 'voi',
-                id: 'ptFusionWLSync',
-                source: true,
-                target: false,
-              },
-            ],
-          },
-          displaySets: [
-            {
-              options: {
-                voi: {
-                  windowWidth: 5,
-                  windowCenter: 2.5,
-                },
-                voiInverted: true,
-              },
-              id: 'ptDisplaySet',
-            },
-          ],
-        },
-        {
-          viewportOptions: {
-            viewportId: 'fusionAXIAL',
-            viewportType: 'volume',
-            orientation: 'axial',
-            toolGroupId: 'fusionToolGroup',
-            initialImageOptions: {
-              // index: 5,
-              preset: 'first', // 'first', 'last', 'middle'
-            },
-            syncGroups: [
-              {
-                type: 'cameraPosition',
-                id: 'axialSync',
-                source: true,
-                target: true,
-              },
-              {
-                type: 'voi',
-                id: 'ctWLSync',
-                source: false,
-                target: true,
-              },
-              {
-                type: 'voi',
-                id: 'fusionWLSync',
-                source: true,
-                target: true,
-              },
-              {
-                type: 'voi',
-                id: 'ptFusionWLSync',
-                source: false,
-                target: true,
-              },
-            ],
-          },
-          displaySets: [
-            {
-              id: 'ctDisplaySet',
-            },
-            {
-              options: {
-                colormap: 'hsv',
-                voi: {
-                  windowWidth: 5,
-                  windowCenter: 2.5,
-                },
-              },
-              id: 'ptDisplaySet',
-            },
-          ],
-        },
-        {
-          viewportOptions: {
-            viewportId: 'fusionSAGITTAL',
-            viewportType: 'volume',
-            orientation: 'sagittal',
-            toolGroupId: 'fusionToolGroup',
-            // initialImageOptions: {
-            //   index: 180,
-            //   preset: 'middle', // 'first', 'last', 'middle'
-            // },
-            syncGroups: [
-              {
-                type: 'cameraPosition',
-                id: 'sagittalSync',
-                source: true,
-                target: true,
-              },
-              {
-                type: 'voi',
-                id: 'ctWLSync',
-                source: false,
-                target: true,
-              },
-              {
-                type: 'voi',
-                id: 'fusionWLSync',
-                source: true,
-                target: true,
-              },
-              {
-                type: 'voi',
-                id: 'ptFusionWLSync',
-                source: false,
-                target: true,
-              },
-            ],
-          },
-          displaySets: [
-            {
-              id: 'ctDisplaySet',
-            },
-            {
-              options: {
-                colormap: 'hsv',
-                voi: {
-                  windowWidth: 5,
-                  windowCenter: 2.5,
-                },
-              },
-              id: 'ptDisplaySet',
-            },
-          ],
-        },
-        {
-          viewportOptions: {
-            viewportId: 'fusionCoronal',
-            viewportType: 'volume',
-            orientation: 'coronal',
-            toolGroupId: 'fusionToolGroup',
-            // initialImageOptions: {
-            //   index: 180,
-            //   preset: 'middle', // 'first', 'last', 'middle'
-            // },
-            syncGroups: [
-              {
-                type: 'cameraPosition',
-                id: 'coronalSync',
-                source: true,
-                target: true,
-              },
-              {
-                type: 'voi',
-                id: 'ctWLSync',
-                source: false,
-                target: true,
-              },
-              {
-                type: 'voi',
-                id: 'fusionWLSync',
-                source: true,
-                target: true,
-              },
-              {
-                type: 'voi',
-                id: 'ptFusionWLSync',
-                source: false,
-                target: true,
-              },
-            ],
-          },
-          displaySets: [
-            {
-              id: 'ctDisplaySet',
-            },
-            {
-              options: {
-                colormap: 'hsv',
-                voi: {
-                  windowWidth: 5,
-                  windowCenter: 2.5,
-                },
-              },
-              id: 'ptDisplaySet',
-            },
-          ],
-        },
-        {
-          viewportOptions: {
-            viewportId: 'mipSagittal',
-            viewportType: 'volume',
-            orientation: 'sagittal',
-            background: [1, 1, 1],
-            toolGroupId: 'mipToolGroup',
-            syncGroups: [
-              {
-                type: 'voi',
-                id: 'ptWLSync',
-                source: true,
-                target: true,
-              },
-              {
-                type: 'voi',
-                id: 'ptFusionWLSync',
-                source: true,
-                target: false,
-              },
-            ],
-
-            // Custom props can be used to set custom properties which extensions
-            // can react on.
-            customViewportProps: {
-              // We use viewportDisplay to filter the viewports which are displayed
-              // in mip and we set the scrollbar according to their rotation index
-              // in the cornerstone extension.
-              hideOverlays: true,
-            },
-          },
-          displaySets: [
-            {
-              options: {
-                blendMode: 'MIP',
-                slabThickness: 'fullVolume',
-                voi: {
-                  windowWidth: 5,
-                  windowCenter: 2.5,
-                },
-                voiInverted: true,
-              },
-              id: 'ptDisplaySet',
-            },
-          ],
-        },
+        ctAXIAL,
+        ctSAGITTAL,
+        ctCORONAL,
+        ptAXIAL,
+        ptSAGITTAL,
+        ptCORONAL,
+        fusionAXIAL,
+        fusionSAGITTAL,
+        fusionCORONAL,
+        mipSAGITTAL,
       ],
       createdDate: '2021-02-23T18:32:42.850Z',
+    },
+    {
+      name: 'stage-2',
+      viewportStructure: {
+        layoutType: 'grid',
+        properties: {
+          rows: 2,
+          columns: 2,
+        },
+      },
+      viewports: [ctAXIAL, fusionAXIAL, ptAXIAL, mipSAGITTAL],
+    },
+    {
+      name: '2x3-layout',
+      viewportStructure: {
+        layoutType: 'grid',
+        properties: {
+          rows: 2,
+          columns: 3,
+        },
+      },
+      viewports: [
+        ctAXIAL,
+        ctSAGITTAL,
+        ctCORONAL,
+        ptAXIAL,
+        ptSAGITTAL,
+        ptCORONAL,
+      ],
+    },
+    {
+      name: '2x4-layout',
+      viewportStructure: {
+        layoutType: 'grid',
+        properties: {
+          rows: 2,
+          columns: 4,
+          layoutOptions: [
+            {
+              x: 0,
+              y: 0,
+              width: 1 / 4,
+              height: 1 / 2,
+            },
+            {
+              x: 1 / 4,
+              y: 0,
+              width: 1 / 4,
+              height: 1 / 2,
+            },
+            {
+              x: 2 / 4,
+              y: 0,
+              width: 1 / 4,
+              height: 1 / 2,
+            },
+            {
+              x: 3 / 4,
+              y: 0,
+              width: 1 / 4,
+              height: 1,
+            },
+            {
+              x: 0,
+              y: 1 / 2,
+              width: 1 / 4,
+              height: 1 / 2,
+            },
+            {
+              x: 1 / 4,
+              y: 1 / 2,
+              width: 1 / 4,
+              height: 1 / 2,
+            },
+            {
+              x: 2 / 4,
+              y: 1 / 2,
+              width: 1 / 4,
+              height: 1 / 2,
+            },
+          ],
+        },
+      },
+      viewports: [
+        ptCORONAL,
+        ptSAGITTAL,
+        ptAXIAL,
+        mipSAGITTAL,
+        fusionCORONAL,
+        fusionSAGITTAL,
+        fusionAXIAL,
+      ],
     },
   ],
   numberOfPriorsReferenced: -1,
