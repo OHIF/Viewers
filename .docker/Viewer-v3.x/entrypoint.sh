4bf161f54be0b8ca05a790042ac6c85dd333469a
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2023-04-05 12:53:35 -0400

    feat(core): updates Dockerfile-Local-Build

--- .docker/Viewer-v3.x/entrypoint.sh
+++ .docker/Viewer-v3.x/entrypoint.sh
@@ -30,6 +30,9 @@ if [ -n "$CLIENT_ID" ] || [ -n "$HEALTHCARE_API_ENDPOINT" ]
 	  cp /usr/share/nginx/html/google.js /usr/share/nginx/html/app-config.js
 fi
 
+# Replace environment variables in final config file
+sed -i "s|https://healthcare.googleapis.com/v1/projects/nih-nci-ccr-mib-air-dev-2a0b/locations/us/datasets/mib-air-dicom-dataset-dev/dicomStores/mib-air-dicom-datastore-dev/dicomWeb|$APP_GCP_URL|g" /usr/share/nginx/html/main.*.js
+
 echo "Starting Nginx to serve the OHIF Viewer..."
 
 exec "$@"
