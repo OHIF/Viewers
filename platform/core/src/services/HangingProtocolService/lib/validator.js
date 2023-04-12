f623164a8362f508bb2ec6b23b18885d7b3530b3
Sofien-Sellami <sofien.sellami@esprit.tn>
2023-04-12 17:46:02 +0200

    Refact equal and doesNotEqual Validation

--- platform/core/src/services/HangingProtocolService/lib/validator.js
+++ platform/core/src/services/HangingProtocolService/lib/validator.js
@@ -1,69 +1,139 @@
 import validate from 'validate.js';
 
-validate.validators.equals = function(value, options, key, attributes) {
-  const testValue = options?.value ?? options;
-  if (value !== testValue) {
-    return key + 'must equal ' + testValue;
+// The equals function is used to validate if a given value is equal to another value or an array of values.
+
+validate.validators.equals = function(value, options, key) {
+  const testValue = getTestValue(options);
+
+  // If options is an array, then we need to validate each element in the array
+  if (Array.isArray(testValue)) {
+    // If the array has only one element, then we need to compare the value to that element
+    if (testValue.length === 1) {
+      if (testValue[0] !== value) {
+        return `${key} + ' must equal ' + ${value}`;
+      }
+    } else if (testValue.length !== value.length) {
+      return `${key} +  must be an array of length  + ${testValue.length}`;
+    }
+    // We need to compare each element in the array
+    else {
+      const missingValues = [];
+      for (let i = 0; i < testValue.length; i++) {
+        if (!testValue.includes(value[i])) {
+          missingValues.push(value[i]);
+        }
+      }
+      // If all elements in the array match the test values, then the validation passes
+      if (missingValues.length === 0) {
+      }
+      // Special case for a single missing value
+      else if (missingValues.length === 1) {
+        const missingIndex = testValue.findIndex(elem => !value.includes(elem));
+        const expectedValue = value[value.indexOf(missingValues[0])];
+        return `${key} equals[${missingIndex}] must equal ${expectedValue}`;
+      } else {
+        // General case for multiple missing values
+        const notIncluded = testValue.filter(el => !value.includes(el));
+        const missingIndexes = notIncluded.map(el => testValue.indexOf(el));
+        return `${key} equals[${missingIndexes}] must equal ${missingValues}`;
+      }
+    }
+  }
+  // If options is not an array, we can compare the value directly
+  else if (value !== testValue) {
+    return key + ' must equal ' + testValue;
   }
 };
 
+// The doesNotEqual function is used to validate if a given value is not equal to another value or an array of values.
+
 validate.validators.doesNotEqual = function(value, options, key) {
-  const testValue = options?.value ?? options;
-  if (value === testValue) {
-    return key + 'cannot equal ' + testValue;
+  const testValue = getTestValue(options);
+
+  if (Array.isArray(testValue)) {
+    // If the array has only one element, then we need to compare the value to that element
+    if (testValue.length === 1) {
+      if (testValue[0] === value) {
+        return `${key} must not equal ${value}`;
+      }
+    } else if (testValue.length !== value.length) {
+      return `${key} must be an array of length ${testValue.length}`;
+    } else {
+      const includedValues = [];
+      for (let i = 0; i < testValue.length; i++) {
+        if (testValue.includes(value[i])) {
+          includedValues.push(value[i]);
+        }
+      }
+      if (includedValues.length === 0) {
+      } else if (includedValues.length === 1) {
+        let includedIndex = testValue.findIndex(elem => value.includes(elem));
+        const unexpectedValue = value[value.indexOf(includedValues[0])];
+        return `${key} equals[${includedIndex}] must not equal ${unexpectedValue}`;
+      } else {
+        const includedIndexes = testValue
+            .filter(el => value.includes(el))
+            .map(el => testValue.indexOf(el));
+        return `${key} equals[${includedIndexes}] must not equal ${includedValues}`;
+      }
+    }
+  }
+  // If options is not an array, we can compare the value directly
+  else if (value === testValue) {
+    return key + ' must not equal ' + testValue;
   }
 };
 
 // Ignore case contains.
 // options testValue MUST be in lower case already, otherwise it won't match
-validate.validators.containsI = function (value, options, key) {
-  const testValue = options?.value ?? options;
+validate.validators.containsI = function(value, options, key) {
+  const testValue = getTestValue(options);
   if (Array.isArray(value)) {
     if (
-      value.some(
-        item => !validate.validators.containsI(item.toLowerCase(), options, key)
-      )
+        value.some(
+            item => !validate.validators.containsI(item.toLowerCase(), options, key)
+        )
     ) {
       return undefined;
     }
     return `No item of ${value.join(',')} contains ${JSON.stringify(
-      testValue
+        testValue
     )}`;
   }
   if (Array.isArray(testValue)) {
     if (
-      testValue.some(
-        subTest => !validate.validators.containsI(value, subTest, key)
-      )
+        testValue.some(
+            subTest => !validate.validators.containsI(value, subTest, key)
+        )
     ) {
       return;
     }
     return `${key} must contain at least one of ${testValue.join(',')}`;
   }
   if (
-    testValue &&
-    value.indexOf &&
-    value.toLowerCase().indexOf(testValue) === -1
+      testValue &&
+      value.indexOf &&
+      value.toLowerCase().indexOf(testValue) === -1
   ) {
     return key + 'must contain any case of' + testValue;
   }
 };
 
 validate.validators.contains = function(value, options, key) {
-  const testValue = options?.value ?? options;
+  const testValue = getTestValue(options);
   if (Array.isArray(value)) {
     if (value.some(item => !validate.validators.contains(item, options, key))) {
       return undefined;
     }
     return `No item of ${value.join(',')} contains ${JSON.stringify(
-      testValue
+        testValue
     )}`;
   }
   if (Array.isArray(testValue)) {
     if (
-      testValue.some(
-        subTest => !validate.validators.contains(value, subTest, key)
-      )
+        testValue.some(
+            subTest => !validate.validators.contains(value, subTest, key)
+        )
     ) {
       return;
     }
@@ -75,29 +145,76 @@ validate.validators.contains = function(value, options, key) {
 };
 
 validate.validators.doesNotContain = function(value, options, key) {
-  if (options && value.indexOf && value.indexOf(options.value) !== -1) {
-    return key + 'cannot contain ' + options.value;
+  const testValues = getTestValue(options);
+  if (Array.isArray(testValues)) {
+    let matchFound = false;
+    testValues.forEach(testValue => {
+      if (value.indexOf(testValue) !== -1) {
+        matchFound = true;
+      }
+    });
+    if (matchFound) {
+      return key + ' cannot contain any of ' + testValues.join(',');
+    }
+  } else {
+    if (value.indexOf(testValues) !== -1) {
+      return key + ' cannot contain ' + testValues;
+    }
   }
 };
 
 validate.validators.startsWith = function(value, options, key) {
-  if (options && value.startsWith && !value.startsWith(options.value)) {
-    return key + 'must start with ' + options.value;
+  const testValues = getTestValue(options);
+  if (Array.isArray(testValues)) {
+    if (testValues.length === 1) {
+      if (!value.startsWith(testValues[0])) {
+        return key + ' must start with ' + testValues;
+      }
+    } else return `${key} startsWith must be an array of length 1`;
+  } else {
+    if (!value.startsWith(testValues)) {
+      return key + ' start end with ' + testValues;
+    }
   }
 };
 
 validate.validators.endsWith = function(value, options, key) {
-  if (options && value.endsWith && !value.endsWith(options.value)) {
-    return key + 'must end with ' + options.value;
+  const testValues = getTestValue(options);
+  if (Array.isArray(testValues)) {
+    if (testValues.length === 1) {
+      if (!value.endsWith(testValues[0])) {
+        return key + ' must end with ' + testValues;
+      }
+    } else return `${key} must be an array of length 1`;
+  } else {
+    if (!value.endsWith(testValues)) {
+      return key + ' must end with ' + testValues;
+    }
   }
 };
 
-const getTestValue = options => options?.value ?? options;
+const getTestValue = options => {
+  if (Array.isArray(options)) {
+    return options.map(option => option?.value ?? option);
+  } else {
+    return options?.value ?? options;
+  }
+};
 
 validate.validators.greaterThan = function(value, options, key) {
   const testValue = getTestValue(options);
-  if (value === undefined || value === null || value <= testValue) {
-    return key + 'with value ' + value + ' must be greater than ' + testValue;
+  if (Array.isArray(testValue)) {
+    if (testValue.length === 1) {
+      if (!(value >= testValue[0])) {
+        return `${key}  must be greater than or equal to ${testValue[0]}, but was ${value}`;
+      }
+    } else if (testValue.length > 1) {
+      return key + ' must be an array of length 1';
+    }
+  } else {
+    if (!(value > testValue)) {
+      return key + ' must be greater than ' + testValue;
+    }
   }
 };
 
@@ -105,18 +222,18 @@ validate.validators.range = function(value, options, key) {
   const testValue = getTestValue(options);
   if (value === undefined || value < testValue[0] || value > testValue[1]) {
     return (
-      key +
-      'with value ' +
-      value +
-      ' must be between ' +
-      testValue[0] +
-      ' and ' +
-      testValue[1]
+        key +
+        'with value ' +
+        value +
+        ' must be between ' +
+        testValue[0] +
+        ' and ' +
+        testValue[1]
     );
   }
 };
 
 validate.validators.notNull = value =>
-  value === null || value === undefined ? 'Value is null' : undefined;
+    value === null || value === undefined ? 'Value is null' : undefined;
 
 export default validate;
--- platform/core/src/services/HangingProtocolService/lib/validator.test.js
+++ platform/core/src/services/HangingProtocolService/lib/validator.test.js
@@ -1,139 +1,320 @@
 import validate from './validator.js';
 
 describe('validator', () => {
-  const attributeMap = {
-    str: 'string',
-    upper: 'UPPER',
-    num: 3,
-    nullValue: null,
-    list: ['abc', 'def', 'GHI'],
-  };
+    const attributeMap = {
+        str: 'string',
+        upper: 'UPPER',
+        num: 3,
+        nullValue: null,
+        list: ['abc', 'def', 'GHI'],
+    };
 
-  const options = {
-    format: 'grouped',
-  };
+    const options = {
+        format: 'grouped',
+    };
 
-  describe('contains', () => {
-    it('returns match any list contains', () => {
-      expect(
-        validate(attributeMap, { list: { contains: 'a' } }, [options])
-      ).toBeUndefined();
-      expect(
-        validate(attributeMap, { str: { contains: 'i' } }, [options])
-      ).toBeUndefined();
-      expect(
-        validate(attributeMap, { str: { contains: ['i'] } }, [options])
-      ).toBeUndefined();
-      expect(
-        validate(attributeMap, { list: { contains: ['a'] } }, [options])
-      ).toBeUndefined();
-      expect(
-        validate(attributeMap, { list: { contains: ['z', 'd'] } }, [options])
-      ).toBeUndefined();
-      expect(
-        validate(attributeMap, { list: { contains: ['z'] } }, [options])
-      ).not.toBeUndefined();
-    });
-  });
-
-  describe('containsI', () => {
-    it('returns match any list contains case insensitive', () => {
-      expect(
-        validate(attributeMap, { upper: { containsI: ['bye', 'pre'] } }, [
-          options,
-        ])
-      ).not.toBeUndefined();
-      expect(
-        validate(attributeMap, { list: { containsI: 'hi' } }, [options])
-      ).toBeUndefined();
-      expect(
-        validate(attributeMap, { list: { containsI: ['hi', 'bye'] } }, [
-          options,
-        ])
-      ).toBeUndefined();
-      expect(
-        validate(attributeMap, { list: { containsI: ['bye', 'hi'] } }, [
-          options,
-        ])
-      ).toBeUndefined();
-      expect(
-        validate(attributeMap, { list: { containsI: ['ig', 'hi'] } }, [options])
-      ).toBeUndefined();
-      expect(
-        validate(attributeMap, { upper: { containsI: ['bye', 'per'] } }, [
-          options,
-        ])
-      ).toBeUndefined();
-    });
-  });
-
-  describe('equals', () => {
-    it('returned undefined on equals', () => {
-      expect(
-        validate(attributeMap, { str: { equals: attributeMap.str } }, [options])
-      ).toBeUndefined();
-      expect(
-        validate(
-          attributeMap,
-          { num: { equals: { value: attributeMap.num } } },
-          [options]
-        )
-      ).toBeUndefined();
+    describe('contains', () => {
+        it('returns match any list contains', () => {
+            expect(
+                validate(attributeMap, { list: { contains: 'a' } }, [options])
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { contains: 'i' } }, [options])
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { contains: ['i'] } }, [options])
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { list: { contains: ['a'] } }, [options])
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { list: { contains: ['z', 'd'] } }, [options])
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { list: { contains: ['z'] } }, [options])
+            ).not.toBeUndefined();
+        });
     });
 
-    it('returns error on not equals', () => {
-      expect(
-        validate(attributeMap, { str: { equals: 'abc' } }, [options])
-      ).not.toBeUndefined();
-      expect(
-        validate(
-          attributeMap,
-          { num: { equals: { value: 1 + attributeMap.num } } },
-          [options]
-        )
-      ).not.toBeUndefined();
-    });
-  });
-
-  describe('greaterThan', () => {
-    it('returns undefined on greaterThan', () => {
-      expect(
-        validate(
-          attributeMap,
-          { num: { greaterThan: { value: attributeMap.num - 1 } } },
-          [options]
-        )
-      ).toBeUndefined();
-      expect(
-        validate(attributeMap, { num: { greaterThan: attributeMap.num - 1 } }, [
-          options,
-        ])
-      ).toBeUndefined();
-    });
-
-    it('returns error on not greater than', () => {
-      expect(
-        validate(
-          attributeMap,
-          { num: { greaterThan: { value: attributeMap.num } } },
-          [options]
-        )
-      ).not.toBeUndefined();
-      expect(
-        validate(attributeMap, { num: { greaterThan: attributeMap.num } }, [
-          options,
-        ])
-      ).not.toBeUndefined();
+    describe('containsI', () => {
+        it('returns match any list contains case insensitive', () => {
+            expect(
+                validate(attributeMap, { upper: { containsI: ['bye', 'pre'] } }, [
+                    options,
+                ])
+            ).not.toBeUndefined();
+            expect(
+                validate(attributeMap, { list: { containsI: 'hi' } }, [options])
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { list: { containsI: ['hi', 'bye'] } }, [
+                    options,
+                ])
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { list: { containsI: ['bye', 'hi'] } }, [
+                    options,
+                ])
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { list: { containsI: ['ig', 'hi'] } }, [options])
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { upper: { containsI: ['bye', 'per'] } }, [
+                    options,
+                ])
+            ).toBeUndefined();
+        });
     });
 
-    it('returns error on undefined value', () => {
-      expect(
-        validate(
-          attributeMap,
-          { numUndefined: { greaterThan: { value: 3 } } },
-          [options]
-        )
-      ).not.toBeUndefined();
+    describe('doesNotEqual', () => {
+        it('returns undefined if value does not equal ', () => {
+            expect(
+                validate(attributeMap, { str: { doesNotEqual: 'hello' } }, [options])
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { doesNotEqual: ['hello'] } }, [options])
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { doesNotEqual: { value: 'hello' } } }, [
+                    options,
+                ])
+            ).toBeUndefined();
+            expect(
+                validate(
+                    attributeMap,
+                    { list: { doesNotEqual: ['hello', 'bye', 'ok'] } },
+                    [options]
+                )
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { list: { doesNotEqual: ['ae', 'I', 'ok'] } }, [
+                    options,
+                ])
+            ).toBeUndefined();
+        });
+        it('returns error if value equals specified value', () => {
+            expect(
+                validate(attributeMap, { num: { doesNotEqual: 3 } }, [options])
+            ).not.toBeUndefined();
+            expect(
+                validate(attributeMap, { num: { doesNotEqual: { value: 3 } } }, [
+                    options,
+                ])
+            ).not.toBeUndefined();
+            expect(
+                validate(attributeMap, { num: { doesNotEqual: [3] } }, [options])
+            ).not.toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { doesNotEqual: 'string' } }, [options])
+            ).not.toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { doesNotEqual: ['string'] } }, [options])
+            ).not.toBeUndefined();
+        });
     });
-  });
+    describe('doesNotContain', () => {
+        it('returns undefined if string does not contain specified value', () => {
+            expect(
+                validate(attributeMap, { str: { doesNotContain: 'z' } }, [options])
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { doesNotContain: { value: 'z' } } }, [
+                    options,
+                ])
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { doesNotContain: ['x'] } }, [options])
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { list: { doesNotContain: ['bye', 'hi'] } }, [
+                    options,
+                ])
+            ).toBeUndefined();
+        });
+
+        it('returns error if string contains specified value', () => {
+            expect(
+                validate(attributeMap, { str: { doesNotContain: 's' } }, [options])
+            ).not.toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { doesNotContain: { value: 'r' } } }, [
+                    options,
+                ])
+            ).not.toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { doesNotContain: ['t'] } }, [options])
+            ).not.toBeUndefined();
+            expect(
+                validate(attributeMap, { list: { doesNotContain: ['abc', 'hi'] } }, [
+                    options,
+                ])
+            ).not.toBeUndefined();
+        });
+    });
+
+    describe('equals', () => {
+        it('returned undefined on equals', () => {
+            expect(
+                validate(attributeMap, { str: { equals: attributeMap.str } }, [options])
+            ).toBeUndefined();
+            expect(
+                validate(
+                    attributeMap,
+                    { str: { equals: { value: attributeMap.str } } },
+                    [options]
+                )
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { equals: [attributeMap.str] } }, [
+                    options,
+                ])
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { list: { equals: ['abc', 'GHI', 'def'] } }, [
+                    options,
+                ])
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { list: { equals: attributeMap.list } }, [
+                    options,
+                ])
+            ).toBeUndefined();
+        });
+
+        it('returns error on not equals', () => {
+            expect(
+                validate(attributeMap, { list: { equals: ['def', 'abc'] } }, [options])
+            ).not.toBeUndefined();
+            expect(
+                validate(attributeMap, { list: { equals: ['def', 'abc', 'hi'] } }, [
+                    options,
+                ])
+            ).not.toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { equals: 'abc' } }, [options])
+            ).not.toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { equals: { value: 'abc' } } }, [options])
+            ).not.toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { equals: ['abc'] } }, [options])
+            ).not.toBeUndefined();
+        });
+    });
+    describe('endsWith', () => {
+        it('returns undefined if string ends with specified value', () => {
+            expect(
+                validate(attributeMap, { str: { endsWith: 'g' } }, [options])
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { endsWith: { value: 'g' } } }, [options])
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { endsWith: ['g'] } }, [options])
+            ).toBeUndefined();
+        });
+
+        it('returns error if string does not end with specified value', () => {
+            expect(
+                validate(attributeMap, { str: { endsWith: 'S' } }, [options])
+            ).not.toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { endsWith: { value: 'S' } } }, [options])
+            ).not.toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { endsWith: ['y'] } }, [options])
+            ).not.toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { endsWith: ['g', 'e'] } }, [options])
+            ).not.toBeUndefined();
+        });
+    });
+    describe('startsWith', () => {
+        it('returns undefined if string starts with specified value', () => {
+            expect(
+                validate(attributeMap, { str: { startsWith: { value: 's' } } }, [
+                    options,
+                ])
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { startsWith: 'str' } }, [options])
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { startsWith: ['st'] } }, [options])
+            ).toBeUndefined();
+        });
+
+        it('returns error if string does not starts with specified value', () => {
+            expect(
+                validate(attributeMap, { str: { startsWith: ['st', 's'] } }, [options])
+            ).not.toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { startsWith: 'h' } }, [options])
+            ).not.toBeUndefined();
+            expect(
+                validate(attributeMap, { str: { startsWith: { value: 'h' } } }, [
+                    options,
+                ])
+            ).not.toBeUndefined();
+        });
+    });
+    describe('greaterThan', () => {
+        it('returns undefined on greaterThan', () => {
+            expect(
+                validate(
+                    attributeMap,
+                    { num: { greaterThan: { value: attributeMap.num - 1 } } },
+                    [options]
+                )
+            ).toBeUndefined();
+            expect(
+                validate(attributeMap, { num: { greaterThan: attributeMap.num - 1 } }, [
+                    options,
+                ])
+            ).toBeUndefined();
+            expect(
+                validate(
+                    attributeMap,
+                    { num: { greaterThan: [attributeMap.num - 1] } },
+                    [options]
+                )
+            ).toBeUndefined();
+        });
+
+        it('returns error on not greater than', () => {
+            expect(
+                validate(
+                    attributeMap,
+                    { num: { greaterThan: { value: attributeMap.num } } },
+                    [options]
+                )
+            ).not.toBeUndefined();
+            expect(
+                validate(attributeMap, { num: { greaterThan: attributeMap.num } }, [
+                    options,
+                ])
+            ).not.toBeUndefined();
+            expect(
+                validate(attributeMap, { num: { greaterThan: [attributeMap.num] } }, [
+                    options,
+                ])
+            ).not.toBeUndefined();
+        });
+
+        it('returns error on undefined value', () => {
+            expect(
+                validate(
+                    attributeMap,
+                    { numUndefined: { greaterThan: { value: 3 } } },
+                    [options]
+                )
+            ).not.toBeUndefined();
+            expect(
+                validate(attributeMap, { numUndefined: { greaterThan: [3] } }, [
+                    options,
+                ])
+            ).not.toBeUndefined();
+        });
+    });
 });
