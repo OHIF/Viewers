5bacad29ae3dc6f23d8c3ecd2cf7025151e45802
Alireza <ar.sedghi@gmail.com>
2023-05-08 13:57:44 -0400

    fix thumbnail in tests

--- extensions/default/src/Panels/getImageSrcFromImageId.js
+++ extensions/default/src/Panels/getImageSrcFromImageId.js
@@ -3,14 +3,10 @@
  * @param {*} imageId
  */
 function getImageSrcFromImageId(cornerstone, imageId) {
-  if (!imageId) {
-    return;
-  }
-
   return new Promise((resolve, reject) => {
     const canvas = document.createElement('canvas');
     cornerstone.utilities
-      .loadImageToCanvas({ canvas, imageId })
+      .loadImageToCanvas(canvas, imageId)
       .then(imageId => {
         resolve(canvas.toDataURL());
       })
