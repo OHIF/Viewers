7fd4d14873760a8b56e4e1ae0279e4d8a31baad7
Bill Wallace <wayfarer3130@gmail.com>
2023-05-16 14:06:49 -0400

    Code cleanup

--- platform/core/src/types/index.ts
+++ platform/core/src/types/index.ts
@@ -2,7 +2,7 @@ import * as Extensions from '../extensions/ExtensionManager';
 import * as HangingProtocol from './HangingProtocol';
 import Services from './Services';
 import Hotkey from '../classes/Hotkey';
-import { DisplaySet } from '../services/DisplaySetService/DisplaySetService';
+import type { DisplaySet } from '../services/DisplaySetService/DisplaySetService';
 
 export * from '../services/CustomizationService/types';
 // Separate out some generic types
@@ -18,4 +18,5 @@ export * from './Color';
  * Export the types used within the various services and managers, but
  * not the services/managers themselves, which are exported at the top level.
  */
-export { Extensions, HangingProtocol, Services, Hotkey, DisplaySet };
+export { Extensions, HangingProtocol, Services, Hotkey, };
+export type { DisplaySet };
