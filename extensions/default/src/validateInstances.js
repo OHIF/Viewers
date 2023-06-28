a93028bd2c8b861f929f41029f5185d10052b9bc
rodrigobasilio2022 <rodrigo.basilio@radicalimaging.com>
2023-06-28 09:08:11 -0300

    Document and refactor validateInstance.js file

--- extensions/default/src/validateInstances.js
+++ extensions/default/src/validateInstances.js
@@ -12,6 +12,12 @@ import {
   constructableModalities,
 } from '@ohif/core/src/utils/isDisplaySetReconstructable';
 
+/**
+ * Add a warning to a list of warnings
+ * @param {*} warnings List of warnings
+ * @param {*} warning warning to be added
+ * @returns
+ */
 function addWarning(warnings, warning) {
   if (!warnings.includes(warning)) {
     warnings.push(warning);
@@ -19,6 +25,25 @@ function addWarning(warnings, warning) {
   }
   return false;
 }
+/**
+ * Calculates the scanAxisNormal based on a image orientation vector extract from a frame
+ * @param {*} imageOrientation
+ * @returns
+ */
+function calculateScanAxisNormal(imageOrientation) {
+  const rowCosineVec = vec3.fromValues(
+    imageOrientation[0],
+    imageOrientation[1],
+    imageOrientation[2]
+  );
+  const colCosineVec = vec3.fromValues(
+    imageOrientation[3],
+    imageOrientation[4],
+    imageOrientation[5]
+  );
+  return vec3.cross(vec3.create(), rowCosineVec, colCosineVec);
+}
+
 /**
  * Checks if a series is reconstructable to a 3D volume.
  *
@@ -54,7 +79,7 @@ export default function validateInstances(instances, isReconstructable) {
   if (isMultiframe) {
     checkMultiFrame(sortedInstances[0], warnings);
   } else {
-    checkSingleFrame(sortedInstances, warnings);
+    checkSingleFrames(sortedInstances, warnings);
   }
 
   if (!isReconstructable) {
@@ -63,10 +88,12 @@ export default function validateInstances(instances, isReconstructable) {
   return warnings;
 }
 
+/**
+ * Check various multi frame issues. It calls OHIF core functions
+ * @param {*} multiFrameInstance
+ * @param {*} warnings
+ */
 function checkMultiFrame(multiFrameInstance, warnings) {
-  // If we don't have the PixelMeasuresSequence, then the pixel spacing and
-  // slice thickness isn't specified or is changing and we can't reconstruct
-  // the dataset.
   if (!hasPixelMeasurements(multiFrameInstance)) {
     addWarning(
       warnings,
@@ -86,16 +113,26 @@ function checkMultiFrame(multiFrameInstance, warnings) {
   }
 }
 
-function checkPositionShift(
+/**
+ * Checks if there is a position shift between consecutive frames
+ * @param {*} previousPosition
+ * @param {*} actualPosition
+ * @param {*} scanAxisNormal
+ * @param {*} averageSpacingBetweenFrames
+ * @returns
+ */
+function _checkPositionShift(
   previousPosition,
   actualPosition,
-  normal,
+  scanAxisNormal,
   averageSpacingBetweenFrames
 ) {
+  // predicted position should be the previous position added by the multiplication
+  // of the scanAxisNormal and the average spacing between frames
   const predictedPosition = vec3.scaleAndAdd(
     vec3.create(),
     previousPosition,
-    normal,
+    scanAxisNormal,
     averageSpacingBetweenFrames
   );
   return (
@@ -104,125 +141,188 @@ function checkPositionShift(
   );
 }
 
-function checkSingleFrame(instances, warnings) {
+/**
+ * Check if the frames in a series has different dimensions
+ * @param {*} instances
+ * @returns
+ */
+function checkSeriesDimensions(instances) {
   const firstImage = instances[0];
   const firstImageRows = toNumber(firstImage.Rows);
   const firstImageColumns = toNumber(firstImage.Columns);
+
+  for (let i = 1; i < instances.length; i++) {
+    const instance = instances[i];
+    const { Rows, Columns } = instance;
+
+    if (Rows !== firstImageRows || Columns !== firstImageColumns) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/**
+ * Check if the series has frames with different samplesPerPixel
+ * @param {*} instances
+ * @returns
+ */
+function checkSeriesComponents(instances) {
+  const firstImage = instances[0];
   const firstImageSamplesPerPixel = toNumber(firstImage.SamplesPerPixel);
+
+  for (let i = 1; i < instances.length; i++) {
+    const instance = instances[i];
+    const { SamplesPerPixel } = instance;
+
+    if (SamplesPerPixel !== firstImageSamplesPerPixel) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/**
+ * Check is the series has frames with different orientations
+ * @param {*} instances
+ * @returns
+ */
+function checkSeriesOrientation(instances) {
+  const firstImage = instances[0];
   const firstImageOrientationPatient = toNumber(
     firstImage.ImageOrientationPatient
   );
-  const firstImagePositionPatient = toNumber(firstImage.ImagePositionPatient);
-  const rowCosineVec = vec3.fromValues(
-    firstImageOrientationPatient[0],
-    firstImageOrientationPatient[1],
-    firstImageOrientationPatient[2]
-  );
-  const colCosineVec = vec3.fromValues(
-    firstImageOrientationPatient[3],
-    firstImageOrientationPatient[4],
-    firstImageOrientationPatient[5]
-  );
-  const scanAxisNormal = vec3.cross(vec3.create(), rowCosineVec, colCosineVec);
-  // TODO check warning insert in for
 
-  // Can't reconstruct if we:
-  // -- Have a different dimensions within a displaySet.
-  // -- Have a different number of components within a displaySet.
-  // -- Have different orientations within a displaySet.
-  if (instances.length > 2) {
-    for (let i = 1; i < instances.length; i++) {
-      const instance = instances[i];
-      const { Rows, Columns, SamplesPerPixel } = instance;
-      const imageOrientationPatient = toNumber(
-        instance.ImageOrientationPatient
-      );
+  for (let i = 1; i < instances.length; i++) {
+    const instance = instances[i];
+    const imageOrientationPatient = toNumber(instance.ImageOrientationPatient);
 
-      if (Rows !== firstImageRows || Columns !== firstImageColumns) {
-        addWarning(warnings, 'Series has different dimensions between frames.');
-      }
-
-      if (SamplesPerPixel !== firstImageSamplesPerPixel) {
-        addWarning(
-          warnings,
-          'Series has frames with different number of components.'
-        );
-      }
-
-      if (
-        !_isSameOrientation(
-          imageOrientationPatient,
-          firstImageOrientationPatient
-        )
-      ) {
-        addWarning(warnings, 'Series has frames with different orientations.');
-      }
+    if (
+      !_isSameOrientation(imageOrientationPatient, firstImageOrientationPatient)
+    ) {
+      return false;
     }
   }
+  return true;
+}
+/**
+ * Checks if a series has position shifts between consecutive frames
+ * @param {*} instances
+ * @returns
+ */
+function checkPositionShift(instances) {
+  const firstImageOrientationPatient = toNumber(
+    instances[0].ImageOrientationPatient
+  );
+  const scanAxisNormal = calculateScanAxisNormal(firstImageOrientationPatient);
+  const firstImagePositionPatient = toNumber(instances[0].ImagePositionPatient);
+  const lastIpp = toNumber(
+    instances[instances.length - 1].ImagePositionPatient
+  );
 
-  // Check if frame spacing is approximately equal within a spacingTolerance.
-  // If spacing is on a uniform grid but we are missing frames,
-  // Allow reconstruction, but pass back the number of missing frames.
-  if (instances.length > 2) {
-    const lastIpp = toNumber(
-      instances[instances.length - 1].ImagePositionPatient
+  const averageSpacingBetweenFrames =
+    _getPerpendicularDistance(firstImagePositionPatient, lastIpp) /
+    (instances.length - 1);
+
+  let previousImagePositionPatient = firstImagePositionPatient;
+  for (let i = 1; i < instances.length; i++) {
+    const instance = instances[i];
+    const imagePositionPatient = toNumber(instance.ImagePositionPatient);
+
+    if (
+      _checkPositionShift(
+        previousImagePositionPatient,
+        imagePositionPatient,
+        scanAxisNormal,
+        averageSpacingBetweenFrames
+      )
+    ) {
+      return false;
+    }
+    previousImagePositionPatient = imagePositionPatient;
+  }
+  return true;
+}
+
+/**
+ * Checks if series has spacing issues
+ * @param {*} instances
+ * @param {*} warnings
+ */
+function checkSeriesSpacing(instances, warnings) {
+  const firstImagePositionPatient = toNumber(instances[0].ImagePositionPatient);
+  const lastIpp = toNumber(
+    instances[instances.length - 1].ImagePositionPatient
+  );
+
+  const averageSpacingBetweenFrames =
+    _getPerpendicularDistance(firstImagePositionPatient, lastIpp) /
+    (instances.length - 1);
+
+  let previousImagePositionPatient = firstImagePositionPatient;
+
+  const issuesFound = [];
+  for (let i = 1; i < instances.length; i++) {
+    const instance = instances[i];
+    const imagePositionPatient = toNumber(instance.ImagePositionPatient);
+
+    const spacingBetweenFrames = _getPerpendicularDistance(
+      imagePositionPatient,
+      previousImagePositionPatient
     );
 
-    // We can't reconstruct if we are missing ImagePositionPatient values
-    if (!firstImagePositionPatient || !lastIpp) {
-      addWarning(warnings, 'Series has missing position information.');
-    }
+    const spacingIssue = _getSpacingIssue(
+      spacingBetweenFrames,
+      averageSpacingBetweenFrames
+    );
 
-    const averageSpacingBetweenFrames =
-      _getPerpendicularDistance(firstImagePositionPatient, lastIpp) /
-      (instances.length - 1);
+    if (spacingIssue) {
+      const issue = spacingIssue.issue;
 
-    let previousImagePositionPatient = firstImagePositionPatient;
-
-    const issuesFound = [];
-    for (let i = 1; i < instances.length; i++) {
-      const instance = instances[i];
-      // Todo: get metadata from OHIF.MetadataProvider
-      const imagePositionPatient = toNumber(instance.ImagePositionPatient);
-
-      const spacingBetweenFrames = _getPerpendicularDistance(
-        imagePositionPatient,
-        previousImagePositionPatient
-      );
-
-      if (
-        checkPositionShift(
-          previousImagePositionPatient,
-          imagePositionPatient,
-          scanAxisNormal,
-          averageSpacingBetweenFrames
-        )
-      ) {
-        addWarning(warnings, 'Series has inconsistent position information.');
-      }
-      const spacingIssue = _getSpacingIssue(
-        spacingBetweenFrames,
-        averageSpacingBetweenFrames
-      );
-
-      if (spacingIssue) {
-        const issue = spacingIssue.issue;
-
-        // avoid multiple warning of the same thing
-        if (!issuesFound.includes(issue)) {
-          issuesFound.push(issue);
-          if (issue === reconstructionIssues.MISSING_FRAMES) {
-            addWarning(warnings, 'Series has missing frames.');
-          } else if (issue === reconstructionIssues.IRREGULAR_SPACING) {
-            addWarning(warnings, 'Series has irregular spacing.');
-          }
-        }
-        // we just want to find issues not how many
-        if (issuesFound.length > 1) {
-          break;
+      // avoid multiple warning of the same thing
+      if (!issuesFound.includes(issue)) {
+        issuesFound.push(issue);
+        if (issue === reconstructionIssues.MISSING_FRAMES) {
+          addWarning(warnings, 'Series has missing frames.');
+        } else if (issue === reconstructionIssues.IRREGULAR_SPACING) {
+          addWarning(warnings, 'Series has irregular spacing.');
         }
       }
-      previousImagePositionPatient = imagePositionPatient;
+      // we just want to find issues not how many
+      if (issuesFound.length > 1) {
+        break;
+      }
     }
+    previousImagePositionPatient = imagePositionPatient;
+  }
+}
+
+/**
+ * Runs various checks in a single frame series
+ * @param {*} instances
+ * @param {*} warnings
+ */
+function checkSingleFrames(instances, warnings) {
+  if (instances.length > 2) {
+    if (!checkSeriesDimensions(instances)) {
+      addWarning(warnings, 'Series has different dimensions between frames.');
+    }
+
+    if (!checkSeriesComponents(instances)) {
+      addWarning(
+        warnings,
+        'Series has frames with different number of components.'
+      );
+    }
+
+    if (!checkSeriesOrientation(instances)) {
+      addWarning(warnings, 'Series has frames with different orientations.');
+    }
+
+    if (!checkPositionShift(instances)) {
+      addWarning(warnings, 'Series has inconsistent position information.');
+    }
+
+    checkSeriesSpacing(instances, warnings);
   }
 }
