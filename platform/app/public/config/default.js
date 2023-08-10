a42ad62de0dde1f8f6f32206a6ece67fd46fab3c
rodrigobasilio2022 <rodrigo.basilio@radicalimaging.com>
2023-08-10 10:10:49 -0300

    Restoring default.js

--- platform/app/public/config/default.js
+++ platform/app/public/config/default.js
@@ -46,20 +46,27 @@ window.config = {
         // qidoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
         // wadoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
 
-        wadoUriRoot: 'http://localhost/dicom-web',
-        qidoRoot: 'http://localhost/dicom-web',
-        wadoRoot: 'http://localhost/dicom-web',
-        qidoSupportsIncludeField: true,
-        supportsReject: true,
+        // new server
+        wadoUriRoot: 'https://d33do7qe4w26qo.cloudfront.net/dicomweb',
+        qidoRoot: 'https://d33do7qe4w26qo.cloudfront.net/dicomweb',
+        wadoRoot: 'https://d33do7qe4w26qo.cloudfront.net/dicomweb',
+        qidoSupportsIncludeField: false,
+        supportsReject: false,
         imageRendering: 'wadors',
         thumbnailRendering: 'wadors',
         enableStudyLazyLoad: true,
-        supportsFuzzyMatching: true,
+        supportsFuzzyMatching: false,
         supportsWildcard: true,
-        dicomUploadEnabled: true,
+        staticWado: true,
+        singlepart: 'bulkdata,video',
+        // whether the data source should use retrieveBulkData to grab metadata,
+        // and in case of relative path, what would it be relative to, options
+        // are in the series level or study level (some servers like series some study)
         bulkDataURI: {
           enabled: true,
+          relativeResolution: 'studies',
         },
+        omitQuotationForMultipartRequest: true,
       },
     },
     {
