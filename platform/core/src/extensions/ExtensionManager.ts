812954a5f7f9fecddb80541a44cbed35eb981753
Igor Octaviano <igoroctaviano@gmail.com>
2023-09-04 17:01:05 -0300

    Remove log

--- platform/core/src/extensions/ExtensionManager.ts
+++ platform/core/src/extensions/ExtensionManager.ts
@@ -103,7 +103,6 @@ export default class ExtensionManager extends PubSubService {
     }
 
     this.activeDataSource = dataSource;
-    console.debug('>> New datasource', this.activeDataSource)
 
     this._broadcastEvent(
       ExtensionManager.EVENTS.ACTIVE_DATA_SOURCE_CHANGED,
