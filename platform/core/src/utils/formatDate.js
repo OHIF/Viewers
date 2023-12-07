2a8bba69e45dc441cd740814fb3fa9c8bf04bc58
Pavel <P.A.Pisarev@yandex.ru>
2023-12-07 13:10:46 +0500

    formatDate

--- platform/core/src/utils/formatDate.js
+++ platform/core/src/utils/formatDate.js
@@ -8,7 +8,7 @@ import i18n from 'i18next';
  * @param {string} format Desired date format
  * @returns {string} Formatted date
  */
-export default (date, format = 'DD-MMM-YYYY') => {
+export default (date, format = i18n.t('Common:localDateFormat','DD-MMM-YYYY')) => {
   // moment(undefined) returns the current date, so return the empty string instead
-  return date ? moment(date).format(i18n.t('Common:localDateFormat','MMM-DD-YYYY')) : '';
+  return date ? moment(date).format(format) : '';
 };
