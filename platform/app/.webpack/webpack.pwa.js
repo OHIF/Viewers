4c1d3f8d7b960ba13a4b9c880a3aea776f1ce220
Igor Octaviano <igoroctaviano@gmail.com>
2023-12-14 16:55:20 -0300

    Rollback circle ci changes

--- platform/app/.webpack/webpack.pwa.js
+++ platform/app/.webpack/webpack.pwa.js
@@ -69,8 +69,6 @@ module.exports = (env, argv) => {
         // Hoisted Yarn Workspace Modules
         path.resolve(__dirname, '../../../node_modules'),
         SRC_DIR,
-        path.resolve(__dirname, '../ohif-gcp-mode/node_modules'),
-        path.resolve(__dirname, '../ohif-idc-mode/node_modules'),
       ],
     },
     plugins: [
