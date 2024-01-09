0049f4c0303f0b6ea995972326fc8784259f5a47
Igor Octaviano <igoroctaviano@gmail.com>
2024-01-09 16:22:00 -0300

    feat(hp): enable OHIF to run with partial metadata for large studies at the cost of less effective hanging protocol (#3804)
    Co-authored-by: rodrigobasilio2022 <rodrigo.basilio@radicalimaging.com>
    Co-authored-by: rodrigobasilio2022 <114958722+rodrigobasilio2022@users.noreply.github.com>

--- extensions/default/src/getHangingProtocolModule.js
+++ extensions/default/src/getHangingProtocolModule.js
@@ -14,6 +14,7 @@ const defaultProtocol = {
   editableBy: {},
   protocolMatchingRules: [],
   toolGroupIds: ['default'],
+  hpInitiationCriteria: { minSeriesLoaded: 1 },
   // -1 would be used to indicate active only, whereas other values are
   // the number of required priors referenced - so 0 means active with
   // 0 or more priors.
