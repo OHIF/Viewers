2155d86e684e871c0ee07d54a4104d3a4363ecf5
Alireza <ar.sedghi@gmail.com>
2024-03-21 16:17:55 -0400

    Update canvas width and height for thumbnail images

--- extensions/measurement-tracking/src/panels/PanelStudyBrowserTracking/getImageSrcFromImageId.js
+++ extensions/measurement-tracking/src/panels/PanelStudyBrowserTracking/getImageSrcFromImageId.js
@@ -8,8 +8,8 @@ function getImageSrcFromImageId(cornerstone, imageId) {
     // Note: the default width and height of the canvas is 300x150
     // but we need to set the width and height to the same number since
     // the thumbnails are usually square and we want to maintain the aspect ratio
-    canvas.width = 300;
-    canvas.height = 300;
+    canvas.width = 128 / window.devicePixelRatio;
+    canvas.height = 128 / window.devicePixelRatio;
 
     cornerstone.utilities
       .loadImageToCanvas({ canvas, imageId })
