8959bd1bce8f03ca04044992563fcbd465e73040
Alireza <ar.sedghi@gmail.com>
2024-04-12 14:23:18 -0400

    Update init.tsx in cornerstone extension to add error notification

--- extensions/cornerstone/src/init.tsx
+++ extensions/cornerstone/src/init.tsx
@@ -34,6 +34,8 @@ import { CornerstoneServices } from './types';
 import initViewTiming from './utils/initViewTiming';
 import { colormaps } from './utils/colormaps';
 
+import { debounce } from 'lodash';
+
 const { registerColormap } = csUtilities.colormap;
 
 // TODO: Cypress tests are currently grabbing this from the window?
@@ -291,8 +293,21 @@ export default async function init({
   eventTarget.addEventListener(EVENTS.ELEMENT_ENABLED, elementEnabledHandler.bind(null));
 
   eventTarget.addEventListener(EVENTS.ELEMENT_DISABLED, elementDisabledHandler.bind(null));
-
   colormaps.forEach(registerColormap);
+
+  // Create a debounced function that shows the notification
+  const debouncedShowNotification = debounce(detail => {
+    uiNotificationService.show({
+      title: detail.type,
+      message: detail.message,
+      type: 'error',
+    });
+  }, 300);
+
+  // Event listener
+  eventTarget.addEventListener(EVENTS.ERROR_EVENT, ({ detail }) => {
+    debouncedShowNotification(detail);
+  });
 }
 
 function CPUModal() {
