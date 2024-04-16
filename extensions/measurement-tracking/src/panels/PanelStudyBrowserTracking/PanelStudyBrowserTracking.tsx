0432999ec7de7fe7c7100cb1d894f9c99a5379e9
IbrahimCSAE <ibrahim.mdev@gmail.com>
2024-04-16 07:47:34 -0400

    refactor

--- extensions/measurement-tracking/src/panels/PanelStudyBrowserTracking/PanelStudyBrowserTracking.tsx
+++ extensions/measurement-tracking/src/panels/PanelStudyBrowserTracking/PanelStudyBrowserTracking.tsx
@@ -329,6 +329,65 @@ function PanelStudyBrowserTracking({
     }
   }, [expandedStudyInstanceUIDs, jumpToDisplaySet, tabs]);
 
+  const onClickUntrack = displaySetInstanceUID => {
+    const onConfirm = () => {
+      const displaySet = displaySetService.getDisplaySetByUID(displaySetInstanceUID);
+      sendTrackedMeasurementsEvent('UNTRACK_SERIES', {
+        SeriesInstanceUID: displaySet.SeriesInstanceUID,
+      });
+      const measurements = measurementService.getMeasurements();
+      measurements.forEach(m => {
+        if (m.referenceSeriesUID === displaySet.SeriesInstanceUID) {
+          measurementService.remove(m.uid);
+        }
+      });
+    };
+
+    uiDialogService.create({
+      id: 'untrack-series',
+      centralize: true,
+      isDraggable: false,
+      showOverlay: true,
+      content: Dialog,
+      contentProps: {
+        title: 'Untrack Series',
+        body: () => (
+          <div className="bg-primary-dark p-4 text-white">
+            <p>Are you sure you want to untrack this series?</p>
+            <p className="mt-2">
+              This action cannot be undone and will delete all your existing measurements.
+            </p>
+          </div>
+        ),
+        actions: [
+          {
+            id: 'cancel',
+            text: 'Cancel',
+            type: ButtonEnums.type.secondary,
+          },
+          {
+            id: 'yes',
+            text: 'Yes',
+            type: ButtonEnums.type.primary,
+            classes: ['untrack-yes-button'],
+          },
+        ],
+        onClose: () => uiDialogService.dismiss({ id: 'untrack-series' }),
+        onSubmit: async ({ action }) => {
+          switch (action.id) {
+            case 'yes':
+              onConfirm();
+              uiDialogService.dismiss({ id: 'untrack-series' });
+              break;
+            case 'cancel':
+              uiDialogService.dismiss({ id: 'untrack-series' });
+              break;
+          }
+        },
+      },
+    });
+  };
+
   return (
     <StudyBrowser
       tabs={tabs}
@@ -340,62 +399,7 @@ function PanelStudyBrowserTracking({
         setActiveTabName(clickedTabName);
       }}
       onClickUntrack={displaySetInstanceUID => {
-        const onConfirm = () => {
-          const displaySet = displaySetService.getDisplaySetByUID(displaySetInstanceUID);
-          sendTrackedMeasurementsEvent('UNTRACK_SERIES', {
-            SeriesInstanceUID: displaySet.SeriesInstanceUID,
-          });
-          const measurements = measurementService.getMeasurements();
-          measurements.forEach(m => {
-            if (m.referenceSeriesUID === displaySet.SeriesInstanceUID) {
-              measurementService.remove(m.uid);
-            }
-          });
-        };
-
-        uiDialogService.create({
-          id: 'untrack-series',
-          centralize: true,
-          isDraggable: false,
-          showOverlay: true,
-          content: Dialog,
-          contentProps: {
-            title: 'Untrack Series',
-            body: () => (
-              <div className="bg-primary-dark p-4 text-white">
-                <p>Are you sure you want to untrack this series?</p>
-                <p className="mt-2">
-                  This action cannot be undone and will delete all your existing measurements.
-                </p>
-              </div>
-            ),
-            actions: [
-              {
-                id: 'cancel',
-                text: 'Cancel',
-                type: ButtonEnums.type.secondary,
-              },
-              {
-                id: 'yes',
-                text: 'Yes',
-                type: ButtonEnums.type.primary,
-                classes: ['untrack-yes-button'],
-              },
-            ],
-            onClose: () => uiDialogService.dismiss({ id: 'untrack-series' }),
-            onSubmit: async ({ action }) => {
-              switch (action.id) {
-                case 'yes':
-                  onConfirm();
-                  uiDialogService.dismiss({ id: 'untrack-series' });
-                  break;
-                case 'cancel':
-                  uiDialogService.dismiss({ id: 'untrack-series' });
-                  break;
-              }
-            },
-          },
-        });
+        onClickUntrack(displaySetInstanceUID);
       }}
       onClickThumbnail={() => {}}
       onDoubleClickThumbnail={onDoubleClickThumbnailHandler}
