478ca71b019398d8a58da8aebf6d00bdafa49485
IbrahimCSAE <ibrahim.mdev@gmail.com>
2024-04-16 07:41:16 -0400

    issue 57: For MR 3D volume we should probably apply the MR preset instead of the CT

--- extensions/cornerstone/src/hps/main3D.ts
+++ extensions/cornerstone/src/hps/main3D.ts
@@ -77,8 +77,11 @@ export const main3D = {
             {
               id: 'mprDisplaySet',
               options: {
-                // ToDo: choose appropriate preset
-                displayPreset: 'CT-Bone',
+                displayPreset: {
+                  CT: 'CT-Bone',
+                  MR: 'MR-Default',
+                  default: 'CT-Bone',
+                },
               },
             },
           ],
--- extensions/cornerstone/src/hps/primary3D.ts
+++ extensions/cornerstone/src/hps/primary3D.ts
@@ -77,8 +77,11 @@ export const primary3D = {
             {
               id: 'mprDisplaySet',
               options: {
-                // ToDo: choose appropriate preset
-                displayPreset: 'CT-Bone',
+                displayPreset: {
+                  CT: 'CT-Bone',
+                  MR: 'MR-Default',
+                  default: 'CT-Bone',
+                },
               },
             },
           ],
