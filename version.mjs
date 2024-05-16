4029a8445b0ad35f29b7bfe066805dde6383af3d
Alireza <ar.sedghi@gmail.com>
2024-05-16 15:03:45 -0400

    chore: Update CircleCI workflow to include approval step before NPM publish for release branches (#4138)

--- version.mjs
+++ version.mjs
@@ -19,9 +19,11 @@ async function run() {
 
   let nextVersion;
 
-  if (branchName === 'release') {
+  if (branchName.startsWith('release')) {
     console.log('Branch: release');
-    nextVersion = semver.inc(currentVersion, 'minor');
+    await fs.writeFile('./commit.txt', currentCommitHash);
+    console.log('the version is automatically picked up from the version.txt file');
+    return;
   } else {
     console.log('Branch: master');
     const prereleaseComponents = semver.prerelease(currentVersion);
