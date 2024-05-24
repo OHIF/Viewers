0edf1d6ec044d14baea9de8d15471afe399588a7
Alireza <ar.sedghi@gmail.com>
2024-05-24 14:40:32 -0400

    chore: Update Dialog component imports and add untrack series confirmation dialog

--- platform/ui-next/src/components/Combobox/Combobox.tsx
+++ platform/ui-next/src/components/Combobox/Combobox.tsx
@@ -5,7 +5,14 @@ import { Check, ChevronsUpDown } from 'lucide-react';
 
 import { cn } from '../../lib/utils';
 import { Button } from '../Button/Button';
-import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from '../Command/Command';
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '../Command/Command';
 import { Popover, PopoverContent, PopoverTrigger } from '../Popover/Popover';
 
 export function Combobox({ data = [], placeholder = 'Select item...' }) {
@@ -32,23 +39,28 @@ export function Combobox({ data = [], placeholder = 'Select item...' }) {
         <Command>
           <CommandInput placeholder={`Search ${placeholder.toLowerCase()}...`} />
           <CommandEmpty>No {placeholder.toLowerCase()} found.</CommandEmpty>
-          <CommandGroup>
-            {data.map(item => (
-              <CommandItem
-                key={item.value}
-                value={item.value}
-                onSelect={currentValue => {
-                  setValue(currentValue === value ? '' : currentValue);
-                  setOpen(false);
-                }}
-              >
-                <Check
-                  className={cn('mr-2 h-4 w-4', value === item.value ? 'opacity-100' : 'opacity-0')}
-                />
-                {item.label}
-              </CommandItem>
-            ))}
-          </CommandGroup>
+          <CommandList>
+            <CommandGroup>
+              {data.map(item => (
+                <CommandItem
+                  key={item.value}
+                  value={item.value}
+                  onSelect={currentValue => {
+                    setValue(currentValue === value ? '' : currentValue);
+                    setOpen(false);
+                  }}
+                >
+                  <Check
+                    className={cn(
+                      'mr-2 h-4 w-4',
+                      value === item.value ? 'opacity-100' : 'opacity-0'
+                    )}
+                  />
+                  {item.label}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
         </Command>
       </PopoverContent>
     </Popover>
