44dc8944b4c5ac967be719943bd0dec314f94e94
IbrahimCSAE <ibrahim.mdev@gmail.com>
2024-06-10 14:16:18 -0400

    layout tests

---
+++ tests/AxialPrimary.spec.ts
@@ -0,0 +1,25 @@
+import { test } from '@playwright/test';
+import { visitStudy, checkForScreenshot, screenShotPaths } from './utils';
+
+test.beforeEach(async ({ page }) => {
+  const studyInstanceUID = '2.16.840.1.114362.1.11972228.22789312658.616067305.306.2';
+  const mode = 'Basic Viewer';
+  await visitStudy(page, studyInstanceUID, mode, 2000);
+});
+
+test.describe('Axial Primary Test', async () => {
+  test('should render Axial Primary correctly.', async ({ page }) => {
+    await page.getByTestId('Layout').click();
+    await page
+      .locator('div')
+      .filter({ hasText: /^Axial Primary$/ })
+      .first()
+      .click();
+    await checkForScreenshot(
+      page,
+      page,
+      screenShotPaths.axialPrimary.axialPrimaryDisplayedCorrectly,
+      50
+    );
+  });
+});
--- tests/utils/screenShotPaths.ts
+++ tests/utils/screenShotPaths.ts
@@ -26,6 +26,21 @@ const screenShotPaths = {
   mpr: {
     mprDisplayedCorrectly: 'mprDisplayedCorrectly.png',
   },
+  threeDFourUp: {
+    threeDFourUpDisplayedCorrectly: 'threeDFourUpDisplayedCorrectly.png',
+  },
+  threeDMain: {
+    threeDMainDisplayedCorrectly: 'threeDMainDisplayedCorrectly.png',
+  },
+  threeDPrimary: {
+    threeDPrimaryDisplayedCorrectly: 'threeDPrimaryDisplayedCorrectly.png',
+  },
+  threeDOnly: {
+    threeDOnlyDisplayedCorrectly: 'threeDOnlyDisplayedCorrectly.png',
+  },
+  axialPrimary: {
+    axialPrimaryDisplayedCorrectly: 'axialPrimaryDisplayedCorrectly.png',
+  },
   probe: {
     probeDisplayedCorrectly: 'probeDisplayedCorrectly.png',
   },
