9c802d92806c4135a2e77c7057070fe1f9b04e69
Ibrahim <ibrahim.mdev@gmail.com>
2024-08-15 19:07:01 -0400

    clean up

--- tests/utils/index.ts
+++ tests/utils/index.ts
@@ -4,6 +4,19 @@ import { screenShotPaths } from './screenShotPaths';
 import { simulateClicksOnElement } from './simulateClicksOnElement';
 import { reduce3DViewportSize } from './reduce3DviewportSize';
 import { getMousePosition, initilizeMousePositionTracker } from './mouseUtils';
+import { getSUV } from './getSUV';
+import { getTMTVModalityUnit } from './getTMTVModalityUnit';
+import { clearAllAnnotations } from './clearAllAnnotations';
 
-
-export { visitStudy, checkForScreenshot, screenShotPaths, simulateClicksOnElement, reduce3DViewportSize, getMousePosition, initilizeMousePositionTracker };
+export {
+  visitStudy,
+  checkForScreenshot,
+  screenShotPaths,
+  simulateClicksOnElement,
+  reduce3DViewportSize,
+  getMousePosition,
+  initilizeMousePositionTracker,
+  getSUV,
+  getTMTVModalityUnit,
+  clearAllAnnotations,
+};
