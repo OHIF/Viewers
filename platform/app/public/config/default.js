d8b7e7ff420e37506a4ca3bb69709d3fb780c2d3
Ibrahim <ibrahim.mdev@gmail.com>
2024-09-07 10:25:16 -0400

    turn it off by default

--- platform/app/public/config/default.js
+++ platform/app/public/config/default.js
@@ -16,7 +16,7 @@ window.config = {
   experimentalStudyBrowserSort: false,
   strictZSpacingForVolumeViewport: true,
   groupEnabledModesFirst: true,
-  useExperimentalUI: true,
+  useExperimentalUI: false,
   maxNumRequests: {
     interaction: 100,
     thumbnail: 75,
