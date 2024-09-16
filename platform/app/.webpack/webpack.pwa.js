8598efcb5852d83a2766c1b6c84c0b76adececdd
Pedro Kohler <pedrokohlerbh@gmail.com>
2024-09-16 10:38:31 -0300

    fix: package name

--- platform/app/.webpack/webpack.pwa.js
+++ platform/app/.webpack/webpack.pwa.js
@@ -70,7 +70,6 @@ module.exports = (env, argv) => {
         // Hoisted Yarn Workspace Modules
         path.resolve(__dirname, '../../../node_modules'),
         SRC_DIR,
-        path.resolve(__dirname, 'extensions/ohif-idc-extension/node_modules'),
       ],
     },
     plugins: [
