e08547150881a21b188ccb791c8177f7891e81aa
Igor Octaviano <igoroctaviano@gmail.com>
2024-09-30 17:10:25 -0300

    fix: 🐛 Fix imports for ui-next
    Address import issues

--- platform/ui-next/src/components/SidePanel/SidePanel.tsx
+++ platform/ui-next/src/components/SidePanel/SidePanel.tsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import React, { useCallback, useEffect, useState } from 'react';
 import { Icons } from '../Icons';
 import { TooltipTrigger, TooltipContent, TooltipProvider, Tooltip } from '../Tooltip';
-import Separator from '../Separator';
+import { Separator } from '../Separator';
 
 type StyleMap = {
   open: {
