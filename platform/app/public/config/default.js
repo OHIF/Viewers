327f2cb1cf075eab8cd5de5c00daa4ffe49f3587
Ibrahim <ibrahim.mdev@gmail.com>
2024-10-04 12:16:56 -0400

    update

--- platform/app/public/config/default.js
+++ platform/app/public/config/default.js
@@ -483,3 +483,20 @@ window.config = {
     },
   ],
 };
+
+function waitForElement(selector, maxAttempts = 20, interval = 25) {
+  return new Promise(resolve => {
+    let attempts = 0;
+
+    const checkForElement = setInterval(() => {
+      const element = document.querySelector(selector);
+
+      if (element || attempts >= maxAttempts) {
+        clearInterval(checkForElement);
+        resolve();
+      }
+
+      attempts++;
+    }, interval);
+  });
+}
