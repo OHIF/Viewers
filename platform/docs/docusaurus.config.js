ca472d8ab2cc240c97e14b16e06670bca46f3fab
Alireza <ar.sedghi@gmail.com>
2024-10-10 17:05:58 -0400

    mfs finally works

--- platform/docs/docusaurus.config.js
+++ platform/docs/docusaurus.config.js
@@ -45,7 +45,7 @@ module.exports = {
       },
     }),
     // path.resolve(__dirname, './pluginOHIFWebpackConfig.js'),
-    // path.resolve(__dirname, './postcss.js'),
+    // /path.resolve(__dirname, './postcss.js'),
     'docusaurus-plugin-image-zoom', // 3rd party plugin for image click to pop
     [
       '@docusaurus/plugin-client-redirects',
--- platform/docs/tailwind.config.js
+++ platform/docs/tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // presets: [require('../ui/tailwind.config.js')],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
