65c5942c4b070579811da7855d93181081331340
Ibrahim <ibrahim.mdev@gmail.com>
2024-10-11 11:38:41 -0400

    fixes

--- extensions/default/src/getPanelModule.tsx
+++ extensions/default/src/getPanelModule.tsx
@@ -8,14 +8,12 @@ import i18n from 'i18next';
 // - show errors in UI for thumbnails if promise fails
 
 function getPanelModule({ commandsManager, extensionManager, servicesManager }) {
-  const wrappedMeasurementPanel = ({ renderHeader, getCloseIcon, tab }) => {
+  const wrappedMeasurementPanel = ({ tab }) => {
     return (
       <PanelMeasurementTable
         commandsManager={commandsManager}
         servicesManager={servicesManager}
         extensionManager={extensionManager}
-        renderHeader={renderHeader}
-        getCloseIcon={getCloseIcon}
         tab={tab}
       />
     );
