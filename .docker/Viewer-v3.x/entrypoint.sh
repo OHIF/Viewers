7988f289d46704749ba6473c495b8af5d5daf9da
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2024-11-12 09:53:32 -0500

    chore: updates entrypoint.sh

--- .docker/Viewer-v3.x/entrypoint.sh
+++ .docker/Viewer-v3.x/entrypoint.sh
@@ -5,6 +5,15 @@
 sed -i "s|https://healthcare.googleapis.com/v1/projects/nih-nci-ccr-mib-air-dev-2a0b/locations/us/datasets/mib-air-dicom-dataset-dev/dicomStores/mib-air-dicom-datastore-dev/dicomWeb|$APP_GCP_URL|g" /usr/share/nginx/html/app-config.js
 sed -i "s|airstudies-dev|$APP_URL_PREFIX|g" /usr/share/nginx/html/app.bundle.*.js
 
+mkdir "/etc/nginx/certs"
+
+chmod 644 "/etc/nginx/certs"
+
+aws ssm get-parameter --name "$APP_CERT" --with-decryption --query Parameter.Value --output text > /etc/nginx/certs/wildcard_ccr_cancer_gov.crt
+aws ssm get-parameter --name "$APP_CERT_KEY" --with-decryption --query Parameter.Value --output text > /etc/nginx/certs/wildcard_ccr_cancer_gov.key
+aws ssm get-parameter --name "$APP_CERT_CHAIN" --with-decryption --query Parameter.Value --output text > /etc/nginx/certs/wildcard_ccr_truster_chain.crt
+aws ssm get-parameter --name "$APP_DHPARAM" --with-decryption --query Parameter.Value --output text > /etc/nginx/certs/dhparam.pem
+
 echo "Starting Nginx to serve the OHIF Viewer..."
 
 exec "$@"
