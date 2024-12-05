e0f23ea766792c0d9b0fc026c93e1769ef031369
Ibrahim <ibrahim.mdev@gmail.com>
2024-12-05 09:55:57 -0500

    disable parallel in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  fullyParallel: true,
+  fullyParallel: !!process.env.CI,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
