e53be5b030b9e1bee3c7a935d97b1d3707e3bacd
Bill Wallace <wayfarer3130@gmail.com>
2024-12-19 15:33:04 -0500

    Update the default data source as the other one isn't working

--- platform/app/public/config/kheops.js
+++ platform/app/public/config/kheops.js
@@ -26,7 +26,7 @@ window.config = {
   // filterQueryParam: false,
   // Uses the ohif datasource as the default - this requires that KHEOPS be
   // configured with an OHIF path to .../viewer/dicomwebproxy
-  defaultDataSourceName: 'ohif2',
+  defaultDataSourceName: 'ohif3',
   /* Dynamic config allows user to pass "configUrl" query string this allows to load config without recompiling application. The regex will ensure valid configuration source */
   // dangerouslyUseDynamicConfig: {
   //   enabled: true,
