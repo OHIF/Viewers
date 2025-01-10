182776312655074447e01da5d591490f2fc61ee5
Jeremy Bouhi <jeremy.bouhi@deemea.com>
2025-01-10 14:56:49 +0100

    Create config for Sandbox

--- platform/app/public/config/default.js
+++ platform/app/public/config/default.js
@@ -80,6 +80,29 @@ window.config = {
         },
       },
     },
+    {
+      namespace: '@ohif/extension-default.dataSourcesModule.dicomweb',
+      sourceName: 'SANDBOX',
+      configuration: {
+        friendlyName: 'sandbox-didier',
+        name: 'sandbox-didier',
+        wadoUriRoot: 'https://sandbox.deemea.com/didier',
+        qidoRoot: 'https://sandbox.deemea.com/didier',
+        wadoRoot: 'https://sandbox.deemea.com/didier',
+        qidoSupportsIncludeField: true,
+        supportsReject: true,
+        imageRendering: 'wadors',
+        thumbnailRendering: 'wadors',
+        enableStudyLazyLoad: true,
+        supportsFuzzyMatching: true,
+        supportsWildcard: true,
+        dicomUploadEnabled: true,
+        omitQuotationForMultipartRequest: true,
+        bulkDataURI: {
+          enabled: true,
+        },
+      },
+    },
   ],
   httpErrorHandler: error => {
     // This is 429 when rejected from the public idc sandbox too often.
