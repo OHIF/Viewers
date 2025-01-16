e5af6487f10bb1775eeab5f3369343ff24a7aa90
sedghi <ar.sedghi@gmail.com>
2025-01-15 22:26:52 -0500

    attribute

--- extensions/default/src/DicomWebDataSource/index.ts
+++ extensions/default/src/DicomWebDataSource/index.ts
@@ -473,6 +473,7 @@ function createDicomWebApi(dicomWebConfig: DicomWebConfig, servicesManager) {
         const naturalizedInstances = instances.map(addRetrieveBulkData);
 
         // Adding instanceMetadata to OHIF MetadataProvider
+        //
         naturalizedInstances.forEach(instance => {
           instance.wadoRoot = dicomWebConfig.wadoRoot;
           instance.wadoUri = dicomWebConfig.wadoUri;
