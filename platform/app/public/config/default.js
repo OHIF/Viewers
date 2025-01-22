f8cc8828bbc8d1c0dec1c9cffa62b35a4f9483d2
sedghi <ar.sedghi@gmail.com>
2025-01-22 11:24:52 -0500

    update cs3d

--- platform/app/public/config/default.js
+++ platform/app/public/config/default.js
@@ -6,13 +6,15 @@ window.config = {
   // whiteLabeling: {},
   extensions: [],
   modes: [],
+  useCPURendering: true,
+  showCPUFallbackMessage: false,
   customizationService: {},
   showStudyList: true,
   // some windows systems have issues with more than 3 web workers
   maxNumberOfWebWorkers: 3,
   // below flag is for performance reasons, but it might not work for all servers
   showWarningMessageForCrossOrigin: true,
-  showCPUFallbackMessage: true,
+  // showCPUFallbackMessage: true,
   showLoadingIndicator: true,
   experimentalStudyBrowserSort: false,
   strictZSpacingForVolumeViewport: true,
