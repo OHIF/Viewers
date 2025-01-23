9429caf6cf158fc2170b7177fd636d4305acdaa6
Ibrahim <ibrahim.mdev@gmail.com>
2025-01-23 01:42:00 -0500

    minor fix

--- platform/ui-next/src/components/ThumbnailList/ThumbnailList.tsx
+++ platform/ui-next/src/components/ThumbnailList/ThumbnailList.tsx
@@ -22,12 +22,12 @@ const ThumbnailList = ({
   );
 
   return (
-    <div>
+    <div className="flex flex-col gap-[4px] pt-[4px] pr-[2.5px] pl-[5px] pb-[4px]">
       {/* Thumbnail Items */}
       {thumbnailItems.length > 0 && (
         <div
           id="ohif-thumbnail-list"
-          className="ohif-scrollbar bg-bkg-low grid grid-cols-[repeat(auto-fit,_minmax(0,135px))] place-items-start gap-[4px] overflow-y-hidden pt-[4px] pr-[2.5px] pl-[5px] pb-[4px]"
+          className="ohif-scrollbar bg-bkg-low grid grid-cols-[repeat(auto-fit,_minmax(0,135px))] place-items-start gap-[4px] overflow-y-hidden"
         >
           {thumbnailItems.map(item => {
             const { displaySetInstanceUID, numInstances, ...rest } = item;
@@ -54,7 +54,7 @@ const ThumbnailList = ({
       {listItems.length > 0 && (
         <div
           id="ohif-thumbnail-list"
-          className="ohif-scrollbar bg-bkg-low grid grid-cols-[repeat(auto-fit,_minmax(0,275px))] place-items-start gap-[2px] overflow-y-hidden pt-[4px] pr-[2.5px] pl-[5px] pb-[4px]"
+          className="ohif-scrollbar bg-bkg-low grid grid-cols-[repeat(auto-fit,_minmax(0,275px))] place-items-start gap-[2px] overflow-y-hidden"
         >
           {listItems.map(item => {
             const { displaySetInstanceUID, numInstances, ...rest } = item;
