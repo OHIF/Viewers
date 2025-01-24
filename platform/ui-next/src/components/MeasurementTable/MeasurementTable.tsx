6cd7543a7264598676195d896fa137a021b75849
Devu Jayalekshmi <devu.jayalekshmi@trenser.com>
2025-01-24 18:48:25 +0530

    feat-untrack dialogue

--- platform/ui-next/src/components/MeasurementTable/MeasurementTable.tsx
+++ platform/ui-next/src/components/MeasurementTable/MeasurementTable.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { ButtonEnums, Dialog } from '@ohif/ui';
+
 import { DataRow, PanelSection } from '../../index';
 import { createContext } from '../../lib/createContext';
+import { Button, Icons } from '../../index';
 
 interface MeasurementTableContext {
   data?: any[];
   onAction?: (e, command: string | string[], uid: string) => void;
   disableEditing?: boolean;
   isExpanded: boolean;
+  uiDialogService: unknown;
+  onUntrackConfirm: () => void;
 }
 
 const [MeasurementTableProvider, useMeasurementTableContext] =
@@ -25,6 +30,8 @@ const MeasurementTable = ({
   title,
   children,
   disableEditing = false,
+  uiDialogService,
+  onUntrackConfirm,
 }: MeasurementDataProps) => {
   const { t } = useTranslation('MeasurementTable');
   const amount = data.length;
@@ -39,6 +46,72 @@ const MeasurementTable = ({
       <PanelSection defaultOpen={true}>
         <PanelSection.Header className="bg-secondary-dark">
           <span>{`${t(title)} (${amount})`}</span>
+          <Button
+            size="sm"
+            variant="ghost"
+            className="pl-0.5"
+            onClick={event => {
+              event.stopPropagation();
+              uiDialogService.create({
+                id: 'untrack-and-delete-all-measurements',
+                centralize: true,
+                isDraggable: false,
+                showOverlay: true,
+                content: Dialog,
+                contentProps: {
+                  title: 'Untrack and Delete All Measurements',
+                  body: () => (
+                    <div className="bg-primary-dark text-white">
+                      <p>Are you sure you want to untrack study and delete all measurements?</p>
+                      <p className="mt-2">This action cannot be undone.</p>
+                    </div>
+                  ),
+                  actions: [
+                    {
+                      id: 'cancel',
+                      text: 'Cancel',
+                      type: ButtonEnums.type.secondary,
+                    },
+                    {
+                      id: 'yes',
+                      text: 'Untrack and Delete All',
+                      type: ButtonEnums.type.primary,
+                      classes: ['untrack-and-delete-all-yes-button'],
+                    },
+                  ],
+                  onClose: () =>
+                    uiDialogService.dismiss({ id: 'untrack-and-delete-all-measurements' }),
+                  onSubmit: async ({ action }) => {
+                    switch (action.id) {
+                      case 'yes':
+                        onUntrackConfirm();
+                        uiDialogService.dismiss({ id: 'untrack-and-delete-all-measurements' });
+                        break;
+                      case 'cancel':
+                        uiDialogService.dismiss({ id: 'untrack-and-delete-all-measurements' });
+                        break;
+                    }
+                  },
+                  onStart: () => {
+                    console.log('Dialog drag started');
+                  },
+                  onDrag: (_event: unknown, data: unknown) => {
+                    console.log('Dialog is being dragged', data);
+                  },
+                  onStop: () => {
+                    console.log('Dialog drag stopped');
+                  },
+                  defaultPosition: { x: 0, y: 0 },
+                  onClickOutside: () => {
+                    uiDialogService.dismiss({ id: 'delete-all-measurements' });
+                  },
+                },
+              });
+            }}
+          >
+            <Icons.Delete />
+            Untrack
+          </Button>
         </PanelSection.Header>
         <PanelSection.Content>{children}</PanelSection.Content>
       </PanelSection>
