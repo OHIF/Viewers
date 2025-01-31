0188338897ac8fcfcdcdf92d8a4f8615619c1085
Arul Mozhi <arul.mozhi@trenser.com>
2025-01-31 16:36:24 +0530

    fix for label on measure issue

--- extensions/default/src/utils/promptLabelAnnotation.js
+++ extensions/default/src/utils/promptLabelAnnotation.js
@@ -1,39 +1,42 @@
 import { showLabelAnnotationPopup } from './callInputDialog';
 
 function promptLabelAnnotation({ servicesManager }, ctx, evt) {
-  const { measurementService, customizationService } = servicesManager.services;
-  const { viewportId, StudyInstanceUID, SeriesInstanceUID, measurementId } = evt;
+  const { measurementService, customizationService, toolGroupService } = servicesManager.services;
+  const { viewportId, StudyInstanceUID, SeriesInstanceUID, measurementId, toolName } = evt;
   return new Promise(async function (resolve) {
-    const labelConfig = customizationService.getCustomization('measurementLabels');
-    const renderContent = customizationService.getCustomization('ui.labellingComponent');
-    const measurement = measurementService.getMeasurement(measurementId);
-    if(measurement.label) {
+    const toolGroup = toolGroupService.getToolGroupForViewport(viewportId);
+    const activeToolOptions = toolGroup.getToolConfiguration(toolName);
+    if(activeToolOptions.getTextCallback) {
       resolve({
         StudyInstanceUID,
         SeriesInstanceUID,
         viewportId,
       })
+    } else {
+      const labelConfig = customizationService.getCustomization('measurementLabels');
+      const measurement = measurementService.getMeasurement(measurementId);
+      const renderContent = customizationService.getCustomization('ui.labellingComponent');
+      const value = await showLabelAnnotationPopup(
+        measurement,
+        servicesManager.services.uiDialogService,
+        labelConfig,
+        renderContent
+      );
+
+      measurementService.update(
+        measurementId,
+        {
+          ...value,
+        },
+        true
+      );
+
+      resolve({
+        StudyInstanceUID,
+        SeriesInstanceUID,
+        viewportId,
+      });
     }
-    const value = await showLabelAnnotationPopup(
-      measurement,
-      servicesManager.services.uiDialogService,
-      labelConfig,
-      renderContent
-    );
-
-    measurementService.update(
-      measurementId,
-      {
-        ...value,
-      },
-      true
-    );
-
-    resolve({
-      StudyInstanceUID,
-      SeriesInstanceUID,
-      viewportId,
-    });
   });
 }
 
