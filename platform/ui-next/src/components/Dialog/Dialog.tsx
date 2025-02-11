6969908a98dbffc0c476fc91e54cc86321ba360c
Dan Rukas <dan.rukas@gmail.com>
2025-02-11 14:55:57 -0500

    Updated default dialog close color
    Now uses "primary" color

--- platform/ui-next/src/components/Dialog/Dialog.tsx
+++ platform/ui-next/src/components/Dialog/Dialog.tsx
@@ -43,7 +43,7 @@ const DialogContent = React.forwardRef<
     >
       {children}
       <DialogPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
-        <Cross2Icon className="h-4 w-4" />
+        <Cross2Icon className="text-primary h-4 w-4" />
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
     </DialogPrimitive.Content>
