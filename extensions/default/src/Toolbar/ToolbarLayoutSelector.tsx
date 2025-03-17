2b2eb92b196fc846eb1152fbe4bb256e8c449703
sedghi <ar.sedghi@gmail.com>
2025-03-17 11:32:07 -0400

    fix

--- extensions/default/src/Toolbar/ToolbarLayoutSelector.tsx
+++ extensions/default/src/Toolbar/ToolbarLayoutSelector.tsx
@@ -127,7 +127,7 @@ function ToolbarLayoutSelectorWithServices({
   );
 
   return (
-    <div>
+    <div id="Layout">
       <LayoutSelector
         onSelectionChange={handleSelectionChange}
         {...props}
