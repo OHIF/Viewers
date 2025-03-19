1d6a9d31ba99f6c673c1342b5d1be19704b29d3b
sedghi <ar.sedghi@gmail.com>
2025-03-19 18:11:19 -0400

    update

--- extensions/cornerstone/src/services/SyncGroupService/createHydrateSegmentationSynchronizer.ts
+++ extensions/cornerstone/src/services/SyncGroupService/createHydrateSegmentationSynchronizer.ts
@@ -1,4 +1,4 @@
-import { Types, getEnabledElementByViewportId } from '@cornerstonejs/core';
+import { Enums as CoreEnums, Types, getEnabledElementByViewportId } from '@cornerstonejs/core';
 import {
   SynchronizerManager,
   Synchronizer,
@@ -8,6 +8,7 @@ import {
 
 const { createSynchronizer } = SynchronizerManager;
 const { SEGMENTATION_REPRESENTATION_MODIFIED } = Enums.Events;
+const { BlendModes } = CoreEnums;
 
 export default function createHydrateSegmentationSynchronizer(
   synchronizerName: string,
@@ -40,11 +41,10 @@ const segmentationRepresentationModifiedCallback = async (
   sourceEvent: Event,
   { servicesManager, options }: { servicesManager: AppTypes.ServicesManager; options: unknown }
 ) => {
-  debugger;
   const event = sourceEvent as ToolsTypes.EventTypes.SegmentationRepresentationModifiedEventType;
 
-  const { segmentationId, viewportId } = event.detail;
-  const { segmentationService, hangingProtocolService } = servicesManager.services;
+  const { segmentationId } = event.detail;
+  const { segmentationService } = servicesManager.services;
 
   const targetViewportId = targetViewport.viewportId;
 
@@ -77,5 +77,9 @@ const segmentationRepresentationModifiedCallback = async (
   await segmentationService.addSegmentationRepresentation(targetViewportId, {
     segmentationId,
     type,
+    config: {
+      blendMode:
+        viewport.getBlendMode() === 1 ? BlendModes.LABELMAP_EDGE_PROJECTION_BLEND : undefined,
+    },
   });
 };
