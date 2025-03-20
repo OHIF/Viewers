2db7ef7f6fb5a30a9a3197a509855ebd605a3d81
Sai Shanmukha Narumanchi <nss10@outlook.com>
2025-03-20 14:40:31 -0500

    Adding a null check in `addRetrieveBulkDataNaturalized` (#233)

--- extensions/default/src/DicomWebDataSource/index.js
+++ extensions/default/src/DicomWebDataSource/index.js
@@ -387,6 +387,9 @@ function createDicomWebApi(dicomWebConfig, servicesManager) {
        * This is done recursively, for sub-sequences.
        */
       const addRetrieveBulkDataNaturalized = (naturalized, instance = naturalized) => {
+        if (!naturalized) {
+          return naturalized;
+        }
         for (const key of Object.keys(naturalized)) {
           const value = naturalized[key];
 
