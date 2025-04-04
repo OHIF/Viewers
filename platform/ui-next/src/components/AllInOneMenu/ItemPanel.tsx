dd4347a4aa7d67f525b4ae68c8dc8372cc2603d0
Dan Rukas <dan.rukas@gmail.com>
2025-04-04 13:17:29 -0400

    Minor fixes

--- platform/ui-next/src/components/AllInOneMenu/ItemPanel.tsx
+++ platform/ui-next/src/components/AllInOneMenu/ItemPanel.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode, useContext, useEffect } from 'react';
 import { MenuContext } from './Menu';
-import { ScrollArea } from '../ScrollArea/ScrollArea';
+import { ScrollArea } from '@ohif/ui-next';
 
 type ItemPanelProps = {
   label?: string;
@@ -22,14 +22,12 @@ const ItemPanel = ({ label, index = 0, children, maxHeight = '250px' }: ItemPane
         style={{ maxHeight, height: '100%' }}
         className="flex flex-col overflow-hidden"
       >
-        <div style={{ height: '400px', background: 'rgba(255,0,0,0.2)' }}>
-          <ScrollArea
-            className="h-full w-full flex-grow"
-            type="always"
-          >
-            <div className="flex flex-col">{children}</div>
-          </ScrollArea>
-        </div>
+        <ScrollArea
+          className="h-full w-full flex-grow"
+          type="always"
+        >
+          <div className="flex flex-col">{children}</div>
+        </ScrollArea>
       </div>
     )
   );
