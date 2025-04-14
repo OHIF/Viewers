21931526b7cd2b744326e0e5ffe4f4e237240794
sedghi <ar.sedghi@gmail.com>
2025-04-14 11:04:30 -0400

    refactor: enhance overlay handling in ViewportDataOverlayMenu
    - Added support for 'SR' modality in derivedOverlayModalities.
    - Improved the logic in getEnhancedDisplaySets to determine overlayability based on frameOfReferenceUID and volume validity.
    - Updated the rendering logic to separate derived and non-derived overlays, enhancing the user interface.
    - Introduced ScrollArea for better display of overlay options.

--- extensions/cornerstone/src/components/ViewportDataOverlaySettingMenu/ViewportDataOverlayMenu.tsx
+++ extensions/cornerstone/src/components/ViewportDataOverlaySettingMenu/ViewportDataOverlayMenu.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Icons, Separator } from '@ohif/ui-next';
-import { SegmentationRepresentations } from '@cornerstonejs/tools/enums';
+import { Button, Icons, ScrollArea, Separator } from '@ohif/ui-next';
+import { utilities as csUtils } from '@cornerstonejs/core';
 import { useSystem } from '@ohif/core';
 
-const derivedOverlayModalities = ['SEG', 'RTSTRUCT'];
+const derivedOverlayModalities = ['SEG', 'RTSTRUCT', 'SR'];
 
 function getEnhancedDisplaySets({ viewportId, services }) {
   const { displaySetService, viewportGridService } = services;
@@ -19,10 +19,46 @@ function getEnhancedDisplaySets({ viewportId, services }) {
   );
 
   const backgroundDisplaySet = viewportDisplaySets[0].label;
+  const backGroundCanBeVolume = csUtils.isValidVolume(viewportDisplaySets[0].imageIds);
+
+  // we need to cross check each other displaySet with the main background displaySet
+  // to see if they are overridable or not. One thing is they need to be in the same frameOfReferenceUID
+
+  const enhancedDisplaySets = otherDisplaySets.map(displaySet => {
+    const isOverlayable = true;
+
+    if (displaySet.frameOfReferenceUID !== backgroundDisplaySet.frameOfReferenceUID) {
+      return {
+        ...displaySet,
+        isOverlayable: false,
+      };
+    }
+
+    if (!derivedOverlayModalities.includes(displaySet.Modality)) {
+      if (!backGroundCanBeVolume) {
+        return {
+          ...displaySet,
+          isOverlayable: false,
+        };
+      }
+
+      if (!csUtils.isValidVolume(displaySet.images.map(image => image.imageId))) {
+        return {
+          ...displaySet,
+          isOverlayable: false,
+        };
+      }
+    }
+
+    return {
+      ...displaySet,
+      isOverlayable,
+    };
+  });
 
   return {
     backgroundDisplaySet,
-    enhancedDisplaySets: otherDisplaySets,
+    enhancedDisplaySets,
   };
 }
 
@@ -30,69 +66,79 @@ function ViewportDataOverlayMenu({ viewportId }: withAppTypes<{ viewportId: stri
   const { servicesManager } = useSystem();
   const { displaySetService, viewportGridService } = servicesManager.services;
 
-  const [derivedOverlayDisplaySets, setDerivedOverlayDisplaySets] = useState([]);
-
   const { backgroundDisplaySet, enhancedDisplaySets } = getEnhancedDisplaySets({
     viewportId,
     services: { displaySetService, viewportGridService },
   });
 
-  // useEffect(() => {
-  //   const displaySets = displaySetService.getActiveDisplaySets();
-  //   const derivedOverlayDisplaySets = displaySets.filter(displaySet =>
-  //     derivedOverlayModalities.includes(displaySet.Modality)
-  //   );
-  //   setDerivedOverlayDisplaySets(derivedOverlayDisplaySets);
-  // }, []);
+  const derivedOverlays = enhancedDisplaySets.filter(ds =>
+    derivedOverlayModalities.includes(ds.Modality)
+  );
+  const nonDerivedOverlays = enhancedDisplaySets.filter(
+    ds => !derivedOverlayModalities.includes(ds.Modality)
+  );
 
   return (
     <div className="bg-muted flex h-full w-[262px] flex-col rounded p-3">
-      <span className="text-muted-foreground mb-2 text-xs font-semibold">Current Viewport</span>
       <span className="text-foreground flex-grow text-[13px]">{`Background: ${backgroundDisplaySet}`}</span>
-      <ul className="space-y-1">
-        {enhancedDisplaySets.map(displaySet => (
-          <li
-            key={displaySet.displaySetInstanceUID}
-            className="flex items-center text-sm"
-          >
-            <Button
-              variant="ghost"
-              className="text-muted-foreground w-full"
-              // onClick={() => removeSegmentationFromViewport(segmentation.segmentationId)}
-            >
-              <div className="flex w-full items-center justify-between">
-                <Icons.Plus className="mr-2 h-6 w-6" />
-                <span className="text-foreground">{displaySet.label}</span>
-                <span className="text-muted-foreground ml-2">{displaySet.Modality}</span>
-              </div>
-            </Button>
-          </li>
-        ))}
-      </ul>
-      {/* {derivedOverlayDisplaySets.length > 0 && (
+
+      {nonDerivedOverlays.length > 0 && (
         <>
-          <Separator className="bg-input mb-3" />
-          <span className="text-muted-foreground mb-2 text-xs font-semibold">Available</span>
+          <Separator className="my-3" />
+          <span className="text-muted-foreground mb-2 text-xs font-semibold">Foreground</span>
           <ul className="space-y-1">
-            {derivedOverlayDisplaySets.map(({ displaySetInstanceUID, Modality }) => (
-              <li
-                key={displaySetInstanceUID}
-                className="flex items-center text-sm"
-              >
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  className="text-muted-foreground mr-2"
-                  // onClick={() => addSegmentationToViewport(segmentationId)}
+            <ScrollArea className="h-[150px]">
+              {nonDerivedOverlays.map(displaySet => (
+                <li
+                  key={displaySet.displaySetInstanceUID}
+                  className="flex items-center text-sm"
                 >
-                  <Icons.Plus className="h-6 w-6" />
-                </Button>
-                <span className="text-foreground/60">{Modality}</span>
-              </li>
-            ))}
+                  <Button
+                    variant="ghost"
+                    disabled={!displaySet.isOverlayable}
+                    className="text-muted-foreground w-full"
+                  >
+                    <div className="flex w-full items-center justify-between">
+                      <Icons.Plus className="mr-2 h-6 w-6" />
+                      <span className="text-foreground">{displaySet.label}</span>
+                      <span className="text-muted-foreground ml-2 mr-2">{displaySet.Modality}</span>
+                    </div>
+                  </Button>
+                </li>
+              ))}
+            </ScrollArea>
           </ul>
         </>
-      )} */}
+      )}
+
+      {derivedOverlays.length > 0 && (
+        <>
+          <Separator className="my-3" />
+          <span className="text-muted-foreground mb-2 text-xs font-semibold">Derived Overlays</span>
+          <ul className="space-y-1">
+            <ScrollArea className="h-[150px]">
+              {derivedOverlays.map(displaySet => (
+                <li
+                  key={displaySet.displaySetInstanceUID}
+                  className="flex items-center text-sm"
+                >
+                  <Button
+                    variant="ghost"
+                    className="text-muted-foreground w-full"
+                    disabled={!displaySet.isOverlayable}
+                  >
+                    <div className="flex w-full items-center justify-between">
+                      <Icons.Plus className="mr-2 h-6 w-6" />
+                      <span className="text-foreground">{displaySet.label}</span>
+                      <span className="text-muted-foreground ml-2 mr-2">{displaySet.Modality}</span>
+                    </div>
+                  </Button>
+                </li>
+              ))}
+            </ScrollArea>
+          </ul>
+        </>
+      )}
     </div>
   );
 }
