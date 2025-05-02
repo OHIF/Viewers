e94bb3c1d40448f083c028bcdac24b4c6abd4d54
Rafael Osipov <rafael.osipov@outlook.com>
2025-05-02 21:50:07 +0300

    chore: Added slash to import directive for consinstency purposes as PR processing robot requested

--- platform/i18n/src/locales/index.js
+++ platform/i18n/src/locales/index.js
@@ -7,7 +7,7 @@ import fr from './fr/';
 import ja_JP from './ja-JP/';
 import nl from './nl/';
 import pt_BR from './pt-BR/';
-import ru from './ru';
+import ru from './ru/';
 import vi from './vi/';
 import zh from './zh/';
 import test_lng from './test-LNG/';
