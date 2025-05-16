214137edca9c835a21bec36dd17601892841c3fd
Joe Boccanfuso <joe.boccanfuso@radicalimaging.com>
2025-05-16 12:55:20 -0400

    feat(viewport): Added getter for presentation id provider.
    This allows for creating a decorated version of an existing provider.

--- platform/core/src/services/ViewportGridService/ViewportGridService.ts
+++ platform/core/src/services/ViewportGridService/ViewportGridService.ts
@@ -1,5 +1,10 @@
 import { PubSubService } from '../_shared/pubSubServiceInterface';
 
+type PresentationIdProvider = (
+  id: string,
+  { viewport, viewports, isUpdatingSameViewport }
+) => unknown;
+
 class ViewportGridService extends PubSubService {
   public static readonly EVENTS = {
     ACTIVE_VIEWPORT_ID_CHANGED: 'event::activeviewportidchanged',
@@ -20,10 +25,7 @@ class ViewportGridService extends PubSubService {
 
   serviceImplementation = {};
   servicesManager: AppTypes.ServicesManager;
-  presentationIdProviders: Map<
-    string,
-    (id: string, { viewport, viewports, isUpdatingSameViewport, servicesManager }) => unknown
-  >;
+  presentationIdProviders: Map<string, PresentationIdProvider>;
 
   constructor({ servicesManager }) {
     super(ViewportGridService.EVENTS);
@@ -32,11 +34,15 @@ class ViewportGridService extends PubSubService {
     this.presentationIdProviders = new Map();
   }
 
-  public addPresentationIdProvider(
-    id: string,
-    provider: (id: string, { viewport, viewports, isUpdatingSameViewport }) => unknown
-  ): void {
+  public addPresentationIdProvider(id: string, provider: PresentationIdProvider): void {
     this.presentationIdProviders.set(id, provider);
+  }
+
+  /**
+   * Gets the presentation provider with the given id.
+   */
+  public getPresentationIdProvider(id: string): PresentationIdProvider {
+    return this.presentationIdProviders.get(id);
   }
 
   public setIsReferenceViewable(viewportId: string, isReferenceViewable: boolean): void {
