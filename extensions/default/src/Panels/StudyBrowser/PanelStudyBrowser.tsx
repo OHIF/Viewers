d525fbf3276a0a37feaf68eb3744a7d906b076f2
Dan Elkis <dan.elkis@signalpet.com>
2025-05-20 19:24:58 +0300

    fix(OnThumbnailDoubleClickHandler: Added null protection when the double list handler configuration is not provided

--- extensions/default/src/Panels/StudyBrowser/PanelStudyBrowser.tsx
+++ extensions/default/src/Panels/StudyBrowser/PanelStudyBrowser.tsx
@@ -86,7 +86,7 @@ function PanelStudyBrowser({
         appConfig: extensionManager._appConfig,
       };
 
-      const handlers = customHandler?.callbacks.map(callback => callback(setupArgs));
+      const handlers = customHandler?.callbacks.map(callback => callback(setupArgs)) ?? [];
 
       for (const handler of handlers) {
         await handler(displaySetInstanceUID);
