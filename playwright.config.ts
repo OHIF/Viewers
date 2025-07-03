813355a69ba957ec5e46d096cd6061cb3cab93cf
Joe Boccanfuso <joe.boccanfuso@radicalimaging.com>
2025-07-03 12:11:12 -0400

    playwright blob reporter uses outputDir instead of outputFolder.

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,14 @@ export default defineConfig({
   workers: process.env.CI ? 6 : undefined,
   snapshotPathTemplate: './tests/screenshots{/projectName}/{testFilePath}/{arg}{ext}',
   outputDir: './tests/test-results',
-  reporter: [[process.env.CI ? 'blob' : 'html', { outputFolder: './tests/playwright-report' }]],
+  reporter: [
+    [
+      process.env.CI ? 'blob' : 'html',
+      process.env.CI
+        ? { outputDir: './tests/playwright-report' }
+        : { outputFolder: './tests/playwright-report' },
+    ],
+  ],
   globalTimeout: 800_000,
   timeout: 800_000,
   use: {
