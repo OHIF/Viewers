c2c451ab6847aede488b96be9b74b1c90e6704a5
Joe Boccanfuso <joe.boccanfuso@radicalimaging.com>
2025-07-15 18:05:52 -0400

    Fix broken test.

--- tests/TMTVCSVReport.spec.ts
+++ tests/TMTVCSVReport.spec.ts
@@ -32,11 +32,14 @@ test('should create and download the TMTV CSV report correctly', async ({ page }
     tmtvCSVReportContent,
     'Expected the file to start with specific column/value headers'
   ).toMatch(
-    /^id,label,min,max,mean,stdDev,median,skewness,kurtosis,count,maxLPS,minLPS,center,volume,lesionGlycolysis,peakValue,peakLPS,PatientID,PatientName,StudyInstanceUID,SeriesInstanceUID,StudyDate/
+    /^id,label,min,max,mean,stdDev,median,skewness,kurtosis,count,maxLPS,minLPS,center,volume,peakValue,peakLPS,lesionGlycolysis,PatientID,PatientName,StudyInstanceUID,SeriesInstanceUID,StudyDate/
   );
   expect(tmtvCSVReportContent, 'Expected the patient name to be present').toContain(
     'Water Phantom'
   );
   expect(tmtvCSVReportContent).toContain('Patient ID,202009231');
   expect(tmtvCSVReportContent).toContain('Study Date,20200923');
+  expect(tmtvCSVReportContent, 'Expected no objects to be output.').not.toContain(
+    '[object Object]'
+  );
 });
