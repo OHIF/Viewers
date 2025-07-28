37df1c444df47f1f10630d2fe8d82933f72c887b
Joe Boccanfuso <joe.boccanfuso@radicalimaging.com>
2025-07-27 22:09:16 -0400

    fix(colorbar): Allow for AdvancedRenderingControls to be placed at the top or bottom of the viewport.

--- platform/core/src/hooks/useViewportMousePosition.ts
+++ platform/core/src/hooks/useViewportMousePosition.ts
@@ -2,24 +2,35 @@ import { useState, useEffect } from 'react';
 import { useViewportRef } from './useViewportRef';
 import { useViewportSize } from './useViewportSize';
 
+interface NormalizedBox {
+  minX: number;
+  minY: number;
+  maxX: number;
+  maxY: number;
+}
+
 interface MousePosition {
   x: number;
   y: number;
   isInViewport: boolean;
+  relativeX: number;
   relativeY: number; // Position as percentage from top (0-1)
   isInBottomPercentage: (percentage: number) => boolean;
+  isWithinNormalizedBox?: (normalizedBox: NormalizedBox) => boolean;
 }
 
 function useViewportMousePosition(viewportId: string): MousePosition {
   const viewportRef = useViewportRef(viewportId);
-  const { height, clientRect } = useViewportSize(viewportId);
+  const { width, height, clientRect } = useViewportSize(viewportId);
 
   const [mousePosition, setMousePosition] = useState<MousePosition>({
     x: 0,
     y: 0,
     isInViewport: false,
     relativeY: 0,
+    relativeX: 0,
     isInBottomPercentage: (percentage: number) => false,
+    isWithinNormalizedBox: (normalizedBox: NormalizedBox) => false,
   });
 
   useEffect(() => {
@@ -38,18 +49,35 @@ function useViewportMousePosition(viewportId: string): MousePosition {
 
       const isInViewport = x >= 0 && x <= clientRect.width && y >= 0 && y <= clientRect.height;
 
+      const relativeX = Math.max(0, Math.min(1, x / width));
       const relativeY = Math.max(0, Math.min(1, y / height));
 
       const isInBottomPercentage = (percentage: number) => {
-        return relativeY >= 1 - percentage / 100;
+        return isWithinNormalizedBox({
+          minX: 0,
+          minY: 1 - percentage / 100,
+          maxX: 1,
+          maxY: 1,
+        });
+      };
+
+      const isWithinNormalizedBox = (normalizedBox: NormalizedBox) => {
+        return (
+          relativeX >= normalizedBox.minX &&
+          relativeX <= normalizedBox.maxX &&
+          relativeY >= normalizedBox.minY &&
+          relativeY <= normalizedBox.maxY
+        );
       };
 
       setMousePosition({
         x,
         y,
         isInViewport,
+        relativeX,
         relativeY,
         isInBottomPercentage,
+        isWithinNormalizedBox,
       });
     };
 
@@ -58,7 +86,7 @@ function useViewportMousePosition(viewportId: string): MousePosition {
     return () => {
       document.removeEventListener('mousemove', handleMouseMove);
     };
-  }, [viewportRef, height, clientRect]);
+  }, [viewportRef, height, clientRect, width]);
 
   return mousePosition;
 }
