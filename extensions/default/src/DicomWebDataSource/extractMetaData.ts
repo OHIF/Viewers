f87a452bd82e90bace7f092a761fdcc60e59a1b0
Luis M. Santos <luis.santos2@nih.gov>
2025-08-06 14:00:27 -0400

    chore: refactored the study metadata reconstruction logic to allow merging of data from the qido search into copies of a single reference slice metadata. It saves on unnecessary data retrieval and thus makes the viewer loading very snappy.
    Signed-off-by: Luis M. Santos <luis.santos2@nih.gov>

--- extensions/default/src/DicomWebDataSource/extractMetaData.ts
+++ extensions/default/src/DicomWebDataSource/extractMetaData.ts
@@ -1,11 +1,15 @@
-
 import dcmjs from 'dcmjs';
 import getImageIdsForInstance from './utils/getImageId';
+
 const { DicomMetaDictionary } = dcmjs.data;
 const { naturalizeDataset } = DicomMetaDictionary;
 
 
 export type DicomStructure = any;
+export type DicomStructureData = DicomStructure[];
+export type SeriesDicomStructureData = DicomStructureData[];
+export type RawDicomInstances = PromiseFulfilledResult<any[]>;
+export type SettledRawDicomInstances = RawDicomInstances[];
 export type DicomSeriesHeaderMetaData = {
   StudyInstanceUID: string,
   StudyDescription: string,
@@ -19,16 +23,16 @@ export type DicomSeriesHeaderMetaData = {
 }
 
 export type DicomSeriesMetaData = Map<string, DicomSeriesHeaderMetaData>;
-export type DicomInstancesMetaData = Map<string, DicomStructure[]>;
+export type DicomInstancesMetaData = Map<string, DicomStructureData>;
 
 export type DicomStudyMetaData = {
   seriesSummaryMetadata: DicomSeriesMetaData,
   instancesPerSeries: DicomInstancesMetaData
 }
 
-export function dicomWebToDicomStructure(data: PromiseSettledResult<any>[]): DicomStructure[] {
-  let naturalizedInstancesMetadata: DicomStructure[] = [];
-  data.forEach((seriesInstances: PromiseFulfilledResult<any[]>) => {
+export function dicomWebToDicomStructure(data: SettledRawDicomInstances): DicomStructureData {
+  let naturalizedInstancesMetadata: DicomStructureData = [];
+  data.forEach((seriesInstances: RawDicomInstances) => {
     return seriesInstances.value.map((instance) => {
       naturalizedInstancesMetadata.push(naturalizeDataset(instance));
     });
@@ -37,41 +41,81 @@ export function dicomWebToDicomStructure(data: PromiseSettledResult<any>[]): Dic
   return naturalizedInstancesMetadata;
 }
 
-export function reconstructStudyMetaData(data: DicomStructure[], dicomWebConfig): DicomStudyMetaData {
+export function generateStudyMetaData(data: DicomStructure[], dicomWebConfig): DicomStudyMetaData {
   const seriesSummaryMetadata = new Map<string, DicomSeriesHeaderMetaData>();
-  const instancesPerSeries = new Map<string, DicomStructure[]>();
+  const instancesPerSeries = new Map<string, DicomStructureData>();
 
-  data.forEach(instance => {
-    if (!seriesSummaryMetadata[instance.SeriesInstanceUID]) {
-      seriesSummaryMetadata[instance.SeriesInstanceUID] = {
-        StudyInstanceUID: instance.StudyInstanceUID,
-        StudyDescription: instance.StudyDescription,
-        SeriesInstanceUID: instance.SeriesInstanceUID,
-        SeriesDescription: instance.SeriesDescription,
-        SeriesNumber: instance.SeriesNumber,
-        SeriesTime: instance.SeriesTime,
-        SOPClassUID: instance.SOPClassUID,
-        ProtocolName: instance.ProtocolName,
-        Modality: instance.Modality,
-      };
-    }
+  data.forEach((series: DicomStructureData) => {
+    series.forEach((instance) => {
+      const seriesInstanceUID = instance.SeriesInstanceUID;
+      console.log(seriesInstanceUID);
+      console.log(instance);
+      if (!seriesSummaryMetadata[seriesInstanceUID]) {
+        seriesSummaryMetadata[seriesInstanceUID] = {
+          StudyInstanceUID: instance.StudyInstanceUID,
+          StudyDescription: instance.StudyDescription,
+          SeriesInstanceUID: seriesInstanceUID,
+          SeriesDescription: instance.SeriesDescription,
+          SeriesNumber: instance.SeriesNumber,
+          SeriesTime: instance.SeriesTime,
+          SOPClassUID: instance.SOPClassUID,
+          ProtocolName: instance.ProtocolName,
+          Modality: instance.Modality,
+        };
+      }
 
-    if (!instancesPerSeries[instance.SeriesInstanceUID]) {
-      instancesPerSeries[instance.SeriesInstanceUID] = [];
-    }
+      if (!instancesPerSeries[seriesInstanceUID]) {
+        instancesPerSeries[seriesInstanceUID] = [];
+      }
 
-    const imageId = getImageIdsForInstance({
-      instance,
-      frame: undefined,
-      config: dicomWebConfig
+      const imageId = getImageIdsForInstance({
+        instance,
+        frame: undefined,
+        config: dicomWebConfig
+      });
+
+      instance.imageId = imageId;
+      instance.wadoRoot = dicomWebConfig.wadoRoot;
+      instance.wadoUri = dicomWebConfig.wadoUri;
+
+      instancesPerSeries[instance.SeriesInstanceUID].push(instance)
     });
 
-    instance.imageId = imageId;
-    instance.wadoRoot = dicomWebConfig.wadoRoot;
-    instance.wadoUri = dicomWebConfig.wadoUri;
-
-    instancesPerSeries[instance.SeriesInstanceUID].push(instance)
   });
 
   return {seriesSummaryMetadata, instancesPerSeries}
+}
+
+export function generateInstanceMetaData (
+  instanceQIDOMeta: SettledRawDicomInstances,
+  instanceWADOMeta: SettledRawDicomInstances
+): DicomStructureData
+{
+  const naturalizedQIDOMetadata= instanceQIDOMeta;
+  const naturalizedInstancesMetadata= dicomWebToDicomStructure(instanceWADOMeta);
+  const newNaturalizedInstancesMetadata: DicomStructureData = [];
+
+  for(let i = 0; i < naturalizedQIDOMetadata.length; i++) {
+    const referenceMeta = naturalizedInstancesMetadata[i];
+    const seriesInstances = naturalizedQIDOMetadata[i];
+    const newInstances: DicomStructureData = [];
+
+    seriesInstances.value.forEach((instance) => {
+      let newInstance = JSON.parse(JSON.stringify(referenceMeta));
+
+      newInstance.BitsAllocated = instance.bitsAllocated;
+      newInstance.Columns = instance.columns;
+      newInstance.Rows = instance.rows;
+      newInstance.InstanceNumber = instance.instanceNumber;
+      newInstance.SeriesInstanceUID = instance.seriesInstanceUID;
+      newInstance.SOPClassUID = instance.sopClassUID;
+      newInstance.SOPInstanceUID = instance.sopInstanceUID;
+      newInstance.StudyInstanceUID = instance.studyInstanceUID;
+
+      newInstances.push(newInstance);
+    })
+    newNaturalizedInstancesMetadata.push(newInstances)
+  }
+
+  return newNaturalizedInstancesMetadata;
 }
