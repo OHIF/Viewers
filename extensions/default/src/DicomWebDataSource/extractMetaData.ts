492ee523e196ce94ba5536addb6d080867ac33ea
Luis M. Santos <luis.santos2@nih.gov>
2025-08-11 10:32:17 -0400

    chore Added docstrings in extractMetaData.
    Signed-off-by: Luis M. Santos <luis.santos2@nih.gov>

--- extensions/default/src/DicomWebDataSource/extractMetaData.ts
+++ extensions/default/src/DicomWebDataSource/extractMetaData.ts
@@ -1,13 +1,22 @@
+/**
+ * Tools for extracting key metadata items from QIDO and WADO queries such that it is digestible
+ * by the viewer.
+ */
 import dcmjs from 'dcmjs';
 import getImageIdsForInstance from './utils/getImageId';
+import {DicomWebConfig} from './dicomWebConfig';
 
 const { DicomMetaDictionary } = dcmjs.data;
 const { naturalizeDataset } = DicomMetaDictionary;
 
-
 export type RawDicomInstance = PromiseFulfilledResult<any>;
 export type RawDicomInstances = RawDicomInstance[];
 export type SettledRawDicomInstances = RawDicomInstances[];
+
+/**
+ * Series level metadata header type. Here, we collect all of the metadata necessary for describing
+ * an input study
+ */
 export type DicomSeriesHeaderMetaData = {
   StudyInstanceUID: string,
   StudyDescription: string,
@@ -20,6 +29,10 @@ export type DicomSeriesHeaderMetaData = {
   Modality: string,
 }
 
+/**
+ * This type adds the fields we need to track an instance. Some of the data can be obtained from
+ * QIDO responses and other data such as ImagePositionPatient (IPP) comes from WADO responses.
+ */
 export interface DicomReferenceMetadata extends DicomSeriesHeaderMetaData {
   SOPInstanceUID: string,
   ImagePositionPatient?: number[],
@@ -37,16 +50,38 @@ export type DicomStructureData = DicomStructure[];
 export type DicomSeriesMetaData = Map<string, DicomSeriesHeaderMetaData>;
 export type DicomInstancesMetaData = Map<string, DicomStructureData>;
 
+/**
+ * This type puts together all of the instance metadata and series metadata in one structure that
+ * mirrors the data structure expected by the Viewer.
+ */
 export type DicomStudyMetaData = {
   seriesSummaryMetadata: DicomSeriesMetaData,
   instancesPerSeries: DicomInstancesMetaData
 }
 export type DicomSeriesStructureData = DicomStructureData[];
 
+/**
+ * Takes a list of settled promises containing fulfilled promises and returns a list of lists of
+ * fulfilled promises.
+ *
+ * The goal here is to take the data returned by `dicomweb-client`, which lacks type information and
+ * begin adding type annotations.
+ *
+ * @param instances list of settled promises containing fulfilled promises
+ */
 export function dicomWebToSettledRawDicomInstances(instances: any[]): SettledRawDicomInstances {
   return instances.map((promise) => promise.value)
 }
 
+
+/**
+ * Converts a list of a list of fulfilled promises into a list of `DicomStructure`.
+ * This function calls the `dcmjs` `naturalizeDataset` function to cast the input into a DICOM
+ * structure. I then take this structure and push it into a list that assumes the input is a
+ * `DicomStructure`. From this point on, the metadata of interest is annotated for linter checks.
+ *
+ * @param data list of a list of fulfilled promises
+ */
 export function dicomWebToDicomStructure(data: RawDicomInstances): DicomStructureData {
   let naturalizedInstancesMetadata: DicomStructureData = [];
   data.forEach((seriesInstances) => {
@@ -59,7 +94,20 @@ export function dicomWebToDicomStructure(data: RawDicomInstances): DicomStructur
   return naturalizedInstancesMetadata;
 }
 
-export function generateStudyMetaData(data: DicomSeriesStructureData, dicomWebConfig): DicomStudyMetaData {
+/**
+ * Goes through each input instances and generates a series summary header and sorts the instances.
+ * The idea is to organize the data into two lists. One list for the series metadata header and
+ * one list for the instances in `DicomStructure` layout.
+ *
+ * Something else we do is generate an image ID to be associated with the input the instance.
+ * We need the system configuration for this process.
+ * We also use the configuration to associate the WADO root and uri urls to the instances for later
+ * instance retrieval.
+ *
+ * @param data list of list of `DicomStructure` instances
+ * @param dicomWebConfig reference to system configuration to adjust the instance metadata.
+ */
+export function generateStudyMetaData(data: DicomSeriesStructureData, dicomWebConfig: DicomWebConfig): DicomStudyMetaData {
   const seriesSummaryMetadata = new Map<string, DicomSeriesHeaderMetaData>();
   const instancesPerSeries = new Map<string, DicomStructureData>();
 
@@ -100,7 +148,24 @@ export function generateStudyMetaData(data: DicomSeriesStructureData, dicomWebCo
   return {seriesSummaryMetadata, instancesPerSeries}
 }
 
-
+/**
+ * Patch the instance IPP assuming a uniform change between slices such that they are in roughly
+ * the correct location in space for 3D reconstruction.
+ *
+ * The IPP delta between slices in a series is typically in the z direction and uniform.
+ * To generate the new slice IPP, I use the first and last slices as guides to obtain the
+ * magnitude and direction of the change. Only the z axis is modified.
+ *
+ * The IPP describes the general position of the slices in space relative to an orientation vector
+ * and in frame of refence.
+ *
+ * Formula is IPPz = (lastz - firstz) / total_slices.
+ *
+ * @param firstSlice
+ * @param lastSlice
+ * @param indx
+ * @param totalSliceCount
+ */
 export function generateInstanceReferenceMetadata(
   firstSlice: DicomReferenceMetadata,
   lastSlice: DicomReferenceMetadata,
@@ -123,6 +188,19 @@ export function generateInstanceReferenceMetadata(
   return reference;
 }
 
+/**
+ * Using one of the WADO instances, generate the missing instance metadata structures needed by the
+ * Viewer to determine if 3D reconstruction is possible.
+ *
+ * This function uses the QIDO metadata to determine how many series to process and how many slices
+ * for each series to prepare. The QIDO metadata is first casted to a `DicomStructureData` for easy
+ * handling.
+ *
+ * The result is series => [first, last] >> [first, ..., last].
+ *
+ * @param instanceQIDOMeta
+ * @param instanceWADOMeta
+ */
 export function generateInstanceMetaData (
   instanceQIDOMeta: any[],
   instanceWADOMeta: SettledRawDicomInstances
@@ -135,7 +213,6 @@ export function generateInstanceMetaData (
   instanceWADOMeta.forEach(
     instances => naturalizedInstancesMetadata.push(dicomWebToDicomStructure(instances))
   );
-  console.log(naturalizedInstancesMetadata);
 
   for(let i = 0; i < naturalizedQIDOMetadata.length; i++) {
     const referenceMetaData = naturalizedInstancesMetadata[i];
