9f6b03032a23952e02619afc0f4a9559881795f0
Luis M. Santos <luis.santos2@nih.gov>
2025-08-11 10:37:27 -0400

    chore Added docstrings in retrieveInstanceMetadata.
    Signed-off-by: Luis M. Santos <luis.santos2@nih.gov>

--- extensions/default/src/DicomWebDataSource/retrieveInstanceMetadata.ts
+++ extensions/default/src/DicomWebDataSource/retrieveInstanceMetadata.ts
@@ -2,7 +2,18 @@ import { retrieveStudyMetadata } from './retrieveStudyMetadata';
 import DICOMwebClient from 'dicomweb-client/types/api';
 import { DicomWebConfig, BulkDataURIConfig } from './dicomWebConfig';
 
-
+/**
+ * Retrieval of study metadata updated to allow optional passthrough of SOPInstanceUID so that we can
+ * retrieve individual slices instead of the full series or study.
+ *
+ * @param wadoDicomWebClient client needed to execute retrieval
+ * @param StudyInstanceUID
+ * @param enableStudyLazyLoad
+ * @param instanceMeta Instance metadata from which we can obtain the series uid and instance uid if available
+ * @param sortCriteria
+ * @param sortFunction
+ * @param dicomWebConfig system configuration structure.
+ */
 export async function retrieveInstanceMetadata(
   wadoDicomWebClient: DICOMwebClient,
   StudyInstanceUID: string,
