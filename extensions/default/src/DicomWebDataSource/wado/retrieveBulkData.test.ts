d50c710b9cfdb8fab32a24165c89e115db6db0ad
Luis M. Santos <luis.santos2@nih.gov>
2025-08-12 15:44:43 -0400

    chore added unit tests for core functions in retrieveBulkData. Updated the Jest configuration to map the dicomweb-client module to the correct path.
    Signed-off-by: Luis M. Santos <luis.santos2@nih.gov>

---
+++ extensions/default/src/DicomWebDataSource/wado/retrieveBulkData.test.ts
@@ -0,0 +1,35 @@
+import {addRetrieveBulkData, addRetrieveBulkDataNaturalized} from './retrieveBulkData';
+import {
+  naturalizedInstances,
+  client,
+  dicomWebConfig,
+  expectedNaturalizedInstances,
+} from '../utils/data.test';
+import * as assert from 'node:assert';
+
+describe('retrieveBulkData', () => {
+
+
+  it('should be able to add Bulk data uri to naturalized instances [addRetrieveBulkDataNaturalized]', () => {
+    const result = naturalizedInstances.map(
+      instance => addRetrieveBulkDataNaturalized(
+        instance,
+        client,
+        dicomWebConfig,
+      )
+    );
+    expect(result.toString()).toEqual(expectedNaturalizedInstances.toString());
+  });
+
+
+  it('should be able to add Bulk data uri to naturalized instances [addRetrieveBulkData]', () => {
+    const result = naturalizedInstances.map(
+      instance => addRetrieveBulkData(
+        instance,
+        client,
+        dicomWebConfig,
+      )
+    );
+    expect(result.toString()).toEqual(expectedNaturalizedInstances.toString());
+  });
+});
