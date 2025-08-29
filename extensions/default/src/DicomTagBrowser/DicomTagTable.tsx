02693eadc6c1c2c0ebfe8dde26b41ab023b5fa2f
Dan Rukas <dan.rukas@gmail.com>
2025-08-29 12:49:45 -0400

    Fix: adjusted width for tag browser cols

--- extensions/default/src/DicomTagBrowser/DicomTagTable.tsx
+++ extensions/default/src/DicomTagBrowser/DicomTagTable.tsx
@@ -39,7 +39,7 @@ const RowComponent = ({
     <div
       style={{ ...style, ...rowStyle }}
       className={classNames(
-        'hover:bg-secondary-main border-input text-foreground flex w-full flex-row items-center break-all bg-black text-base transition duration-300',
+        'hover:bg-primary/25 border-input text-foreground flex w-full flex-row items-center break-all bg-black text-base',
         lineHeightClassName
       )}
       key={keyPrefix}
