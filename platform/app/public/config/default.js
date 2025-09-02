72904608f14fc3fbb992013dced98183a6545e01
JeremyDeemea <148856385+JeremyDeemea@users.noreply.github.com>
2025-09-02 12:50:53 +0200

    Update santy QA url (#96)

--- platform/app/public/config/default.js
+++ platform/app/public/config/default.js
@@ -327,9 +327,9 @@ window.config = {
       configuration: {
         friendlyName: 'santy-didier',
         name: 'santy-didier',
-        wadoUriRoot: 'https://santy.deemea.com/api/v1/didier',
-        qidoRoot: 'https://santy.deemea.com/api/v1/didier',
-        wadoRoot: 'https://santy.deemea.com/api/v1/didier',
+        wadoUriRoot: 'https://santy-qa.deemea.com/api/v1/didier',
+        qidoRoot: 'https://santy-qa.deemea.com/api/v1/didier',
+        wadoRoot: 'https://santy-qa.deemea.com/api/v1/didier',
         qidoSupportsIncludeField: false,
         imageRendering: 'wadors',
         thumbnailRendering: 'wadors',
