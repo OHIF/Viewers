f0bfbcff6316426811fa2f3fd84b7d4cb09d791c
Joe Boccanfuso <joe.boccanfuso@radicalimaging.com>
2025-09-09 08:24:42 -0400

    Removed default multi-tab customization from the configuration file.

--- platform/app/public/config/default.js
+++ platform/app/public/config/default.js
@@ -6,9 +6,7 @@ window.config = {
   // whiteLabeling: {},
   extensions: [],
   modes: [],
-  customizationService: {
-    'panelSegmentation.isMultiTab': false,
-  },
+  customizationService: {},
   showStudyList: true,
   // some windows systems have issues with more than 3 web workers
   maxNumberOfWebWorkers: 3,
