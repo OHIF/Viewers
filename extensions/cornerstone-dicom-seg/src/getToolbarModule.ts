6eb1510feb47e3d51fbe02823ff92998dba39aed
Joe Boccanfuso <joe.boccanfuso@radicalimaging.com>
2025-09-09 13:51:45 -0400

    PR feedback.

--- extensions/cornerstone-dicom-seg/src/getToolbarModule.ts
+++ extensions/cornerstone-dicom-seg/src/getToolbarModule.ts
@@ -68,13 +68,7 @@ export function getToolbarModule({ servicesManager }: withAppTypes) {
     },
     {
       name: 'evaluate.cornerstone.segmentation',
-      evaluate: ({
-        viewportId,
-        button,
-        toolNames,
-        disabledText,
-        segmentationRepresentationType,
-      }) => {
+      evaluate: ({ viewportId, button, toolNames, disabledText }) => {
         // Todo: we need to pass in the button section Id since we are kind of
         // forcing the button to have black background since initially
         // it is designed for the toolbox not the toolbar on top
