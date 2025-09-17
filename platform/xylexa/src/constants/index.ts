7a95115d9dc2193ce9e1cfca97229f08424c5e9d
rabahalishah-xylexa <rabah.ali@xylexa.ai>
2025-09-17 11:22:53 +0500

    update: updates API URL

--- platform/xylexa/src/constants/index.ts
+++ platform/xylexa/src/constants/index.ts
@@ -1,7 +1,7 @@
 export const API_URL =
-  process.env.REACT_APP_API_URL || 'http://192.168.18.74:8888/api/';
+  process.env.REACT_APP_API_URL || 'https://backend.xylexa.ai/api/';
 
 export const AUTH_API_URL =
-  process.env.REACT_APP_AUTH_API_URL || 'http://192.168.18.74/keycloak/realms/orthanc/protocol/openid-connect/';
+  process.env.REACT_APP_AUTH_API_URL || 'https://keycloakpacs.xylexa.ai/keycloak/realms/orthanc/protocol/openid-connect/';
 
 export * from './localStorageKeys';
