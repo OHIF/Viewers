a30e9b825909d1161abfaddc6db8605f3930e8a9
Bill Wallace <wayfarer3130@gmail.com>
2025-09-29 18:51:03 -0400

    Fix re-enter mode layout issue

--- modes/basic/src/index.tsx
+++ modes/basic/src/index.tsx
@@ -1,9 +1,12 @@
+import update from 'immutability-helper';
+import { ToolbarService, utils } from '@ohif/core';
+
 import initToolGroups from './initToolGroups';
 import toolbarButtons from './toolbarButtons';
 import { id } from './id';
-import { ToolbarService } from '@ohif/core';
 
 const { TOOLBAR_SECTIONS } = ToolbarService;
+const { structuredCloneWithFunctions } = utils;
 
 /**
  * Define non-imaging modalities.
@@ -72,16 +75,16 @@ export const extensionDependencies = {
 };
 
 export const sopClassHandlers = [
-      dicomvideo.sopClassHandler,
-      dicomSeg.sopClassHandler,
-      dicomPmap.sopClassHandler,
-      ohif.sopClassHandler,
-      ohif.wsiSopClassHandler,
-      dicompdf.sopClassHandler,
-      dicomsr.sopClassHandler3D,
-      dicomsr.sopClassHandler,
-      dicomRT.sopClassHandler,
-    ];
+  dicomvideo.sopClassHandler,
+  dicomSeg.sopClassHandler,
+  dicomPmap.sopClassHandler,
+  ohif.sopClassHandler,
+  ohif.wsiSopClassHandler,
+  dicompdf.sopClassHandler,
+  dicomsr.sopClassHandler3D,
+  dicomsr.sopClassHandler,
+  dicomRT.sopClassHandler,
+];
 
 /**
  * Indicate this is a valid mode if:
@@ -91,271 +94,277 @@ export const sopClassHandlers = [
  *   - it contains at least one modality other than the nonModeMOdalities.
  */
 export function isValidMode({ modalities }) {
-      const modalities_list = modalities.split('\\');
+  const modalities_list = modalities.split('\\');
 
-      if( this.modeModalities?.length ) {
-        for(const modeModality of this.modeModalities) {
-          if (Array.isArray(modeModality) && modeModality.every(m => modalities.indexOf(m)!==-1)) {
-            return { valid: true, description: `Matches ${modeModality.join(', ')}`};
-          } else if (modalities.indexOf(modeModality) ) {
-            return { valid: true, description: `Matches ${modeModality}`};
-          }
-        }
-        return { valid: false, description: `None of the mode modalities match: ${JSON.stringify(this.modeModalities)}`}
+  if (this.modeModalities?.length) {
+    for (const modeModality of this.modeModalities) {
+      if (Array.isArray(modeModality) && modeModality.every(m => modalities.indexOf(m) !== -1)) {
+        return { valid: true, description: `Matches ${modeModality.join(', ')}` };
+      } else if (modalities.indexOf(modeModality)) {
+        return { valid: true, description: `Matches ${modeModality}` };
       }
-
-      return {
-        valid: !!modalities_list.find(modality => this.nonModeModalities.indexOf(modality) === -1),
-        description:
-          `The mode does not support studies that ONLY include the following modalities: ${this.nonModeModalities.join(', ')}`,
-      };
     }
+    return {
+      valid: false,
+      description: `None of the mode modalities match: ${JSON.stringify(this.modeModalities)}`,
+    };
+  }
 
-export function onModeEnter({ servicesManager, extensionManager, commandsManager, panelService, segmentationService }: withAppTypes) {
-      const { measurementService, toolbarService, toolGroupService, customizationService } =
-        servicesManager.services;
+  return {
+    valid: !!modalities_list.find(modality => this.nonModeModalities.indexOf(modality) === -1),
+    description: `The mode does not support studies that ONLY include the following modalities: ${this.nonModeModalities.join(', ')}`,
+  };
+}
 
-      measurementService.clearMeasurements();
+export function onModeEnter({
+  servicesManager,
+  extensionManager,
+  commandsManager,
+  panelService,
+  segmentationService,
+}: withAppTypes) {
+  const { measurementService, toolbarService, toolGroupService, customizationService } =
+    servicesManager.services;
 
-      // Init Default and SR ToolGroups
-      initToolGroups(extensionManager, toolGroupService, commandsManager);
+  measurementService.clearMeasurements();
 
-      toolbarService.register(this.toolbarButtons);
+  // Init Default and SR ToolGroups
+  initToolGroups(extensionManager, toolGroupService, commandsManager);
 
-      for(const [key,section] of Object.entries(this.toolbarSections)) {
-        toolbarService.updateSection(key,section);
-      }
+  toolbarService.register(this.toolbarButtons);
 
-      if( !this.enableSegmentationEdit ) {
-        customizationService.setCustomizations({
-          'panelSegmentation.disableEditing': {
-            $set: true,
+  for (const [key, section] of Object.entries(this.toolbarSections)) {
+    toolbarService.updateSection(key, section);
+  }
+
+  if (!this.enableSegmentationEdit) {
+    customizationService.setCustomizations({
+      'panelSegmentation.disableEditing': {
+        $set: true,
+      },
+    });
+  }
+
+  // // ActivatePanel event trigger for when a segmentation or measurement is added.
+  // // Do not force activation so as to respect the state the user may have left the UI in.
+  if (this.activatePanelTrigger) {
+    this._activatePanelTriggersSubscriptions = [
+      ...panelService.addActivatePanelTriggers(
+        cornerstone.segmentation,
+        [
+          {
+            sourcePubSubService: segmentationService,
+            sourceEvents: [segmentationService.EVENTS.SEGMENTATION_ADDED],
           },
-        });
-      }
+        ],
+        true
+      ),
+      ...panelService.addActivatePanelTriggers(
+        cornerstone.measurements,
+        [
+          {
+            sourcePubSubService: measurementService,
+            sourceEvents: [
+              measurementService.EVENTS.MEASUREMENT_ADDED,
+              measurementService.EVENTS.RAW_MEASUREMENT_ADDED,
+            ],
+          },
+        ],
+        true
+      ),
+      true,
+    ];
+  }
+}
 
-      // // ActivatePanel event trigger for when a segmentation or measurement is added.
-      // // Do not force activation so as to respect the state the user may have left the UI in.
-      if( this.activatePanelTrigger ) {
-        this._activatePanelTriggersSubscriptions = [
-        ...panelService.addActivatePanelTriggers(
-          cornerstone.segmentation,
-          [
-            {
-              sourcePubSubService: segmentationService,
-              sourceEvents: [segmentationService.EVENTS.SEGMENTATION_ADDED],
-            },
-          ],
-          true
-        ),
-        ...panelService.addActivatePanelTriggers(
-          cornerstone.measurements,
-          [
-            {
-              sourcePubSubService: measurementService,
-              sourceEvents: [
-                measurementService.EVENTS.MEASUREMENT_ADDED,
-                measurementService.EVENTS.RAW_MEASUREMENT_ADDED,
-              ],
-            },
-          ],
-          true
-        ),
-        true,
-      ];
-      }
-    };
+export function onModeExit({ servicesManager }: withAppTypes) {
+  const {
+    toolGroupService,
+    syncGroupService,
+    segmentationService,
+    cornerstoneViewportService,
+    uiDialogService,
+    uiModalService,
+  } = servicesManager.services;
 
-export function onModeExit({ servicesManager }: withAppTypes)  {
-      const {
-        toolGroupService,
-        syncGroupService,
-        segmentationService,
-        cornerstoneViewportService,
-        uiDialogService,
-        uiModalService,
-      } = servicesManager.services;
+  this._activatePanelTriggersSubscriptions.forEach(sub => sub.unsubscribe());
+  this._activatePanelTriggersSubscriptions.length = 0;
 
-      this._activatePanelTriggersSubscriptions.forEach(sub => sub.unsubscribe());
-      this._activatePanelTriggersSubscriptions = [];
-
-      uiDialogService.hideAll();
-      uiModalService.hide();
-      toolGroupService.destroy();
-      syncGroupService.destroy();
-      segmentationService.destroy();
-      cornerstoneViewportService.destroy();
-    };
+  uiDialogService.hideAll();
+  uiModalService.hide();
+  toolGroupService.destroy();
+  syncGroupService.destroy();
+  segmentationService.destroy();
+  cornerstoneViewportService.destroy();
+}
 
 export const toolbarSections = {
+  [TOOLBAR_SECTIONS.primary]: [
+    'MeasurementTools',
+    'Zoom',
+    'Pan',
+    'TrackballRotate',
+    'WindowLevel',
+    'Capture',
+    'Layout',
+    'Crosshairs',
+    'MoreTools',
+  ],
 
-        [TOOLBAR_SECTIONS.primary]: [
-        'MeasurementTools',
-        'Zoom',
-        'Pan',
-        'TrackballRotate',
-        'WindowLevel',
-        'Capture',
-        'Layout',
-        'Crosshairs',
-        'MoreTools',
-      ],
+  [TOOLBAR_SECTIONS.viewportActionMenu.topLeft]: ['orientationMenu', 'dataOverlayMenu'],
 
-      [TOOLBAR_SECTIONS.viewportActionMenu.topLeft]: [
-        'orientationMenu',
-        'dataOverlayMenu',
-      ],
+  [TOOLBAR_SECTIONS.viewportActionMenu.bottomMiddle]: ['AdvancedRenderingControls'],
 
-      [TOOLBAR_SECTIONS.viewportActionMenu.bottomMiddle]: [
-        'AdvancedRenderingControls',
-      ],
+  AdvancedRenderingControls: [
+    'windowLevelMenuEmbedded',
+    'voiManualControlMenu',
+    'Colorbar',
+    'opacityMenu',
+    'thresholdMenu',
+  ],
 
-      AdvancedRenderingControls: [
-        'windowLevelMenuEmbedded',
-        'voiManualControlMenu',
-        'Colorbar',
-        'opacityMenu',
-        'thresholdMenu',
-      ],
+  [TOOLBAR_SECTIONS.viewportActionMenu.topRight]: [
+    'modalityLoadBadge',
+    'trackingStatus',
+    'navigationComponent',
+  ],
 
-      [TOOLBAR_SECTIONS.viewportActionMenu.topRight]: [
-        'modalityLoadBadge',
-        'trackingStatus',
-        'navigationComponent',
-      ],
+  [TOOLBAR_SECTIONS.viewportActionMenu.bottomLeft]: ['windowLevelMenu'],
 
-      [TOOLBAR_SECTIONS.viewportActionMenu.bottomLeft]: [
-        'windowLevelMenu',
-      ],
+  MeasurementTools: [
+    'Length',
+    'Bidirectional',
+    'ArrowAnnotate',
+    'EllipticalROI',
+    'RectangleROI',
+    'CircleROI',
+    'PlanarFreehandROI',
+    'SplineROI',
+    'LivewireContour',
+  ],
 
-      MeasurementTools: [
-        'Length',
-        'Bidirectional',
-        'ArrowAnnotate',
-        'EllipticalROI',
-        'RectangleROI',
-        'CircleROI',
-        'PlanarFreehandROI',
-        'SplineROI',
-        'LivewireContour',
-      ],
+  MoreTools: [
+    'Reset',
+    'rotate-right',
+    'flipHorizontal',
+    'ImageSliceSync',
+    'ReferenceLines',
+    'ImageOverlayViewer',
+    'StackScroll',
+    'invert',
+    'Probe',
+    'Cine',
+    'Angle',
+    'CobbAngle',
+    'Magnify',
+    'CalibrationLine',
+    'TagBrowser',
+    'AdvancedMagnify',
+    'UltrasoundDirectionalTool',
+    'WindowLevelRegion',
+    'SegmentLabelTool',
+  ],
+};
 
-      MoreTools: [
-        'Reset',
-        'rotate-right',
-        'flipHorizontal',
-        'ImageSliceSync',
-        'ReferenceLines',
-        'ImageOverlayViewer',
-        'StackScroll',
-        'invert',
-        'Probe',
-        'Cine',
-        'Angle',
-        'CobbAngle',
-        'Magnify',
-        'CalibrationLine',
-        'TagBrowser',
-        'AdvancedMagnify',
-        'UltrasoundDirectionalTool',
-        'WindowLevelRegion',
-        'SegmentLabelTool',
-      ],
-    };
-
-export const basicLayout =
-       {
-            id: ohif.layout,
-            props: {
-              leftPanels: [ohif.thumbnailList],
-              leftPanelResizable: true,
-              rightPanels: [cornerstone.segmentation, cornerstone.measurements],
-              rightPanelClosed: true,
-              rightPanelResizable: true,
-              viewports: [
-                {
-                  namespace: cornerstone.viewport,
-                  displaySetsToDisplay: [
-                    ohif.sopClassHandler,
-                    dicomvideo.sopClassHandler,
-                    ohif.wsiSopClassHandler,
-                  ],
-                },
-                {
-                  namespace: dicomsr.viewport,
-                  displaySetsToDisplay: [dicomsr.sopClassHandler, dicomsr.sopClassHandler3D],
-                },
-                {
-                  namespace: dicompdf.viewport,
-                  displaySetsToDisplay: [dicompdf.sopClassHandler],
-                },
-                {
-                  namespace: dicomSeg.viewport,
-                  displaySetsToDisplay: [dicomSeg.sopClassHandler],
-                },
-                {
-                  namespace: dicomPmap.viewport,
-                  displaySetsToDisplay: [dicomPmap.sopClassHandler],
-                },
-                {
-                  namespace: dicomRT.viewport,
-                  displaySetsToDisplay: [dicomRT.sopClassHandler],
-                },
-              ],
-            },
-          };
+export const basicLayout = {
+  id: ohif.layout,
+  props: {
+    leftPanels: [ohif.thumbnailList],
+    leftPanelResizable: true,
+    rightPanels: [cornerstone.segmentation, cornerstone.measurements],
+    rightPanelClosed: true,
+    rightPanelResizable: true,
+    viewports: [
+      {
+        namespace: cornerstone.viewport,
+        displaySetsToDisplay: [
+          ohif.sopClassHandler,
+          dicomvideo.sopClassHandler,
+          ohif.wsiSopClassHandler,
+        ],
+      },
+      {
+        namespace: dicomsr.viewport,
+        displaySetsToDisplay: [dicomsr.sopClassHandler, dicomsr.sopClassHandler3D],
+      },
+      {
+        namespace: dicompdf.viewport,
+        displaySetsToDisplay: [dicompdf.sopClassHandler],
+      },
+      {
+        namespace: dicomSeg.viewport,
+        displaySetsToDisplay: [dicomSeg.sopClassHandler],
+      },
+      {
+        namespace: dicomPmap.viewport,
+        displaySetsToDisplay: [dicomPmap.sopClassHandler],
+      },
+      {
+        namespace: dicomRT.viewport,
+        displaySetsToDisplay: [dicomRT.sopClassHandler],
+      },
+    ],
+  },
+};
 
 export function layoutTemplate() {
-  return this.layoutInstance;
+  return structuredCloneWithFunctions(this.layoutInstance);
 }
 
 export const basicRoute = {
   path: 'basic',
   layoutTemplate,
   layoutInstance: basicLayout,
-}
+};
 
 export const modeInstance = {
-    // TODO: We're using this as a route segment
-    // We should not be.
-    id,
-    routeName: 'basic',
-    // Don't hide this by default - see the registration later to hide the basic
-    // instance by default.
-    hide: false,
-    displayName: 'Non-Longitudinal Basic',
-    _activatePanelTriggersSubscriptions: [],
-    toolbarSections,
+  // TODO: We're using this as a route segment
+  // We should not be.
+  id,
+  routeName: 'basic',
+  // Don't hide this by default - see the registration later to hide the basic
+  // instance by default.
+  hide: false,
+  displayName: 'Non-Longitudinal Basic',
+  _activatePanelTriggersSubscriptions: [],
+  toolbarSections,
 
-    /**
-     * Lifecycle hooks
-     */
-    onModeEnter,
-    onModeExit,
-    validationTags: {
-      study: [],
-      series: [],
-    },
+  /**
+   * Lifecycle hooks
+   */
+  onModeEnter,
+  onModeExit,
+  validationTags: {
+    study: [],
+    series: [],
+  },
 
-    isValidMode,
-    routes: [ basicRoute ],
-    extensions: extensionDependencies,
-    // Default protocol gets self-registered by default in the init
-    hangingProtocol: 'default',
-    // Order is important in sop class handlers when two handlers both use
-    // the same sop class under different situations.  In that case, the more
-    // general handler needs to come last.  For this case, the dicomvideo must
-    // come first to remove video transfer syntax before ohif uses images
-    sopClassHandlers,
-    toolbarButtons,
-    enableSegmentationEdit: false,
-    nonModeModalities: NON_IMAGE_MODALITIES,
-  };
+  isValidMode,
+  routes: [basicRoute],
+  extensions: extensionDependencies,
+  // Default protocol gets self-registered by default in the init
+  hangingProtocol: 'default',
+  // Order is important in sop class handlers when two handlers both use
+  // the same sop class under different situations.  In that case, the more
+  // general handler needs to come last.  For this case, the dicomvideo must
+  // come first to remove video transfer syntax before ohif uses images
+  sopClassHandlers,
+  toolbarButtons,
+  enableSegmentationEdit: false,
+  nonModeModalities: NON_IMAGE_MODALITIES,
+};
 
+/**
+ * Creates a mode on this object, using immutability-helper to apply changes
+ * from modeConfiguration into the modeInstance.
+ */
 export function modeFactory({ modeConfiguration }) {
-  return { ...this.modeInstance, ...modeConfiguration };
+  let modeInstance = this.modeInstance;
+  if (modeConfiguration) {
+    modeInstance = update(modeInstance, modeConfiguration);
+  }
+  return modeInstance;
 }
-
 
 export const mode = {
   id,
