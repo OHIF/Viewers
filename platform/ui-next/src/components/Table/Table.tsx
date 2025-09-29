08f751c7b08eb9768a818a4f3b4aeb599e3e0e12
Dan Rukas <dan.rukas@gmail.com>
2025-09-29 12:13:05 -0400

    Added pinned table header

--- platform/ui-next/src/components/Table/Table.tsx
+++ platform/ui-next/src/components/Table/Table.tsx
@@ -2,9 +2,13 @@ import * as React from 'react';
 
 import { cn } from '../../lib/utils';
 
-const Table = React.forwardRef<HTMLTableElement, React.HTMLAttributes<HTMLTableElement>>(
-  ({ className, ...props }, ref) => (
-    <div className="relative w-full overflow-auto">
+type TableProps = React.HTMLAttributes<HTMLTableElement> & {
+  containerClassName?: string;
+};
+
+const Table = React.forwardRef<HTMLTableElement, TableProps>(
+  ({ className, containerClassName, ...props }, ref) => (
+    <div className={cn('relative w-full overflow-auto', containerClassName)}>
       <table
         ref={ref}
         className={cn('caption-bottom text-muted-foreground w-full text-base', className)}
