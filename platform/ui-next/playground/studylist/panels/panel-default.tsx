0047f8d3ae0ca69e759278e49c99b0dc62f363d2
Dan Rukas <dan.rukas@gmail.com>
2025-10-24 12:52:13 -0400

    Initial Summary component

--- platform/ui-next/playground/studylist/panels/panel-default.tsx
+++ platform/ui-next/playground/studylist/panels/panel-default.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import { Table, TableHeader, TableRow, TableHead } from '../../../src/components/Table';
+import { Summary } from './panel-summary';
 
 export function PanelDefault() {
   return (
-    <div className="flex flex-col gap-1">
-      <Table noScroll>
-        <TableHeader className="border-0 [&_tr]:border-b-0">
-          <TableRow className="border-b-0">
-            <TableHead className="bg-background sticky top-0 z-10 rounded-t-md">
-              <div className="flex items-center">
-                <span className="text-foreground text-xl font-medium">Studies</span>
-              </div>
-            </TableHead>
-          </TableRow>
-        </TableHeader>
-      </Table>
-
-      <div className="text-muted-foreground pl-2 text-lg">Select a study to preview</div>
-    </div>
+    <Summary>
+      <Summary.Patient />
+      <Summary.Workflows />
+    </Summary>
   );
 }
