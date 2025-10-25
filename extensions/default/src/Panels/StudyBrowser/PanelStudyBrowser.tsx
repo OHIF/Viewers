335091c3255c7abe2aece10b371fc56c5c89a736
Kacper <collab@gonciu.dev>
2025-10-25 11:59:23 +0200

    feat: Remove segmentation button

--- extensions/default/src/Panels/StudyBrowser/PanelStudyBrowser.tsx
+++ extensions/default/src/Panels/StudyBrowser/PanelStudyBrowser.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback, useRef } from 'react';
-import { Button, useImageViewer } from '@ohif/ui-next';
+import { useImageViewer } from '@ohif/ui-next';
 import { useSystem, utils } from '@ohif/core';
 import { useNavigate } from 'react-router-dom';
 import { useViewportGrid, StudyBrowser, Separator } from '@ohif/ui-next';
@@ -424,49 +424,6 @@ function PanelStudyBrowser({
           className="bg-black"
           thickness="2px"
         />
-        <div className="flex justify-end px-4 py-3">
-          <Button
-            variant="secondary"
-            size="small"
-            className="!bg-primary flex items-center gap-2 !text-white hover:scale-[1.02] hover:brightness-110"
-            onClick={goToSegmentation}
-            disabled={!displaySetService.activeDisplaySets.length}
-          >
-            <span
-              aria-hidden="true"
-              className="flex items-center justify-center"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="20"
-                height="20"
-                className="!h-[20px] !w-[20px] fill-current text-white"
-              >
-                <g fillRule="evenodd">
-                  <circle
-                    cx="12"
-                    cy="12"
-                    r="12"
-                    fill="currentColor"
-                    fillOpacity="1"
-                  />
-                  <path
-                    fill="#348cfd"
-                    fillRule="nonzero"
-                    d="M17.207 10.793c.36.36.388.928.083 1.32l-.083.094-5 5c-.39.39-1.024.39-1.414 0-.36-.36-.388-.928-.083-1.32l.083-.094 4.292-4.293-4.292-4.293c-.36-.36-.388-.928-.083-1.32l.083-.094c.36-.36.928-.388 1.32-.083l.094.083 5 5z"
-                  />
-                  <path
-                    fill="#348cfd"
-                    fillRule="nonzero"
-                    d="M17.5 11.5c0 .513-.386.936-.883.993l-.117.007H6c-.552 0-1-.448-1-1 0-.513.386-.936.883-.993L6 10.5h10.5c.552 0 1 .448 1 1z"
-                  />
-                </g>
-              </svg>
-            </span>
-            Segmentation
-          </Button>
-        </div>
       </>
 
       <StudyBrowser
