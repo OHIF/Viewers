b92492965d94f33eee4e4d1a11dcf20d74cc82c4
Vinicius Alves <viniciusfariaresende@gmail.com>
2025-11-12 07:28:37 -0300

    fix: 🐛 renaming layout sub page object to layoutSection

--- tests/AxialPrimary.spec.ts
+++ tests/AxialPrimary.spec.ts
@@ -8,7 +8,7 @@ test.beforeEach(async ({ page }) => {
 
 test.describe('Axial Primary Test', async () => {
   test('should render Axial Primary correctly.', async ({ page, mainToolbarPageObject }) => {
-    await mainToolbarPageObject.layout.axialPrimary.click();
+    await mainToolbarPageObject.layoutSection.axialPrimary.click();
     await checkForScreenshot(
       page,
       page,
