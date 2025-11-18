58be6095f411d07b608aed62604f2861b4296eaa
Igor Octaviano <igoroctaviano@gmail.com>
2025-11-18 15:50:17 -0300

    Use ohif data soure

--- tests/Scoord3dProbe.spec.ts
+++ tests/Scoord3dProbe.spec.ts
@@ -5,7 +5,7 @@ test.beforeEach(async ({ page }) => {
   const studyInstanceUID = '1.3.6.1.4.1.14519.5.2.1.7310.5101.860473186348887719777907797922';
   const mode = 'viewer';
   
-  await visitStudy(page, studyInstanceUID, mode, 5000);
+  await visitStudy(page, studyInstanceUID, mode, 5000, 'ohif');
   
   // Log the actual URL that was loaded
   const currentUrl = page.url();
