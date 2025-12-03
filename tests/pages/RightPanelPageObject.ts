a4dbfdbf004527bf31da243f0ddcc84360bc5926
Vinicius Faria <viniciusfariaresende@gmail.com>
2025-12-03 14:39:04 -0300

    feat: 🎸 added new Right Panel Page Object to playwright

---
+++ tests/pages/RightPanelPageObject.ts
@@ -0,0 +1,267 @@
+import { Locator, Page } from '@playwright/test';
+
+import { OverlayPageObject } from './OverlayPageObject';
+
+export class RightPanelPageObject {
+  readonly page: Page;
+  private readonly overlayPageObject: OverlayPageObject;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.overlayPageObject = new OverlayPageObject(page);
+  }
+
+  private getActionsMenu(row: Locator) {
+    const actionsButton = row.getByTestId('actionsMenuTrigger');
+
+    return {
+      button: actionsButton,
+      click: async () => {
+        await actionsButton.click();
+      },
+      delete: async () => {
+        await actionsButton.click();
+        await this.page.getByTestId('Delete').click();
+      },
+      toggleLock: async () => {
+        await actionsButton.click();
+        await this.page.getByTestId('LockToggle').click();
+      },
+      unlock: async () => {
+        await actionsButton.click();
+        await this.page.getByTestId('Unlock').click();
+      },
+      rename: async (text: string) => {
+        await actionsButton.click();
+        await this.page.getByTestId('Rename').click();
+        await this.overlayPageObject.dialog.input.fillAndSave(text);
+      },
+    };
+  }
+
+  private getPanelRowDataObject(row: Locator) {
+    const getActionsMenu = (row: Locator) => this.getActionsMenu.bind(this)(row);
+
+    return {
+      get actions() {
+        return getActionsMenu(row);
+      },
+      click: async () => {
+        await row.click();
+      },
+      locator: row,
+      toggleVisibility: async () => {
+        await row.getByTestId('visibilityToggle').click();
+      },
+    };
+  }
+
+  private getPanelRowByIdx(index: number) {
+    const row = this.page.getByTestId('data-row').nth(index);
+    return this.getPanelRowDataObject(row);
+  }
+
+  private getPanelRowByText(text: string) {
+    const row = this.page.getByTestId('data-row').filter({ hasText: text });
+    return this.getPanelRowDataObject(row);
+  }
+
+  async toggle() {
+    await this.page.getByTestId('side-panel-header-right').click();
+  }
+
+  get measurementsPanel() {
+    const page = this.page;
+    const getMeasurementByIdx = (index: number) => this.getPanelRowByIdx(index);
+    const getMeasurementByText = (text: string) => this.getPanelRowByText(text);
+
+    return {
+      get menuButton() {
+        const button = page.getByTestId('trackedMeasurements-btn');
+        return {
+          button,
+          click: async () => {
+            await button.click();
+          },
+        };
+      },
+      panel: {
+        deleteAll: async () => {
+          await page.getByRole('button', { name: 'Delete' }).click();
+        },
+        getMeasurementCount: async () => {
+          return await page.getByTestId('data-row').count();
+        },
+        locator: page.getByTestId('trackedMeasurements-panel').last(),
+        nthMeasurement(index: number) {
+          return getMeasurementByIdx(index);
+        },
+        measurementByText(text: string) {
+          return getMeasurementByText(text);
+        },
+      },
+    };
+  }
+
+  get segmentationPanel() {
+    const page = this.page;
+    const getSegmentationByIdx = (index: number) => this.getPanelRowByIdx(index);
+    const getSegmentationByText = (text: string) => this.getPanelRowByText(text);
+
+    return {
+      get addSegmentationButton() {
+        const button = page.getByTestId('addSegmentation');
+        return {
+          button,
+          click: async () => {
+            await button.click();
+          },
+        };
+      },
+      get contourMenuButton() {
+        const button = page.getByTestId('panelSegmentationWithToolsContour-btn');
+        return {
+          button,
+          click: async () => {
+            await button.click();
+          },
+        };
+      },
+      get labelMapMenuButton() {
+        const button = page.getByTestId('panelSegmentationWithToolsLabelMap-btn');
+        return {
+          button,
+          click: async () => {
+            await button.click();
+          },
+        };
+      },
+      get menuButton() {
+        const button = page.getByTestId(/^panelSegmentation.*-btn$/);
+        return {
+          button,
+          click: async () => {
+            await button.click();
+          },
+        };
+      },
+      panel: {
+        getSegmentationCount: async () => {
+          return await page.getByTestId('data-row').count();
+        },
+        // No data-cy exists in this panel, using Segmentation header button
+        locator: page.getByRole('button', { name: 'Segmentations' }),
+        nthSegmentation(index: number) {
+          return getSegmentationByIdx(index);
+        },
+        segmentationByText(text: string) {
+          return getSegmentationByText(text);
+        },
+      },
+      tools: {
+        get brush() {
+          const button = page.getByTestId('Brush-btn');
+          const input = page.locator(`css=div[data-cy="brush-radius"] input`);
+          return {
+            button,
+            input,
+            click: async () => {
+              await button.click();
+            },
+            setRadius: async (radius: number) => {
+              await input.fill(radius.toString());
+            },
+          };
+        },
+        get eraser() {
+          const button = page.getByTestId('Eraser-btn');
+          const input = page.locator(`css=div[data-cy="eraser-radius"] input`);
+          return {
+            button,
+            input,
+            click: async () => {
+              await button.click();
+            },
+            setRadius: async (radius: number) => {
+              await input.fill(radius.toString());
+            },
+          };
+        },
+        get threshold() {
+          const button = page.getByTestId('Threshold-btn');
+          const input = page.locator(`css=div[data-cy="threshold-radius"] input`);
+          return {
+            button,
+            input,
+            click: async () => {
+              await button.click();
+            },
+            setRadius: async (radius: number) => {
+              await input.fill(radius.toString());
+            },
+          };
+        },
+      },
+      getSegmentationByText(text: string) {
+        const segmentation = page.getByTestId('data-row').filter({ hasText: text });
+        return {
+          locator: segmentation,
+          click: async () => {
+            await segmentation.click();
+          },
+        };
+      },
+    };
+  }
+
+  get tmtvPanel() {
+    const page = this.page;
+
+    return {
+      get addSegmentationButton() {
+        const button = page.getByTestId('addSegmentation');
+        return {
+          button,
+          click: async () => {
+            await button.click();
+          },
+        };
+      },
+      async exportTmtvCsvReport() {
+        await page.getByTestId('exportTmtvCsvReport').click();
+      },
+      tools: {
+        get brush() {
+          const button = page.getByTestId('Brush-btn');
+          const input = page.locator(`css=div[data-cy="brush-radius"] input`);
+          return {
+            button,
+            input,
+            click: async () => {
+              await button.click();
+            },
+            setRadius: async (radius: number) => {
+              await input.fill(radius.toString());
+            },
+          };
+        },
+        get rectangleROIThreshold() {
+          const button = page.getByTestId('RectangleROIStartEndThreshold-btn');
+          const input = page.getByTestId('percentage-of-max-suv-input');
+          return {
+            button,
+            click: async () => {
+              await button.click();
+            },
+            getPercentageOfMaxSUV: async () => {
+              return await input.inputValue();
+            },
+            setPercentageOfMaxSUV: async (percentage: string) => {
+              await input.fill(percentage);
+            },
+          };
+        },
+      },
+    };
+  }
+}
