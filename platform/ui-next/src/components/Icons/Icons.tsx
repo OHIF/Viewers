eef755ef05e38d5271b1c79e4dd191d723895470
Dan Rukas <dan.rukas@gmail.com>
2025-12-10 16:39:17 -0500

    fix(routes): display 404 feedback page for unmatched URLs (#5627)
    * Added 404 feedback and design
    
    * restore message and showGoBackButton props

--- platform/ui-next/src/components/Icons/Icons.tsx
+++ platform/ui-next/src/components/Icons/Icons.tsx
@@ -231,6 +231,7 @@ import ChevronLeft from './Sources/ChevronLeft';
 import StatusAlert from './Sources/StatusAlert';
 import Undo from './Sources/Undo';
 import TabContours from './Sources/TabContours';
+import IllustrationNotFound from './Sources/IllustrationNotFound';
 //
 //
 type IconProps = React.HTMLAttributes<SVGElement>;
@@ -798,6 +799,7 @@ export const Icons = {
   Undo,
   Redo,
   JumpToSlice,
+  IllustrationNotFound,
 
   /** Adds an icon to the set of icons */
   addIcon: (name: string, icon) => {
