03dc90944032fe83b5751944b94a662885c1ac1c
Martin Bellehumeur, M. Eng. <23396581+mbellehumeur@users.noreply.github.com>
2025-12-11 19:13:32 +0100

    refactor: add new translations to test-LNG

--- platform/i18n/src/locales/test-LNG/index.js
+++ platform/i18n/src/locales/test-LNG/index.js
@@ -2,6 +2,8 @@ import AboutModal from './AboutModal.json';
 import Buttons from './Buttons.json';
 import CineDialog from './CineDialog.json';
 import Common from './Common.json';
+import Colormaps from './Colormaps.json';
+import DataSourceConfiguration from './DataSourceConfiguration.json';
 import DatePicker from './DatePicker.json';
 import ErrorBoundary from './ErrorBoundary.json';
 import Header from './Header.json';
@@ -11,7 +13,10 @@ import DataRow from './DataRow.json';
 import Messages from './Messages.json';
 import Modals from './Modals.json';
 import Modes from './Modes.json';
+import Onboarding from './Onboarding.json';
+import PanelSUV from './PanelSUV.json';
 import PatientInfo from './PatientInfo.json';
+import ROIThresholdConfiguration from './ROIThresholdConfiguration.json';
 import SegmentationPanel from './SegmentationPanel.json';
 import SidePanel from './SidePanel.json';
 import StudyBrowser from './StudyBrowser.json';
@@ -35,6 +40,8 @@ export default {
     Buttons,
     CineDialog,
     Common,
+    Colormaps,
+    DataSourceConfiguration,
     DatePicker,
     ErrorBoundary,
     Header,
@@ -44,7 +51,10 @@ export default {
     Messages,
     Modals,
     Modes,
+    Onboarding,
+    PanelSUV,
     PatientInfo,
+    ROIThresholdConfiguration,
     SegmentationPanel,
     SidePanel,
     StudyBrowser,
