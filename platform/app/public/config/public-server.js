3dbec44298f9ad40dbd74cdb4df8d65379942920
Psda2 <abeypalapsd.21@itfac.mrt.ac.lk>
2026-01-14 15:44:50 +0530

    Integrate public server and enable annotations

---
+++ platform/app/public/config/public-server.js
@@ -0,0 +1,43 @@
+window.config = {
+  routerBasename: '/',
+  showStudyList: true,
+  extensions: [
+    '@ohif/extension-measurement-tracking',
+    '@ohif/extension-cornerstone-dicom-sr',
+    '@ohif/extension-cornerstone-dicom-seg',
+  ],
+  modes: [],
+  // below flag is for performance reasons, but it might not work for all servers
+  showWarningMessageForCrossOrigin: true,
+  showCPUFallbackMessage: true,
+  showLoadingIndicator: true,
+  strictZSpacingForVolumeViewport: true,
+  defaultDataSourceName: 'aws',
+  dataSources: [
+    {
+      namespace: '@ohif/extension-default.dataSourcesModule.dicomweb',
+      sourceName: 'aws',
+      configuration: {
+        friendlyName: 'AWS S3 Static wado server',
+        name: 'aws',
+        wadoUriRoot: 'https://d14fa38qiwhyfd.cloudfront.net/dicomweb',
+        qidoRoot: 'https://d14fa38qiwhyfd.cloudfront.net/dicomweb',
+        wadoRoot: 'https://d14fa38qiwhyfd.cloudfront.net/dicomweb',
+        qidoSupportsIncludeField: false,
+        imageRendering: 'wadors',
+        thumbnailRendering: 'wadors',
+        enableStudyLazyLoad: true,
+        supportsFuzzyMatching: true,
+        supportsWildcard: false,
+        staticWado: true,
+        singlepart: 'bulkdata,video',
+        bulkDataURI: {
+          enabled: true,
+          relativeResolution: 'studies',
+          transform: url => url.replace('/pixeldata.mp4', '/rendered'),
+        },
+        omitQuotationForMultipartRequest: true,
+      },
+    },
+  ],
+};
