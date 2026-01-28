e09139ba129b80addb3873636dc98d78ec5b6924
Martin Bellehumeur, M. Eng. <23396581+mbellehumeur@users.noreply.github.com>
2026-01-28 16:25:44 +0100

    refactor: adjust size range and step in OrientationController for improved precision in user input

--- extensions/cornerstone/src/components/WindowLevelActionMenu/OrientationController.tsx
+++ extensions/cornerstone/src/components/WindowLevelActionMenu/OrientationController.tsx
@@ -204,8 +204,8 @@ export function OrientationController({ viewportId }: { viewportId?: string }): 
         <Numeric.Container
           mode="singleRange"
           min={0.02}
-          max={0.15}
-          step={0.01}
+          max={0.04}
+          step={0.005}
           value={size}
           onChange={onSizeChange}
         >
