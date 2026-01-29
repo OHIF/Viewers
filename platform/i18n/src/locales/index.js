7ba88804ace6a6fef1c2547fe1347acf20661acf
Martin Bellehumeur, M. Eng. <23396581+mbellehumeur@users.noreply.github.com>
2026-01-29 16:26:33 +0100

    Remove Dutch (no reviewers found)

--- platform/i18n/src/locales/index.js
+++ platform/i18n/src/locales/index.js
@@ -5,7 +5,6 @@ import en_US from './en-US/';
 import es from './es/';
 import fr from './fr/';
 import ja_JP from './ja-JP/';
-import nl from './nl/';
 import pt_BR from './pt-BR/';
 import ru from './ru/';
 import vi from './vi/';
@@ -22,7 +21,6 @@ export default {
   ...es,
   ...fr,
   ...ja_JP,
-  ...nl,
   ...pt_BR,
   ...ru,
   ...vi,
