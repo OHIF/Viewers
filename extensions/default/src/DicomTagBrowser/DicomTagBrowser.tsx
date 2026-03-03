e2db8129595d19a25ab7592badb97e599cdc33ca
顾争 <281620758@qq.com>
2026-03-03 14:16:45 +0800

    fix(DicomTagBrowser): prevent Instance Number text wrapping

--- extensions/default/src/DicomTagBrowser/DicomTagBrowser.tsx
+++ extensions/default/src/DicomTagBrowser/DicomTagBrowser.tsx
@@ -156,7 +156,7 @@ const DicomTagBrowser = ({
           </div>
           {shouldShowInstanceList && (
             <div className="mx-auto mt-0.5 flex w-1/4 flex-col">
-              <span className="text-muted-foreground flex h-6 items-center pb-2 text-base">
+              <span className="text-muted-foreground whitespace-nowrap flex h-6 items-center pb-2 text-base">
                 Instance Number ({instanceNumber} of {activeDisplaySet?.images?.length})
               </span>
               <Slider
