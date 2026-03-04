1acf134c9594e4b2d0a4965821161665abcfc820
Ghadeer Albattarni <165973963+GhadeerAlbattarni@users.noreply.github.com>
2026-03-04 15:28:41 -0500

    fix(microscopy): rename measurement in microscopy mode (#5866)

---
+++ tests/MicroscopyPanel.spec.ts
@@ -0,0 +1,33 @@
+import { test, visitStudy, expect } from './utils';
+
+test.beforeEach(async ({ page }) => {
+  const studyInstanceUID = '1.2.276.0.7230010.3.1.2.296485376.1.1665793212.499772';
+  const mode = 'microscopy';
+  await visitStudy(page, studyInstanceUID, mode, 5000);
+});
+test('should rename a microscopy measurement label', async ({
+  page,
+  mainToolbarPageObject,
+  DOMOverlayPageObject,
+  rightPanelPageObject,
+  viewportPageObject,
+}) => {
+  const newLabel = 'Renamed Measurement';
+
+  await mainToolbarPageObject.measurementTools.line.click();
+
+  await viewportPageObject.active.clickAt([{ x: 400, y: 200 }]);
+  await page.waitForTimeout(200);
+  await viewportPageObject.active.clickAt([{ x: 550, y: 250 }]);
+
+  const measurementRow = rightPanelPageObject.microscopyPanel.nthMeasurement(0);
+  await expect(measurementRow.locator).toBeVisible();
+
+  await expect(measurementRow.title).toHaveText('(empty)');
+
+  await measurementRow.actions.rename(newLabel);
+
+  await expect(DOMOverlayPageObject.dialog.input.locator).toBeHidden();
+
+  await expect(measurementRow.title).toHaveText(newLabel);
+});
