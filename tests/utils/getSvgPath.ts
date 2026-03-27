35628334015513c503f607fd58e60308825ca04b
Ghadeer Albattarni <165973963+GhadeerAlbattarni@users.noreply.github.com>
2026-03-27 10:49:39 -0400

    update getSvgPath file in utils

--- tests/utils/getSvgPath.ts
+++ tests/utils/getSvgPath.ts
@@ -1,11 +1,7 @@
 import { ViewportPageObject } from '../pages/ViewportPageObject';
 
 const getSvgPath = async (viewportPageObject: ViewportPageObject, viewportId = 'default') => {
-  return viewportPageObject
-    .getById(viewportId)
-    .svg('path')
-    .first()
-    .getAttribute('d');
+  return (await viewportPageObject.getById(viewportId)).svg('path').first().getAttribute('d');
 };
 
 export { getSvgPath };
