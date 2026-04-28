1f62e699a2aeb2b3a8e69e095b8c3c53e684e1ec
salim kanoun <salim.kanoun@gmail.com>
2026-04-28 22:50:21 +0200

    add hideUndoRedo settings

--- platform/app/public/config/netlify.js
+++ platform/app/public/config/netlify.js
@@ -5,6 +5,7 @@ window.config = {
   extensions: [],
   modes: [],
   showStudyList: true,
+  hideUndoRedo: false,
   // below flag is for performance reasons, but it might not work for all servers
   showWarningMessageForCrossOrigin: true,
   showCPUFallbackMessage: true,
