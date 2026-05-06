32d8540cd910dedda7b36023e522c2dc27e52a12
Joe Boccanfuso <joe.boccanfuso@radicalimaging.com>
2026-05-06 13:54:15 -0400

    Remove obsolete tests.

--- tests/Worklist.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, checkForScreenshot, screenShotPaths } from './utils';
-
-test.beforeEach(async ({ page }) => {
-  await page.goto(`/?datasources=ohif`);
-  await page.waitForLoadState('domcontentloaded');
-  await page.waitForLoadState('networkidle');
-  await page.waitForTimeout(2000);
-});
