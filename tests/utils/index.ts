74c59860daa77d53d7a10077e2c33a5a66c9267d
Bill Wallace <wayfarer3130@gmail.com>
2026-05-07 17:20:29 -0400

    Add customization test

--- tests/utils/index.ts
+++ tests/utils/index.ts
@@ -1,4 +1,4 @@
-import { visitStudy } from './visitStudy';
+import { visitStudy, visitStudyOptions } from './visitStudy';
 import {
   addOHIFConfiguration,
   addOHIFGlobalCustomizations,
@@ -32,6 +32,7 @@ import {
 
 export {
   visitStudy,
+  visitStudyOptions,
   addOHIFConfiguration,
   addOHIFGlobalCustomizations,
   checkForScreenshot,
