2258c7957a18ed0f1a75d2bd5d379006797b2882
diattamo <mmddiatta@gmail.com>
2026-05-20 21:18:58 -0400

    feat(tests): Add tests for duplicating contour segments (#6002)

--- tests/utils/index.ts
+++ tests/utils/index.ts
@@ -24,7 +24,7 @@ import { clearAllAnnotations } from './clearAllAnnotations';
 import { scrollVolumeViewport } from './scrollVolumeViewport';
 import { attemptAction } from './attemptAction';
 import { addLengthMeasurement } from './addLengthMeasurement';
-import { getSvgPath } from './getSvgPath';
+import { getSvgAttribute } from './getSvgAttribute';
 import { navigateWithViewportArrow } from './navigateWithViewportArrow';
 import { test, expect } from './fixture';
 import { subscribeToMeasurementAdded } from './subscribeToMeasurement';
@@ -57,7 +57,7 @@ export {
   attemptAction,
   addLengthMeasurement,
   subscribeToMeasurementAdded,
-  getSvgPath,
+  getSvgAttribute,
   navigateWithViewportArrow,
   waitForAnyViewportNeedsRender,
   waitForViewportsRendered,
